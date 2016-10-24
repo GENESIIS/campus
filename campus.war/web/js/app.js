eeaf8e64dce70f17863756b9e97af968bfcbd684
tharakaGen <tharaka@genesiis.com>
2016-10-24 13:40:43 +0530

    c1-ui-campus-landing-page-tr:header top part done

---
+++ campus.war/web/js/app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by tharaka on 10/24/2016.
+ */
+
+angular.module('CampusApp',['ngRoute']) 
+
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/',{
+                templateUrl : "partials/landing.html"
+            })
+            .when('/about',{
+                templateUrl : "partials/about.html"
+            })
+            .when('/contact',{
+                templateUrl : "partials/contact.html"
+            });
+    });
