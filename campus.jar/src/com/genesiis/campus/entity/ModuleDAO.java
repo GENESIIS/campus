36c24e72fdbe0d5dc0ae4e1ab3cbba1f30605b6a
chathuriM <madushani@genesiis.com>
2016-10-25 14:32:18 +0530

    c13-display course details-INIT ModuleDAO class-cm

---
+++ campus.jar/src/com/genesiis/campus/entity/ModuleDAO.java
@@ -0,0 +1,64 @@
+package com.genesiis.campus.entity;
+
+//20161025 CM c13-Display course details INIT ModuleDAO.java
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Collection;
+
+public class ModuleDAO implements ICrud{
+
+	@Override
+	public int add(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<Collection<String>> findById(Object code)
+			throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Collection<String>> getAll() throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int add(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
