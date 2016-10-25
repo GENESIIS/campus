4da5dc844dce96ebb9fb35147109478774d39328
chathuriM <madushani@genesiis.com>
2016-10-25 09:06:23 +0530

    c13-Display-course-details-Modified IDataHelper interface-cm

--- campus.jar/src/com/genesiis/campus/util/IDataHelper.java
+++ campus.jar/src/com/genesiis/campus/util/IDataHelper.java
@@ -1,5 +1,7 @@
 package com.genesiis.campus.util;
 
+import javax.servlet.http.HttpSession;
+
 import com.genesiis.campus.entity.IView;
 
 //20161024 DN c10-contacting-us-page created the initial version of the IDataHelper.java
@@ -14,5 +16,17 @@ public interface IDataHelper {
 	public String getParameter(String paramName);
 
 	public void setAttribute(String Name, Object o);
+	
+	public String[] getParameterValues(String name);
+
+	HttpSession getSession(boolean status);
+	
+	String getRemoteAddress();
+
+	String getHeader(String name);
+
+	String getRedirectPage();
+
+	void setRedirectPage(String pageName);
 
 }
