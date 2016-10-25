c0bbd34b4604c85616b45ee06496f412fcb2e11c
jayani1991 <jayani@genesiis.com>
2016-10-25 09:46:52 +0530

    c7-list-higher-education-courses DataHelper class modified -JH

--- campus.jar/src/com/genesiis/campus/util/DataHelper.java
+++ campus.jar/src/com/genesiis/campus/util/DataHelper.java
@@ -16,79 +16,142 @@ import com.genesiis.campus.validation.Operation;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class DataHelper implements IDataHelper {
-
-	Logger logger = Logger.getLogger(this.getClass());
+	static Logger logger = Logger.getLogger(DataHelper.class.getName());
 
 	private static HttpServletRequest request;
-	private static HttpServletResponse response;
-
+	
 	private String cco = "";
 	private String commandChoice = "";
+	private String redirectPage = "login.jsp";
 
 	public DataHelper(HttpServletRequest request) {
 		this.request = request;
-		this.response = response;
 	}
 
 	/**
-	 * @author pabodha
 	 * @return String
 	 * @param request
-	 * This will pass the CCO (Command Class Code) to the servlet
+	 *            This will pass the CCO (Command Class Code) to the servlet
 	 **/
-	@Override
 	public String getCommandCode() {
 		return request.getParameter("CCO");
+
 	}
 
 	/**
-	 * @author pabodha
 	 * @return String This will be use to select jsp page
-	 * **/
+	 **/
 	@Override
 	public String getResultPage(String cco) {
-		String resultPage = "index.jsp";
+		String resultPage = "login.jsp";
 		Operation o = Operation.BAD_OPERATION;
 		o = Operation.getOperation(cco);
-		switch (o) {		
-		case BAD_OPERATION:
-			resultPage = o.getPageURL();
-			break;
+		switch (o) {
+		
 		default:
 			break;
 		}
 		return resultPage;
 	}
 
+	/**
+	 * getParameterValues() returns the set of parameter values thats bound to
+	 * the passed parameter name if exists. If the seeking parameter name is not
+	 * existing method returns null String array
+	 * @return String array if the parameter exists else null
+	 * @param String
+	 *            parameter name
+	 */
 	@Override
-	public IView getResultView(String cco) throws Exception{
+	public void setRedirectPage(String pageName) {
+		this.redirectPage = pageName;
+	}
+
+	@Override
+	public String getRedirectPage() {
+		return this.redirectPage;
+	}
+
+	@Override
+	public IView getResultView(String cco) throws Exception {
 		IView result = new View();
 		String sPath = request.getServletPath();
 		try {
 			final ICmdFactory factory = FactoryProducer.getFactory(sPath);
-			final ICommand iCommand= factory.getCommand(cco);
-			if(iCommand != null){
-			result=iCommand.execute(this, result);
+			final ICommand iCommand = factory.getCommand(cco);
+			if (iCommand != null) {
+				result = iCommand.execute(this, result);
 			}
 		} catch (Exception e) {
-			logger.info("getResultView() : "+ e.toString());
+			logger.info("getResultView() : " + e.toString());
 			throw e;
 		}
- 		return result;
+		return result;
 	}
 
-	@Override
+	/**
+	 * getParameter() returns the parameter value thats bound to the passed
+	 * parameter name if exists. If the seeking parameter name is not existing
+	 * method returns null String
+	 * @return String if the parameter exists else null
+	 * @param String parameter name
+	 */
 	public String getParameter(String paramName) {
-		// TODO Auto-generated method stub
-		return null;
+		return request.getParameter(paramName);
 	}
 
+	/**
+	 * setAttribute() method sets new Attribute to the HttpRequest
+	 * @param String Name of the request attribute
+	 * @param Object the value of the attribute to be set
+	 * @return void
+	 */
 	@Override
 	public void setAttribute(String Name, Object o) {
-		// TODO Auto-generated method stub
-		
+		request.setAttribute(Name, o);
+	}
+
+	/**
+	 * getParameterValues() returns the set of parameter values thats bound to
+	 * the passed parameter name if exists. If the seeking parameter name is not
+	 * existing method returns null String array
+	 * @return String array if the parameter exists else null
+	 * @param String parameter name
+	 */
+	@Override
+	public String[] getParameterValues(String name) {
+		return request.getParameterValues(name);
+	}
+
+	/**
+	 * getSession() returns a HttpSession binded with request
+	 * @return HttpSession
+	 */
+	@Override
+	public HttpSession getSession(boolean status) {
+		return request.getSession(status);
+	}
+
+	/*
+	 * getSession() returns an ip address which the request is coming
+	 * @return String
+	 */
+	@Override
+	public String getRemoteAddress() {
+		return request.getRemoteAddr();
+	}
+
+	/**
+	 * getSession() returns User-Agent which is the browser
+	 * @return String
+	 */
+	@Override
+	public String getHeader(String name) {
+		return request.getHeader(name);
+
 	}
 
 }
