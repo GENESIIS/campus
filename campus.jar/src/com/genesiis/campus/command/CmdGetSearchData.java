9ec540a1a05c10567c29822185ad31dbb52a73ef
pabodhaW <pabodha@genesiis.com>
2016-10-25 23:10:01 +0530

    c11-criteria-based-filter-search: modified execute() method.

--- campus.jar/src/com/genesiis/campus/command/CmdGetSearchData.java
+++ campus.jar/src/com/genesiis/campus/command/CmdGetSearchData.java
@@ -9,47 +9,22 @@ import java.util.Map;
 
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.util.QueryBuildingHelper;
 
 import org.apache.log4j.Logger;
 
 public class CmdGetSearchData implements ICommand {
-	static org.apache.log4j.Logger log = Logger
-			.getLogger(CmdGetSearchData.class.getName());
+	static Logger log = Logger.getLogger(CmdGetSearchData.class.getName());
 
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
 		String searchData = helper.getParameter("jsonData");
-		log.info("jsonData " + searchData);
-		log.info("dynamicQuery " + dynamicQuery());
+		QueryBuildingHelper qbh = new QueryBuildingHelper();
+		String query = qbh.dynamicQuery(qbh.assignMapData((String) qbh.extractFromJason(searchData)), "");
 		return null;
 	}
 
-	public String dynamicQuery() {
-		String query = "";
 
-		String query2 = "SELECT * FROM PROGRAMME WHERE ";
-
-		Map<String, int[]> map = new HashMap<String, int[]>();
-		map.put("LEVEL", new int[] { 1, 2, 3, 3 });
-		String var = "";
-		for (String key : map.keySet()) {
-			int[] value = map.get(key);
-			String criteria = key + " IN (";
-			String csv = String.join(",", new String[] { "1", "2" }) + ")"; // a,b
-
-			// StringBuilder result = new StringBuilder();
-			// for(int val : map.get(key)) {
-			// result.append(Integer.toString((val)));
-			// result.append(",");
-			// }
-			// return result.length() > 0 ? result.substring(0, result.length()
-			// - 1): "";
-			criteria = criteria.concat(csv);
-			criteria = criteria.concat(" AND ");
-			var = criteria;
-		}
-		return query2;
-	}
 
 }
