78213b241d1ee61f13f40e94742aa45ec29d81c0
jayani1991 <jayani@genesiis.com>
2016-10-25 13:10:13 +0530

    c7-list-higher-education-courses header comments added to the list command class -JH

--- campus.jar/src/com/genesiis/campus/command/CmdListHigherEducationCourses.java
+++ campus.jar/src/com/genesiis/campus/command/CmdListHigherEducationCourses.java
@@ -3,16 +3,19 @@ package com.genesiis.campus.command;
 
 import java.sql.SQLException;
 
+//20161025 JH c7-list-higher-education-courses command class CmdListHigherEducationCourses.java created
+//20161025 JH c7-list-higher-education-courses execute method created and coding
+
 import org.apache.log4j.Logger;
 
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.util.IDataHelper;
 
-public class CmdListHECourses implements ICommand {
-	static org.apache.log4j.Logger log = Logger.getLogger(CmdListHECourses.class.getName());
+public class CmdListHigherEducationCourses implements ICommand {
+	static org.apache.log4j.Logger log = Logger.getLogger(CmdListHigherEducationCourses.class.getName());
 	private IView programmeData;
 	
-	public CmdListHECourses(IView programmeData){
+	public CmdListHigherEducationCourses(IView programmeData){
 		this.programmeData = programmeData;
 	}
 	
