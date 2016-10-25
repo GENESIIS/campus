c28f3457a3cca032e410686c34394de1de0737dd
chathuriM <madushani@genesiis.com>
2016-10-25 10:30:41 +0530

    c13-display course details- Modified execute() method-cm

--- campus.jar/src/com/genesiis/campus/command/CmdViewProgramme.java
+++ campus.jar/src/com/genesiis/campus/command/CmdViewProgramme.java
@@ -1,10 +1,15 @@
 package com.genesiis.campus.command;
 
 //20161025 CM c13-Display course details INIT CmdViewProgramme.java
+//20161025 CM c13-Display course details Modified execute() method. 
 
 import java.sql.SQLException;
+import java.util.Collection;
 
+import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.ProgrammeDAO;
+import com.genesiis.campus.entity.model.Programme;
 import com.genesiis.campus.util.IDataHelper;
 
 import org.apache.log4j.Logger;
@@ -21,7 +26,22 @@ public class CmdViewProgramme implements ICommand{
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
-		return null;
+		try {
+			final Programme programme=new Programme();
+			 ICrud programmeDAO=new ProgrammeDAO();
+			int programmeId= Integer.parseInt(helper.getParameter("programmeCode"));
+			
+			programme.setCode(programmeId);
+			
+			Collection<Collection<String>> programmeDAOCollection = programmeDAO.findById(programme);
+			view.setCollection(programmeDAOCollection);
+		
+		} catch (Exception e) {
+			log.info("execute() : e" + e.toString());
+			throw e;
+		}
+
+		return view;
 	}
 
 }
