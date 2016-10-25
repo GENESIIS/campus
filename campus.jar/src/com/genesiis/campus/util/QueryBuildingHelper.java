023dc24acf136be07f1c457aef6e7646d2200e3c
pabodhaW <pabodha@genesiis.com>
2016-10-25 23:11:32 +0530

    c11-criteria-based-filter-search: c11-criteria-based-filter-search INIT QueryBuildingHelper.java to implement query building helper method.

---
+++ campus.jar/src/com/genesiis/campus/util/QueryBuildingHelper.java
@@ -0,0 +1,75 @@
+package com.genesiis.campus.util;
+
+//20161025 PN c11-criteria-based-filter-search INIT QueryBuildingHelper.java to implement query building helper method. 
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+
+/**
+ * @author pabodha This class is to get help on building dynamic SQL queries.
+ */
+public class QueryBuildingHelper {
+	/**
+	 * To create dynamic query according to the incoming data
+	 * 
+	 * @author pabodha
+	 * @param queryMap
+	 * @param mainQuery
+	 * @return String finalQuery
+	 */
+	public String dynamicQuery(Map<String, String[]> queryMap, String mainQuery) {
+		String query = "";
+		Map<String, String[]> map = queryMap;
+		String var = "";
+		for (String key : map.keySet()) {
+			String[] value = map.get(key);
+			String criteria = key + " IN (";
+			String attributes = String.join(",", value) + ")";
+
+			criteria = criteria.concat(attributes);
+			criteria = criteria.concat(" AND ");
+			var = criteria;
+		}
+		query = mainQuery.concat(var);
+		return query;
+	}
+
+	/**
+	 * @author pabodha
+	 * @param qString
+	 * @return map with criteria and value array
+	 */
+	public Map<String, String[]> assignMapData(String qString) {
+		Map<String, String[]> map = new HashMap<String, String[]>();
+
+		String[] criteriaValueSet = qString.split("&");
+		for (int i = 0; i < criteriaValueSet.length; i++) {
+			String var = criteriaValueSet[i];
+			String[] data = var.split("=");
+			String criteria = data[0].trim();
+			String valueSet = data[1].trim();
+
+			String[] valuesToArray = valueSet.split(",");
+			map.put(criteria, valuesToArray);
+		}
+		return map;
+	}
+
+	/**
+	 * @author pabodha
+	 * @param data
+	 * @return Object extracted from passed json object.
+	 */
+	public Object extractFromJason(String data) {
+		Gson gson = new Gson();
+		String queryString = "";
+		try {
+			queryString = gson.fromJson(data, String.class);
+		} catch (Exception e) {
+			log.info("ExtractFromgson - Exception " + e);
+		}
+		return queryString;
+	}
+}
