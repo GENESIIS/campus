61d3d6b128c4ffc96f04f13eaf0324b6e7c63694
jayani1991 <jayani@genesiis.com>
2016-10-26 12:28:51 +0530

    c7-list-higher-education-courses PublicCmdFactory.java added -JH

---
+++ campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
@@ -0,0 +1,30 @@
+package com.genesiis.campus.factory;
+
+//20161026 DN c10-contacting-us-page amended by inserting CONTACT_US_PUBLC entry to the map
+
+import com.genesiis.campus.command.CmdListHigherEducationCourses;
+import com.genesiis.campus.command.ICommand;
+import com.genesiis.campus.validation.Operation;
+
+public class PublicCmdFactory implements ICmdFactory {
+
+	private ICommand command = null;
+	static {	
+		map.put(Operation.LIST_HIGHER_EDUCATION_PROGRAMMES, new CmdListHigherEducationCourses() );	
+	}
+	@Override
+	public ICommand getCommand(String cco) {
+		Operation o = Operation.BAD_OPERATION;
+		o = Operation.getOperation(cco);
+		switch (o) {
+		
+		case LIST_HIGHER_EDUCATION_PROGRAMMES:
+			command = map.get(o);
+			break;
+		default:
+			break;
+		}
+		return command;
+	}
+
+}
