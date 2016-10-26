cc89dcd24dc7bb68581fd6398cf8f7433acb3c02
chathuriM <madushani@genesiis.com>
2016-10-26 21:26:15 +0530

    c9-inquiry form for institute- INIT CmdSendInstituteInquiry class-cm

---
+++ campus.jar/src/com/genesiis/campus/command/CmdSendInstituteInquiry.java
@@ -0,0 +1,19 @@
+package com.genesiis.campus.command;
+
+//20161026 CM c9-make inquiry for institute INIT CmdSendInstituteInquiry.java
+
+import java.sql.SQLException;
+
+import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.util.IDataHelper;
+
+public class CmdSendInstituteInquiry implements ICommand{
+
+	@Override
+	public IView execute(IDataHelper helper, IView view) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
