b4e9e2f4bb8d956c66c7f525f052f81a70dbb510
dushanthacb <dushantha@genesiis.com>
2016-10-26 17:15:48 +0530

    20161026 Dn c10-contacting-us-page created the package

---
+++ campus.jar/src/com/genesiis/campus/entity/model/SystemConfig.java
@@ -0,0 +1,17 @@
+package com.genesiis.campus.entity.model;
+//20161026 DN c10-contacting-us-page initial version of the SystemConfig.java 
+
+
+/**
+ * SystemConfig class represents the Organization to which
+ * the SuperAdmin belongs and it holds the configuration settings
+ * of the Organization * 
+ * @author DN
+ *
+ */
+public class SystemConfig {
+	
+	
+	
+
+}
