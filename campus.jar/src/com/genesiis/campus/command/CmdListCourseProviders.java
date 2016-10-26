405afad5d86ba930d33fe8e07057521cf54d033a
jayani1991 <jayani@genesiis.com>
2016-10-26 16:29:28 +0530

    c7-higher-education-landing-page CmdListInstitutes.java class renamed as CmdListCourseProviders.java -JH

--- campus.jar/src/com/genesiis/campus/command/CmdListCourseProviders.java
+++ campus.jar/src/com/genesiis/campus/command/CmdListCourseProviders.java
@@ -2,18 +2,19 @@ package com.genesiis.campus.command;
 
 //20161025 JH c7-list-higher-education-courses command class CmdListInstitutes.java created
 //20161025 JH c7-list-higher-education-courses execute method modified
+//20161026 JH c7-higher-education-landing-page CmdListInstitutes.java class renamed as CmdListCourseProviders.java
 
 import java.sql.SQLException;
 
 import com.genesiis.campus.entity.IView;
-import com.genesiis.campus.entity.InstituteDAO;
+import com.genesiis.campus.entity.CourseProviderDAO;
 import com.genesiis.campus.util.IDataHelper;
 
 import org.apache.log4j.Logger;
 
-public class CmdListInstitutes implements ICommand{
+public class CmdListCourseProviders implements ICommand{
 
-	static org.apache.log4j.Logger log = Logger.getLogger(CmdListInstitutes.class.getName());	
+	static org.apache.log4j.Logger log = Logger.getLogger(CmdListCourseProviders.class.getName());	
 	
 	/**
 	 * used to list institutes based on the given criteria
