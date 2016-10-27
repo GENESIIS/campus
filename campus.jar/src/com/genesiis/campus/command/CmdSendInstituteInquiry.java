4073ba4e38edb34c47f1262b02f424ce3e2903df
chathuriM <madushani@genesiis.com>
2016-10-27 15:06:04 +0530

    c9-inquiry form for institute-Add comments to methods-cm

--- campus.jar/src/com/genesiis/campus/command/CmdSendInstituteInquiry.java
+++ campus.jar/src/com/genesiis/campus/command/CmdSendInstituteInquiry.java
@@ -2,6 +2,7 @@ package com.genesiis.campus.command;
 
 //20161026 CM c9-make inquiry for institute INIT CmdSendInstituteInquiry.java
 //20161027 CM c9-make inquiry for institute Modified execute() method
+//20161027 CM c9-make inquiry for institute Add method comment
 
 import java.sql.SQLException;
 
@@ -13,29 +14,44 @@ import com.genesiis.campus.entity.model.InstituteInquiry;
 import com.genesiis.campus.util.IDataHelper;
 import com.google.gson.Gson;
 
-public class CmdSendInstituteInquiry implements ICommand{
+public class CmdSendInstituteInquiry implements ICommand {
 
-	static Logger log = Logger.getLogger(CmdSendInstituteInquiry.class.getName());
-	
+	static Logger log = Logger.getLogger(CmdSendInstituteInquiry.class
+			.getName());
+
+	/**
+	 * @author Chathuri
+	 * @param helepr
+	 *            IDataHelper object of Object type view IView object of object
+	 *            type
+	 * @return View object to servlet
+	 */
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
-		 String gsonData=helper.getParameter("jsonData");
-		 
-		InstituteInquiry data=getInstituteInquirydetails(gsonData);
-		
-		InstituteInquiryDAO inquiryDAO=new InstituteInquiryDAO();
-		int result=inquiryDAO.add(data);
+		String message = "Unsuccessfull";
+		String gsonData = helper.getParameter("jsonData");
+		Gson gson = new Gson();
+		InstituteInquiry data = getInstituteInquirydetails(gsonData);
+
+		InstituteInquiryDAO inquiryDAO = new InstituteInquiryDAO();
+		int result = inquiryDAO.add(data);
+		if (result > 0) {
+			message = "Inquiry Send successfylly";
+
+		}
+
+		helper.setAttribute("result", gson.toJson(message));
 		return view;
 	}
 
 	public Object extractFromJason(String gsonData) {
 		Gson gson = new Gson();
 		String message = "";
-		InstituteInquiry instituteInquiry=null;
+		InstituteInquiry instituteInquiry = null;
 		try {
-			 instituteInquiry = gson.fromJson(gsonData, InstituteInquiry.class);
-		
+			instituteInquiry = gson.fromJson(gsonData, InstituteInquiry.class);
+
 		} catch (Exception exception) {
 			log.error("extractFromJason(): " + exception.toString());
 			throw exception;
@@ -43,9 +59,16 @@ public class CmdSendInstituteInquiry implements ICommand{
 		return instituteInquiry;
 	}
 
-
+	/**
+	 * Exctract data from json object and assign it to InstituteInquiry object
+	 * 
+	 * @author Chathuri
+	 * @param data
+	 *            String type
+	 * @return InstituteInquery object
+	 */
 	private InstituteInquiry getInstituteInquirydetails(String data) {
-	
+
 		InstituteInquiry instituteInquiry = (InstituteInquiry) extractFromJason(data);
 
 		return instituteInquiry;
