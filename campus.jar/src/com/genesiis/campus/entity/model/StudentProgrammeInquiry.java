6faf462b9cadcf7afc675f3fcdab3d023dc50875
Anuradha Silva <anuradha@genesiis.com>
2016-10-27 17:38:14 +0530

    C8-inquiry-form-for-course DAO class modification WIP

---
+++ campus.jar/src/com/genesiis/campus/entity/model/StudentProgrammeInquiry.java
@@ -0,0 +1,136 @@
+package com.genesiis.campus.entity.model;
+
+import java.sql.Date;
+
+//20161027 AS C8-inquiry-form-for-course CmdSendCourseInquiry class created.
+
+public class StudentProgrammeInquiry {
+
+	private int code;
+	private String studentName;
+	private int studentEmail;
+	private String telephoneCountryCode;
+	private String telephoneAreaCode;
+	private String telephone;
+	private String inquiryTitle;
+	private String inquiry;
+	private String student;
+	private String programme;
+	private String crtBy;
+	private Date crtOn;
+	private Date modOn;
+	private String modBy;
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public int getStudentEmail() {
+		return studentEmail;
+	}
+
+	public void setStudentEmail(int studentEmail) {
+		this.studentEmail = studentEmail;
+	}
+
+	public String getTelephoneCountryCode() {
+		return telephoneCountryCode;
+	}
+
+	public void setTelephoneCountryCode(String telephoneCountryCode) {
+		this.telephoneCountryCode = telephoneCountryCode;
+	}
+
+	public String getTelephoneAreaCode() {
+		return telephoneAreaCode;
+	}
+
+	public void setTelephoneAreaCode(String telephoneAreaCode) {
+		this.telephoneAreaCode = telephoneAreaCode;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getInquiryTitle() {
+		return inquiryTitle;
+	}
+
+	public void setInquiryTitle(String inquiryTitle) {
+		this.inquiryTitle = inquiryTitle;
+	}
+
+	public String getInquiry() {
+		return inquiry;
+	}
+
+	public void setInquiry(String inquiry) {
+		this.inquiry = inquiry;
+	}
+
+	public String getStudent() {
+		return student;
+	}
+
+	public void setStudent(String student) {
+		this.student = student;
+	}
+
+	public String getProgramme() {
+		return programme;
+	}
+
+	public void setProgramme(String programme) {
+		this.programme = programme;
+	}
+
+	public String getCrtBy() {
+		return crtBy;
+	}
+
+	public void setCrtBy(String crtBy) {
+		this.crtBy = crtBy;
+	}
+
+	public Date getCrtOn() {
+		return crtOn;
+	}
+
+	public void setCrtOn(Date crtOn) {
+		this.crtOn = crtOn;
+	}
+
+	public Date getModOn() {
+		return modOn;
+	}
+
+	public void setModOn(Date modOn) {
+		this.modOn = modOn;
+	}
+
+	public String getModBy() {
+		return modBy;
+	}
+
+	public void setModBy(String modBy) {
+		this.modBy = modBy;
+	}
+
+}
---
+++ campus.war/web/dist/js/institute/institute.helper.js
@@ -0,0 +1,80 @@
+/**
+ * //20161027 AS C8-inquiry-form-for-course institute.helper.js  created.
+ * //20161027 AS C8-inquiry-form-for-course sendCourseInquiry() method data validation and data send as json object.
+ */ 
+ 
+ 
+ function sendCourseInquiry() {
+	var fullName = $("#fullname").val();
+	var email = $("#email").val();
+	var countryCode = $("#countryCode").val();
+	var areaCode = $("#areaCode").val();
+	var telephoneNumber = $("#telNum").val();
+	var inquiryTitle = $("#inquiryTitle").val();
+	var inquiry = $("#inquiry").val();
+	
+	var fullNametb = isEmptyfield(fullName);
+	var emailtb = isEmptyfield(email);
+	var countryCodetb = isEmptyfield(countryCode);
+	var areaCodetb = isEmptyfield(areaCode);
+	var telephoneNumbertb = isEmptyfield(telephoneNumber);
+	var inquiryTitletb = isEmptyfield(inquiryTitle);
+	var inquirytb = isEmptyfield(inquiry);
+
+	if (fullNametb == false) {
+		document.getElementById('fmemployeeidError').innerHTML = "** Invalid Name.";
+	}
+	if (emailtb == false) {
+		document.getElementById('relationshipError').innerHTML = "** Email can not be Empty.";
+	}
+	if (countryCodetb == false) {
+		document.getElementById('relationbirthdateError').innerHTML = "** Country Code can not be Empty.";
+	}
+	if (areaCodetb == false) {
+		document.getElementById('relationnameError').innerHTML = "** Area Code cannot be Empty.";
+	}
+	if (telephoneNumbertb == false) {
+		document.getElementById('relationshipError').innerHTML = "** Telephone can not be Empty.";
+	}
+	if (inquiryTitletb == false) {
+		document.getElementById('relationbirthdateError').innerHTML = "** Inquiry Title can not be Empty.";
+	}
+	if (inquirytb == false) {
+		document.getElementById('relationnameError').innerHTML = "** inquiry cannot be Empty.";
+	}
+	
+	if ((employeeIdtb == true) && (relationshiptb == true)
+			&& (relationDateofbirthtb == true) && (relationNametb == true)) {
+		var jsonData = {
+			"fullName" : fullName,
+			"email" : email,
+			"countryCode" : countryCode,
+			"areaCode" : areaCode,
+			"telephoneNumber" : telephoneNumber,
+			"inquiryTitle" : inquiryTitle,
+			"inquiry" : inquiry
+		};
+
+		$.ajax({
+			type : "POST",
+			url : 'InstituteController',
+			data : {
+				jsonData : JSON.stringify(jsonData),
+				task : "SCI"
+			},
+			dataType : "json",
+			success : function(data) {
+				alert(data);
+				if (data == "Details added successfully.") {
+					clearFamilydetails();
+				}
+			},
+			error : function(e) {
+				alert("Error " + e);
+				console.log(e);
+			}
+		});
+	}
+}
+ 
+ 
