259005c693a77f7ca47a6f77fffa789b6046ef26
jayani1991 <jayani@genesiis.com>
2016-10-27 17:18:00 +0530

    c7-higher-education-landing-page list higher education programmes command class coding wip -JH

--- campus.jar/src/com/genesiis/campus/command/CmdListHigherEducationProgrammes.java
+++ campus.jar/src/com/genesiis/campus/command/CmdListHigherEducationProgrammes.java
@@ -5,6 +5,7 @@ package com.genesiis.campus.command;
 //20161025 JH c7-list-higher-education-courses execute method coding 
 //20161026 JH c7-list-higher-education-courses CmdListHigherEducationCourses modified
 //20161026 JH c7-higher-education-landing-page class renamed as CmdListHigherEducationProgrammes.java
+//20161027 JH c7-higher-education-landing-page execute method coding
 
 import java.sql.SQLException;
 import java.text.DateFormat;
@@ -23,55 +24,71 @@ import com.genesiis.campus.util.IDataHelper;
 import com.genesiis.campus.validation.SystemMessage;
 
 public class CmdListHigherEducationProgrammes implements ICommand {
-	static org.apache.log4j.Logger log = Logger.getLogger(CmdListHigherEducationProgrammes.class.getName());
+	static org.apache.log4j.Logger log = Logger
+			.getLogger(CmdListHigherEducationProgrammes.class.getName());
 	private IView programmeData;
-	
-	public CmdListHigherEducationProgrammes(){
+
+	public CmdListHigherEducationProgrammes() {
 	}
-	
+
 	/**
 	 * used to list higher education courses
-	 * @param IDataHelper 
+	 * 
+	 * @param IDataHelper
 	 * @param IView
 	 * @return Iview
 	 * @author JH
 	 */
-	
+
 	@Override
 	public IView execute(IDataHelper helper, IView iview) throws SQLException,
 			Exception {
-		
+
 		final HigherEducationProgrammeDAO higherEducationProgrammeDAO = new HigherEducationProgrammeDAO();
 		final Programme programme = new Programme();
 		Collection<Collection<String>> programmes = null;
-		
+
 		SystemMessage systemMessage = SystemMessage.UNKNOWN;
 		
-		try{
-			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-			Date date = new Date();
-			String category = helper.getParameter("categoryId");
-			
+		final String category = helper.getParameter("categoryId");
+		
+
+		try {
+
+			java.util.Date utilDate = new java.util.Date();
+			java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+
+
+
 			programme.setActive(true);
-			programme.setExpiryDate(date);
+			programme.setExpiryDate(sqlDate);
 			programme.setCategory(Integer.parseInt(category));
-			
+			programme.setProgrammeStatus(1);
+
 			programmes = higherEducationProgrammeDAO.findById(programme);
 			iview.setCollection(programmes);
-		
-			final CourseProvider courseProvider = new CourseProvider();.
-			courseProvider.setCourseProviderType(Integer.parseInt(category));
-			courseProvider.setCourseProviderStatus(1);
-			
-			final CourseProviderDAO courseProviderDAO = new CourseProviderDAO();
-			final Collection<Collection<String>> courseProviders = courseProviderDAO.findById(courseProvider);
-		
-		}catch(Exception exception){
+
+		} catch (Exception exception) {
 			log.error("execute() : " + exception);
 			systemMessage = SystemMessage.ERROR;
 			throw exception;
+		} finally {
+			//get course providers
+			
+			CourseProviderDAO courseProviderDAO =  new CourseProviderDAO();
+	
+			
+			CourseProvider courseProvider = new CourseProvider();
+			courseProvider.setCourseProviderType(Integer.parseInt(category));
+			courseProvider.setCourseProviderStatus(1);
+
+			final Collection<Collection<String>> courseProviders  = courseProviderDAO
+					.findById(courseProvider);
+			helper.setAttribute("week", courseProviders);
 		}
-		
+
+		helper.setAttribute("message", systemMessage.message());
+
 		return iview;
 	}
 }
