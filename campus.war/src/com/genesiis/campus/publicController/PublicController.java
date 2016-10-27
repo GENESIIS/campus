eb9102fdc5082631ccfa3428c47578a79c7cb589
jayani1991 <jayani@genesiis.com>
2016-10-27 16:35:13 +0530

    c7-higher-education-landing-page fixed servlet issue -JH

--- campus.war/src/com/genesiis/campus/publicController/PublicController.java
+++ campus.war/src/com/genesiis/campus/publicController/PublicController.java
@@ -1,5 +1,6 @@
 package com.genesiis.campus.publicController;
 
+//20161027 JH c7-higher-education-landing-page fixed servlet issue
 
 import java.io.IOException;
 
@@ -8,47 +9,42 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.log4j.Logger;
 
 import com.genesiis.campus.controller.CampusController;
 
+import org.apache.log4j.Logger;
+
+/**
+ * Servlet implementation class CompanyController
+ */
+@WebServlet("/PublicController")
 public class PublicController extends CampusController {
-	
-	static Logger log = Logger.getLogger(PublicController.class.getName());
-
+	static org.apache.log4j.Logger log = Logger.getLogger(PublicController.class.getName());
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-	 public PublicController() {
-	        super();
-	    }
 
-
-	    public void init() throws ServletException {
-
-		}
-	    
-		/**
-		 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-		 */
-		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			this.doPost(request, response);
-		}
-
-		/**
-		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-		 */
-		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			processRequest(request, response);
-		}
-		
-		protected void processRequest(HttpServletRequest 
-				  request, HttpServletResponse response)
-				  throws ServletException, java.io.IOException {
-				  super.doPost(request, response);
-		}
-
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public PublicController() {
+		super();
 	}
-	
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		super.doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		super.doPost(request, response);
+	}
+
+}
