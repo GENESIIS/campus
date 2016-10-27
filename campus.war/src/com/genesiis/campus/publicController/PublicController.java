7da4c857bead239db553c75afc3856934b128953
dushanthacb <dushantha@genesiis.com>
2016-10-27 17:17:13 +0530

    c10-contacting-us-page add the @@WebServlet part added

--- campus.war/src/com/genesiis/campus/publicController/PublicController.java
+++ campus.war/src/com/genesiis/campus/publicController/PublicController.java
@@ -12,43 +12,45 @@ import org.apache.log4j.Logger;
 
 import com.genesiis.campus.controller.CampusController;
 
-public class PublicController extends CampusController {
-	
+@WebServlet("/PublicController")
+public class PublicController extends CampusController {	
 	static Logger log = Logger.getLogger(PublicController.class.getName());
-
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-	 public PublicController() {
-	        super();
-	    }
+	public PublicController() {
+		super();
+	}
 
+	public void init() throws ServletException {
 
-	    public void init() throws ServletException {
-
-		}
+	}
 	    
-		/**
-		 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-		 */
-		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			this.doPost(request, response);
-		}
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
 
-		/**
-		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-		 */
-		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			processRequest(request, response);
-		}
-		
-		protected void processRequest(HttpServletRequest 
-				  request, HttpServletResponse response)
-				  throws ServletException, java.io.IOException {
-				  super.doPost(request, response);
-		}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	protected void processRequest(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException,
+			java.io.IOException {
+		super.doPost(request, response);
+	}
 
 	}
 	
