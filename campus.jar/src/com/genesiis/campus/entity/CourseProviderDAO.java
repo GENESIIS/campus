c177991a97c8e8a6ea8723047a0e898e0dcd85ce
Miyuru-Genesiis <miyuru@genesiis.com>
2016-10-28 04:45:46 +0530

    c5-corporate-training-landing-page Modified findByIt() method to correct SQL query so only distinct results are shown

--- campus.jar/src/com/genesiis/campus/entity/CourseProviderDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/CourseProviderDAO.java
@@ -2,6 +2,10 @@ package com.genesiis.campus.entity;
 
 //20161027 MM c5-corporate-training-landing-page INIT CourseProviderDAO.java 
 //				and implemented findById()
+//20161028 MM c5-corporate-training-landing-page Modified findByIt() method to use 
+// 				Programme object to retrieve argument (categoryCode) to use for search
+//20161028 MM c5-corporate-training-landing-page Modified findByIt() method to correct
+//				SQL query so only distinct results are shown
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -34,11 +38,10 @@ public class CourseProviderDAO implements ICrud {
 		PreparedStatement stmt = null;
 
 		try {
-			CourseProvider courseProvider = (CourseProvider) code;
-			int categoryCode = courseProvider.getCourseProviderType();
-			
-			String query = "SELECT * FROM [CAMPUS].[COURSEPROVIDER] WHERE COURSEPROVIDERTYPE = ?";
-			String courseProviderQuery = "SELECT cp.* FROM [CAMPUS].[PROGRAMME] p "
+			Programme programme = (Programme) code;
+			int categoryCode = programme.getCategory();
+
+			String query = "SELECT DISTINCT cp.* FROM [CAMPUS].[PROGRAMME] p "
 					+ "JOIN [CAMPUS].[CATEGORY] c ON (p.CATEGORY = c.CODE AND c.CODE = ?) "
 					+ "JOIN [CAMPUS].[COURSEPROVIDER] cp ON (cp.CODE = p.COURSEPROVIDER)";
 
@@ -69,7 +72,7 @@ public class CourseProviderDAO implements ICrud {
 		return courseProviderList;
 	}
 	
-	private void retrieveCourseProvidersFromResultSet(ResultSet rs, Collection<Collection<String>> deptList) throws SQLException {
+	private void retrieveCourseProvidersFromResultSet(ResultSet rs, Collection<Collection<String>> courseProviderList) throws SQLException {
 
 		while (rs.next()) {
 			final ArrayList<String> singleCourseProvider = new ArrayList<String>();
@@ -79,14 +82,12 @@ public class CourseProviderDAO implements ICrud {
 			singleCourseProvider.add(rs.getString("COURSEINQUIRYEMAIL"));
 			singleCourseProvider.add(rs.getString("HEADERIMAGEPATH"));
 			singleCourseProvider.add(rs.getString("LOGOIMAGEPATH"));
-			singleCourseProvider.add(rs.getString("ISACTIVE"));
 			singleCourseProvider.add(rs.getString("DESCRIPTION"));
 			singleCourseProvider.add(rs.getString("UNIQUEPREFIX"));
 			singleCourseProvider.add(rs.getString("SPECIALITY"));
 			singleCourseProvider.add(rs.getString("WEBLINK"));
 			singleCourseProvider.add(rs.getString("FACEBOOKURL"));
 			singleCourseProvider.add(rs.getString("TWITTERURL"));
-			singleCourseProvider.add(rs.getString("PROGRAMMESTATUS"));
 			singleCourseProvider.add(rs.getString("MYSPACEURL"));
 			singleCourseProvider.add(rs.getString("LINKEDINURL"));
 			singleCourseProvider.add(rs.getString("INSTAGRAMURL"));
@@ -95,7 +96,7 @@ public class CourseProviderDAO implements ICrud {
 			singleCourseProvider.add(rs.getString("EXPIRATIONDATE"));
 			singleCourseProvider.add(rs.getString("MYSPACEURL"));
 			final Collection<String> singleCourseProviderCollection = singleCourseProvider;
-			deptList.add(singleCourseProviderCollection);
+			courseProviderList.add(singleCourseProviderCollection);
 		}
 	}
 
