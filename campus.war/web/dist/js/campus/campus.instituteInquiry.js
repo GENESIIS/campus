781606d7e1ac6d86e4111a5f8453ea3890184524
chathuriM <madushani@genesiis.com>
2016-10-27 15:10:05 +0530

    c9-inquiry form for institute- Modified js file-cm

--- campus.war/web/dist/js/campus/campus.instituteInquiry.js
+++ campus.war/web/dist/js/campus/campus.instituteInquiry.js
@@ -3,29 +3,19 @@
  */
 
 /*******************************************************************************
- * To Add data
+ * To Add data. Get data and sent to InstituteController.java
  ******************************************************************************/
-// Get data and sent to InstituteController.java.
 function addInstituteInquiry() {
-	alert("hi0");
+
 	var fullname = $("#fullnamee").val();
-	alert("Fullname: " + fullname);
 	var email = $("#email").val();
-	alert("email: " + email);
 	var countryCode = $("#countryCode").val();
-	alert("Country Code: " + countryCode);
 	var areaCode = $("#areaCode").val();
-	alert("areaCode: " + areaCode);
 	var telephoneNumber = $("#telNum").val();
-	alert("telephone: " + telephoneNumber);
 	var inquiryTitle = $("#inquiryTitle").val();
-	alert("intuiryTitle : " + inquiryTitle);
 	var inquiry = $("#inquiry").val();
-	alert("inquiy: " + inquiry);
 	var courseProvider = $("#courseProviderCode").text();
-	alert("courseProvider: " + courseProvider);
 	var student = $("#studentCode").text();
-	alert("studentCode: " + student);
 	alert(fullname + " - " + email + " - " + countryCode + " - " + areaCode
 			+ " - " + telephoneNumber + " - " + inquiryTitle + " - " + inquiry
 			+ " - " + courseProvider + " - " + student);
@@ -42,14 +32,14 @@ function addInstituteInquiry() {
 		"courseProvider" : courseProvider,
 		"student" : student,
 	};
-	
+
 	if ((fullname == "") || (email == "") || (areaCode == "")
 			|| (telephoneNumber == "") || (inquiryTitle == "")
 			|| (inquiry == "")) {
 		alert("Please fill the Empty fields.");
 
 	} else {
-		alert("ok");
+		
 		$.ajax({
 
 			type : "POST",
@@ -61,9 +51,10 @@ function addInstituteInquiry() {
 			dataType : "json",
 			success : function(data) {
 				alert(data);
-				/*if (data == "Inquiry send successfully.") {
-				
-				}*/
+				/*
+				 * if (data == "Inquiry send successfully.") {
+				 *  }
+				 */
 			},
 			error : function(e) {
 				alert("Error " + e);
