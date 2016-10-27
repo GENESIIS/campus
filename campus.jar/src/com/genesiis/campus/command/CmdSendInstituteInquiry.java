809f447cc340941d26c3f7821c22a9d7d5238fa3
chathuriM <madushani@genesiis.com>
2016-10-27 14:29:47 +0530

    c9-inquiry form for institute-Modified execute() method-cm

--- campus.jar/src/com/genesiis/campus/command/CmdSendInstituteInquiry.java
+++ campus.jar/src/com/genesiis/campus/command/CmdSendInstituteInquiry.java
@@ -1,19 +1,53 @@
 package com.genesiis.campus.command;
 
 //20161026 CM c9-make inquiry for institute INIT CmdSendInstituteInquiry.java
+//20161027 CM c9-make inquiry for institute Modified execute() method
 
 import java.sql.SQLException;
 
+import org.apache.log4j.Logger;
+
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.InstituteInquiryDAO;
+import com.genesiis.campus.entity.model.InstituteInquiry;
 import com.genesiis.campus.util.IDataHelper;
+import com.google.gson.Gson;
 
 public class CmdSendInstituteInquiry implements ICommand{
 
+	static Logger log = Logger.getLogger(CmdSendInstituteInquiry.class.getName());
+	
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
-		// TODO Auto-generated method stub
-		return null;
+		 String gsonData=helper.getParameter("jsonData");
+		 
+		InstituteInquiry data=getInstituteInquirydetails(gsonData);
+		
+		InstituteInquiryDAO inquiryDAO=new InstituteInquiryDAO();
+		int result=inquiryDAO.add(data);
+		return view;
 	}
 
+	public Object extractFromJason(String gsonData) {
+		Gson gson = new Gson();
+		String message = "";
+		InstituteInquiry instituteInquiry=null;
+		try {
+			 instituteInquiry = gson.fromJson(gsonData, InstituteInquiry.class);
+		
+		} catch (Exception exception) {
+			log.error("extractFromJason(): " + exception.toString());
+			throw exception;
+		}
+		return instituteInquiry;
+	}
+
+
+	private InstituteInquiry getInstituteInquirydetails(String data) {
+	
+		InstituteInquiry instituteInquiry = (InstituteInquiry) extractFromJason(data);
+
+		return instituteInquiry;
+	}
 }
