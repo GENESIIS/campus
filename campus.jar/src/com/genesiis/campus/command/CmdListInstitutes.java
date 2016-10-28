5673a10f5022e680419394147909d3b5374bd9c3
Dumani Jayasinghe <dumani@genesiis.com>
2016-10-28 15:11:30 +0530

    c6-list-available-institutes-on-the-view implementing execute() method

--- campus.jar/src/com/genesiis/campus/command/CmdListInstitutes.java
+++ campus.jar/src/com/genesiis/campus/command/CmdListInstitutes.java
@@ -1,13 +1,15 @@
 package com.genesiis.campus.command;
 
 //DJ 20161026 c6-list-available-institutes-on-the-view created CmdListInstitutes.java
+//DJ 20161026 c6-list-available-institutes-on-the-view implementing execute() method
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.entity.InstituteProviderDAO;
-import com.genesiis.campus.entity.model.Institutes;
+import com.genesiis.campus.entity.model.Institute;
 import com.genesiis.campus.util.IDataHelper;
 import com.genesiis.campus.validation.SystemMessage;
 
@@ -17,22 +19,26 @@ public class CmdListInstitutes implements ICommand  {
 	static Logger log = Logger.getLogger(CmdListInstitutes.class.getName());
 	
 	@Override
-	public IView execute(IDataHelper helper, IView view) throws SQLException,
+	public IView execute(IDataHelper helper, IView iview) throws SQLException,
 			Exception {
 		final InstituteProviderDAO providerDAO=new InstituteProviderDAO();		
-		SystemMessage systemMessage = SystemMessage.UNKNOWN;
-		
-		final Collection<Collection<String>> institutes =new ArrayList<Collection<String>>();
+		SystemMessage systemMessage = SystemMessage.UNKNOWN;		
 		try {
-			final Institutes institute=new Institutes();
-			providerDAO.findById(institute);
-			
+			int categoryCode =0;
+			final Institute institute=new Institute();
+			if (helper.getParameter("category") != null) {
+				categoryCode = Integer.parseInt(helper.getParameter("category"));
+				institute.setCategory(categoryCode);
+			}
+			final Collection<Collection<String>> institutes=providerDAO.findById(institute);
+			iview.setCollection(institutes);
+			//helper.setAttribute("courseProviders", institutes);
 		} catch (Exception exception) {
 			log.error("execute() : " + exception);
 			systemMessage = SystemMessage.ERROR;
 			throw exception;
 		}
-		return null;
+		return iview;
 	}
 
 }
