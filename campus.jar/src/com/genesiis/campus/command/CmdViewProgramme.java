ceed33a7823a93991868a2d5e8d411b82eb2e595
chathuriM <madushani@genesiis.com>
2016-10-28 09:40:18 +0530

    c13-display course details- Moified execute method. Created new attribute-cm

--- campus.jar/src/com/genesiis/campus/command/CmdViewProgramme.java
+++ campus.jar/src/com/genesiis/campus/command/CmdViewProgramme.java
@@ -4,6 +4,7 @@ package com.genesiis.campus.command;
 //20161025 CM c13-Display course details Modified execute() method. 
 //20161025 CM c13-Display course details Modified execute() method. Set ProgrammeView attribute.
 //20161026 CM c13-Display course details Modified execute() method. Remove Module attribute.
+//20161026 CM c13-Display course details Modified execute() method. Created  programmeView attribute.
 
 import java.sql.SQLException;
 import java.util.Collection;
@@ -19,47 +20,51 @@ import com.genesiis.campus.util.IDataHelper;
 
 import org.apache.log4j.Logger;
 
-public class CmdViewProgramme implements ICommand{
+public class CmdViewProgramme implements ICommand {
 
 	static Logger log = Logger.getLogger(CmdViewProgramme.class.getName());
 	private IView programmeData;
-	
+
 	public CmdViewProgramme() {
 		// TODO Auto-generated constructor stub
 	}
+
 	public CmdViewProgramme(IView programmeData) {
-		this.programmeData=programmeData;
+		this.programmeData = programmeData;
 	}
-	
-	
+
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
 		try {
-			final Programme programme=new Programme();
-			 ICrud programmeDAO=new ProgrammeDAO();
-			 ICrud semesterDAO=new SemesterDAO();
-			 ICrud intakeDAO=new IntakeDAO();
-			 ICrud moduleDAO=new ModuleDAO();
-			 
-			int programmeId= Integer.parseInt(helper.getParameter("programmeCode"));
-			
-			programme.setCode(programmeId);
-			
-			Collection<Collection<String>> programmeDAOCollection = programmeDAO.findById(programme);
-			//view.setCollection(programmeDAOCollection);
-			
-			Collection<Collection<String>> semesterDAOCollection = semesterDAO.findById(programme);
-		//	view.setCollection(semesterDAOCollection);
-			
+			final Programme programme = new Programme();
+			ICrud programmeDAO = new ProgrammeDAO();
+			ICrud semesterDAO = new SemesterDAO();
+			ICrud intakeDAO = new IntakeDAO();
+			ICrud moduleDAO = new ModuleDAO();
 
-			Collection<Collection<String>> moduleDAOCollection = moduleDAO.findById(programme);
+			int programmeId = Integer.parseInt(helper
+					.getParameter("programmeCode"));
+
+			programme.setCode(programmeId);
+
+			Collection<Collection<String>> programmeDAOCollection = programmeDAO
+					.findById(programme);
+			// view.setCollection(programmeDAOCollection);
+
+			Collection<Collection<String>> semesterDAOCollection = semesterDAO
+					.findById(programme);
+			// view.setCollection(semesterDAOCollection);
+
+			Collection<Collection<String>> moduleDAOCollection = moduleDAO
+					.findById(programme);
 			view.setCollection(moduleDAOCollection);
-			
-			Collection<Collection<String>> intakeDAOCollection = intakeDAO.findById(programme);
-			
+
+			Collection<Collection<String>> intakeDAOCollection = intakeDAO
+					.findById(programme);
+
 			helper.setAttribute("semesterView", semesterDAOCollection);
-		//	helper.setAttribute("moduleView", moduleDAOCollection);
+			helper.setAttribute("programmeView", moduleDAOCollection);
 			helper.setAttribute("intakeView", intakeDAOCollection);
 		} catch (Exception e) {
 			log.info("execute() : e" + e.toString());
