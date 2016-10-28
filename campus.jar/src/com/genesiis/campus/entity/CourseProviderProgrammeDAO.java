62beaf483e981ef10c3a4606a7bdbf99a82e5ab4
jayani1991 <jayani@genesiis.com>
2016-10-28 13:02:50 +0530

    c7-higher-education-landing-page course provider DAO class renbamed and getAll sql query modified -JH

--- campus.jar/src/com/genesiis/campus/entity/CourseProviderProgrammeDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/CourseProviderProgrammeDAO.java
@@ -5,6 +5,8 @@ package com.genesiis.campus.entity;
 //20161026 JH c7-higher-education-landing-page InstituteDAO.java renamed as CourseProviderDAO.java
 //20161026 JH c7-higher-education-landing-page findById(Object code) method coding 
 //20161027 JH c7-higher-education-landing-page findById(Object code) method modified
+//20161028 JH c7-higher-education-landing-page findById(Object code) query modified 
+//20161028 JH c7-higher-education-landing-page CourseProviderDAO.java renamed as CourseProviderProgrammeDAO.java
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -18,9 +20,10 @@ import org.apache.log4j.Logger;
 import com.genesiis.campus.entity.model.CourseProvider;
 import com.genesiis.campus.util.ConnectionManager;
 
-public class CourseProviderDAO implements ICrud{
-	
-	static org.apache.log4j.Logger log = Logger.getLogger(CourseProviderDAO.class.getName());
+public class CourseProviderProgrammeDAO implements ICrud {
+
+	static org.apache.log4j.Logger log = Logger
+			.getLogger(CourseProviderProgrammeDAO.class.getName());
 
 	@Override
 	public int add(Object object) throws SQLException, Exception {
@@ -40,59 +43,72 @@ public class CourseProviderDAO implements ICrud{
 		return 0;
 	}
 
-	
 	/**
-	 * findById(Object code) is used to get all of the course providers
-	 * belongs to the given course provider type and where they are active.
-	 * Ex: course provider type = higher education, status = 1
+	 * findById(Object code) is used to get all of the course providers belongs
+	 * to the given course provider type and where they are active. Ex: course
+	 * category = higher education, status = 1
 	 * 
-	 * @param Object code
+	 * @param Object
+	 *            code
 	 * @return collection of type String
 	 * @author JH
 	 */
 	@Override
 	public Collection<Collection<String>> findById(Object code)
 			throws SQLException, Exception {
-	
+
 		Connection conn = null;
 		PreparedStatement preparedStatement = null;
-	
-		
+
 		int status = 0;
 		String returnMessage = "";
 		final Collection<Collection<String>> courseProviderCollection = new ArrayList<Collection<String>>();
-		
-		String getAllQuery = "SELECT * FROM [CAMPUS].[COURSEPROVIDER ] WHERE COURSEPROVIDERTYPE =? and COURSEPROVIDERSTATUS = ? ";
-				
-		try{
-			
-			
-			final 	CourseProvider courseProvider  = (CourseProvider) code;
+
+		/**
+		 * getAllQuery used to query the database to retrieve data based on
+		 * several criteria. 1. select programme stat for each programme for
+		 * given category 2. programmes that were expired within a year from the
+		 * current date is considered 3.
+		 */
+		String getAllQuery = " SELECT p.COURSEPROVIDER , COUNT(*) FROM [CAMPUS].[PROGRAMME] p "
+				+ " INNER JOIN [CAMPUS].[PROGRAMMESTAT] ps ON p.CODE = ps.PROGRAMME AND p.CATEGORY = ?"
+				+ " AND p.EXPIRYDATE > GETDATE() "
+				+ " INNER JOIN [CAMPUS].[COURSEPROVIDER] cp on cp.CODE = p.COURSEPROVIDER AND cp.COURSEPROVIDERSTATUS = 1 "
+				+ " GROUP BY p.COURSEPROVIDER ORDER BY  count(*) DESC  ";
+
+		try {
+
+			final CourseProvider courseProvider = (CourseProvider) code;
 			conn = ConnectionManager.getConnection();
 			preparedStatement = conn.prepareStatement(getAllQuery);
-			
 
 			preparedStatement.setInt(1, courseProvider.getCourseProviderType());
-			preparedStatement.setInt(2, courseProvider.getCourseProviderStatus());
-			
+			preparedStatement.setInt(2,
+					courseProvider.getCourseProviderStatus());
+
 			final ResultSet rs = preparedStatement.executeQuery();
-			
-			while(rs.next()){
+
+			while (rs.next()) {
 				final ArrayList<String> singleCourseProviderList = new ArrayList<String>();
-				
+
 				log.info(rs.getString("CODE"));
 				singleCourseProviderList.add(rs.getString("CODE"));
 				singleCourseProviderList.add(rs.getString("UNIQUEPREFIX"));
 				singleCourseProviderList.add(rs.getString("NAME"));
 				singleCourseProviderList.add(rs.getString("GENERALEMAIL"));
-				singleCourseProviderList.add(rs.getString("COURSEINQUIRYEMAIL"));
-				singleCourseProviderList.add(rs.getString("LANDPHONECOUNTRYCODE"));
-				singleCourseProviderList.add(rs.getString("LANDPHONECOUNTRYCODE"));
+				singleCourseProviderList
+						.add(rs.getString("COURSEINQUIRYEMAIL"));
+				singleCourseProviderList.add(rs
+						.getString("LANDPHONECOUNTRYCODE"));
+				singleCourseProviderList.add(rs
+						.getString("LANDPHONECOUNTRYCODE"));
 				singleCourseProviderList.add(rs.getString("LANDPHONEAREACODE"));
 				singleCourseProviderList.add(rs.getString("LANDPHONENO"));
 				singleCourseProviderList.add(rs.getString("LANDPHONE2NO"));
-				singleCourseProviderList.add(rs.getString("MOBILEPHONECOUNTRYCODE"));
-				singleCourseProviderList.add(rs.getString("MOBILEPHONENETWORKCODE"));
+				singleCourseProviderList.add(rs
+						.getString("MOBILEPHONECOUNTRYCODE"));
+				singleCourseProviderList.add(rs
+						.getString("MOBILEPHONENETWORKCODE"));
 				singleCourseProviderList.add(rs.getString("MOBILEPHONENO"));
 				singleCourseProviderList.add(rs.getString("HEADERIMAGEPATH"));
 				singleCourseProviderList.add(rs.getString("LOGOIMAGEPATH"));
@@ -111,22 +127,25 @@ public class CourseProviderDAO implements ICrud{
 				singleCourseProviderList.add(rs.getString("ACCOUNTTYPE"));
 				singleCourseProviderList.add(rs.getString("ISTUTORRELATED"));
 				singleCourseProviderList.add(rs.getString("ISADMINALLOWED"));
-				singleCourseProviderList.add(rs.getString("COURSEPROVIDERSTATUS"));
-				singleCourseProviderList.add(rs.getString("COURSEPROVIDERTYPE"));
+				singleCourseProviderList.add(rs
+						.getString("COURSEPROVIDERSTATUS"));
+				singleCourseProviderList
+						.add(rs.getString("COURSEPROVIDERTYPE"));
 				singleCourseProviderList.add(rs.getString("PRINCIPAL"));
 				singleCourseProviderList.add(rs.getString("TUTOR"));
 				singleCourseProviderList.add(rs.getString("CRTON"));
 				singleCourseProviderList.add(rs.getString("CRTBY"));
 				singleCourseProviderList.add(rs.getString("MODON"));
-				singleCourseProviderList.add(rs.getString("MODBY"));			
-				
+				singleCourseProviderList.add(rs.getString("MODBY"));
+
 				final Collection<String> singleCourseProviderCollection = singleCourseProviderList;
-				courseProviderCollection.add(singleCourseProviderCollection);  
-				
+				courseProviderCollection.add(singleCourseProviderCollection);
+
 			}
 
-		}catch (SQLException exception) {
-			log.error("findById(Object code) sql exception" + exception.toString());
+		} catch (SQLException exception) {
+			log.error("findById(Object code) sql exception"
+					+ exception.toString());
 			throw exception;
 
 		} catch (Exception exception) {
