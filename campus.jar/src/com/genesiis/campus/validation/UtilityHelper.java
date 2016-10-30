21bd98cec7a8c7da665990e0d303877b55cdca3d
Dumani Jayasinghe <dumani@genesiis.com>
2016-10-30 22:59:28 +0530

    c6-list-available-institutes-on-the-view INIT UtilityHelper class. implemented isInteger() method

--- campus.jar/src/com/genesiis/campus/validation/UtilityHelper.java
+++ campus.jar/src/com/genesiis/campus/validation/UtilityHelper.java
@@ -1,6 +1,7 @@
 package com.genesiis.campus.validation;
 
 //20161030 DJ c6-list-available-institutes-on-the-view INIT UtilityHelper class. implemented isNotEmpty() method
+//20161030 DJ c6-list-available-institutes-on-the-view INIT UtilityHelper class. implemented isInteger() method
 
 /**
  * @author DJ Utility Helper class to  server side validate. Class name can be change when merging to master branch.
@@ -32,6 +33,22 @@ public class UtilityHelper {
 			status = true;
 		}
 		return status;
+	}
+	
+	/**
+	 * @author DJ
+	 * @param string
+	 * @return boolean to validate is given string contains a numeric value.
+	 * **/
+	public static boolean isInteger(String string) {		
+		try {
+			Integer.parseInt(string);
+		} catch (NumberFormatException e) {
+			return false;
+		} catch (NullPointerException e) {
+			return false;
+		}
+		return true;
 	}
 
 }
