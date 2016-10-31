e8a0224744e25053ba3b6c62582b5f2852437d32
pabodhaW <pabodha@genesiis.com>
2016-10-31 10:14:06 +0530

    c11: modified displayCategory() method to display details coming from the DB on dropdown.

--- campus.war/web/dist/js/filterSearch/ui-populate-helper.js
+++ campus.war/web/dist/js/filterSearch/ui-populate-helper.js
@@ -1,34 +1,15 @@
 //20161030 PN this file contains all the functions to load the details for the filter search
 
-$(document).ready(function() {
+function displayCategory() {
 	$.get('PublicController', {
 		CCO : 'LIST_CATEGORY_DATA'
 	}, function(response) {
 		alert(response);
 		var categories = $("#categoryName");
 		categories.find('option').remove();
-		$.each(response, function(index, value) {	
-			var res = value.toString();
-			var data = res.split(",");
-			alert(data[0]+data[1]);
-			var x = data[0].toString();
-			var y = data[1].toString();
-			$('<option>').val(x).text(y).appendTo(categories)
+		$.each(response, function(index, value) {
+			alert(value);
+			select.append($('<option></option>').val(value).html(value));
 		});
 	});
-
-	$('#categoryName').change(function(event) {
-		var categoryCode = $("categoryName").val();
-		$.get('PublicController', {
-			categoryCode : categoryCode,
-			CCO : 'LIST_MAJOR_DATA'
-		}, function(jsonResponse) {
-			alert(response);
-//			var select = $('#player');
-//			select.find('option').remove();
-//			$.each(jsonResponse, function(index, value) {
-//				$('<option>').val(value).text(value).appendTo(select);
-//			});
-		});
-	});
-});
+}
