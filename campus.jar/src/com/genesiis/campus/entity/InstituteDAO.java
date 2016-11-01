2322aec3d7ff609bca84b0b312a7e8f52b0fde6e
pabodhaW <pabodha@genesiis.com>
2016-11-01 16:16:47 +0530

    c11-criteria-based-filter-search implemented findById() method.

---
+++ campus.jar/src/com/genesiis/campus/entity/InstituteDAO.java
@@ -0,0 +1,110 @@
+package com.genesiis.campus.entity;
+
+//20161101 PN c11-criteria-based-filter-search implemented getAll() method for retrieve existing details
+//		   PN c11-criteria-based-filter-search implemented findById() method. 
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.log4j.Logger;
+
+import com.genesiis.campus.util.ConnectionManager;
+
+public class InstituteDAO implements ICrud{
+	static Logger log = Logger.getLogger(InstituteDAO.class.getName());
+	
+	@Override
+	public int add(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<Collection<String>> findById(Object code)
+			throws SQLException, Exception {
+		int categoryCode = (Integer) code;
+		final Collection<Collection<String>> allInstituteList = new ArrayList<Collection<String>>();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+
+		try {
+			conn = ConnectionManager.getConnection();
+			String query = "SELECT DISTINCT l.CODE,l.NAME,l.UNIQUEPREFIX, l.DESCRIPTION FROM [CAMPUS].[COURSEPROVIDER] l JOIN [CAMPUS].[PROGRAMME] p ON l.CODE = p.COURSEPROVIDER JOIN [CAMPUS].[CATEGORY] m ON m.CODE = p.CATEGORY WHERE m.CODE = ? AND m.ISACTIVE = 1;";
+
+			stmt = conn.prepareStatement(query);
+			stmt.setInt(1, categoryCode);
+			final ResultSet rs = stmt.executeQuery();
+			
+			
+			while (rs.next()) {
+				final ArrayList<String> singleInstituteList = new ArrayList<String>();
+				singleInstituteList.add(rs.getString("CODE"));
+				singleInstituteList.add(rs.getString("NAME"));
+				singleInstituteList.add(rs.getString("UNIQUEPREFIX"));		
+				singleInstituteList.add(rs.getString("DESCRIPTION"));
+
+				final Collection<String> singleLevelCollection = singleInstituteList;
+				allInstituteList.add(singleLevelCollection);
+			}
+		} catch (SQLException sqlException) {
+			log.info("getAll(): SQLE " + sqlException.toString());
+			throw sqlException;
+		} catch (Exception e) {
+			log.info("getAll(): E " + e.toString());
+			throw e;
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		}
+		return allInstituteList;
+	}
+
+	@Override
+	public Collection<Collection<String>> getAll() throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int add(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
