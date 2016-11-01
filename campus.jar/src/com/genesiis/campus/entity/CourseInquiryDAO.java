528ef5481c20be3bf7700540fb637bb0756e3adf
Anuradha Silva <anuradha@genesiis.com>
2016-11-01 17:21:54 +0530

    C8-inquiry-form-for-course add method  query modified

--- campus.jar/src/com/genesiis/campus/entity/CourseInquiryDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/CourseInquiryDAO.java
@@ -3,6 +3,7 @@ package com.genesiis.campus.entity;
 ////20161027 AS C8-inquiry-form-for-course CourseInquiryDAO.java created 
 //20161027 AS C8-inquiry-form-for-course add method modified
 //20161031 AS C8-inquiry-form-for-course add method  query modified
+//20161101 AS C8-inquiry-form-for-course add method  query modified
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -20,7 +21,7 @@ public class CourseInquiryDAO implements ICrud {
 	@Override
 	public int add(Object object) throws SQLException, Exception {
 
-		String query = "INSERT INTO CAMPUS.STUDENTPROGRAMINQUIRY ( NAME, EMAIL, TELEPHONECOUNTRYCODE, TELEPHONEAREACODE, TELEPHONENUM, INQUIRYTITLE, INQUIRYTEXT, INQUIRYDATE, INQUIRYTIME, STUDENT, PROGRAMME, ISACTIVE, CRTON, CRTBY) VALUES( ?, ?, ?, ?, ?, ?, ?, getdate(), getdate(), '1', '1', '1', getdate(), 'admin') ";
+		String query = "INSERT INTO CAMPUS.STUDENTPROGRAMINQUIRY ( NAME, EMAIL, TELEPHONECOUNTRYCODE, TELEPHONEAREACODE, TELEPHONENUM, INQUIRYTITLE, INQUIRYTEXT, INQUIRYDATE, INQUIRYTIME, STUDENT, PROGRAMME, ISACTIVE, CRTON, CRTBY) VALUES( ?, ?, ?, ?, ?, ?, ?, getdate(), getdate(), '1', '7', '1', getdate(), 'admin') ";
 		Connection conn = null;
 		PreparedStatement ps = null;
 		StudentProgrammeInquiry spi = (StudentProgrammeInquiry) object;
@@ -31,17 +32,19 @@ public class CourseInquiryDAO implements ICrud {
 			conn = ConnectionManager.getConnection();
 			ps = conn.prepareStatement(query.toString());
 			ps.setString(1, spi.getStudentName());
+			log.info(spi.getStudentName());
 			ps.setString(2, spi.getStudentEmail());
+			log.info(spi.getStudentEmail());
 			ps.setString(3, spi.getTelephoneCountryCode());
 			ps.setString(4, spi.getTelephoneAreaCode());
 			ps.setString(5, spi.getTelephone());
 			ps.setString(6, spi.getInquiryTitle());
 			ps.setString(7, spi.getInquiry());
 
-		//	ps.setInt(10, spi.getStudent());
-		//	ps.setInt(11, spi.getProgramme());
+			// ps.setInt(10, spi.getStudent());
+			// ps.setInt(11, spi.getProgramme());
 			// ps.setString(12, "1");
-		//	ps.setString(14, "admin");
+			// ps.setString(14, "admin");
 			status = ps.executeUpdate();
 
 		} catch (SQLException exception) {
