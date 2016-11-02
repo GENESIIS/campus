18dcaf5c53c54d82d53cf3bc869a6931b8e9cf0f
dushanthacb <dushantha@genesiis.com>
2016-11-02 16:41:28 +0530

    c10--contacting-us-page-dn campus.jar/src/com/genesiis/campus/util/MailServerManager.java 20161101

--- campus.jar/src/com/genesiis/campus/util/MailServerManager.java
+++ campus.jar/src/com/genesiis/campus/util/MailServerManager.java
@@ -1,4 +1,5 @@
 package com.genesiis.campus.util;
+//20161101 DN c10-contacting-us-page create MailServerManager.java
 
 import java.sql.SQLException;
 import java.util.logging.Logger;
@@ -7,23 +8,29 @@ import javax.mail.Session;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
-
+/**
+ * MailServerManager class access data from server(e.g. jboss) 
+ * configuration xml file and creates a Mail session.
+ * @author DN
+ * @since 20161101
+ *
+ */
 public class MailServerManager {
 	
-	static Logger log = Logger.getLogger(ConnectionManager.class.getName());
+	static Logger log = Logger.getLogger(MailServerManager.class.getName());
 
 	private static final String SESSION_JNDI_NAME = "java:jboss/mail/Default";
 	// The SESSION_JNDI_NAME is the property for mail session source name that the
 	// application looking at runtime.
 	private static Session session;
 
-	// This static{} block runs first when running the ConnectionManager class
+	// This static{} block runs first when running the MailServerManager class
 	// object.
 	static {
 		try {
 			// From the Session name, we get the Session which we have
-			// mentioned in campus.xml file.
-			session = (Session) new InitialContext().lookup(SESSION_JNDI_NAME);
+			// mentioned in server configuration file (e.g.campus.xml,xeno.xml)
+			session = (Session) new InitialContext().lookup(SESSION_JNDI_NAME);			
 		} catch (NullPointerException e) {
 			log.info("NullPointerException: look up mail Session: " + e.toString());
 		} catch (NamingException e) {
@@ -37,8 +44,8 @@ public class MailServerManager {
 		}
 	}
 
-	// This method only return the Connection type variable for other classed,
-	// to access the Mail session through that.
+	// This method only return the Session type variable for other classes,
+	// to access the Mail session.
 	public static Session mailSession() throws SQLException {		
 		return session;
 		
