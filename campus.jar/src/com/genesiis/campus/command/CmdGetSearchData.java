3f4c5801cabe8835fa5cde6bb06cc3d9fbb23460
pabodhaW <pabodha@genesiis.com>
2016-11-02 16:27:14 +0530

    c11-criteria-based-filter-search modified execute() method get searchdata and pass it to ProgramDAO.

--- campus.jar/src/com/genesiis/campus/command/CmdGetSearchData.java
+++ campus.jar/src/com/genesiis/campus/command/CmdGetSearchData.java
@@ -3,12 +3,17 @@ package com.genesiis.campus.command;
 //20161025 PN c11-criteria-based-filter-search INIT CmdGetSearchData.java
 //			  c11-criteria-based-filter-search implementing execute() method.
 //20161027 PN c11-criteria-based-filter-search modified execute() method for test LuceneAPI and QueryBuildingHelper.java class
+//20161102 PN c11-criteria-based-filter-search modified execute() method get searchdata and pass it to ProgramDAO.
 
 import java.sql.SQLException;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.genesiis.campus.entity.CategoryDAO;
+import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.ProgrammeDAO;
 import com.genesiis.campus.util.IDataHelper;
 import com.genesiis.campus.util.LuceneDemo;
 import com.genesiis.campus.util.LuceneTest;
@@ -22,62 +27,36 @@ public class CmdGetSearchData implements ICommand {
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
-		String searchData = helper.getParameter("jsonData");
-		QueryBuildingHelper qbh = new QueryBuildingHelper();
-		String query = qbh.dynamicQuery(qbh.assignMapData(qbh.extractFromJason(searchData)), "");
-		System.out.println(query);
-		
-//		
 
-//		LuceneTest obj = new LuceneTest();
-//
-//		/** creating index */
-//		obj.createIndex();
-
-//		/** searching simple keyword */
-//		System.out.println("==================searching simple keyword===========================");
-//		obj.search("1");
-//
-//		/** searching using wild card */
-//		System.out.println("==================searching using wild card Admin*===========================");
-//		obj.search("1");
-//		
-//		/** searching using wild card */
-//		System.out.println("==================searching using wild card *Admin*===========================");
-//		obj.search("*Admin*");
-//		
-//		/** searching using wild card */
-//		System.out.println("==================searching using wild card *Admin===========================");
-//		obj.search("*Admin");
-
-//		/** searching using logical OR operator */
-//		System.out.println("==================searching using logical OR operator===========================");
-//		obj.search("Admin OR User AND 1");
-//
-//		/** searching using logical AND operator */
-//		System.out.println("==================searching using logical AND operator===========================");
-//		obj.search("Admin AND 1");
-		
-//		LuceneDemo luceneDemo = new LuceneDemo();		
-//		//create Lucene index
-//		luceneDemo.createLuceneIndex();
-//		//create IndexSearcher
-//		luceneDemo.createIndexSearcher();
-//		luceneDemo.termQueryExample();
-//		luceneDemo.prefixQueryExample();
-//		luceneDemo.booleanQueryExample();
-//		luceneDemo.phraseQueryExample();
-//		luceneDemo.wildCardQueryExample();
-//		luceneDemo.fuzzyQueryExample();
-//		luceneDemo.queryParserExample();
-//		luceneDemo.fieldBoostFactorExample();
-//		luceneDemo.sortBySenderExample();
-//		luceneDemo.filterExample();
-//		luceneDemo.deletDocumentFromIndex();
-//		return null;
+		ICrud programmeDAO = new ProgrammeDAO(); 
+		try {
+			String searchData =  helper.getParameter("searchData");
+			QueryBuildingHelper qbh = new QueryBuildingHelper();
+			String tempquery = qbh.dynamicQuery(qbh.assignMapData(qbh.extractFromJason(searchData)), "");
+			log.info("tempquery "+tempquery);
+			Collection<Collection<String>> categoryCollection = programmeDAO.findById(searchData);
+			view.setCollection(categoryCollection);
+		} catch (SQLException sqle) {
+			log.info("execute() : sqle" + sqle.toString());
+			throw sqle;
+		} catch (Exception e) {
+			log.info("execute() : e" + e.toString());
+			throw e;
+		}
 		return view;
 	}
 
-
+//	ICrud categoryDAO = new CategoryDAO();
+//	try {
+//		Collection<Collection<String>> categoryCollection = categoryDAO.getAll();
+//		iview.setCollection(categoryCollection);
+//	} catch (SQLException sqle) {
+//		log.info("execute() : sqle" + sqle.toString());
+//		throw sqle;
+//	} catch (Exception e) {
+//		log.info("execute() : e" + e.toString());
+//		throw e;
+//	}
+//	return iview;
 
 }
