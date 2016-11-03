020fbf673084e28b3a01af70a437f6f31ad61992
chathuriM <madushani@genesiis.com>
2016-11-03 13:43:20 +0530

    c9-Inquiry Form for institute-Created new attribute to enum class-cm

--- campus.jar/src/com/genesiis/campus/validation/SystemMessage.java
+++ campus.jar/src/com/genesiis/campus/validation/SystemMessage.java
@@ -1,41 +1,22 @@
 package com.genesiis.campus.validation;
 
+//20161026 CM c9-make-inquiry-for-institute INIT SystemMessage.java
+//20161103 CM c9-make-inquiry-for-institute added RECAPTCHAVERIFICATION attribute.
 
 public enum SystemMessage {	
 	ADDED("Details added successfully."), 
 	UPDATED("Details updated successfully."), 
-	DELETED("Details deleted successfully."), 
-	IDFOUND("ID found."), 
-	IDNOTFOUND("ID not found."), 
+	DELETED("Details deleted successfully."),
 	ERROR("Unsuccessful."), 	
 	UNKNOWN(""),
 	EMPTYFIELD("Field is Empty."),
 	SUCCESS("Successfull"),
+	RECAPTCHAVERIFICATION("Please verify You're not a robot "),
 	INQUIRYSENT("Inquiry sent Successfully"),
 	INVALIDECODE("Invalid Country Code."),
 	INVALIDEAREA("Invalid Area Code."),
-	FAILED_TO_CREATE("Object not created"),
 	NICERROR("Invalid NIC Number."),
-	PHONENUMBERERROR("Invalid Telephone Number."),
-	MOBILENUMBERERROR("Invalid Mobile Number."),
-	EMAILERROR("Invalid Email."),
-	INVALIDDATE("Invalid DateFormat."),
-	EMPTYVALUES("Empty data Fields."),
-	INVALIDBIRTDAY("Invalid Birthdate."),
-	INVALIDENDDATE("Invalid End date."),
-	INVALIDAMOUNT("Invalid Amount."),
-	NOTADDED("Problem when inserting data."), 	
-	NOTUPDATED("Problem when updating data."), 	
-	MANDATORYFIELDREQUIRED("Fields marked with asterisk (*) are required"), 	
-	MEDICALHISTORYEMPLOYEEID("Medical History Employee id not valid."),
-	MEDICALHISTORYAILMENT("Medical History Ailment not valid."),
-	MEDICALHISTORYDESCRIPTION("Medical History Description not valid"),
-	MEDICALHISTORYPASS("pass"),
-	MEDICALHISTORYSUCCESS("success"),
-	MEDICALHISTORYNOID("No Medical History ID obtained."),		
-	MEDICALREPORTDESCRIPTIONNOTVALID("Medical Report Description not valid."),
-	MEDICALREPORTPATHNOTVALID("Medical Report path not valid."),
-	MEDICALREPORTNOTVALID("Medical Report not valid");
+	PHONENUMBERERROR("Invalid Telephone Number.");
 
 	
 	private String message;
