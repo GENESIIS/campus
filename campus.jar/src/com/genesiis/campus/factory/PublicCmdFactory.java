94b3d744e85e752c0f96cb00dd876d15af698dbb
Dumani Jayasinghe <dumani@genesiis.com>
2016-11-03 10:42:14 +0530

    c6-list-available-institutes-on-the-view amended by inserting LIST_ALL_COURSE_PROVIDERS entry to the map

--- campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
@@ -2,9 +2,10 @@ package com.genesiis.campus.factory;
 
 //20161026 DN c10-contacting-us-page amended by inserting CONTACT_US_PUBLC entry to the map
 //20161031 DJ c6-list-available-institutes-on-the-view amended by inserting LIST_TOP_COURSE_PROVIDERS entry to the map
+//20161103 DJ c6-list-available-institutes-on-the-view amended by inserting LIST_ALL_COURSE_PROVIDERS entry to the map
 
 import com.genesiis.campus.command.CmdGenerateEmail;
-import com.genesiis.campus.command.CmdListInstitutes;
+import com.genesiis.campus.command.CmdListMoreCourseProviders;
 import com.genesiis.campus.command.CmdListTopCourseProviders;
 import com.genesiis.campus.command.ICommand;
 import com.genesiis.campus.validation.Operation;
@@ -13,11 +14,11 @@ public class PublicCmdFactory implements ICmdFactory {
 
 	private ICommand command = null;
 	static {	
-		map.put(Operation.CONTACT_US_PUBLC, new CmdGenerateEmail());
-		//map.put(Operation.LIST_ALL_INSTITUTES, new CmdListInstitutes());
+		map.put(Operation.CONTACT_US_PUBLC, new CmdGenerateEmail());		
 		map.put(Operation.LIST_TOP_COURSE_PROVIDERS, new CmdListTopCourseProviders());
-		  
+		map.put(Operation.LIST_ALL_COURSE_PROVIDERS, new CmdListMoreCourseProviders());		  
 	}
+	
 	@Override
 	public ICommand getCommand(String cco) {
 		Operation o = Operation.BAD_OPERATION;
@@ -28,6 +29,9 @@ public class PublicCmdFactory implements ICmdFactory {
 			command = map.get(o);
 			break;
 		case LIST_TOP_COURSE_PROVIDERS:
+			command = map.get(o);
+			break;
+		case LIST_ALL_COURSE_PROVIDERS:
 			command = map.get(o);
 			break;
 		default:
