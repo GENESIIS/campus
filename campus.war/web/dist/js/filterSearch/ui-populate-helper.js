f97f8aeeb64bbc0572e3ae88ae4741b9f2d1ee64
pabodhaW <pabodha@genesiis.com>
2016-11-03 16:08:05 +0530

    c11: modified addsearchData() method to load data dynamically.

--- campus.war/web/dist/js/filterSearch/ui-populate-helper.js
+++ campus.war/web/dist/js/filterSearch/ui-populate-helper.js
@@ -1,7 +1,7 @@
 //20161030 PN this file contains all the functions to load the details for the filter search
 //20161101 PN c11-criteria-based-filter-search implemented displayInstitute(), displayMajor(), displayLevel(), displayDistricts() and pad() method.
 //20161102 PN c11-criteria-based-filter-search implemented getSelectedData(), getValueUsingParentTag() and addsearchData() method.
-
+//20161102 PN c11-criteria-based-filter-search modified addsearchData() method to load data dynamically.
 /**
  * This method id to load category details
  */
@@ -25,104 +25,87 @@ function displayCategory() {
  * This method id to load major details
  */
 function displayMajor() {
-	var val = document.getElementById("categorylist").value;
-	var opts = document.getElementById('categoryName').childNodes;
-	for (var i = 0; i < opts.length; i++) {
-		if (opts[i].value === val) {
-			var categoryCode = opts[i].text;
-			if(categoryCode){
-			$.get('PublicController', {
-				categoryCode : categoryCode,
-				CCO : 'LIST_MAJOR_DATA'
-			}, function(response) {
-				var count = 0 ;
-				var secondChoice = $("#select-item1");
-				secondChoice.find('li').remove();
-				$.each(response, function(index, value) {
-					var res = value.toString();
-					var data = res.split(",");
-					var x = data[0].toString();
-					var y = data[1].toString();
-					secondChoice.append('<li><a href="javascript:"><input name="major" type="checkbox" value="'+ x +'"></a>' + y + '</li>');
-					count++;
-				});
-				$("#majorCount").text(" " +pad(count, 2));
-			});	
-		}
-			break;
-		}
-	}
+	var categoryCode = getSelectedData('categorylist', 'categoryName');
+	
+	$.get('PublicController', {
+		categoryCode : categoryCode,
+		CCO : 'LIST_MAJOR_DATA'
+	}, function(response) {
+		var count = 0 ;
+		var secondChoice = $("#select-item1");
+		secondChoice.find('li').remove();
+		$.each(response, function(index, value) {
+			var res = value.toString();
+			var data = res.split(",");
+			var x = data[0].toString();
+			var y = data[1].toString();
+			secondChoice.append('<li><a href="javascript:"><input name="major" type="checkbox" value="'+ x +'"></a>' + y + '</li>');
+			count++;
+		});
+		$("#majorCount").text(" " +pad(count, 2));
+	});	
+	
 }
 
 /**
  * This method id to load CourseProvider details
  */
 function displayCourseProvider() {
-	var val = document.getElementById("categorylist").value;
-	var opts = document.getElementById('categoryName').childNodes;
-	for (var i = 0; i < opts.length; i++) {
-		if (opts[i].value === val) {
-			var categoryCode = opts[i].text;
-			if(categoryCode){
-			$.get('PublicController', {
-				categoryCode : categoryCode,
-				CCO : 'LIST_INSTITUTE_DATA'
-			}, function(response) {
-				var institueName = $("#institueName");
-				institueName.find('option').remove();
-				$.each(response, function(index, value) {
-					var res = value.toString();
-					var data = res.split(",");
-					var x = data[0].toString();
-					var y = data[1].toString();
-					var z = data[2].toString();
-					$('<option>').val(z+"-"+y).text(x).appendTo(institueName);
-				});
-			});
-			}
-			break;
-		}
-	}
+	var categoryCode = getSelectedData('categorylist', 'categoryName');
+	
+	$.get('PublicController', {
+		categoryCode : categoryCode,
+		CCO : 'LIST_INSTITUTE_DATA'
+	}, function(response) {
+		var institueName = $("#institueName");
+		institueName.find('option').remove();
+		$.each(response, function(index, value) {
+			var res = value.toString();
+			var data = res.split(",");
+			var x = data[0].toString();
+			var y = data[1].toString();
+			var z = data[2].toString();
+			$('<option>').val(z+"-"+y).text(x).appendTo(institueName);
+		});
+	});
+	
+	
 }
 
 /**
  * This method id to load level details
  */
 function displayLevel() {
-	var val = document.getElementById("categorylist").value;
-	var opts = document.getElementById('categoryName').childNodes;
-	for (var i = 0; i < opts.length; i++) {
-		if (opts[i].value === val) {
-			var categoryCode = opts[i].text;
-			if(categoryCode){
-			$.get('PublicController', {
-				categoryCode : categoryCode,
-				CCO : 'LIST_LEVEL_DATA'
-			}, function(response) {
-				var count = 0 ;
-				var secondChoice = $("#select-item2");
-				secondChoice.find('li').remove();
-				$.each(response, function(index, value) {
-					var res = value.toString();
-					var data = res.split(",");
-					var x = data[0].toString();
-					var y = data[1].toString();
-					secondChoice.append('<li><a href="javascript:"><input name="level" type="checkbox" value="'+ x +'"></a>' + y + '</li>');
-					count++;
-				});
-				$("#levelCount").text(" " +pad(count, 2));
-			});
-			}
-			break;
-		}
-	}
+	var categoryCode = getSelectedData('categorylist', 'categoryName');
+	
+	$.get('PublicController', {
+		categoryCode : categoryCode,
+		CCO : 'LIST_LEVEL_DATA'
+	}, function(response) {
+		var count = 0 ;
+		var secondChoice = $("#select-item2");
+		secondChoice.find('li').remove();
+		$.each(response, function(index, value) {
+			var res = value.toString();
+			var data = res.split(",");
+			var x = data[0].toString();
+			var y = data[1].toString();
+			secondChoice.append('<li><a href="javascript:"><input name="level" type="checkbox" value="'+ x +'"></a>' + y + '</li>');
+			count++;
+		});
+		$("#levelCount").text(" " +pad(count, 2));
+	});
+
 }
 
 /**
  * This method id to load district details
  */
 function displayDistricts() {
+	var instituteCode = getSelectedData('instituelist', 'institueName');
+	
 	$.get('PublicController', {
+		instituteCode : instituteCode,
 		CCO : 'LIST_DISTRICT_DATA'
 	}, function(response) {
 		var districtName = $("#districtName");
@@ -141,9 +124,11 @@ function displayDistricts() {
 
 function displayDetailsOnLoad() {
 	displayCategory();
-	displayMajor();
 	displayCourseProvider();
+	displayMajor();
+	
 	displayDistricts();	
+	addsearchData();
 }
 
 function displayDetailsOnChange() {
@@ -173,24 +158,46 @@ function addsearchData(){
 	var z = 'MAJOR=' + getValueUsingParentTag('#select-item1 input:checked') + '&';
 
 	var a = 'DISTRICT=' + getSelectedData('districtlist', 'districtName');
-	var searchData = x+y+z;
+	var searchData = x+y+z+a;
 
-	$.ajax({
-		type : "POST",
-		url : 'PublicController',
-		data : {
-			searchData : JSON.stringify(searchData),
-			CCO : "GET_SEARCH_DATA"
-		},
-		dataType : "json",
-		success : function(data) {
-			
-		},
-		error : function(e) {
-			alert("Error " + e);
-			console.log(e);
-		}
+	$.get('PublicController', {
+		searchData : JSON.stringify(searchData),
+		CCO : "GET_SEARCH_DATA"
+	}, function(response) {
+		var count = 0 ;
+		var dataTable = $("#example");
+		dataTable.find('tr:gt(0)').remove();
+		$.each(response, function(index, value) {
+			var res = value.toString();
+			var data = res.split(",");
+			dataTable.append('<tr>' +
+					'<th>' +
+					'<div class="provider-info">' +
+						'<a href="javascript:">' +
+						'<img src="dist/i/'+ data[6].toString().trim() +'" alt="'+ data[4].toString() +'" width="200" height="100">' +
+						'</a>' +
+					'</div>' +
+				'</th>' +
+				'<th>' +
+					'<div class="result-box clearfix">' +
+						'<div class="course-name">' +
+							'<a href="javascript:">' + data[1].toString() +
+							'<span class="provider-name">' + " @"+ data[5].toString() +
+							'</span>' +
+							'</a>' +
+						'</div>' +
+						'<div class="course-info">' +
+							'<p>'+ data[2].toString() + '</p>' +
+						'</div>' +
+					'</div>' +
+				'</th>' +
+				'<th>' + data[3].toString() + '</th>' +
+			'</tr>');
+			count++;
+		});
+		$("#courseCount").text(" " +pad(count, 2));
 	});
+	
 }
 
 /**
@@ -221,12 +228,16 @@ function getValueUsingParentTag(checkboxList){
  */
 function getSelectedData(listname, elementName) {
 	var selectedValue = "";
-	var val = document.getElementById(listname).value;
-	var opts = document.getElementById(elementName).childNodes;
-	for (var i = 0; i < opts.length; i++) {
-		if (opts[i].value === val) {
-			selectedValue = opts[i].text;
-			break;
+	var val = document.getElementById(listname);
+
+	if (val != null) {
+		var data = val.value;
+		var opts = document.getElementById(elementName).childNodes;
+		for (var i = 0; i < opts.length; i++) {
+			if (opts[i].value === data) {
+				selectedValue = opts[i].text;
+				break;
+			}
 		}
 	}
 	return selectedValue;
