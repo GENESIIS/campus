ad4c55edda65bc94f44004dd6d29d1f16bb7734a
chathuriM <madushani@genesiis.com>
2016-11-03 13:43:47 +0530

    c9-Inquiry Form for institute-INIT and Modified CaptchaResponse class-cm

---
+++ campus.jar/src/com/genesiis/campus/entity/model/CaptchaResponse.java
@@ -0,0 +1,15 @@
+package com.genesiis.campus.entity.model;
+
+//20161026 CM c9-make-inquiry-for-institute INIT CmdSendInstituteInquiry.java
+
+public class CaptchaResponse {
+    public boolean success;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+}
