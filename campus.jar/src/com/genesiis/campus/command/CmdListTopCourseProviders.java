9a232feb815570135102fb1833b37466ee53cc0a
Dumani Jayasinghe <dumani@genesiis.com>
2016-11-03 07:17:55 +0530

    c6-list-available-institutes-on-the-view Implemented execute()

--- campus.jar/src/com/genesiis/campus/command/CmdListTopCourseProviders.java
+++ campus.jar/src/com/genesiis/campus/command/CmdListTopCourseProviders.java
@@ -1,6 +1,7 @@
 package com.genesiis.campus.command;
 
 //DJ 20161026 c6-list-available-institutes-on-the-view created CmdListTopCourseProviders.java
+//DJ 20161103 c6-list-available-institutes-on-the-view Implemented execute()
 
 import java.sql.SQLException;
 import java.util.Collection;
@@ -11,26 +12,31 @@ import com.genesiis.campus.entity.model.CourseProvider;
 import com.genesiis.campus.util.IDataHelper;
 import com.genesiis.campus.validation.SystemMessage;
 
+import org.apache.log4j.Logger;
+
 public class CmdListTopCourseProviders implements ICommand   {
+	
+	static Logger log = Logger.getLogger(CmdListTopCourseProviders.class.getName());
 
 	@Override
 	public IView execute(IDataHelper helper, IView iview) throws SQLException,
 			Exception {
 		final CourseProviderDAO providerDAO = new CourseProviderDAO();
 		SystemMessage systemMessage = SystemMessage.UNKNOWN;
-		int categoryCode = 0;
-		
+		int categoryCode = 0;		
 		try {
 			final CourseProvider provider = new CourseProvider();
 			categoryCode=1;
 			final Collection<Collection<String>> institutes = providerDAO.findById(provider);
 			
 			final Collection<Collection<String>> topViewedCourseProviders = providerDAO.findTopViewedProviders(provider);
-			iview.setCollection(institutes);
-		} catch (Exception e) {
-			// TODO: handle exception
+			iview.setCollection(topViewedCourseProviders);
+		} catch (Exception exception) {
+			log.error("execute() : " + exception);
+			systemMessage = SystemMessage.ERROR;
+			throw exception;
 		}
-		return null;
+		return iview;
 	}
 
 }
