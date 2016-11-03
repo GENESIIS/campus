90ade33a9a71d92076b5cf74e7682c4d90372bc8
jayani1991 <jayani@genesiis.com>
2016-11-03 22:58:12 +0530

    c7-higher-education-landing-page PublicCmdFactory.java hash map modified : changed command class name -JH

--- campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
@@ -2,8 +2,9 @@ package com.genesiis.campus.factory;
 
 //20161026 DN c10-contacting-us-page amended by inserting CONTACT_US_PUBLC entry to the map
 //20161026 JH c7-higher-education-landing-page amended LIST_HIGHER_EDUCATION_PROGRAMMES to the map
+//20161103 JH c7-higher-education-landing-page modified command class name for 'LHEP' CCO
 
-import com.genesiis.campus.command.CmdListCategoryProgramme;
+import com.genesiis.campus.command.CmdListCategoryLandingPage;
 import com.genesiis.campus.command.ICommand;
 import com.genesiis.campus.validation.Operation;
 
@@ -11,7 +12,7 @@ public class PublicCmdFactory implements ICmdFactory {
 
 	private ICommand command = null;
 	static {	
-		map.put(Operation.LIST_HIGHER_EDUCATION_PROGRAMMES, new CmdListCategoryProgramme() );	
+		map.put(Operation.LIST_HIGHER_EDUCATION_PROGRAMMES, new CmdListCategoryLandingPage() );	
 	}
 	@Override
 	public ICommand getCommand(String cco) {
