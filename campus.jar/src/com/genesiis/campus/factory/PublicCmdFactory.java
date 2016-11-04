8ce6f806a627a284c0fd21d7247d8eafe51f6e34
jayani1991 <jayani@genesiis.com>
2016-11-04 13:21:26 +0530

    c7-higher-education-landing-page removed switch case from public command factory -JH

--- campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
@@ -4,9 +4,9 @@ package com.genesiis.campus.factory;
 //20161026 JH c7-higher-education-landing-page amended LIST_HIGHER_EDUCATION_PROGRAMMES to the map
 //20161103 JH c7-higher-education-landing-page modified command class name for 'LHEP' CCO
 //20161104 JH c7-higher-education-landing-page add  LIST_CATEGORY_LANDING_PAGE to the map
+//20161104 JH c7-higher-education-landing-page removed  LIST_HIGHER_EDUCATION_PROGRAMMES from the map
 
 import com.genesiis.campus.command.CmdListCategoryLandingPage;
-import com.genesiis.campus.command.CmdListCategoryProgramme;
 import com.genesiis.campus.command.ICommand;
 import com.genesiis.campus.validation.Operation;
 
@@ -14,7 +14,6 @@ public class PublicCmdFactory implements ICmdFactory {
 
 	private ICommand command = null;
 	static {	
-		map.put(Operation.LIST_HIGHER_EDUCATION_PROGRAMMES, new CmdListCategoryProgramme() );	
 		map.put(Operation.LIST_CATEGORY_LANDING_PAGE, new CmdListCategoryLandingPage());
 	}
 	@Override
@@ -22,10 +21,7 @@ public class PublicCmdFactory implements ICmdFactory {
 		Operation o = Operation.BAD_OPERATION;
 		o = Operation.getOperation(cco);
 		switch (o) {
-		
-		case LIST_HIGHER_EDUCATION_PROGRAMMES:
-			command = map.get(o);
-			break;
+
 		case LIST_CATEGORY_LANDING_PAGE:
 			command = map.get(o);
 			break;
