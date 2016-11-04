94ba70ab3d3b29e6dc9747dd5f19cdc58f890c05
jayani1991 <jayani@genesiis.com>
2016-11-04 10:02:53 +0530

    c7-higher-education-landing-page add new enum class to the PublicCmdFactory class -JH

--- campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
@@ -3,8 +3,10 @@ package com.genesiis.campus.factory;
 //20161026 DN c10-contacting-us-page amended by inserting CONTACT_US_PUBLC entry to the map
 //20161026 JH c7-higher-education-landing-page amended LIST_HIGHER_EDUCATION_PROGRAMMES to the map
 //20161103 JH c7-higher-education-landing-page modified command class name for 'LHEP' CCO
+//20161104 JH c7-higher-education-landing-page add  LIST_CATEGORY_LANDING_PAGE to the map
 
 import com.genesiis.campus.command.CmdListCategoryLandingPage;
+import com.genesiis.campus.command.CmdListCategoryProgramme;
 import com.genesiis.campus.command.ICommand;
 import com.genesiis.campus.validation.Operation;
 
@@ -12,7 +14,8 @@ public class PublicCmdFactory implements ICmdFactory {
 
 	private ICommand command = null;
 	static {	
-		map.put(Operation.LIST_HIGHER_EDUCATION_PROGRAMMES, new CmdListCategoryLandingPage() );	
+		map.put(Operation.LIST_HIGHER_EDUCATION_PROGRAMMES, new CmdListCategoryProgramme() );	
+		map.put(Operation.LIST_CATEGORY_LANDING_PAGE, new CmdListCategoryLandingPage());
 	}
 	@Override
 	public ICommand getCommand(String cco) {
@@ -21,6 +24,9 @@ public class PublicCmdFactory implements ICmdFactory {
 		switch (o) {
 		
 		case LIST_HIGHER_EDUCATION_PROGRAMMES:
+			command = map.get(o);
+			break;
+		case LIST_CATEGORY_LANDING_PAGE:
 			command = map.get(o);
 			break;
 		default:
