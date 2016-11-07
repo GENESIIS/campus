0059dd6105ffe71f3f36e0ab0950dc1d84c5dd0f
tharakaGen <tharaka@genesiis.com>
2016-11-07 09:49:55 +0530

    20161107 TR c1 added - header.html

---
+++ campus.war/web/js/main.js
@@ -0,0 +1,23 @@
+/**
+ * Created by tharaka on 10/24/2016.
+ */
+
+// angular.module('CampusApp',['ngRoute'])
+//
+//     .config(function ($routeProvider) {
+//         $routeProvider
+//             .when('/',{
+//                 templateUrl : "partials/landing.html"
+//             })
+//             .when('/about',{
+//                 templateUrl : "partials/about.html"
+//             })
+//             .when('/contact',{
+//                 templateUrl : "partials/contact.html"
+//             })
+//             .when('/courses',{
+//                 templateUrl : "partials/courses.html"
+//             });
+//     });
+
+w3IncludeHTML(); 
