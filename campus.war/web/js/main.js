8ff8be9aa632e5df6505e9d2537d32ea5d3e5a16
tharakaGen <tharaka@genesiis.com>
2016-11-07 15:56:13 +0530

    20161107 TR -mt-c11 : set slide toggle

--- campus.war/web/js/main.js
+++ campus.war/web/js/main.js
@@ -2,4 +2,13 @@
  * Created by tharaka on 10/24/2016.
  */
 
-w3IncludeHTML(); 
+w3IncludeHTML();
+
+// SlideToogle Controller
+
+$(document).ready(function () {
+    $(".flip").click(function () {
+        $(this).next('.slideable').slideToggle("slow");
+    });
+});
+ 
