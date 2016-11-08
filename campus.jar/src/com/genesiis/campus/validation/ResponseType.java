37c61ec1900620dda3211c3ac0e1d70e6cceb99e
jayani1991 <jayani@genesiis.com>
2016-11-08 15:52:42 +0530

    c7-higher-education-landing-page-MP add header comments for ResponseType enum -JH

--- campus.jar/src/com/genesiis/campus/validation/ResponseType.java
+++ campus.jar/src/com/genesiis/campus/validation/ResponseType.java
@@ -1,6 +1,6 @@
-// 20161108 DN, JH, DJ, AS, CM, MM, PN Initialised enum
-
 package com.genesiis.campus.validation;
+
+//20161108 DN, JH, DJ, AS, CM, MM, PN Initialised enum
 
 /**
  * ResponseType is used to identify in what format the response is to be sent
