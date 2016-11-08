3f77639fa3b4c1ef0b9eb9924c7c0cc97a001638
pabodhaW <pabodha@genesiis.com>
2016-11-08 14:35:55 +0530

    c11: merged with c11-ui-criteria-based-filter-search-MP-tr branch.

---
+++ campus.war/web/dist/js/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by tharaka on 10/24/2016.
+ */
+
+w3IncludeHTML();
+
+// SlideToggle Controller
+
+$(document).ready(function () {
+    $(".flip").click(function () {
+        $(this).next('.slideable').slideToggle("slow");
+    });
+});
+ 
