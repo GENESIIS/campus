67a247a5dbfbb551bef04d5a47597d03d755dcc1
jayani1991 <jayani@genesiis.com>
2016-11-08 15:50:22 +0530

    c7-higher-education-landing-page-MP removed unwanted codes and imports from PublicController -JH

--- campus.war/src/com/genesiis/campus/publicController/PublicController.java
+++ campus.war/src/com/genesiis/campus/publicController/PublicController.java
@@ -1,27 +1,17 @@
 package com.genesiis.campus.publicController;
 
-//20161101 PN c11-criteria-based-filter-search modified the doPost method with a null validation for IView type object.
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+//20161024 DN c10-contacting-us-page created the initial version of the Public Controller
 
 import com.genesiis.campus.controller.CampusController;
-import com.genesiis.campus.entity.IView;
-import com.genesiis.campus.entity.model.Category;
-import com.genesiis.campus.util.DataHelper;
-import com.genesiis.campus.util.IDataHelper;
 
 import org.apache.log4j.Logger;
 
-import com.google.gson.Gson;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class PublicController
@@ -38,57 +28,21 @@ public class PublicController extends CampusController {
 		super();
 	}
 
-	public void init() throws ServletException {
-
-	}
-
 	/**
-	 * @see XenoController#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		// super.doGet(request, response);
-		this.doPost(request, response);
-	}
-
-	/**
-	 * @see XenoController#doPost(HttpServletRequest request,
+	 * @see CampusController#doGet(HttpServletRequest request,
 	 *      HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		 super.doPost(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		super.doGet(request, response);
+	}
 
-//		IDataHelper helper = null;
-//		IView result = null;
-//		String cco = "";
-//		String json = null;
-//
-//		try {
-//			helper = new DataHelper(request);
-//			cco = helper.getCommandCode();
-//			result = helper.getResultView(cco);
-//
-//			List<Object> list = new ArrayList<Object>();
-//			if (result != null) {
-//				for (Collection<String> view : result.getCollection()) {
-//					Object[] category = view.toArray();
-//					list.add(category);
-//				}
-//			}
-//			json = new Gson().toJson(list);
-//
-//		} catch (Exception e) {
-//			log.error("process(): ", e);
-//		}
-//
-//		// request.setAttribute("result", result);
-//		// request.getRequestDispatcher(helper.getResultPage(cco)).forward(
-//		// request, response);
-//
-//		response.setContentType("application/json");
-//		response.getWriter().write(json);
-
+	/**
+	 * @see CampusController#doPost(HttpServletRequest request,
+	 *      HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		super.doPost(request, response);
 	}
 }
