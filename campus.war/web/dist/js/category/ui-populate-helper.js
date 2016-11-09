2cc20706e9c242495c33f4ddd43f09bfb2ef734a
jayani1991 <jayani@genesiis.com>
2016-11-09 17:32:23 +0530

    c7-higher-education-landing-page-MP javascript file for category landing page wip -JH

---
+++ campus.war/web/dist/js/category/ui-populate-helper.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+$(document).ready(function() {
+ 
+  $("#featured-panel").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 1,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+});
