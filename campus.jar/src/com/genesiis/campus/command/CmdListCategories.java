af86e91adeecb6faf42b61c34d0703b1a85159ec
pabodhaW <pabodha@genesiis.com>
2016-11-09 16:58:38 +0530

    c11-criteria-based-filter-search modified execute() to load more than one collection.

--- campus.jar/src/com/genesiis/campus/command/CmdListCategories.java
+++ campus.jar/src/com/genesiis/campus/command/CmdListCategories.java
@@ -1,6 +1,7 @@
 package com.genesiis.campus.command;
 
 //20161028 PN c11-criteria-based-filter-search INIT the class and implemented execute() method.
+//20161109 PN c11-criteria-based-filter-search modified execute() to load more than one collection.
 
 import java.sql.SQLException;
 import java.util.Collection;
@@ -8,8 +9,11 @@ import java.util.Collection;
 import org.apache.log4j.Logger;
 
 import com.genesiis.campus.entity.CategoryDAO;
+import com.genesiis.campus.entity.DistrictDAO;
 import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.InstituteDAO;
+import com.genesiis.campus.entity.MajorDAO;
 import com.genesiis.campus.util.IDataHelper;
 
 public class CmdListCategories implements ICommand{
@@ -38,9 +42,23 @@ public class CmdListCategories implements ICommand{
 	
 		
 		ICrud categoryDAO = new CategoryDAO();
+		ICrud instituteDAO = new InstituteDAO();
+		ICrud districtDAO = new DistrictDAO();
+		ICrud majorDao = new MajorDAO();
+		
 		try {
 			Collection<Collection<String>> categoryCollection = categoryDAO.getAll();
 			iview.setCollection(categoryCollection);
+			
+			Collection<Collection<String>> instituteCollection = instituteDAO.getAll();
+			helper.setAttribute("instituteCollection", instituteCollection);
+			
+			Collection<Collection<String>> districtCollection = districtDAO.getAll();
+			helper.setAttribute("districtCollection", districtCollection);
+			
+			Collection<Collection<String>> majorCollection = majorDao.getAll();
+			helper.setAttribute("majorCollection", majorCollection);
+			
 		} catch (SQLException sqle) {
 			log.info("execute() : sqle" + sqle.toString());
 			throw sqle;
