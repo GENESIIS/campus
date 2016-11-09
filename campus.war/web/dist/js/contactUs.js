56f7778bb3acf5fd4bb863df842c367ae788de71
dushanthacb <dushantha@genesiis.com>
2016-11-09 17:21:03 +0530

    20161109 DN c10-contacting-us-page-MP validateForm() refactor the method

--- campus.war/web/dist/js/contactUs.js
+++ campus.war/web/dist/js/contactUs.js
@@ -1,15 +1,27 @@
 // 20161025 DN c10-contacting-us-page created contactUs.jsp 
 //20161027 DN c10-contacting-us-page amended isValidEmailFormat(),isValidPhoneNumber() method
+//20161109 DN c10-contacting-us-page-MP validateForm() refactor the method
 
 
 function validateForm(){
-	isFieldFilled(isempty(document.contactUsForm.firstName.value),"First Name Field");
-	isFieldFilled(isempty(document.contactUsForm.lastName.value),"Last Name Field");
-	isFieldFilled(isValidEmailFormat(),"Email Field");
-	isFieldFilled(isValidPhoneNumber(document.contactUsForm.contactNumber.value),"Phone Number Field");	
-	isFieldFilled(isempty(document.contactUsForm.subject.value),"Subject Field");
-	isFieldFilled(isempty(document.contactUsForm.message.value),"message Field");
-	//isFieldFilled(isHumanTestPassed(),"Correctly 10-4");
+
+	 if(!(isFieldFilled(isempty(document.contactUsForm.firstName.value),"First Name Field","firstNameError"))){
+		return false;
+	}else if (!(isFieldFilled(isempty(document.contactUsForm.lastName.value),"Last Name Field","lastNameError"))) {
+		return false
+	} else if(!(isFieldFilled(isValidPhoneNumber(document.contactUsForm.contactNumber.value),"Phone Number Field","phoneNumberError"))){
+		return false;
+	} else if(!isFieldFilled(isValidEmailFormat(),"Email Field","emailError")) {
+		return false;
+	} else if (!isFieldFilled(isempty(document.contactUsForm.subject.value),"Subject Field","subjectError")){
+		return false;
+	}else if (!isFieldFilled(isempty(document.contactUsForm.message.value),"message Field","userMessageError")) {
+		return false;
+	}else if (!isFieldFilled(isHumanTestPassed(),"I'm Not A Robot","captureError")) {
+		
+	} else {
+		
+	}
 }
 
 /**
@@ -21,7 +33,9 @@ function validateForm(){
  */
 
 function isHumanTestPassed(){	
-	return (document.contactUsForm.humanTest.value == 6)?true : false;
+	var response = grecaptcha.getResponse();
+	return(response.length!=0)?true:false;
+	
 	
 }
 /**
@@ -31,11 +45,11 @@ function isHumanTestPassed(){
  * @param elementName  string to be append to the produced message
  */
 
-function isFieldFilled(flag, elementName){		
-	if(!flag){
-		alert(elementName+ "must be filled out Correctly");		
+function isFieldFilled(flag, elementName, errorLabelId){		
+	if(!flag){	
+		document.getElementById(errorLabelId).innerHTML = elementName + " Must Be filled Out Correctly!";		
 	}
-	
+	return flag;
 }
 
 /**
