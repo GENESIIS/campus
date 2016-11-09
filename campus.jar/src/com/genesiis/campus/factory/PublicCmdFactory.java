77d5be526bfef3cf2854d988dd387ff09240fbc7
jayani1991 <jayani@genesiis.com>
2016-11-09 12:02:56 +0530

    c7-higher-education-landing-page-MP added CmdListCategories.java command class -JH

--- campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
@@ -1,7 +1,9 @@
 package com.genesiis.campus.factory;
 
 //20161108 JH c7-higher-education-landing-page-mp added PublicCmdFactory.java
+//20161109 JH c7-higher-education-landing-page-mp added new attribute for LIST_CATEGORY_DATA
 
+import com.genesiis.campus.command.CmdListCategories;
 import com.genesiis.campus.command.CmdListCategoryLandingPage;
 import com.genesiis.campus.command.ICommand;
 import com.genesiis.campus.validation.Operation;
@@ -13,6 +15,7 @@ public class PublicCmdFactory implements ICmdFactory {
 	private ICommand command = null;
 	static {	
 		map.put(Operation.LIST_CATEGORY_LANDING_PAGE, new CmdListCategoryLandingPage());
+		map.put(Operation.LIST_CATEGORY_DATA, new CmdListCategories());
 
 	}
 
