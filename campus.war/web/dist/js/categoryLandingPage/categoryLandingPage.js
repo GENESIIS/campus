df9a7da66cf0bc2b70473f6d92fae346f240ee08
Miyuru-Genesiis <miyuru@genesiis.com>
2016-11-10 17:46:39 +0530

    c5-corporate-training-landing-page-MP Implemented pagination controls for the programme list

--- campus.war/web/dist/js/categoryLandingPage/categoryLandingPage.js
+++ campus.war/web/dist/js/categoryLandingPage/categoryLandingPage.js
@@ -3,22 +3,25 @@
  * 20161109 MM c5-corporate-training-landing-page-MP Added code in (document).ready() function
  * 20161110 MM c5-corporate-training-landing-page-MP Added code to display programme data retrieved
  * 20161110 MM c5-corporate-training-landing-page-MP Modified code to properly display the town list for each programme 
+ * 20161110 MM c5-corporate-training-landing-page-MP Implemented pagination controls for the programme list
  */
 
 //alert("entered script");
-
 $(document).ready(function() {
-	getProgrammeData();
+	getProgrammeData(1);
 });
 
-function getProgrammeData() {
+function getProgrammeData(pageNum) {
+	var categoryId = $('#categoryId').val(); // This element is expected to be present in the JSP
+	// IMPORTANT: these values must be validated to be of numeric type before further steps are executed 
+	categoryId = 3; // Hard-coded for the moment
 	$.ajax({
 		url : '/PublicController',
 		method : 'POST',
 		data : {
 			CCO : 'LIST_CATEGORY_PROGRAMMES',
-			category : '3',
-			pageNum : '1'
+			category : categoryId,
+			pageNum : pageNum
 		},
 		dataType : "json",
 		async : false,
@@ -42,7 +45,6 @@ function getProgrammeData() {
 				var levelOrMajorListElement = $('.filtering-area').find('ul.list-inline');
 				levelOrMajorListElement.html(levelOrMajorHtmlFragment);		
 				
-
 				//Construct the Programme listing 
 				var programmesHtmlFragment = '';
 				var programmeCollection = response.result;
@@ -51,12 +53,12 @@ function getProgrammeData() {
 					$.each(programmeCollection, function(index, val) {						
 						programmesHtmlFragment += '<li class="course-info clearfix">';
 						programmesHtmlFragment += '<div class="col-name">';
-						programmesHtmlFragment += '<h1 class="pro-name">' + val[18] + '</h1>';
+						programmesHtmlFragment += '<a href=""><h1 class="pro-name">' + val[18] + '</h1></a>';
 						programmesHtmlFragment += '<div class="pro-logo">';
 						
 						var logoImgPath = response.contextDeployLogoPath + val[22] + "/" + val[16];
 						
-						programmesHtmlFragment += '<img src=' + logoImgPath + ' alt="' + val[18] + ' logo">';
+						programmesHtmlFragment += '<a href=""><img src=' + logoImgPath + ' alt="' + val[18] + ' logo"></a>';
 						programmesHtmlFragment += '</div>';
 						programmesHtmlFragment += '</div>';
 						programmesHtmlFragment += '<div class="col-description">';
@@ -75,13 +77,62 @@ function getProgrammeData() {
 						programmesHtmlFragment += '<div class="col-duration">';
 						programmesHtmlFragment += '<label>' + val[23] + '<br><span>' + val[24] + '</span></label>';
 						programmesHtmlFragment += '</div>';
-						programmesHtmlFragment += '</li>';						
+						programmesHtmlFragment += '</li>';
 					});					
 				}
 				
 				var programmeListElement = $('.filter-result-table').find('ul.result-row');
 				programmeListElement.html(programmesHtmlFragment);	
+				
+				// Construct the paginator
+				var numOfPages = parseInt(response.numOfPages);
+//				numOfPages = 2;
+				var pageNumReceived = parseInt(response.pageNum);
+				
+				if ((numOfPages !== undefined && numOfPages !== null) && 
+						(pageNumReceived !== undefined && pageNumReceived !== null)) {
+					
+					var paginationHtml = '';
+					paginationHtml += '<li class="disabled paginator-button">\
+					      <a href="#">\
+					      <span>\
+					        <span aria-hidden="true">&laquo;</span>\
+					      </span>\
+					      </a>\
+					    </li>';
+					
+					for (var i = 1; i <= numOfPages; i++) {
+						paginationHtml += '<li class="paginator-button';
+						
+						if (i === pageNumReceived) {
+							paginationHtml += ' active';
+						}
+						
+						paginationHtml += '" data-page-number="'+ i +'">\
+						      <a href="#">\
+						      <span>' + i + '</span>\
+						      </a>\
+						    </li>';
+					}
+		
+					paginationHtml +='<li>\
+					      <a href="#">\
+					      <span>\
+					        <span aria-hidden="true">&raquo;</span>\
+					      </span>\
+					      </a>\
+					    </li>';
+					
+					var paginatorListElement = $('div.paginator-div > nav > ul.pagination');
+					paginatorListElement.html(paginationHtml);					
+				}
+				
+				paginatorListElement.find('li.paginator-button').on('click', function() {
+					var pageNumClicked = $(this).attr('data-page-number');
+					getProgrammeData(pageNumClicked);
+				});
 			}
+			
 		},
 		error : function(response) {
 			alert("Ajax Error");
