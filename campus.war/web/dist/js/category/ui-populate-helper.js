add60edda8b52b5102050776f94515bdcf104dba
jayani1991 <jayani@genesiis.com>
2016-11-10 14:56:19 +0530

    c7-higher-education-landing-page-MP removed unwanted javascript code -JH

--- campus.war/web/dist/js/category/ui-populate-helper.js
+++ campus.war/web/dist/js/category/ui-populate-helper.js
@@ -1,16 +1,4 @@
 /**
+ * javascript functions used to help category landing page are included here.
  * 
  */
-$(document).ready(function() {
- 
-  $("#featured-panel").owlCarousel({
- 
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
- 
-      items : 1,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3]
- 
-  });
- 
-});
