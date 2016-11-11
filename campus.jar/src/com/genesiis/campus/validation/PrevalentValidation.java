8147212ef9c0f083146d50add0e8e0fa5c577f83
dushanthacb <dushantha@genesiis.com>
2016-11-11 16:22:49 +0530

    c10-contacting-us-page-MP-dn execute() ContactUsValidation.java is created to support concrete implementation of validation

--- campus.jar/src/com/genesiis/campus/validation/PrevalentValidation.java
+++ campus.jar/src/com/genesiis/campus/validation/PrevalentValidation.java
@@ -1,32 +1,39 @@
 package com.genesiis.campus.validation;
 
+//20161111 DN c10-contacting-us-page-MP ContactUsValidation.java is created
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.genesiis.campus.command.CmdGenerateEmail;
+
 import org.apache.log4j.Logger;
 
-public class ContactUsValidation implements Validatory {
-	static Logger log = Logger.getLogger(ContactUsValidation.class.getName());
+/**
+ * PrevalentValidation intended to provide general validation and it has provided
+ * the implementation for the Validatory interface.
+ * @author dushantha
+ *
+ */
+public class PrevalentValidation implements Validatory {
+	static Logger log = Logger.getLogger(PrevalentValidation.class.getName());
 	@Override
 	public boolean isNotEmpty(String text) throws Exception{
 		boolean status = false;
 		if ((text != null) & (text.isEmpty() == false)) {
 			status = true;
 		}
-		throwCustomError(status," Text Field Validation(s) faied!");
+		throwCustomError(status," Text Field Validation(s) Failed!");
 		return status;
 	}
 	
 	@Override
-	public boolean validateEmail(String email) throws Exception {
+	 public boolean validateEmail(String email) throws Exception {
 		boolean validEmail = false;
 		Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile(
-				"([\\w-\\.]+)@((?:[\\w]+\\.)+)([a-zA-Z]{2,4})",
-				Pattern.CASE_INSENSITIVE);
+				"^([\\w-\\.]+)@((?:[\\w]+\\.)+)([a-zA-Z]{2,4})$");
 		Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
 		validEmail = matcher.find();
-		throwCustomError(validEmail," Email Validation faied!");
+		throwCustomError(validEmail," Email Validation Failed!");
 		return validEmail;
 	}
 
@@ -70,20 +77,34 @@ public class ContactUsValidation implements Validatory {
 			Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^0\\d{9}$");
 			Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(number);
 			validPhoneNumber= matcher.find();
-			throwCustomError(validPhoneNumber," Phone number Validation faied!");
+			throwCustomError(validPhoneNumber," Phone number Validation Failed!");
 			return validPhoneNumber;
 		
 	}
 	
+	/*
+	 * throwCustomError class accepts two parameters and based on the test falsity Exception
+	 * will be thrown. 
+	 * @author DN
+	 * @param test boolean value preferably an out put value of a test.
+	 * @param errorMessage String message which should be displayed when toString() is called
+	 * on the Exception been thrown.
+	 * @throws Exception true Exception been thrown is FailedValidationException 
+	 * which is in the context of field validation
+	 */
 	private void throwCustomError(boolean test,String errorMessage) throws Exception {
 		if(!test){
-			Exception vslidationException = new FailedValidationException(errorMessage);
-			log.error("throwCustomError(): FailedValidationException: "+vslidationException.toString() );
-			throw vslidationException;
+			Exception validationException = new FailedValidationException(errorMessage);
+			log.error("throwCustomError(): FailedValidationException: "+validationException.toString() );
+			throw validationException;
 		}
 	}
 	
-	
+	/**
+	 * FailedValidationException an Exception type which
+	 * extends Exception and intend for field validation errors
+	 * @author DN
+	 */
 	public class FailedValidationException extends Exception
 	{
 	  /**
