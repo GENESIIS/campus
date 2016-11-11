ff0bfe3e46e69d1bff91c09a7aa921118afcf313
Dumani Jayasinghe <dumani@genesiis.com>
2016-11-11 13:33:00 +0530

    c17-provider-criteria-based-filter-search-MP-dj created CategoryDAO.java

---
+++ campus.jar/src/com/genesiis/campus/entity/CategoryDAO.java
@@ -0,0 +1,115 @@
+package com.genesiis.campus.entity;
+
+//DJ 20161111 c17-provider-criteria-based-filter-search-MP-dj created CategoryDAO.java
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.log4j.Logger;
+
+import com.genesiis.campus.util.ConnectionManager;
+
+public class CategoryDAO  implements ICrud{
+	
+	static org.apache.log4j.Logger log = Logger.getLogger(CategoryDAO.class.getName());
+
+	@Override
+	public int add(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<Collection<String>> findById(Object code)
+			throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	/**
+	 * Get all category details
+	 * @param 
+	 * @author DJ
+	 * @return Collection 
+	 */
+	@Override
+	public Collection<Collection<String>> getAll() throws SQLException,
+			Exception {
+		Connection conn=null;
+		PreparedStatement stmt=null;
+		final Collection<Collection<String>> allCategoryList=new ArrayList<Collection<String>>();
+		try {
+			conn=ConnectionManager.getConnection();
+			String sql="SELECT CAT.CODE AS CATEGORYCODE , CAT.NAME AS CATEGORYNAME FROM [CAMPUS].CATEGORY CAT WHERE CAT.ISACTIVE=1 ";
+			
+			stmt=conn.prepareStatement(sql.toString());
+			final ResultSet rs=stmt.executeQuery();
+			
+			while (rs.next()) {				
+				final ArrayList<String> singleCategory = new ArrayList<String>();
+				singleCategory.add(rs.getString("CATEGORYCODE"));				
+				singleCategory.add(rs.getString("CATEGORYNAME"));				
+				allCategoryList.add(singleCategory);
+			}
+		} catch (SQLException sqlException) {
+			log.info("getAll() sqlException" + sqlException.toString());
+			throw sqlException;
+		} catch (Exception e) {
+			log.info("getAll() Exception" + e.toString());
+			throw e;
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		}
+		
+		return allCategoryList;
+	}
+
+	@Override
+	public int add(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<Collection<String>> findById(Object object,
+			Connection conn) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
