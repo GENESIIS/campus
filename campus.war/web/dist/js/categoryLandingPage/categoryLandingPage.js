a8bb2c272424d94629c9c0f99d9a3e8d5f883f8c
Miyuru-Genesiis <miyuru@genesiis.com>
2016-11-11 11:21:21 +0530

    c5-corporate-training-landing-page-MP - Refactored code in getProgrammeData() into several methods to aid re-using of code in function executed when a paginator button or major/level link is clicked

--- campus.war/web/dist/js/categoryLandingPage/categoryLandingPage.js
+++ campus.war/web/dist/js/categoryLandingPage/categoryLandingPage.js
@@ -7,8 +7,19 @@
  */
 
 //alert("entered script");
+window.programmeCollectionFetched = null;
+window.levelOrMajorCollectionFetched = null;
+window.programmeCodeToTownListMapFetched = null;
+window.numOfPagesFetched = null;
+window.pageNumFetched = null;
+window.contextDeployCourseLogoPathFetched = null;
+window.contextDeployLogoPathFetched = null;
+
 $(document).ready(function() {
 	getProgrammeData(1);
+	constructLevelOrMajorMenu();
+	constructProgrammeListing();
+	constructPaginator();
 });
 
 function getProgrammeData(pageNum) {
@@ -29,113 +40,141 @@ function getProgrammeData(pageNum) {
 			alert("Ajax Success");
 			
 			if (response !== undefined && response !== null) {
-				
-				//Construct the Level/Major menu on top of programme displaying area
-				var levelOrMajorHtmlFragment = '';
-				var levelOrMajorCollection = response.levelOrMajorCollection;
-				var programmeCodeToTownListMap = response.programmeCodeToTownListMap;
-				
-				if (levelOrMajorCollection !== undefined && levelOrMajorCollection !== null) {
-					levelOrMajorHtmlFragment += '<li><a href="javascript:">All</a></li>';
-					$.each(levelOrMajorCollection, function(index, val) {
-						levelOrMajorHtmlFragment += '<li><a href="javascript:">'+ val[1] +'</a></li>';
-					});					
-				}
-				
-				var levelOrMajorListElement = $('.filtering-area').find('ul.list-inline');
-				levelOrMajorListElement.html(levelOrMajorHtmlFragment);		
-				
-				//Construct the Programme listing 
-				var programmesHtmlFragment = '';
-				var programmeCollection = response.result;
-				
-				if (programmeCollection !== undefined && programmeCollection !== null) {
-					$.each(programmeCollection, function(index, val) {						
-						programmesHtmlFragment += '<li class="course-info clearfix">';
-						programmesHtmlFragment += '<div class="col-name">';
-						programmesHtmlFragment += '<a href=""><h1 class="pro-name">' + val[18] + '</h1></a>';
-						programmesHtmlFragment += '<div class="pro-logo">';
-						
-						var logoImgPath = response.contextDeployLogoPath + val[22] + "/" + val[16];
-						
-						programmesHtmlFragment += '<a href=""><img src=' + logoImgPath + ' alt="' + val[18] + ' logo"></a>';
-						programmesHtmlFragment += '</div>';
-						programmesHtmlFragment += '</div>';
-						programmesHtmlFragment += '<div class="col-description">';
-						programmesHtmlFragment += '<p>' + val[1] + '</p>';
-						programmesHtmlFragment += '</div>';
-						programmesHtmlFragment += '<div class="col-location">';
-						
-						var townListForCurrentProgramme = programmeCodeToTownListMap[val[0]];
-						
-						$.each(townListForCurrentProgramme, function(i, currentTown) {
-							programmesHtmlFragment += '<a href="javascript:">' + currentTown[1] + '</a></br>';
-						});
-						
-						programmesHtmlFragment += '</div>';
-						// Printing major/Level, entry requirements
-						programmesHtmlFragment += '<div class="col-duration">';
-						programmesHtmlFragment += '<label>' + val[23] + '<br><span>' + val[24] + '</span></label>';
-						programmesHtmlFragment += '</div>';
-						programmesHtmlFragment += '</li>';
-					});					
-				}
-				
-				var programmeListElement = $('.filter-result-table').find('ul.result-row');
-				programmeListElement.html(programmesHtmlFragment);	
-				
-				// Construct the paginator
-				var numOfPages = parseInt(response.numOfPages);
-//				numOfPages = 2;
-				var pageNumReceived = parseInt(response.pageNum);
-				
-				if ((numOfPages !== undefined && numOfPages !== null) && 
-						(pageNumReceived !== undefined && pageNumReceived !== null)) {
-					
-					var paginationHtml = '';
-					paginationHtml += '<li class="disabled paginator-button">\
-					      <a href="#">\
-					      <span>\
-					        <span aria-hidden="true">&laquo;</span>\
-					      </span>\
-					      </a>\
-					    </li>';
-					
-					for (var i = 1; i <= numOfPages; i++) {
-						paginationHtml += '<li class="paginator-button';
-						
-						if (i === pageNumReceived) {
-							paginationHtml += ' active';
-						}
-						
-						paginationHtml += '" data-page-number="'+ i +'">\
-						      <a href="#">\
-						      <span>' + i + '</span>\
-						      </a>\
-						    </li>';
-					}
-		
-					paginationHtml +='<li>\
-					      <a href="#">\
-					      <span>\
-					        <span aria-hidden="true">&raquo;</span>\
-					      </span>\
-					      </a>\
-					    </li>';
-					
-					var paginatorListElement = $('div.paginator-div > nav > ul.pagination');
-					paginatorListElement.html(paginationHtml);					
-				}
-				
-				paginatorListElement.find('li.paginator-button').on('click', function() {
-					var pageNumClicked = $(this).attr('data-page-number');
-					getProgrammeData(pageNumClicked);
-				});
-			}
-			
+				window.programmeCollectionFetched = response.result;
+				window.levelOrMajorCollectionFetched = response.levelOrMajorCollection;
+				window.programmeCodeToTownListMapFetched = response.programmeCodeToTownListMap;
+				window.numOfPagesFetched = response.numOfPages;
+				window.pageNumFetched = response.pageNum;	
+				window.contextDeployCourseLogoPathFetched = response.contextDeployCourseLogoPath;
+				window.contextDeployLogoPathFetched = response.contextDeployLogoPath;			
+			}			
 		},
 		error : function(response) {
 			alert("Ajax Error");
 		}
 	});
+}
+
+function constructLevelOrMajorMenu(){
+
+	var levelOrMajorCollection = window.levelOrMajorCollectionFetched;
+	
+	//Construct the Level/Major menu on top of programme displaying area
+	var levelOrMajorHtmlFragment = '';
+	
+	if (levelOrMajorCollection !== undefined && levelOrMajorCollection !== null) {
+		levelOrMajorHtmlFragment += '<li class="major-or-level-menu-item major-or-level-menu-item-all"><a href="javascript:">All</a></li>';
+		$.each(levelOrMajorCollection, function(index, val) {
+			levelOrMajorHtmlFragment += '<li class="major-or-level-menu-item" data-level-or-major-code="' + val[0] + '"><a href="javascript:">'+ val[1] +'</a></li>';
+		});					
+	}
+	
+	var levelOrMajorListElement = $('.filtering-area').find('ul.list-inline');
+	levelOrMajorListElement.html(levelOrMajorHtmlFragment);	
+	
+	levelOrMajorListElement.find('li.major-or-level-menu-item').on('click', function() {
+		var codeOfLevelOrMajorClicked = $(this).attr('data-level-or-major-code');
+		filterProgrammesOnLevelOrMajor(codeOfLevelOrMajorClicked);
+	});
+}
+
+function constructProgrammeListing() {
+
+	var programmeCollection = window.programmeCollectionFetched;
+	var programmeCodeToTownListMap = window.programmeCodeToTownListMapFetched;
+	var contextDeployLogoPath = window.contextDeployLogoPathFetched;
+	
+	//Construct the Programme listing 
+	var programmesHtmlFragment = '';
+	
+	if (programmeCollection !== undefined && programmeCollection !== null) {
+		$.each(programmeCollection, function(index, val) {						
+			programmesHtmlFragment += '<li class="course-info clearfix">';
+			programmesHtmlFragment += '<div class="col-name">';
+			programmesHtmlFragment += '<a href=""><h1 class="pro-name">' + val[18] + '</h1></a>';
+			programmesHtmlFragment += '<div class="pro-logo">';
+			
+			var logoImgPath = contextDeployLogoPath + val[22] + "/" + val[16];
+			
+			programmesHtmlFragment += '<a href=""><img src=' + logoImgPath + ' alt="' + val[18] + ' logo"></a>';
+			programmesHtmlFragment += '</div>';
+			programmesHtmlFragment += '</div>';
+			programmesHtmlFragment += '<div class="col-description">';
+			programmesHtmlFragment += '<p>' + val[1] + '</p>';
+			programmesHtmlFragment += '</div>';
+			programmesHtmlFragment += '<div class="col-location">';
+			
+			var townListForCurrentProgramme = programmeCodeToTownListMap[val[0]];
+			
+			$.each(townListForCurrentProgramme, function(i, currentTown) {
+				programmesHtmlFragment += '<a href="javascript:">' + currentTown[1] + '</a></br>';
+			});
+			
+			programmesHtmlFragment += '</div>';
+			// Printing major/Level, entry requirements
+			programmesHtmlFragment += '<div class="col-duration">';
+			programmesHtmlFragment += '<label>' + val[23] + '<br><span>' + val[24] + '</span></label>';
+			programmesHtmlFragment += '</div>';
+			programmesHtmlFragment += '</li>';
+		});					
+	}
+	
+	var programmeListElement = $('.filter-result-table').find('ul.result-row');
+	programmeListElement.html(programmesHtmlFragment);	
+	
+}
+
+function constructPaginator() {
+
+	// Construct the paginator
+	var numOfPages = parseInt(window.numOfPagesFetched);
+//	numOfPages = 2;
+	var pageNumReceived = parseInt(window.pageNumFetched);
+	
+	if ((numOfPages !== undefined && numOfPages !== null) && 
+			(pageNumReceived !== undefined && pageNumReceived !== null)) {
+		
+		var paginationHtml = '';
+		paginationHtml += '<li class="disabled paginator-button paginator-button-previous">\
+		      <a href="#">\
+		      <span>\
+		        <span aria-hidden="true">&laquo;</span>\
+		      </span>\
+		      </a>\
+		    </li>';
+		
+		for (var i = 1; i <= numOfPages; i++) {
+			paginationHtml += '<li class="paginator-button';
+			
+			if (i === pageNumReceived) {
+				paginationHtml += ' active';
+			}
+			
+			paginationHtml += '" data-page-number="'+ i +'">\
+			      <a href="#">\
+			      <span>' + i + '</span>\
+			      </a>\
+			    </li>';
+		}
+
+		paginationHtml +='<li class="paginator-button-next">\
+		      <a href="#">\
+		      <span>\
+		        <span aria-hidden="true">&raquo;</span>\
+		      </span>\
+		      </a>\
+		    </li>';
+		
+		var paginatorListElement = $('div.paginator-div > nav > ul.pagination');
+		paginatorListElement.html(paginationHtml);					
+	}
+	
+	paginatorListElement.find('li.paginator-button').on('click', function() {
+		var pageNumClicked = $(this).attr('data-page-number');
+		getProgrammeData(pageNumClicked);
+	});
+}
+
+function filterProgrammesOnLevelOrMajor(levelOrMajorCode) {
+	alert("Level or Major clicked! Code = " + levelOrMajorCode);
 }
