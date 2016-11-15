e62f7f271e1216beafe2284580399b23ec57e1eb
Dumani Jayasinghe <dumani@genesiis.com>
2016-11-15 16:53:29 +0530

    c17-provider-criteria-based-filter-search-MP-dj created LevelDAO.java

---
+++ campus.jar/src/com/genesiis/campus/entity/LevelDAO.java
@@ -0,0 +1,72 @@
+package com.genesiis.campus.entity;
+
+//DJ 20161115 c17-provider-criteria-based-filter-search-MP-dj created LevelDAO.java
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Collection;
+
+public class LevelDAO  implements ICrud{
+	static org.apache.log4j.Logger log = Logger.getLogger(LevelDAO.class.getName());
+
+	@Override
+	public int add(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<Collection<String>> findById(Object code)
+			throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Collection<String>> getAll() throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int add(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<Collection<String>> findById(Object object,
+			Connection conn) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
