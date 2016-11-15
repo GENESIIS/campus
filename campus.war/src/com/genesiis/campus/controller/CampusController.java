85aaf9daa4243c2657cff4df7ce591efe93db01d
chathuriM <madushani@genesiis.com>
2016-11-15 09:41:19 +0530

    c9-Inquiry Form for institute-Modified campus controller servlet according to new design implmetation-cm

--- campus.war/src/com/genesiis/campus/controller/CampusController.java
+++ campus.war/src/com/genesiis/campus/controller/CampusController.java
@@ -1,10 +1,18 @@
 package com.genesiis.campus.controller;
+
 // 20161024 DN c10-contacting-us-page created the initial version of the Servlet Controller
 //20161107 DN, JH, DJ, AS, CM, MM public-controller-testing Changed implementation of process()
 //	to support returning JSON as well as JSP as response
+//20161109 PN, MM public-controller-testing-2 Changed implementation of process() so that when composing 
+//	JSON content a Java Map is utilised so the returned JSON is in proper format.
+//20161114 MM public-controller-testing-2 Changed implementation of process() so that even when 
+//view.getCollection() returns null, the rest of the Objects set as 
+//attributes to DataHelper are included in the JSON object created
 
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -19,11 +27,9 @@ import com.genesiis.campus.util.IDataHelper;
 import com.genesiis.campus.validation.ResponseType;
 import com.google.gson.Gson;
 
-
-
 /**
- * Servlet implementation class CampusController
- * extract from XecoController.java
+ * Servlet implementation class CampusController extract from
+ * XecoController.java
  * 
  * 
  */
@@ -33,7 +39,6 @@ public class CampusController extends HttpServlet {
 	static Logger log = Logger.getLogger(CampusController.class.getName());
 	private static final long serialVersionUID = 1L;
 
-	
 	@Override
 	public void init() throws ServletException {
 		super.init();
@@ -63,39 +68,34 @@ public class CampusController extends HttpServlet {
 
 		try {
 			result = helper.getResultView(cco);
+			Gson gson = new Gson();
+			if (ResponseType.JSP.equals(responseType)) {
 
-			if (ResponseType.JSP.equals(responseType)) {  
-	
 				request.setAttribute("result", result);
 				request.getRequestDispatcher(helper.getResultPage(cco))
 						.forward(request, response);
-				
-			} else if (ResponseType.JSON.equals(responseType)) {  
-				
-				StringBuilder json = new StringBuilder();
-				Gson gson = new Gson();
-				json.append("{result:");
-				if (result.getCollection() != null) {
-					
-					json.append(gson.toJson(result.getCollection()));
-	
-					Enumeration<String> attributeNames = request
-							.getAttributeNames();
-	
-					while (attributeNames.hasMoreElements()) {
-						String currentAttributeName = attributeNames.nextElement();
-						Object object = helper.getAttribute(currentAttributeName);
-						String objectInJSON = gson.toJson(object);
-						json.append(", " + currentAttributeName + ":" + objectInJSON);
-					}
+
+			} else if (ResponseType.JSON.equals(responseType)) {
+
+				Map<String, Object> objectMap = new LinkedHashMap<String, Object>();
+
+				if (result != null && result.getCollection() != null) {
+					objectMap.put("result", result.getCollection());
 				} else {
-					json.append("NO-DATA");
+					objectMap.put("result", "NO-DATA");
 				}
-				
-				json.append("}");
-				
+
+				Enumeration<String> attributeNames = request
+						.getAttributeNames();
+
+				while (attributeNames.hasMoreElements()) {
+					String currentAttributeName = attributeNames.nextElement();
+					Object object = helper.getAttribute(currentAttributeName);
+					objectMap.put(currentAttributeName, object);
+				}
+
+				response.getWriter().write(gson.toJson(objectMap));
 				response.setContentType("application/json");
-				response.getWriter().write(json.toString());
 			}
 
 		} catch (Exception e) {
