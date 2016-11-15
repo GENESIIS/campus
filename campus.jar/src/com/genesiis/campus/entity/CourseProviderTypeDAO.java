ac9c547e1b3d1bb9a407ff0e334472538f918c01
Dumani Jayasinghe <dumani@genesiis.com>
2016-11-15 15:58:18 +0530

    c17-provider-criteria-based-filter-search-MP-dj created CourseProviderTypeDAO.java,getAll() method

---
+++ campus.jar/src/com/genesiis/campus/entity/CourseProviderTypeDAO.java
@@ -0,0 +1,118 @@
+package com.genesiis.campus.entity;
+
+//DJ 20161115 c17-provider-criteria-based-filter-search-MP-dj created CourseProviderTypeDAO.java
+//DJ 20161115 c17-provider-criteria-based-filter-search-MP-dj Implement getAll()
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.log4j.Logger;
+
+import com.genesiis.campus.util.ConnectionManager;
+
+public class CourseProviderTypeDAO implements ICrud{
+	
+	static org.apache.log4j.Logger log = Logger.getLogger(CourseProviderTypeDAO.class.getName());
+
+	@Override
+	public int add(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<Collection<String>> findById(Object code)
+			throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/**
+	 * Get all course provider type details
+	 * @param 
+	 * @author DJ
+	 * @return Collection 
+	 */
+
+	@Override
+	public Collection<Collection<String>> getAll() throws SQLException,
+			Exception {
+		Connection conn=null;
+		PreparedStatement stmt=null;
+		final Collection<Collection<String>> allCourseProviderTypeList=new ArrayList<Collection<String>>();
+		try {
+			conn=ConnectionManager.getConnection();
+			String sql="SELECT CPTYPE.CODE AS CPTYPECODE , CPTYPE.NAME AS CPTYPENAME FROM [CAMPUS].COURSEPROVIDERTYPE CPTYPE WHERE CPTYPE.ISACTIVE=1 ";
+			
+			stmt=conn.prepareStatement(sql.toString());
+			final ResultSet rs=stmt.executeQuery();
+			
+			while (rs.next()) {				
+				final ArrayList<String> singleCPType = new ArrayList<String>();
+				singleCPType.add(rs.getString("CPTYPECODE"));				
+				singleCPType.add(rs.getString("CPTYPENAME"));				
+				allCourseProviderTypeList.add(singleCPType);
+			}
+		} catch (SQLException sqlException) {
+			log.info("getAll() sqlException" + sqlException.toString());
+			throw sqlException;
+		} catch (Exception e) {
+			log.info("getAll() Exception" + e.toString());
+			throw e;
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		}
+		
+		return allCourseProviderTypeList;
+	}
+
+	@Override
+	public int add(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<Collection<String>> findById(Object object,
+			Connection conn) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
