634d6958883b9903ac9c41aa2c822d7d272318ab
jayani1991 <jayani@genesiis.com>
2016-11-16 23:27:24 +0530

    c7-higher-education-landing-page-MP header ui-populate-helper.js backend coding wip -JH

--- campus.war/web/dist/js/header/ui-populate-helper.js
+++ campus.war/web/dist/js/header/ui-populate-helper.js
@@ -1,26 +1,29 @@
-//20161109 JH c7-higher-eduction-landing-page-MP 
-//20161109 JH c7-higher-eduction-landing-page-MP 
+//20161109 JH c7-higher-education-landing-page-MP ui-populate-helper.js created
+//20161116 JH c7-higher-education-lanidng-page-MP change displayCategory() me
 
 /**
  * This method is to load category details
  */
-function displayCategory() {
-    alert("Hello! I am an alert box!");
-	$.get('PublicController', {
-		CCO : 'LIST_LEVEL_DATA'
-	}, function(response) {
-		var result = $("#categoryName");
-	
-	
-	});
-}
 
-function getCategory(){
-	$.get('PublicController', {
-		CCO : 'LIST_LEVEL_DATA'
-	}, function(response) {
-		var result = $("#categoryName");
-	
-	
+window.programmeCollection = null;
+
+function getCategoryData() {
+
+	$.ajax({
+		url : '/PublicController',
+		method : 'POST',
+		data : {
+			'CCO' : 'LIST_CATEGORY_DATA'
+		},
+		dataType : "json",
+		async : false,
+		success : function(response) {
+
+			if (response !== undefined && response !== null) {
+				window.programmeCollection = response.result;
+
+				alert(programmeCollection);
+			}
+		},
 	});
 }
