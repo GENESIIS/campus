d6719ee40f98042def588bc261d9b8d13a24515d
Dumani Jayasinghe <dumani@genesiis.com>
2016-11-17 10:55:07 +0530

    c17-provider-criteria-based-filter-search created CourseProviderSearchDTO.java

---
+++ campus.jar/src/com/genesiis/campus/entity/model/CourseProviderSearchDTO.java
@@ -0,0 +1,50 @@
+package com.genesiis.campus.entity.model;
+//DJ 20161117 c17-provider-criteria-based-filter-search created CourseProviderSearchDTO.java
+
+public class CourseProviderSearchDTO {
+	
+	private int courseProviderCode;
+	private int category;
+	private int level;
+	private int major;
+	private int courserProviderType;
+	private int district;	
+	
+	public int getCourseProviderCode() {
+		return courseProviderCode;
+	}
+	public void setCourseProviderCode(int courseProviderCode) {
+		this.courseProviderCode = courseProviderCode;
+	}
+	public int getCategory() {
+		return category;
+	}
+	public void setCategory(int category) {
+		this.category = category;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public int getMajor() {
+		return major;
+	}
+	public void setMajor(int major) {
+		this.major = major;
+	}
+	public int getCourserProviderType() {
+		return courserProviderType;
+	}
+	public void setCourserProviderType(int courserProviderType) {
+		this.courserProviderType = courserProviderType;
+	}
+	public int getDistrict() {
+		return district;
+	}
+	public void setDistrict(int district) {
+		this.district = district;
+	}
+
+}
