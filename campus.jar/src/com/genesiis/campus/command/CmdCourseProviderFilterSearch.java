8d74be575d0c9b4785de74df2d95495cbbc3bfb4
Dumani Jayasinghe <dumani@genesiis.com>
2016-11-17 13:36:16 +0530

    c17-provider-criteria-based-filter-search Implement execute() method

--- campus.jar/src/com/genesiis/campus/command/CmdCourseProviderFilterSearch.java
+++ campus.jar/src/com/genesiis/campus/command/CmdCourseProviderFilterSearch.java
@@ -1,5 +1,6 @@
 package com.genesiis.campus.command;
 //DJ 20161117 c17-provider-criteria-based-filter-search created CmdCourseProviderFilterSearch.java
+//DJ 20161117 c17-provider-criteria-based-filter-search Implement execute() method
 
 import java.sql.SQLException;
 import java.util.Collection;
@@ -10,6 +11,7 @@ import com.genesiis.campus.entity.model.CourseProvider;
 import com.genesiis.campus.entity.model.CourseProviderSearchDTO;
 import com.genesiis.campus.util.IDataHelper;
 import com.genesiis.campus.validation.SystemMessage;
+import com.genesiis.campus.validation.UtilityHelper;
 
 import org.apache.log4j.Logger;
 
@@ -31,6 +33,23 @@ public class CmdCourseProviderFilterSearch implements ICommand  {
 		
 		try {
 			final CourseProviderSearchDTO providerSearchDTO = new CourseProviderSearchDTO();
+			int categoryCode = 0;
+			//Set input filter values to providerSearchDTO			
+			String categoryCodeString = helper.getParameter("categoryCode");
+			if (UtilityHelper.isNotEmpty(categoryCodeString)) {
+				if (UtilityHelper.isInteger(categoryCodeString)) {
+					categoryCode = Integer.parseInt(categoryCodeString);
+					providerSearchDTO.setCategory(categoryCode);
+				}				
+			}
+			
+			//TODO:set filter values
+			//isgetAll == true--> set all the param to 0
+			/*providerSearchDTO.setCourserProviderType(courserProviderType);
+			providerSearchDTO.setMajor(major);
+			providerSearchDTO.setLevel(level);
+			providerSearchDTO.setDistrict(district);*/
+			
 			
 			final Collection<Collection<String>> courseProviderSearchResults = providerDAO.findFilterdCourseProviders(providerSearchDTO);
 			iView.setCollection(courseProviderSearchResults);
