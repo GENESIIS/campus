8825d48d15e7e350ef1b77707e98690db32d7203
Miyuru-Genesiis <miyuru@genesiis.com>
2016-11-18 18:53:45 +0530

    c2-integrate-google-banners-MP - Added fields crtOn, crtBy, modOn, modBy and their getters & setters and moved to from entity to entity.model package

--- campus.jar/src/com/genesiis/campus/entity/model/Banner.java
+++ campus.jar/src/com/genesiis/campus/entity/model/Banner.java
@@ -1,6 +1,7 @@
 // 20161116 MM c2-integrate-google-banners INIT Created class Banner
-
-package com.genesiis.campus.entity;
+// 20161116 MM c2-integrate-google-banners Moved class inside com.genesiis.campus.entity.model package
+// 20161116 MM c2-integrate-google-banners Added fields crtOn, crtBy, modOn, modBy and their getters & setters
+package com.genesiis.campus.entity.model;
 
 import java.util.Date;
 
@@ -15,6 +16,10 @@ public class Banner {
 	int bannerStatus;
 	boolean isActive;
 	int pageSlot;
+	private Date crtOn;
+	private String crtBy;
+	private Date modOn;
+	private String modBy;
 	
 	public int getCode() {
 		return code;
@@ -75,6 +80,30 @@ public class Banner {
 	}
 	public void setPageSlot(int pageSlot) {
 		this.pageSlot = pageSlot;
+	}
+	public Date getCrtOn() {
+		return crtOn;
+	}
+	public String getCrtBy() {
+		return crtBy;
+	}
+	public Date getModOn() {
+		return modOn;
+	}
+	public String getModBy() {
+		return modBy;
+	}
+	public void setCrtOn(Date crtOn) {
+		this.crtOn = crtOn;
+	}
+	public void setCrtBy(String crtBy) {
+		this.crtBy = crtBy;
+	}
+	public void setModOn(Date modOn) {
+		this.modOn = modOn;
+	}
+	public void setModBy(String modBy) {
+		this.modBy = modBy;
 	}
 	
 }
