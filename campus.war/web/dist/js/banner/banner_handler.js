404d070d232de2a2999873e0439cf480570f86c1
Miyuru-Genesiis <miyuru@genesiis.com>
2016-11-21 05:33:58 +0530

    c2-integrate-google-banners-MP -  Deviced solution to display banners, of not only a single slot, but all slots, for only exactly the specified time in the DB for each of those banners, using an array of functions and setTimeout() JS function. Fixed issue with event.target and  returning the wrong url when trying to open the URL that tha banner has been set to to open in the DB

--- campus.war/web/dist/js/banner/banner_handler.js
+++ campus.war/web/dist/js/banner/banner_handler.js
@@ -3,55 +3,66 @@
  * 20161118 MM c2-integrate-google-banners-MP Put in code to rotate the banners in slots
  * 20161118 MM c2-integrate-google-banners-MP Included code to send Ajax request to update 
  * 				banner click statistics upon clicking of banners
+ * 20161119 MM c2-integrate-google-banners-MP Fixed issue of banners being displayed slot 
+ * 				after slot (not simultaneously)
+ * 20161119 MM c2-integrate-google-banners-MP Made banners of a single slot display for 
+ * 				only the period as specified in the DB for that banner
+ * 20161120 MM c2-integrate-google-banners-MP Devices solution to display banners, of not 
+ *				only a single slot, but all slots for only exactly the specified time in the 
+ *				DB for each of those banners with an array of functions and setTimeout() JS function
+ * 20161120 MM c2-integrate-google-banners-MP Fixed issue with event.target and $(this) 
+ * 				returning the wrong url when sending trying to open the URL that tha banner
+ * 				has been set to to open in the DB
+ *  
  */
 
-$(window).load(function() { //start after HTML, images have loaded
- 
-    var InfiniteRotator =
-    {
-        init: function()
-        {
-            //initial fade-in time (in milliseconds)
-            var initialFadeIn = 100;
- 
-            //interval between items (in milliseconds)
-            var itemInterval = 2500;
- 
-            //cross-fade time (in milliseconds)
-            var fadeTime = 250;
- 
-            //count number of items
-            var numberOfItems = $('.rotating-item').length;
- 
-            //set current item
-            var currentItem = 0;
- 
-            //show first item
-            $('.rotating-item').eq(currentItem).fadeIn(initialFadeIn);
- 
-            //loop through the items
-            var infiniteLoop = setInterval(function(){
-                $('.rotating-item').eq(currentItem).fadeOut(fadeTime);
- 
-                if(currentItem == numberOfItems -1){
-                    currentItem = 0;
-                }else{
-                    currentItem++;
-                }
-                $('.rotating-item').eq(currentItem).fadeIn(fadeTime);
- 
-            }, itemInterval);
-        }
-    };
- 
-    InfiniteRotator.init();    
-    
-    $('.banner').on('click', function() {
-//    	alert('a banner was clicked.');
-    	sendBannerStatisticsUpdateRequest($(this));
-    });
- 
+// Hack to enable parameter passing for setInterval() method in IE9 and below
+/*@cc_on
+  // conditional IE < 9 only fix
+  @if (@_jscript_version <= 9)
+  (function(f){
+     window.setTimeout=f(window.setTimeout);
+     window.setInterval=f(window.setInterval);
+  })(function(f){return function(c,t){var a=[].slice.call(arguments,2);return f(function(){c instanceof Function?c.apply(this,a):eval(c)},t)}});
+  @end
+@*/
+
+// Banner-rotation code 
+var bannerSlotWrappers = $('.rotating-item-wrapper');
+var bannerSlotTimers = [];
+
+bannerSlotWrappers.each(function(index){
+	bannerSlotTimers.push(function (slotIndex) {
+		var shownBanner = $(bannerSlotWrappers.get(slotIndex)).find('.banner-shown');
+		var t1 = shownBanner.data('timeout');      
+		
+		setTimeout(function () {
+			var activeBanner = $(bannerSlotWrappers.get(slotIndex)).find('.banner-shown');
+			activeBanner.removeClass('banner-shown');
+			var anchor = activeBanner.parent().next();
+			var nextBanner = null;
+			if (anchor.length == 0) {
+				nextBanner = activeBanner.parent().siblings().first().children('img');
+			} else {
+				nextBanner = anchor.children('img');
+			}
+			nextBanner.addClass('banner-shown');
+			bannerSlotTimers[slotIndex](slotIndex);
+		}, t1 * 1000, slotIndex);
+	});
 });
+
+$.each(bannerSlotTimers, function(index, value) {
+	value(index);
+});
+
+// Binding event handler to elements
+$('.banner').on('click', function(e) {
+	e.preventDefault();
+	var url = $($(this).parents('.rotating-item-wrapper').find('img.banner-shown').get(0)).parents('a').attr('href');
+	sendBannerStatisticsUpdateRequest($(this));
+	window.open(url, '_blank');
+}); 
 
 
 // Event handler for sending DB add operation upon clicking of a banner
@@ -70,15 +81,18 @@ function sendBannerStatisticsUpdateRequest(banner) {
 		},
 		dataType : "json",
 		async : false,
-		success : function(response) {
-//			alert("Ajax Success");
-			
-//			alert(response.result);
-			
-			
+		success : function(response) {			
+			var operationStatus = response.operationStatus; 			
+			if(operationStatus != undefined && operationStatus != null) {
+				if (operationStatus === 'SUCCESS') {
+					console.log('Banner-statistics successfully updated.');
+				} else if (operationStatus === 'FAILURE') {
+					console.log('Banner-statistics update failed.');
+				}
+			}
 		},
 		error : function(response) {			
-			alert("Ajax Error");
+			console.log('Ajax request failed.');
 		}
 	});
 }
