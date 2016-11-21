afeb9a7de7b61855c7aae8b80c615833a2903835
chathuriM <madushani@genesiis.com>
2016-11-21 12:08:31 +0530

    c36-Add tutor details-INIT CmdAddTutorProfileDetails class-cm

---
+++ campus.jar/src/com/genesiis/campus/command/CmdAddTutorProfile.java
@@ -0,0 +1,17 @@
+package com.genesiis.campus.command;
+
+import java.sql.SQLException;
+
+import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.util.IDataHelper;
+
+public class CmdAddTutorProfile implements ICommand{
+
+	@Override
+	public IView execute(IDataHelper helper, IView view) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
