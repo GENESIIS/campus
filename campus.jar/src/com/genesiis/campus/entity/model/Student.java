81080b744cd375fc035f43736361bf9fc2eb72d4
Miyuru-Genesiis <miyuru@genesiis.com>
2016-11-22 05:43:39 +0530

    c25-student-login-create-dashboard-MP Initialised file Student.java

---
+++ campus.jar/src/com/genesiis/campus/entity/model/Student.java
@@ -0,0 +1,16 @@
+package com.genesiis.campus.entity.model;
+
+//20161121 MM c25-student-login-create-dashboard-MP INIT Initialised Student.java
+
+public class Student {
+	int code;
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+}
