8a1f7d7421e5095ab680ceb454995879c2f2488d
Miyuru-Genesiis <miyuru@genesiis.com>
2016-11-22 05:45:59 +0530

    c5-corporate-training-landing-page - Initialised file

---
+++ campus.war/src/com/genesiis/campus/studentController/StudentController.java
@@ -0,0 +1,48 @@
+package com.genesiis.campus.studentController;
+
+//20161122 MM c5-corporate-training-landing-page - INIT - Initialised file
+
+import com.genesiis.campus.controller.CampusController;
+
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PublicController
+ */
+@WebServlet("/StudentController")
+public class StudentController extends CampusController {
+	static Logger log = Logger.getLogger(StudentController.class.getName());
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public StudentController() {
+		super();
+	}
+
+	/**
+	 * @see CampusController#doGet(HttpServletRequest request,
+	 *      HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		super.doGet(request, response);
+	}
+
+	/**
+	 * @see CampusController#doPost(HttpServletRequest request,
+	 *      HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		super.doPost(request, response);
+	}
+}
