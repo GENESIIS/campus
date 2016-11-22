952b11ce620e98117309b06416449ba0c4a1caa3
pabodhaW <pabodha@genesiis.com>
2016-11-22 15:24:18 +0530

    c27-upload-user-image: modified execute() method to create the folder using studentCode to store image.

--- campus.jar/src/com/genesiis/campus/command/CmdUploadProfileImg.java
+++ campus.jar/src/com/genesiis/campus/command/CmdUploadProfileImg.java
@@ -1,12 +1,17 @@
 package com.genesiis.campus.command;
 
 //20161121 PN c27-upload-user-image: INIT CmdUploadProfileImg.java class and implemented execute() method.
+//20161122 PN c27-upload-user-image: modified execute() method to create the folder using studentCode to store image.
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
 
+import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.SystemConfigDAO;
+import com.genesiis.campus.entity.model.SystemConfiguration;
 import com.genesiis.campus.util.FileUtility;
 import com.genesiis.campus.util.IDataHelper;
 import com.google.gson.Gson;
@@ -26,22 +31,24 @@ public class CmdUploadProfileImg implements ICommand {
 		Gson gson = new Gson();
 		FileUtility fu = new FileUtility();
 		ArrayList<FileItem> files = new ArrayList<FileItem>();
-		String institute = "DEFX";
-		try {
-
-			Map<String, String[]> args = helper.getParameterMap();
-			institute = ((String[]) args.get("IID"))[0];
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		
+		int StudentCode = 1; //This needs to be assign from the session.
 
 		try {
 
 			files = (ArrayList<FileItem>) helper.getFiles();
-			String uploadPath = "C:/sdb/ctxdeploy/career.war/images/uploads/institutes";//SystemConfiguration.getInstance().getConfigValueOne(SystemConfigCodes.INSTITUTE_UPLOAD_PATH.getValue());
-			String war =        "C:/sdb/ctxdeploy/career.war/images/uploads/institutes";//SystemConfiguration.getInstance().getConfigValueTwo(SystemConfigCodes.INSTITUTE_UPLOAD_PATH.getValue());
-			fu.setUploadPath(uploadPath + "/" + "PABODHA" + "/articles/");
+			
+			String uploadPath = "";
+			ICrud sysconfigDAO = new SystemConfigDAO();
+			Collection<Collection<String>> sysconfigCollection = sysconfigDAO.findById("USER_PIC_UPLOAD_PATH");
+			for (Collection<String> collection : sysconfigCollection) {
+				Object[] config = collection.toArray();
+				uploadPath = (String) config[2];
+			}
+			
+			String uploadPathuploadPath = uploadPath;
+			String war = uploadPath;
+			fu.setUploadPath(uploadPath + "/"+Integer.toString(StudentCode)+"/");
 
 			for (FileItem item : files) {
 
