247c6b892b06a7a68942081dcf0312fa99b3377b
jayani1991 <jayani@genesiis.com>
2016-11-22 17:03:52 +0530

    c39-add-course-provider CourseProviderDAO.java class created and coding wip

--- campus.jar/src/com/genesiis/campus/command/CmdAddFeaturedProvider.java
+++ campus.jar/src/com/genesiis/campus/command/CmdAddFeaturedProvider.java
@@ -8,16 +8,35 @@ import java.sql.SQLException;
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.util.IDataHelper;
 
-public class CmdAddFeaturedProvider implements ICommand{
+import org.apache.log4j.Logger;
 
+public class CmdAddFeaturedProvider implements ICommand{
+	static Logger log = Logger.getLogger(CmdAddFeaturedProvider.class.getName());
+	
+	private IView courseProviderData;
+
+	public CmdAddFeaturedProvider(IView courseProviderData) {
+		this.courseProviderData = courseProviderData;
+	}
+
+	public CmdAddFeaturedProvider() {
+
+	}
+	
 	/**
 	 * execute method used to handle the request related to featured course provider 
 	 * registration. 
+	 * @author JH
+	 * @param IDataHelper
+	 * @param IView
+	 * @return IView Objects
 	 */
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
-		// TODO Auto-generated method stub
+		
+		
+		
 		return null;
 	}
 
