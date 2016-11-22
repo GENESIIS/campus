550e8a9a3dd99f9bd78954865fbb728c4cafe403
chathuriM <madushani@genesiis.com>
2016-11-22 10:03:29 +0530

    c36-Add tutor details- Modified execute() method-cm

--- campus.jar/src/com/genesiis/campus/factory/TutorCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/TutorCmdFactory.java
@@ -2,14 +2,17 @@ package com.genesiis.campus.factory;
 
 //20161121 CM c36-add-tutor-information INIT TutorCmdFactory.java
 //20161121 CM c36-add-tutor-information Modified getCommand() method. 
+//20161122 CM c36-add-tutor-information Modified getCommand() method. 
 
 import com.genesiis.campus.command.CmdAddTutorProfile;
+import com.genesiis.campus.command.CmdLoadTownDetails;
 import com.genesiis.campus.command.ICommand;
 import com.genesiis.campus.validation.Operation;
 
 public class TutorCmdFactory implements ICmdFactory {
 	static {
 		map.put(Operation.ADD_TUTOR_PROFILE_DETAILS, new CmdAddTutorProfile());
+		map.put(Operation.LIST_TOWN_DATA, new CmdLoadTownDetails());
 	}
 	@Override
 	public ICommand getCommand(String cco) {
@@ -20,6 +23,9 @@ public class TutorCmdFactory implements ICmdFactory {
 		case ADD_TUTOR_PROFILE_DETAILS:
 			command = map.get(o);
 			break;
+		case LIST_TOWN_DATA:
+			command = map.get(o);
+			break;	
 		default:
 			break;
 		}
