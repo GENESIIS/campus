c35df5db98e376d7345c9c5dac9d88ce8a5504a2
Miyuru-Genesiis <miyuru@genesiis.com>
2016-11-23 13:57:06 +0530

    c2-integrate-google-banners Added JavaDoc comment

--- campus.jar/src/com/genesiis/campus/command/CmdLoadBannerPage.java
+++ campus.jar/src/com/genesiis/campus/command/CmdLoadBannerPage.java
@@ -1,5 +1,6 @@
 //20161003 MM c2-integrate-google-banners GetGoogleAdverts.java created
 //20161116 MM c2-integrate-google-banners Renamed to CmdGetGoogleAdverts.java
+//20161123 MM c2-integrate-google-banners Added JavaDoc comment
 
 package com.genesiis.campus.command;
 
@@ -15,15 +16,33 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 
+/**
+ * This is a dummy command class that was developed to demonstrate that after
+ * the processing of a client request that is received into a command class,
+ * when the programme-control traverses framework classes, any banners
+ * applicable to the JSP that is related to the current operation 
+ * (as mapped in Operation enum) will automatically get loaded
+ * and displayed on the page. This is to be excluded during integration.
+ * 
+ * @author miyuru
+ * 
+ */
 public class CmdLoadBannerPage implements ICommand {
 
 	static Logger Log = Logger.getLogger(CmdLoadBannerPage.class.getName());
-	
+
+	/**
+	 * Executes business logic and returns an object of type IView that wraps data 
+	 * needed to be displayed
+	 * @param IDataHelper
+	 * @param IView
+	 * 
+	 * @return IView 
+	 */
 	@Override
-	public IView execute(IDataHelper helper, IView view) throws SQLException, Exception {
-		
-		BannerData.setBannerDetails(helper, helper.getResultPage(helper.getCommandCode()));
-		
+	public IView execute(IDataHelper helper, IView view) throws SQLException,
+			Exception {
+
 		return view;
 	}
 }
