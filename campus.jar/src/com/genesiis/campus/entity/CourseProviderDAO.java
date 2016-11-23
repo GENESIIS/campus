08297f8b8ad448f4b12acd5844af3b66d61381eb
jayani1991 <jayani@genesiis.com>
2016-11-23 23:08:55 +0530

    c39-add-course-provider course provider DAO class doing wip

--- campus.jar/src/com/genesiis/campus/entity/CourseProviderDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/CourseProviderDAO.java
@@ -1,16 +1,61 @@
 package com.genesiis.campus.entity;
 
 //20161122 JH c39-add-course-provider CourseProviderDAO created
+//20161123 JH c39-add-course-provider add method code modified
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Collection;
 
-public class CourseProviderDAO implements ICrud{
+import org.apache.log4j.Logger;
 
+import com.genesiis.campus.util.ConnectionManager;
+
+public class CourseProviderDAO implements ICrud{
+	
+	static org.apache.log4j.Logger log = Logger.getLogger(CourseProviderDAO.class.getName());
+
+	/**
+	 * add method used to create a new featured course provider record. 
+	 * @author JH
+	 * @param object type of hash map
+	 * @return int to indicate success or fail
+	 */
 	@Override
 	public int add(Object object) throws SQLException, Exception {
-		// TODO Auto-generated method stub
+	
+		Connection conn = null;
+		PreparedStatement preparedStatement = null;
+		int status = 0;
+		
+		try{
+			conn = ConnectionManager.getConnection();
+			
+			String query = "INSERT INTO [CAMPUS].[COURSEPROVIDER](UNIQUEPREFIX ,SHORTNAME, NAME, DESCRIPTION, GENERALEMAIL,"
+			+" COURSEINQUIRYEMAIL, LANDPHONECOUNTRYCODE, LANDPHONEAREACODE ,LANDPHONENO ,LANDPHONE2NO ,"
+			+" FAXNO ,MOBILEPHONECOUNTRYCODE, MOBILEPHONENETWORKCODE, MOBILEPHONENO, HEADERIMAGEPATH,"
+			+" LOGOIMAGEPATH, SPECIALITY ,WEBLINK, FACEBOOKURL, TWITTERURL, MYSPACEURL , LINKEDINURL, INSTAGRAMURL ,"
+            +" VIBERNUMBER, WHATSAPPNUMBER, EXPIRATIONDATE, ADDRESS1, ADDRESS2, ADDRESS3, ACCOUNTTYPE, "
+            +" HEADOFFICETOWN, ISTUTORRELATED, ISADMINALLOWED, COURSEPROVIDERSTATUS, COURSEPROVIDERTYPE, "
+            +" PRINCIPAL, TUTOR, CRTON, CRTBY, MODON, MODBY )"
+            + "VALUES ( ?, ?, ? , ? , ?, ?, ? , ?, ?, ?, "
+		  +" ? ,?, ?, ? , ? , ?, ?, ? , ?, ?, ?, ?, ?, "
+		+"   ?, ?, ? , ? , ?, ?, ? , ?, ?, ?,?, ?, ?, ?, getDate(), ?, getDate() )";
+			preparedStatement = conn.prepareStatement(query);
+			status = preparedStatement.executeUpdate();
+			
+			
+		}catch(SQLException sqlException){
+			log.error("add method SQL Exception " + sqlException.toString());
+			throw sqlException;
+			
+		}catch (Exception exception) {
+			log.error("add method Exception " + exception.toString());
+			throw exception;
+		}finally{
+			
+		}
 		return 0;
 	}
 
