36760a128418834c6d8bd514c47c9e3d0e663dcf
Dumani Jayasinghe <dumani@genesiis.com>
2016-11-23 16:15:56 +0530

    c17-provider-criteria-based-filter-search add isGetAllCPTypes,isGetAllMajors,isGetAllLevels,cpTypeList attributes

--- campus.jar/src/com/genesiis/campus/entity/model/CourseProviderSearchDTO.java
+++ campus.jar/src/com/genesiis/campus/entity/model/CourseProviderSearchDTO.java
@@ -1,5 +1,10 @@
 package com.genesiis.campus.entity.model;
 //DJ 20161117 c17-provider-criteria-based-filter-search created CourseProviderSearchDTO.java
+//DJ 20161123 c17-provider-criteria-based-filter-search add isGetAllCPTypes,isGetAllMajors,isGetAllLevels,cpTypeList attributes
+
+import java.util.List;
+
+
 
 public class CourseProviderSearchDTO {
 	
@@ -10,6 +15,10 @@ public class CourseProviderSearchDTO {
 	private int courserProviderType;
 	private int district;	
 	private boolean isGetAll;
+	private boolean isGetAllCPTypes;
+	private boolean isGetAllMajors;
+	private boolean isGetAllLevels;
+	private List<Integer> cpTypeList;
 	
 	public int getCourseProviderCode() {
 		return courseProviderCode;
@@ -53,6 +62,29 @@ public class CourseProviderSearchDTO {
 	public void setGetAll(boolean isGetAll) {
 		this.isGetAll = isGetAll;
 	}
-	
+	public boolean isGetAllCPTypes() {
+		return isGetAllCPTypes;
+	}
+	public void setGetAllCPTypes(boolean isGetAllCPTypes) {
+		this.isGetAllCPTypes = isGetAllCPTypes;
+	}
+	public boolean isGetAllMajors() {
+		return isGetAllMajors;
+	}
+	public void setGetAllMajors(boolean isGetAllMajors) {
+		this.isGetAllMajors = isGetAllMajors;
+	}
+	public boolean isGetAllLevels() {
+		return isGetAllLevels;
+	}
+	public void setGetAllLevels(boolean isGetAllLevels) {
+		this.isGetAllLevels = isGetAllLevels;
+	}
+	public List<Integer> getCpTypeList() {
+		return cpTypeList;
+	}
+	public void setCpTypeList(List<Integer> cpTypeList) {
+		this.cpTypeList = cpTypeList;
+	}
 
 }
