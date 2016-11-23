6667990e0c29b36562e3148150be16965bdb5906
jayani1991 <jayani@genesiis.com>
2016-11-23 20:37:49 +0530

    c39-add-course-provider course provider account entity class code modified

--- campus.jar/src/com/genesiis/campus/entity/model/CourseProviderAccount.java
+++ campus.jar/src/com/genesiis/campus/entity/model/CourseProviderAccount.java
@@ -22,5 +22,163 @@ public class CourseProviderAccount {
 	
 	private Date crtOn;
 	private Date modOn;
+	/**
+	 * @return the code
+	 */
+	public int getCode() {
+		return code;
+	}
+	/**
+	 * @param code the code to set
+	 */
+	public void setCode(int code) {
+		this.code = code;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/**
+	 * @return the crtBy
+	 */
+	public String getCrtBy() {
+		return crtBy;
+	}
+	/**
+	 * @param crtBy the crtBy to set
+	 */
+	public void setCrtBy(String crtBy) {
+		this.crtBy = crtBy;
+	}
+	/**
+	 * @return the mobBy
+	 */
+	public String getMobBy() {
+		return mobBy;
+	}
+	/**
+	 * @param mobBy the mobBy to set
+	 */
+	public void setMobBy(String mobBy) {
+		this.mobBy = mobBy;
+	}
+	/**
+	 * @return the isActive
+	 */
+	public int getIsActive() {
+		return isActive;
+	}
+	/**
+	 * @param isActive the isActive to set
+	 */
+	public void setIsActive(int isActive) {
+		this.isActive = isActive;
+	}
+	/**
+	 * @return the courseProvider
+	 */
+	public int getCourseProvider() {
+		return courseProvider;
+	}
+	/**
+	 * @param courseProvider the courseProvider to set
+	 */
+	public void setCourseProvider(int courseProvider) {
+		this.courseProvider = courseProvider;
+	}
+	/**
+	 * @return the userType
+	 */
+	public int getUserType() {
+		return userType;
+	}
+	/**
+	 * @param userType the userType to set
+	 */
+	public void setUserType(int userType) {
+		this.userType = userType;
+	}
+	/**
+	 * @return the crtOn
+	 */
+	public Date getCrtOn() {
+		return crtOn;
+	}
+	/**
+	 * @param crtOn the crtOn to set
+	 */
+	public void setCrtOn(Date crtOn) {
+		this.crtOn = crtOn;
+	}
+	/**
+	 * @return the modOn
+	 */
+	public Date getModOn() {
+		return modOn;
+	}
+	/**
+	 * @param modOn the modOn to set
+	 */
+	public void setModOn(Date modOn) {
+		this.modOn = modOn;
+	}
+	
+	
 
 }
