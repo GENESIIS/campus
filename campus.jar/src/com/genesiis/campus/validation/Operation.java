f406f0446f3366702d6c2a69613462b9063ffa32
dushanthacb <dushantha@genesiis.com>
2016-11-23 16:59:31 +0530

    C18-student-signup-without-using-third-party-application-dn add SIGN_UP_WO_THRD_PARTY enum

--- campus.jar/src/com/genesiis/campus/validation/Operation.java
+++ campus.jar/src/com/genesiis/campus/validation/Operation.java
@@ -5,6 +5,7 @@ package com.genesiis.campus.validation;
 //20161029 PN c11-criteria-based-filter-search: added LIST_LEVEL_DATA,LIST_TOWN_DATA,LIST_MAJOR_DATA,LIST_DISTRICT_DATA attributes
 //20161101 PN c11-criteria-based-filter-search: added LIST_INSTITUTE_DATA attribute.
 //20161109 DN c10-contacting-us-MP change the path of ContactUs.jsp page
+//20161123 DN C18-student-signup-without-using-third-party-application-dn add SIGN_UP_WO_THRD_PARTY enum
 
 /**
  * Operation class facilitate in binding the CCO
@@ -15,6 +16,7 @@ public enum Operation {
 
 	CONTACT_US_PUBLC("FBTSA",ResponseType.JSP, "dist/partials/contactUs.jsp"),
 	GET_SEARCH_DATA("GET_SEARCH_DATA", ResponseType.JSP, "index.jsp"),
+	SIGN_UP_WO_THRD_PARTY("SIWOTP",ResponseType.JSON,""),
 	BAD_OPERATION("BO", ResponseType.INAPPLICABLE, "commandNotFound.jsp"),
 	LIST_CATEGORY_DATA("LIST_CATEGORY_DATA", ResponseType.JSON, ""),
 	LIST_LEVEL_DATA("LIST_LEVEL_DATA", ResponseType.JSP, "index.jsp"),
