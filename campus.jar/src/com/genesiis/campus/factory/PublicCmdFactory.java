8777463d7a3abf1e63a79aa3c02af29b7dd221ca
Dumani Jayasinghe <dumani@genesiis.com>
2016-11-24 16:00:34 +0530

    c17-provider-criteria-based-filter-search by inserting LIST_FILTER_SEARCH_CATEGORY_TYPES entry to the map

--- campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
@@ -4,9 +4,11 @@ package com.genesiis.campus.factory;
 //20161031 DJ c6-list-available-institutes-on-the-view amended by inserting LIST_TOP_COURSE_PROVIDERS entry to the map
 //20161103 DJ c6-list-available-institutes-on-the-view amended by inserting LIST_ALL_COURSE_PROVIDERS entry to the map
 //20161121 DJ c17-provider-criteria-based-filter-search by inserting LIST_FILTER_SEARCH_COURSE_PROVIDERS entry to the map
+//20161124 DJ c17-provider-criteria-based-filter-search by inserting LIST_FILTER_SEARCH_CATEGORY_TYPES entry to the map
 
 
 import com.genesiis.campus.command.CmdCourseProviderFilterSearch;
+import com.genesiis.campus.command.CmdListCategoryTypes;
 import com.genesiis.campus.command.CmdListMoreCourseProviders;
 import com.genesiis.campus.command.CmdListTopCourseProviders;
 import com.genesiis.campus.command.ICommand;
@@ -19,6 +21,7 @@ public class PublicCmdFactory implements ICmdFactory {
 		map.put(Operation.LIST_TOP_COURSE_PROVIDERS, new CmdListTopCourseProviders());
 		map.put(Operation.LIST_ALL_COURSE_PROVIDERS, new CmdListMoreCourseProviders());
 		map.put(Operation.LIST_FILTER_SEARCH_COURSE_PROVIDERS, new CmdCourseProviderFilterSearch());
+		map.put(Operation.LIST_FILTER_SEARCH_CATEGORY_TYPES, new CmdListCategoryTypes());
 		
 	}
 	
@@ -34,6 +37,9 @@ public class PublicCmdFactory implements ICmdFactory {
 			command = map.get(o);
 			break;
 		case LIST_FILTER_SEARCH_COURSE_PROVIDERS:
+			command = map.get(o);
+			break;
+		case LIST_FILTER_SEARCH_CATEGORY_TYPES:
 			command = map.get(o);
 			break;
 		default:
