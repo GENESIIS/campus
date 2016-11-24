a2705a91550d476973712e6d833e4efb53ec08cb
Dumani Jayasinghe <dumani@genesiis.com>
2016-11-24 15:59:23 +0530

    c17-provider-criteria-based-filter-search add  LIST_FILTER_SEARCH_CATEGORY_TYPES

--- campus.jar/src/com/genesiis/campus/validation/Operation.java
+++ campus.jar/src/com/genesiis/campus/validation/Operation.java
@@ -6,6 +6,7 @@ package com.genesiis.campus.validation;
 //20161110 DJ c6-list-available-institutes-on-the-view INT-Adjust the implementation to newly changed framework
 //20161115 DJ c17-provider-criteria-based-filter-search set viewMoreCourseProviders.jsp path.
 //20161121 DJ c17-provider-criteria-based-filter-search add  LIST_FILTER_SEARCH_COURSE_PROVIDERS.
+//20161124 DJ c17-provider-criteria-based-filter-search add  LIST_FILTER_SEARCH_CATEGORY_TYPES.
 
 /**
  * Operation class facilitate in binding the CCO
@@ -16,7 +17,8 @@ public enum Operation {
 	BAD_OPERATION("BO", ResponseType.INAPPLICABLE, "commandNotFound.jsp"),	
 	LIST_TOP_COURSE_PROVIDERS("LIST_TOP_COURSE_PROVIDERS",ResponseType.JSP,"dist/partials/topCourseProviders.jsp"),
 	LIST_ALL_COURSE_PROVIDERS("LIST_ALL_COURSE_PROVIDERS",ResponseType.JSON," "),
-	LIST_FILTER_SEARCH_COURSE_PROVIDERS("LIST_FILTER_SEARCH_COURSE_PROVIDERS",ResponseType.JSON," ");
+	LIST_FILTER_SEARCH_COURSE_PROVIDERS("LIST_FILTER_SEARCH_COURSE_PROVIDERS",ResponseType.JSON," "),
+	LIST_FILTER_SEARCH_CATEGORY_TYPES("LIST_FILTER_SEARCH_CATEGORY_TYPES",ResponseType.JSON," ");
 			
 	private final String commandString;
 	private final ResponseType responseType;
