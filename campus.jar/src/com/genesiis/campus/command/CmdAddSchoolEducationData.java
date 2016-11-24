887ccca2e1a2015b8d28b1977d858ebfa3314f19
pabodhaW <pabodha@genesiis.com>
2016-11-24 23:25:01 +0530

    c26-add-student-details: INIT CmdAddSchoolEducationData.java class.

---
+++ campus.jar/src/com/genesiis/campus/command/CmdAddSchoolEducationData.java
@@ -0,0 +1,17 @@
+package com.genesiis.campus.command;
+//20161124 PN c26-add-student-details: INIT CmdAddSchoolEducationData.java class.
+
+import java.sql.SQLException;
+
+import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.util.IDataHelper;
+
+public class CmdAddSchoolEducationData implements ICommand {
+
+	@Override
+	public IView execute(IDataHelper helper, IView view) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
