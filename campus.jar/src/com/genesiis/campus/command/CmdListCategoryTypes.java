2bc95379cffe5080c4ef12b9cac0a703653115c5
Dumani Jayasinghe <dumani@genesiis.com>
2016-11-25 10:02:50 +0530

    c17-provider-criteria-based-filter-search execute method implementation

--- campus.jar/src/com/genesiis/campus/command/CmdListCategoryTypes.java
+++ campus.jar/src/com/genesiis/campus/command/CmdListCategoryTypes.java
@@ -1,10 +1,13 @@
 package com.genesiis.campus.command;
+
 //DJ 20161124 c17-provider-criteria-based-filter-search created CmdListCategoryTypes.java
+//DJ 20161125 c17-provider-criteria-based-filter-search execute method implementation 
 
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import com.genesiis.campus.entity.CourseProviderDAO;
@@ -15,13 +18,14 @@ import com.genesiis.campus.entity.MajorDAO;
 import com.genesiis.campus.entity.model.CourseProviderResultDTO;
 import com.genesiis.campus.util.IDataHelper;
 import com.genesiis.campus.validation.SystemMessage;
+import com.genesiis.campus.validation.UtilityHelper;
 
 import org.apache.log4j.Logger;
 
-public class CmdListCategoryTypes implements ICommand  {
-	
+public class CmdListCategoryTypes implements ICommand {
+
 	static Logger log = Logger.getLogger(CmdListCategoryTypes.class.getName());
-	
+
 	/**
 	 * @author DJ
 	 * @param helper
@@ -34,34 +38,51 @@ public class CmdListCategoryTypes implements ICommand  {
 			Exception {
 		final CourseProviderDAO providerDAO = new CourseProviderDAO();
 		SystemMessage systemMessage = SystemMessage.UNKNOWN;
-		
-		//List categorywise types		
-		//CourseProviderResultDTO dto=new CourseProviderResultDTO(); 
-		int catCode=0;
-		final List <CourseProviderResultDTO> categoryTypes=new CourseProviderDAO().getCategoryWiseTypes(catCode);
-		
-		final Set<Integer> cpTypeSet=new HashSet<>();
-		final Set<Integer> majorSet=new HashSet<>();
-		final Set<Integer> levelSet=new HashSet<>();
-		
-		for(CourseProviderResultDTO dto:categoryTypes){
-			cpTypeSet.add(dto.getCourserProviderType());
-			majorSet.add(dto.getMajor());
-			levelSet.add(dto.getLevel());
+
+		// List categorywise types
+		// CourseProviderResultDTO dto=new CourseProviderResultDTO();
+		int categoryCode = 0;
+		String categoryCodeString = helper.getParameter("categoryCode");
+		if (UtilityHelper.isNotEmpty(categoryCodeString)) {
+			if (UtilityHelper.isInteger(categoryCodeString)) {
+				categoryCode = Integer.parseInt(categoryCodeString);
+			}
 		}
-		
-		//List course provider types for the drop down
-		final Collection<Collection<String>> cpTypeList=new CourseProviderTypeDAO().findById("code");
-		helper.setAttribute("cpTypeList", cpTypeList);
-		
-		//List Majors for the drop down
-		final Collection<Collection<String>> majorList=new MajorDAO().findById("code");
-		helper.setAttribute("majorList", majorList);
-		
-		//List Levels for the drop down			
-		final Collection<Collection<String>> levelList=new LevelDAO().findById("code");
-		helper.setAttribute("levelList", levelList);
-		return null;
+		if (categoryCode > 0) {
+
+			final List<CourseProviderResultDTO> categoryTypes = new CourseProviderDAO()
+					.getCategoryWiseTypes(categoryCode);
+
+			final Set<Integer> cpTypeSet = new HashSet<>();
+			final Set<Integer> majorSet = new HashSet<>();
+			final Set<Integer> levelSet = new HashSet<>();
+
+			for (CourseProviderResultDTO dto : categoryTypes) {
+				cpTypeSet.add(dto.getCourserProviderType());
+				majorSet.add(dto.getMajor());
+				levelSet.add(dto.getLevel());
+			}
+
+			// List course provider types for the drop down
+			if (cpTypeSet != null && !cpTypeSet.isEmpty()) {
+				final Collection<Collection<String>> cpTypeList = new CourseProviderTypeDAO().findByCPTypes(cpTypeSet);
+				helper.setAttribute("cpTypeList", cpTypeList);
+			}
+
+			// List Majors for the drop down
+			if (majorSet != null && !majorSet.isEmpty()) {
+				final Collection<Collection<String>> majorList = new MajorDAO().findById(majorSet);
+				helper.setAttribute("majorList", majorList);
+			}
+
+			// List Levels for the drop down
+			if (levelSet != null && !levelSet.isEmpty()) {
+				final Collection<Collection<String>> levelList = new LevelDAO().findById(levelSet);
+				helper.setAttribute("levelList", levelList);
+			}
+
+		}
+		return view;
 	}
 
 }
