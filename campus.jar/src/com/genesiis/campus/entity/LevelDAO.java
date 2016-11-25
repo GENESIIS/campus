bb7136353159ffb51726c0273b723c7889595c69
Dumani Jayasinghe <dumani@genesiis.com>
2016-11-25 11:25:04 +0530

    c17-provider-criteria-based-filter-search-MP-dj Implement findLevelsByLevelCodes() method

--- campus.jar/src/com/genesiis/campus/entity/LevelDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/LevelDAO.java
@@ -2,6 +2,7 @@ package com.genesiis.campus.entity;
 
 //DJ 20161115 c17-provider-criteria-based-filter-search-MP-dj created LevelDAO.java
 //DJ 20161118 c17-provider-criteria-based-filter-search-MP-dj created getAll() method
+//DJ 20161125 c17-provider-criteria-based-filter-search-MP-dj Implement findLevelsByLevelCodes() method
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,10 +10,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 
 import com.genesiis.campus.util.ConnectionManager;
+import com.genesiis.campus.util.DaoHelper;
 
 
 public class LevelDAO  implements ICrud{
@@ -113,5 +116,52 @@ public class LevelDAO  implements ICrud{
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	/**
+	 * Get all education level list details by level code set
+	 * @param levelCodeSet
+	 * @author DJ
+	 * @return Collection 
+	 */
+	public Collection<Collection<String>> findLevelsByLevelCodes(Set<Integer> levelCodeSet)  throws SQLException{
+		Connection conn=null;
+		PreparedStatement stmt=null;
+		ResultSet rs=null;
+		final Collection<Collection<String>> allLevelList=new ArrayList<Collection<String>>();
+		try {
+			conn=ConnectionManager.getConnection();			
+			final StringBuilder sb =new StringBuilder("SELECT LEVEL.CODE AS LEVELCODE , LEVEL.NAME AS LEVELNAME FROM [CAMPUS].LEVEL LEVEL  ");
+			sb.append(" WHERE LEVEL.ISACTIVE=1 AND LEVEL.CODE IN (");
+			boolean doneOne = false;
+			for (Integer code : levelCodeSet) {
+				if (doneOne) {
+					sb.append(", ");
+				}
+				sb.append(code);
+				doneOne = true;
+			}
+			sb.append(")" );
+			stmt=conn.prepareStatement(sb.toString());
+		    rs=stmt.executeQuery();
+			
+			while (rs.next()) {				
+				final ArrayList<String> singleLevel = new ArrayList<String>();
+				singleLevel.add(rs.getString("LEVELCODE"));				
+				singleLevel.add(rs.getString("LEVELNAME"));				
+				allLevelList.add(singleLevel);
+			}
+		} catch (SQLException sqlException) {
+			log.info("findLevelsByLevelCodes() sqlException" + sqlException.toString());
+			throw sqlException;
+		} catch (Exception e) {
+			log.info("findLevelsByLevelCodes() Exception" + e.toString());
+			throw e;
+		} finally {
+			DaoHelper.cleanup(conn, stmt, rs);
+		}
+		
+		return allLevelList;
+	}
+
 
 }
