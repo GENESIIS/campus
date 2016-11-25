3497517c48bf69fca276e94cb9d1e2933e305bf6
Dumani Jayasinghe <dumani@genesiis.com>
2016-11-25 10:01:52 +0530

    c17-provider-criteria-based-filter-search-MP-dj Implement findByCPTypes() method

--- campus.jar/src/com/genesiis/campus/entity/CourseProviderTypeDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/CourseProviderTypeDAO.java
@@ -2,6 +2,7 @@ package com.genesiis.campus.entity;
 
 //DJ 20161115 c17-provider-criteria-based-filter-search-MP-dj created CourseProviderTypeDAO.java
 //DJ 20161115 c17-provider-criteria-based-filter-search-MP-dj Implement getAll()
+//DJ 20161125 c17-provider-criteria-based-filter-search-MP-dj Implement findByCPTypes() method
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,10 +10,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 
 import com.genesiis.campus.util.ConnectionManager;
+import com.genesiis.campus.util.DaoHelper;
 
 public class CourseProviderTypeDAO implements ICrud{
 	
@@ -113,6 +116,52 @@ public class CourseProviderTypeDAO implements ICrud{
 			Connection conn) throws SQLException, Exception {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	/**
+	 * Get all course provider type details by course provider type codes
+	 * @param cpTypeSet
+	 * @author DJ
+	 * @return Collection 
+	 */
+	public Collection<Collection<String>> findByCPTypes(Set<Integer> cpTypeSet) throws SQLException{
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		final Collection<Collection<String>> courseProviderTypeList = new ArrayList<Collection<String>>();
+		try {
+			conn = ConnectionManager.getConnection();
+			final StringBuilder sb = new StringBuilder(" SELECT CPTYPE.CODE AS CPTYPECODE , CPTYPE.NAME AS CPTYPENAME FROM [CAMPUS].COURSEPROVIDERTYPE CPTYPE ");
+			sb.append("	WHERE CPTYPE.ISACTIVE=1 AND CPTYPE.CODE IN (");
+			boolean doneOne = false;
+			for (Integer code : cpTypeSet) {
+				if (doneOne) {
+					sb.append(", ");
+				}
+				sb.append(code);
+				doneOne = true;
+			}
+			sb.append(" ) ");
+
+			stmt = conn.prepareStatement(sb.toString());
+			rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				final ArrayList<String> singleCPType = new ArrayList<String>();
+				singleCPType.add(rs.getString("CPTYPECODE"));
+				singleCPType.add(rs.getString("CPTYPENAME"));
+				courseProviderTypeList.add(singleCPType);
+			}
+		} catch (SQLException sqlException) {
+			log.info("findByCPTypes() sqlException" + sqlException.toString());
+			throw sqlException;
+		} catch (Exception e) {
+			log.info("findByCPTypes() Exception" + e.toString());
+			throw e;
+		} finally {
+			DaoHelper.cleanup(conn, stmt, rs);
+		}
+
+		return courseProviderTypeList;
 	}
 
 }
