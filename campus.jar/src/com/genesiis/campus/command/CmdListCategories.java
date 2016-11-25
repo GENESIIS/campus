b4c1502bb533ecac0238b56388fb1be09ae3765b
jayani1991 <jayani@genesiis.com>
2016-11-25 14:22:57 +0530

    c7-higher-education-landing-page-MP QA modifications: load category logo using system config enum

--- campus.jar/src/com/genesiis/campus/command/CmdListCategories.java
+++ campus.jar/src/com/genesiis/campus/command/CmdListCategories.java
@@ -2,6 +2,7 @@ package com.genesiis.campus.command;
 
 //20161028 PN c11-criteria-based-filter-search INIT the class and implemented execute() method.
 //20161117 JH c7-higher-education-landing-page-MP code modifications : mx code review 
+//20161125 JH c7-higher-education-landing-page-MP QA modifications: load category logo using system config enum
 
 import java.sql.SQLException;
 import java.util.Collection;
@@ -12,6 +13,7 @@ import com.genesiis.campus.entity.CategoryDAO;
 import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.validation.SystemConfig;
 
 public class CmdListCategories implements ICommand{
 	static Logger log = Logger.getLogger(CmdListCategories.class.getName());
@@ -39,9 +41,13 @@ public class CmdListCategories implements ICommand{
 	
 		
 		ICrud categoryDAO = new CategoryDAO();
+		final String contextDeployCategoryLogoPath = SystemConfig.CATEGORY_LOGO_PATH.getValue1();
+		
 		try {
 			Collection<Collection<String>> categoryCollection = categoryDAO.getAll();
 			iview.setCollection(categoryCollection);
+			
+			helper.setAttribute("categoryLogoPath", contextDeployCategoryLogoPath);
 		} catch (SQLException sqle) {
 			log.info("execute() : SQL Exception " + sqle.toString());
 			throw sqle;
