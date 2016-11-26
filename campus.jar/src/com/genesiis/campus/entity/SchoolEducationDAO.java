42b92266bc8daa566fcae80eba8280e3e02f3a26
pabodhaW <pabodha@genesiis.com>
2016-11-26 22:45:49 +0530

    c26-add-student-details: modified findByIdMethod() method by setting country name to the return collection.

--- campus.jar/src/com/genesiis/campus/entity/SchoolEducationDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/SchoolEducationDAO.java
@@ -1,6 +1,7 @@
 package com.genesiis.campus.entity;
 //20161124 PN c26-add-student-details: INIT SchoolEducationDAO.java class.
 //20161125 PN c26-add-student-details: implemented findByIdMethod().
+//20161126 PN c26-add-student-details: modified findByIdMethod() method by setting country name to the return collection.
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -11,6 +12,7 @@ import java.util.Collection;
 import org.apache.log4j.Logger;
 
 import com.genesiis.campus.command.CmdGetSchoolEducationData;
+import com.genesiis.campus.entity.model.SchoolEducation;
 import com.genesiis.campus.util.ConnectionManager;
 
 public class SchoolEducationDAO implements ICrud{
@@ -57,7 +59,16 @@ public class SchoolEducationDAO implements ICrud{
 				singleEducationList.add(rs.getString("STUDENT"));
 				singleEducationList.add(rs.getString("SCHOOLGRADE"));
 				singleEducationList.add(rs.getString("MAJOR"));
-				singleEducationList.add(rs.getString("COUNTRY"));
+				
+				//Get country name, if in a case to pass the name to dataList
+				ICrud country2dao = new Country2DAO();
+				String countryName = "";
+				Collection<Collection<String>> country = country2dao.findById(Integer.parseInt(rs.getString("COUNTRY")));			
+				for (Collection<String> collection : country) {
+					Object[] cdata = collection.toArray();
+					countryName = (String) cdata[1];
+				}
+				singleEducationList.add(countryName);
 				singleEducationList.add(rs.getString("RESULT"));
 				singleEducationList.add(rs.getString("INDEXNO"));
 				singleEducationList.add(rs.getString("SCHOOL"));
@@ -93,14 +104,81 @@ public class SchoolEducationDAO implements ICrud{
 
 	@Override
 	public int add(Object object, Connection conn) throws SQLException, Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		SchoolEducation data = (SchoolEducation) object;
+		PreparedStatement preparedStatement = null;
+		Connection connection = conn;
+
+		String query = "INSERT INTO [CAMPUS].[SCHOOLEDUCATION] ([STUDENT], [SCHOOLGRADE], [MAJOR], [COUNTRY], "
+				+ "[RESULT], [INDEXNO], [SCHOOL], [ACHIVEDON], [DESCRIPTION], [CRTON], [CRTBY], [MEDIUM]) "
+				+ "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?,(getdate()), ?, ?);";
+		int result = -1;
+
+		try {
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, data.getStudent());
+			preparedStatement.setInt(2, data.getSchoolGrade());
+			preparedStatement.setInt(3, data.getMajor());
+			preparedStatement.setInt(4, data.getCountry());
+			preparedStatement.setInt(5, data.getResult());
+			preparedStatement.setString(6, data.getIndexNo());
+			preparedStatement.setString(7, data.getSchoolName());
+			preparedStatement.setDate(8, data.getAchievedOn());
+			preparedStatement.setString(9, data.getDescription());
+			preparedStatement.setString(10, data.getCrtBy());
+			preparedStatement.setInt(11, data.getMedium());
+			result = preparedStatement.executeUpdate();
+		} catch (SQLException sqle) {
+			log.info("add(): SQLE: " + sqle.toString());
+			throw sqle;
+		} catch (Exception ex) {
+			log.info("add(): E: " + ex.toString());
+			throw ex;
+		} finally {
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+		}
+		return result;
 	}
 
 	@Override
 	public int update(Object object, Connection conn) throws SQLException, Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		SchoolEducation data = (SchoolEducation) object;
+		PreparedStatement preparedStatement = null;
+		Connection connection = conn;
+
+		String query = "UPDATE [CAMPUS].[SCHOOLEDUCATION] SET "
+				+ "[SCHOOLGRADE]=?, [MAJOR]=?, [COUNTRY]=?, [RESULT]=?, [INDEXNO]=?, [SCHOOL]=?, "
+				+ "[ACHIVEDON]=?, [DESCRIPTION]=?, [MODON]=(getdate()), [MODBY]=?, [MEDIUM]=? "
+				+ "WHERE [STUDENT]=?;";
+		int result = -1;
+
+		try {
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, data.getSchoolGrade());
+			preparedStatement.setInt(2, data.getMajor());
+			preparedStatement.setInt(3, data.getCountry());
+			preparedStatement.setInt(4, data.getResult());
+			preparedStatement.setString(5, data.getIndexNo());
+			preparedStatement.setString(6, data.getSchoolName());
+			preparedStatement.setDate(7, data.getAchievedOn());
+			preparedStatement.setString(8, data.getDescription());
+			preparedStatement.setString(9, data.getModBy());
+			preparedStatement.setInt(10, data.getMedium());
+			preparedStatement.setInt(11, data.getStudent());
+			result = preparedStatement.executeUpdate();
+		} catch (SQLException sqle) {
+			log.info("add(): SQLE: " + sqle.toString());
+			throw sqle;
+		} catch (Exception ex) {
+			log.info("add(): E: " + ex.toString());
+			throw ex;
+		} finally {
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+		}
+		return result;
 	}
 
 	@Override
