a6b9bc0d5e0fd122fe339cee64b506c40459cf6e
pabodhaW <pabodha@genesiis.com>
2016-11-28 14:52:13 +0530

    c26-add-student-details: implemented addProfessionalExpForm() method, validateProfessionalExpForm() method and clearProfessionalExpForm() method.

--- campus.war/web/dist/js/student/student-helper.js
+++ campus.war/web/dist/js/student/student-helper.js
@@ -3,6 +3,7 @@
  * details. implemented getAjaxData(response), displayDetails() methods.
  * implemented addEducationDetails(), clearSchoolEducationForm() methods.
  * 20161126 PN c26-add-student-details: implemented validateForm() and modified code in addEducationDetails() method.
+ * 20161128 PN c26-add-student-details: implemented addProfessionalExpForm() method, validateProfessionalExpForm() method and clearProfessionalExpForm() method.
  */
 
 $(document).ready(function() {
@@ -148,7 +149,6 @@ function addEducationDetails() {
 
 /**
  * This is to clear form filled data
- * 
  * @returns void
  */
 function clearSchoolEducationForm() {
@@ -181,3 +181,208 @@ function validateForm(){
 		return true;
 	}
 }
+
+/**
+ * This is to clear form filled data Professional Education.
+ * @returns void
+ */
+function clearProfessionalExpForm() {
+	$('#industryoftheOrganization').get(0).selectedIndex = 0;
+	$('#jobCategory').get(0).selectedIndex = 0;
+	$('#organization').val("");
+	$('#designation').val("");
+	$('#commencedOn').val("");
+	$('#achievedOn').val("");
+	$('#jobDescription').val("");
+}
+
+function validateProfessionalExpForm(){
+	isDropdownSelected(isemptyDropdown(("organization")),"Organization","organizationError");
+	isDropdownSelected(isemptyDropdown(("designation")),"Designation","designationError");
+	isDropdownSelected(isemptyDropdown(("commencedOn")),"Commenced on date","commencedOnError");
+	isDropdownSelected(isemptyDropdown(("achievedOn")),"Achieved on date","achievedOnError");
+	
+	isDropdownSelected(isemptyDropdown('industryoftheOrganization'),"Industry of the Organization","industryoftheOrganizationError");
+	isDropdownSelected(isemptyDropdown('jobCategory'),"Job Category","jobCategoryError");
+
+	if(($('#organizationError').text() != '')||($('#designationError').text() != '')||($('#commencedOnError').text() != '')||
+	($('#achievedOnError').text() != '')||($('#industryoftheOrganizationError').text() != '')||($('#jobCategoryError').text() != '')){
+		return false;
+	}else{
+		return true;
+	}
+}
+
+//Get data and sent to CmdAddSchoolEducationData.java.
+function addProfessionalExpForm() {
+	if(validateProfessionalExpForm()){
+		$('#industryoftheOrganization').val();
+		$('#jobCategory').val();
+		$('#organization').val();
+		$('#designation').val();
+		$('#commencedOn').val();
+		$('#achievedOn').val();
+		$('#jobDescription').val();
+
+	var jsonData = {
+	};
+}
+
+
+/**
+ * All the required functions for Professional Education. 
+ * @param table
+ * @returns
+ */
+//
+//Updates "Select all" control in a data table
+//
+function updateDataTableSelectAllCtrl(table){
+var $table             = table.table().node();
+var $chkbox_all        = $('tbody input[type="checkbox"]', $table);
+var $chkbox_checked    = $('tbody input[type="checkbox"]:checked', $table);
+var chkbox_select_all  = $('thead input[name="select_all"]', $table).get(0);
+
+//If none of the checkboxes are checked
+if($chkbox_checked.length === 0){
+ chkbox_select_all.checked = false;
+ if('indeterminate' in chkbox_select_all){
+    chkbox_select_all.indeterminate = false;
+ }
+
+//If all of the checkboxes are checked
+} else if ($chkbox_checked.length === $chkbox_all.length){
+ chkbox_select_all.checked = true;
+ if('indeterminate' in chkbox_select_all){
+    chkbox_select_all.indeterminate = false;
+ }
+
+//If some of the checkboxes are checked
+} else {
+ chkbox_select_all.checked = true;
+ if('indeterminate' in chkbox_select_all){
+    chkbox_select_all.indeterminate = true;
+ }
+}
+}
+
+$(document).ready(function (){
+//Array holding selected row IDs
+var rows_selected = [];
+var table = $('#example').DataTable({
+ 'ajax': 'https://api.myjson.com/bins/1us28',
+ 'columnDefs': [{
+    'targets': 0,
+    'searchable':false,
+    'orderable':false,
+    'width':'1%',
+    'className': 'dt-body-center',
+    'render': function (data, type, full, meta){
+        return '<input type="checkbox">';
+    }
+ }],
+ 'order': [1, 'asc'],
+ 'rowCallback': function(row, data, dataIndex){
+    // Get row ID
+    var rowId = data[0];
+
+    // If row ID is in the list of selected row IDs
+    if($.inArray(rowId, rows_selected) !== -1){
+       $(row).find('input[type="checkbox"]').prop('checked', true);
+       $(row).addClass('selected');
+    }
+ }
+});
+
+//Handle click on checkbox
+$('#example tbody').on('click', 'input[type="checkbox"]', function(e){
+ var $row = $(this).closest('tr');
+
+ // Get row data
+ var data = table.row($row).data();
+
+ // Get row ID
+ var rowId = data[0];
+
+ // Determine whether row ID is in the list of selected row IDs 
+ var index = $.inArray(rowId, rows_selected);
+
+ // If checkbox is checked and row ID is not in list of selected row IDs
+ if(this.checked && index === -1){
+    rows_selected.push(rowId);
+
+ // Otherwise, if checkbox is not checked and row ID is in list of selected row IDs
+ } else if (!this.checked && index !== -1){
+    rows_selected.splice(index, 1);
+ }
+
+ if(this.checked){
+    $row.addClass('selected');
+ } else {
+    $row.removeClass('selected');
+ }
+
+ // Update state of "Select all" control
+ updateDataTableSelectAllCtrl(table);
+
+ // Prevent click event from propagating to parent
+ e.stopPropagation();
+});
+
+//Handle click on table cells with checkboxes
+$('#example').on('click', 'tbody td, thead th:first-child', function(e){
+ $(this).parent().find('input[type="checkbox"]').trigger('click');
+});
+
+//Handle click on "Select all" control
+$('thead input[name="select_all"]', table.table().container()).on('click', function(e){
+ if(this.checked){
+    $('#example tbody input[type="checkbox"]:not(:checked)').trigger('click');
+ } else {
+    $('#example tbody input[type="checkbox"]:checked').trigger('click');
+ }
+
+ // Prevent click event from propagating to parent
+ e.stopPropagation();
+});
+
+//Handle table draw event
+table.on('draw', function(){
+ // Update state of "Select all" control
+ updateDataTableSelectAllCtrl(table);
+});
+
+//Handle form submission event 
+$('#frm-example').on('submit', function(e){
+ var form = this;
+
+ // Iterate over all selected checkboxes
+ $.each(rows_selected, function(index, rowId){
+    // Create a hidden element 
+    $(form).append(
+        $('<input>')
+           .attr('type', 'hidden')
+           .attr('name', 'id[]')
+           .val(rowId)
+    );
+    table.row('.selected').remove().draw( false );
+ });
+
+ // FOR DEMONSTRATION ONLY     
+ 
+ // Output form data to a console     
+ //$('#example-console').text($(form).serialize());
+ alert($(form).serialize());
+ //console.log("Form submission", $(form).serialize());
+  
+ // Remove added elements
+ $('input[name="id\[\]"]', form).remove();
+  
+ // Prevent actual form submission
+ e.preventDefault();
+});
+});
+/**
+ * --------------------------------------------------------------------------------------------------------------------------------------------
+ * --------------------------------------------------------------------------------------------------------------------------------------------
+ */
