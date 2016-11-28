188761c6b6a7309b11eca4f69f57e4809d9dbab5
Anuradha Silva <anuradha@genesiis.com>
2016-11-28 18:21:59 +0530

    C19-student-login-without-using-third-party-application DAO class coding wip

--- campus.jar/src/com/genesiis/campus/entity/StudentLoginDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/StudentLoginDAO.java
@@ -1,10 +1,20 @@
 package com.genesiis.campus.entity;
+
 //20161123 AS C19-student-login-without-using-third-party-application-test-as StudentLoginDAO class created.
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 
-public class StudentLoginDAO implements ICrud{
+import org.apache.log4j.Logger;
+
+import com.genesiis.campus.command.CmdStudentLogin;
+import com.genesiis.campus.entity.model.Student;
+
+public class StudentLoginDAO implements ICrud {
+
+	static Logger log = Logger.getLogger(StudentLoginDAO.class.getName());
 
 	@Override
 	public int add(Object object) throws SQLException, Exception {
@@ -27,7 +37,29 @@ public class StudentLoginDAO implements ICrud{
 	@Override
 	public Collection<Collection<String>> findById(Object code)
 			throws SQLException, Exception {
-		// TODO Auto-generated method stub
+		Collection<Collection<String>> dataCollection = new ArrayList<Collection<String>>();
+		Connection conn = null;
+		PreparedStatement preparedStatement = null;
+		String message = "Sorry, you are not a registered user! Please sign up first";
+		final Student student = (Student) code;
+		try {
+		log.info(student.getEmail() + ""+ student.getPassword());
+
+		} catch (Exception exception) {
+			log.error("findById(Object code):  exception"
+					+ exception.toString());
+			throw exception;
+		} finally {
+
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+
+		}
+
 		return null;
 	}
 
@@ -57,6 +89,11 @@ public class StudentLoginDAO implements ICrud{
 			Exception {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	public static Student studentLogin(Student bean) {
+
+		return bean;
 	}
 
 }
