2c001989c5ba23d538ebfb76c2e903aec3703f32
Anuradha Silva <anuradha@genesiis.com>
2016-11-28 18:21:32 +0530

    C19-student-login-without-using-third-party-application command class coding wip

--- campus.jar/src/com/genesiis/campus/command/CmdStudentLogin.java
+++ campus.jar/src/com/genesiis/campus/command/CmdStudentLogin.java
@@ -1,23 +1,78 @@
 package com.genesiis.campus.command;
+
 //20161123 AS C19-student-login-without-using-third-party-application-test-as CmdStudentLogin class created.
+//20161128 AS C19-student-login-without-using-third-party-application-test-as extractFromJason 
 import java.sql.SQLException;
+import java.util.Collection;
 
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.entity.StudentDAO;
+import com.genesiis.campus.entity.StudentLoginDAO;
+import com.genesiis.campus.entity.model.Student;
+import com.genesiis.campus.entity.model.StudentProgrammeInquiry;
 import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.validation.Validator;
+import com.google.gson.Gson;
 
 import org.apache.log4j.Logger;
 
-public class CmdStudentLogin implements ICommand{
+public class CmdStudentLogin implements ICommand {
 
 	static Logger log = Logger.getLogger(CmdStudentLogin.class.getName());
+	
+	private Student data;
+	private Collection<Collection<String>> dataCollection = null;
+	
+	
+	
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
+		String message = "Unsuccessfull";
 
-		log.info("testing 1233455678");
+		String gsonData = helper.getParameter("jsonData");
+		data = getStudentdetails(gsonData);
 
-		return null;
+		log.info("testing Data ................ : " + data);
+
+		String validateResult = Validator.validateLogin(data);
+		log.info(validateResult);
+		if (validateResult.equalsIgnoreCase("True")) {
+			log.info(validateResult);
+			final StudentLoginDAO loginDAO = new StudentLoginDAO();
+			 	dataCollection = loginDAO.findById(data);
+		}
+
+		return view;
+	}
+
+	/**
+	 * extract data fromm json object and assign to StudentProgrammeInquiry
+	 * object
+	 * 
+	 * @author anuradha
+	 * @param gsonData
+	 * @return Student object
+	 */
+
+	private Student getStudentdetails(String gsonData) {
+		Student student = (Student) extractFromJason(gsonData);
+
+		return student;
+	}
+
+	public Object extractFromJason(String gsonData) {
+		Gson gson = new Gson();
+		String message = "";
+		Student student = null;
+		try {
+			student = gson.fromJson(gsonData, Student.class);
+
+		} catch (Exception exception) {
+			log.error("extractFromJason(): " + exception.toString());
+			throw exception;
+		}
+		return student;
 	}
 
 }
