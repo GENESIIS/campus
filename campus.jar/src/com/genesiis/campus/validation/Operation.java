5bfdc39bc8512fd670df4b8a9d670c10bc07c729
pabodhaW <pabodha@genesiis.com>
2016-11-30 23:12:03 +0530

    c27-upload-user-image: GET_USER_PROFILE enum values are changed.

--- campus.jar/src/com/genesiis/campus/validation/Operation.java
+++ campus.jar/src/com/genesiis/campus/validation/Operation.java
@@ -6,13 +6,12 @@ package com.genesiis.campus.validation;
 //20161101 PN c11-criteria-based-filter-search: added LIST_INSTITUTE_DATA attribute.
 //20161109 PN c11-criteria-based-filter-search changed ResponseType.JSP into ResponseType.JSON. 
 //20161121 PN c27-upload-user-image: added UPLOAD_USER_PROFILE attribute to operation Enum.
-
+//20161130 PN c27-upload-user-image: GET_USER_PROFILE enum values are changed.
 /**
  * Operation class facilitate in binding the CCO
  * 
  */
-public enum Operation {
-	
+public enum Operation {	
 	GET_SEARCH_DATA("GET_SEARCH_DATA", ResponseType.JSON, "index.jsp"),
 	BAD_OPERATION("BO", ResponseType.INAPPLICABLE, "commandNotFound.jsp"),
 	LIST_CATEGORY_DATA("LIST_CATEGORY_DATA", ResponseType.JSON, ""),
@@ -22,7 +21,7 @@ public enum Operation {
 	LIST_DISTRICT_DATA("LIST_DISTRICT_DATA", ResponseType.JSON, "index.jsp"),
 	LIST_INSTITUTE_DATA("LIST_INSTITUTE_DATA", ResponseType.JSON, "index.jsp"),
 	UPLOAD_USER_PROFILE("UUP", ResponseType.JSON, "index.jsp"), 
-	GET_USER_PROFILE("GUP", ResponseType.JSON, "index.jsp");
+	GET_USER_PROFILE("GUP", ResponseType.JSP, "dist/partials/student/student-dashboard.jsp");
 			
 	private final String commandString;
 	private final ResponseType responseType;
