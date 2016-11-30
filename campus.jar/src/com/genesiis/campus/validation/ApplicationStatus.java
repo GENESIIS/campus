e9e485c00f640e4e3cfb8f5088f68a17d3abb173
Dumani Jayasinghe <dumani@genesiis.com>
2016-11-30 16:44:46 +0530

    c17-provider-criteria-based-filter-search Implement ApplicationStatus.java

---
+++ campus.jar/src/com/genesiis/campus/validation/ApplicationStatus.java
@@ -0,0 +1,31 @@
+package com.genesiis.campus.validation;
+
+//20161124 JH c7-higher-education-landing-page-MP ApplicationStatus.java created
+
+/**
+* ApplicationStatus enum created to manage all the status values
+* of the application. 
+* @author jayani
+*
+*/
+public enum ApplicationStatus {
+
+	ACTIVE(1),
+	INACTIVE(2),
+	PENDING(3),
+	EXPIRED(4);	
+	
+	private final int statusValue;
+
+	ApplicationStatus(final int statusValue){
+		this.statusValue = statusValue;
+	}
+
+	/**
+	 * @return the statusValue
+	 */
+	public int getStatusValue() {
+		return statusValue;
+	}
+
+}
