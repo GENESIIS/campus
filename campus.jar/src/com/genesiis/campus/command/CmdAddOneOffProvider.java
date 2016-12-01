f92c909a4e47bb7eba98b330d2ebc62b55d0b2cc
jayani1991 <jayani@genesiis.com>
2016-12-02 00:03:58 +0530

    c39-add-course-provider add one off course provider command class coding

--- campus.jar/src/com/genesiis/campus/command/CmdAddOneOffProvider.java
+++ campus.jar/src/com/genesiis/campus/command/CmdAddOneOffProvider.java
@@ -5,6 +5,7 @@ package com.genesiis.campus.command;
 //20161130 JH c39-add-course-provider execute method coding wip
 
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import com.genesiis.campus.entity.ICrud;
@@ -13,6 +14,8 @@ import com.genesiis.campus.entity.OneOffCourseProviderDAO;
 import com.genesiis.campus.entity.model.CourseProvider;
 import com.genesiis.campus.util.IDataHelper;
 import com.genesiis.campus.validation.SystemMessage;
+import com.genesiis.campus.validation.ApplicationStatus;
+import com.genesiis.campus.validation.AccountType;
 
 import org.apache.log4j.Logger;
 
@@ -50,6 +53,57 @@ public class CmdAddOneOffProvider implements ICommand {
 		String systemMessage = null;
 
 		try {
+			
+
+			int pStatus = 0;
+			String expireDate = helper.getParameter("expirationDate");
+			String providerStatus = helper.getParameter("providerStatus");
+			if(providerStatus =="active"){
+				pStatus = ApplicationStatus.ACTIVE.getStatusValue();
+			}
+			if(providerStatus =="inactive"){
+				pStatus = ApplicationStatus.INACTIVE.getStatusValue();
+			}
+			if(providerStatus =="pending"){
+				pStatus = ApplicationStatus.PENDING.getStatusValue();
+			}
+			
+			SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+	        Date parsed = format.parse(expireDate);
+	        java.sql.Date sql = new java.sql.Date(parsed.getTime());
+	        
+			courseProvider.setShortName(helper.getParameter("shortName"));
+			courseProvider.setName(helper.getParameter("providerName"));
+			courseProvider.setDescription(helper.getParameter("aboutMe"));
+			courseProvider.setCourseInquiryEmail(helper.getParameter("inquiryMail"));
+			courseProvider.setLandPhoneCountryCode(helper.getParameter("country"));
+			courseProvider.setLandPhoneAreaCode(helper.getParameter("areaCode"));
+			courseProvider.setLandPhoneNo(helper.getParameter("land1"));
+			courseProvider.setMobilePhoneCountryCode(helper.getParameter("country"));
+			courseProvider.setMobilePhoneNetworkCode(helper.getParameter("networkCode"));
+			courseProvider.setMobilePhoneNumber(helper.getParameter("mobile"));
+			courseProvider.setExpirationDate(sql);
+			
+			courseProvider.setLandPhpneNo2(helper.getParameter("land2"));
+			courseProvider.setFaxNo(helper.getParameter("fax"));
+			courseProvider.setSpeciality(helper.getParameter("specialFeatures"));
+			courseProvider.setWeblink(helper.getParameter("webLink"));
+			courseProvider.setFacebookURL(helper.getParameter("facebook"));
+			courseProvider.setTwitterURL(helper.getParameter("twitter"));
+			courseProvider.setMyspaceURL(helper.getParameter("mySpace"));
+			courseProvider.setLinkedinURL(helper.getParameter("linkdedIn"));
+			courseProvider.setInstagramURL(helper.getParameter("instagram"));
+			courseProvider.setViberNumber(helper.getParameter("viber"));
+			courseProvider.setWhatsappNumber(helper.getParameter("whatsapp"));
+			courseProvider.setAddress1(helper.getParameter("address1"));
+			courseProvider.setAddress2(helper.getParameter("address2"));
+			courseProvider.setAddress3(helper.getParameter("address3"));
+			
+			courseProvider.setGeneralEmail(helper.getParameter("generalEmail"));
+			courseProvider.setCourseProviderType(AccountType.ONE_OFF_COURSE_PROVIDER.getTypeValue());
+//			courseProvider.setTutorRelated(false);
+			courseProvider.setAdminAllowed(true);
+			courseProvider.setCourseProviderStatus(pStatus);
 //			Date date = new Date();
 //			log.info(">>>>>>>>>>>" + date.toString());
 //
