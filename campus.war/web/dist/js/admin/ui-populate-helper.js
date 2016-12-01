6f478d94f4d3c478efbf4af7a96460ad0b18957c
jayani1991 <jayani@genesiis.com>
2016-12-01 21:25:38 +0530

    c39-add-course-provider java script UI helper class coding wip

--- campus.war/web/dist/js/admin/ui-populate-helper.js
+++ campus.war/web/dist/js/admin/ui-populate-helper.js
@@ -8,13 +8,49 @@
 $(document).ready(function() {
 	arrangeUI();
 });
+window.message = null;
 
 function arrangeUI(){
-	document.getElementById('accountInfo').style.display = "none";
 	document.getElementById("logoPanel").style.display = "none";
 
 }
 
 function getProviderType(){
-	
+
+	var commandCode;
+	var radioValue = $("input[name='featured-oneoff']:checked").val();
+	if (radioValue != "") {
+		if (radioValue == "featured") {
+			commandCode = "ADD_FEATURED_COURSE_PROVIDER";
+		}
+		if (radioValue == "one-off") {
+			commandCode = "ADD_ONE_OFF_COURSE_PROVIDER";
+		}
+		if (radioValue == "Tutor") {
+			commandCode = "ADD_ONE_OFF_COURSE_PROVIDER";
+		}
+
+		$
+				.ajax({
+					url : '/AdminController',
+					method : 'POST',
+					data : {
+						'CCO' : commandCode
+					},
+					dataType : "json",
+					async : false,
+					success : function(response) {
+
+						if (response !== undefined && response !== null) {
+							window.message = response.userMessage;
+
+							alert("success" + window.message);
+							document.getElementById("logoPanel").style.display = "block";
+							document.getElementById("basicForm").style.display = "none";
+							
+						}
+					},
+				});
+	}
+
 }
