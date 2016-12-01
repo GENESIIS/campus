672d61d6f05b01ebabc4bfc352849c9c73142715
dushanthacb <dushantha@genesiis.com>
2016-12-01 23:35:59 +0530

    C18-student-signup-without-using-third-party-application-test-dn removed unnecessary comments as per CREV

--- campus.jar/src/com/genesiis/campus/command/CmdSignUpWithoutThirdParty.java
+++ campus.jar/src/com/genesiis/campus/command/CmdSignUpWithoutThirdParty.java
@@ -1,4 +1,6 @@
 package com.genesiis.campus.command;
+//20161128 DN C18-student-signup-without-using-third-party-application-test-dn created CmdSignUpWithoutThirdParty.java
+//20161201 DN C18-student-signup-without-using-third-party-application-test-dn removed unnecessary comments as per CREV
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -19,15 +21,16 @@ import com.genesiis.campus.validation.SystemMessage;
 import com.genesiis.campus.validation.Validatory;
 
 
-
-
-
 import java.sql.Connection;
-
 import com.google.gson.Gson;
-
 import org.apache.log4j.Logger;
 
+/**
+ * this class manages the student sign ups to the system without
+ * third party application, further it implements ICommand interface
+ * @author DN
+ *
+ */
 public class CmdSignUpWithoutThirdParty implements ICommand{
 
 	static Logger log = Logger.getLogger(CmdSignUpWithoutThirdParty.class.getName());
@@ -165,95 +168,7 @@ public Object extractDumyObjectFrom(String gsonData) {
 	return rowStudentObj;
 }
 
-/*
- * RowStudentForJason  class maps the JSON data to raw Student object
- * till induce the brought in data from client side to proper  business model object
- * e.g. Student, 
- * @author dushantha DN
- *
- */
-//	public class RowStudentForJason{
-//		String firstName ;
-//		String lastName  ;
-//		String gender	;
-//		String email;
-//		String mobilePhoneNo;
-//		String pathway	;
-//		String userName	;
-//		String passWord	;
-//		String confirmPw;
-//		boolean isPolicyConfirm	;
-//		
-//		public RowStudentForJason(){
-//			this(Object.class, 0L);
-//		}
-//		
-//		public RowStudentForJason(Class<Object> class1, long l) {
-//			
-//		}
-//
-//		public String getFirstName() {
-//			return firstName;
-//		}
-//		public void setFirstName(String firstName) {
-//			this.firstName = firstName;
-//		}
-//		public String getLastName() {
-//			return lastName;
-//		}
-//		public void setLastName(String lastName) {
-//			this.lastName = lastName;
-//		}
-//		public String getGender() {
-//			return gender;
-//		}
-//		public void setGender(String gender) {
-//			this.gender = gender;
-//		}
-//		public String getEmail() {
-//			return email;
-//		}
-//		public void setEmail(String email) {
-//			this.email = email;
-//		}
-//		public String getMobilePhoneNo() {
-//			return mobilePhoneNo;
-//		}
-//		public void setMobilePhoneNo(String mobilePhoneNo) {
-//			this.mobilePhoneNo = mobilePhoneNo;
-//		}
-//		public String getPathway() {
-//			return pathway;
-//		}
-//		public void setPathway(String pathway) {
-//			this.pathway = pathway;
-//		}
-//		public String getUserName() {
-//			return userName;
-//		}
-//		public void setUserName(String userName) {
-//			this.userName = userName;
-//		}
-//		public String getPassWord() {
-//			return passWord;
-//		}
-//		public void setPassWord(String passWord) {
-//			this.passWord = passWord;
-//		}
-//		public String getConfirmPw() {
-//			return confirmPw;
-//		}
-//		public void setConfirmPw(String confirmPw) {
-//			this.confirmPw = confirmPw;
-//		}
-//		public boolean getIsPolicyConfirm() {
-//			return isPolicyConfirm;
-//		}
-//		public void setIsPolicyConfirm(boolean isPolicyConfirm) {
-//			this.isPolicyConfirm = isPolicyConfirm;
-//		}
-//	}
-	
+
 	/*
 	 * systemMessage() handles the system Messages according to
 	 * the state of the status passed in
