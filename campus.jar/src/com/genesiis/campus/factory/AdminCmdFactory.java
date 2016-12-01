23cad9f3a05fc216b0a6615c258238f4dbe8ae0a
jayani1991 <jayani@genesiis.com>
2016-12-01 21:27:24 +0530

    c39-add-course-provider added new map for ADD_ONE_OFF_COURSE_PROVIDER

--- campus.jar/src/com/genesiis/campus/factory/AdminCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/AdminCmdFactory.java
@@ -3,8 +3,10 @@ package com.genesiis.campus.factory;
 //20161122 JH c39-add-course-provider AdminCmdFactory.java command factory class created
 //20161122 JH c39-add-course-provider implemented ICmdFactory
 //20161122 JH c39-add-course-provider added new map for ADD_FEATURED_COURSE_PROVIDER
+//20161201 JH c39-add-course-provider added new map for ADD_ONE_OFF_COURSE_PROVIDER
 
 import com.genesiis.campus.command.CmdAddFeaturedProvider;
+import com.genesiis.campus.command.CmdAddOneOffProvider;
 import com.genesiis.campus.command.CmdListCategories;
 import com.genesiis.campus.command.CmdListCategoryLandingPage;
 import com.genesiis.campus.command.ICommand;
@@ -18,6 +20,7 @@ public class AdminCmdFactory implements ICmdFactory{
 		map.put(Operation.LIST_CATEGORY_LANDING_PAGE, new CmdListCategoryLandingPage());
 		map.put(Operation.LIST_CATEGORY_DATA, new CmdListCategories());
 		map.put(Operation.ADD_FEATURED_COURSE_PROVIDER, new CmdAddFeaturedProvider());
+		map.put(Operation.ADD_ONE_OFF_COURSE_PROVIDER, new CmdAddOneOffProvider());
 
 	}
 
@@ -30,6 +33,9 @@ public class AdminCmdFactory implements ICmdFactory{
 			command = map.get(o);
 			break;
 		case LIST_CATEGORY_DATA :
+			command = map.get(o);
+			break;
+		case ADD_ONE_OFF_COURSE_PROVIDER:
 			command = map.get(o);
 			break;
 		default:
