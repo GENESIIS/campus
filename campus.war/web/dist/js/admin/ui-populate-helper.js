cd89500f5965f64bb07659c2152cf4c940c7aac4
jayani1991 <jayani@genesiis.com>
2016-12-01 17:19:41 +0530

    c39-add-course-provider javascript ui-populate-helper class created to help client validations and UI handling

---
+++ campus.war/web/dist/js/admin/ui-populate-helper.js
@@ -0,0 +1,20 @@
+/**
+ * This Java script file used to help with admin functions
+ * author JH
+ * 20161201
+ * 
+ */
+
+$(document).ready(function() {
+	arrangeUI();
+});
+
+function arrangeUI(){
+	document.getElementById('accountInfo').style.display = "none";
+	document.getElementById("logoPanel").style.display = "none";
+
+}
+
+function getProviderType(){
+	
+}
