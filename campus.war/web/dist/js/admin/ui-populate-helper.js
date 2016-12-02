140bc065269bcf88318a7fa7d07de7471552effc
jayani1991 <jayani@genesiis.com>
2016-12-02 14:05:58 +0530

    c39-add-course-provider javascript class coding wip

--- campus.war/web/dist/js/admin/ui-populate-helper.js
+++ campus.war/web/dist/js/admin/ui-populate-helper.js
@@ -1,82 +1,58 @@
 /**
- * This Java script file used to help with admin functions
- * author JH
- * 20161201
+ * This Java script file used to help with admin functions author JH 20161201
  * 
  */
 
 $(document).ready(function() {
 	arrangeUI();
 });
-window.message = document.getElementById("usermessage").value;
+var message = document.getElementById("usermessage").value;
 
-function arrangeUI(){
+function arrangeUI() {
 	document.getElementById("logoPanel").style.display = "none";
 
 }
 
-function getProviderType(){
+function getProviderType() {
 
-	var commandCode;
 	var status = false;
 	var radioValue = $("input[name='featured-oneoff']:checked").val();
 	var statusValue = $("input[name='providerStatus']:checked").val();
-	if (radioValue != "") {
-		if (radioValue == "featured") {
-			commandCode = "ADD_FEATURED_COURSE_PROVIDER";
-		}
-		if (radioValue == "one-off") {
-			commandCode = "ADD_ONE_OFF_COURSE_PROVIDER";
-		}
-		if (radioValue == "Tutor") {
-			commandCode = "ADD_ONE_OFF_COURSE_PROVIDER";
-		}
-		alert(commandCode);
-		status = true;
-
-	}else{
-		window.messgae ="please select a course provider type";
-	}
-	var courseProviderStatus ;
-	if(statusValue != ""){
-		if(statusValue == "active"){
-			courseProviderStatus = "active";
-		}
-		if(statusValue == "inactive"){
-			courseProviderStatus = "inactive";
-		}if(statusValue == "pending"){
-			courseProviderStatus = "pending";
-		}
-		status = true;
-	}else{
-		document.getElementById("").innerHTML = "please select the ";
-	}
+//	if (radioValue != "") {
+//		if (radioValue == "featured") {
+//			commandCode = "ADD_FEATURED_COURSE_PROVIDER";
+//		}
+//		if (radioValue == "one-off") {
+//			commandCode = "ADD_ONE_OFF_COURSE_PROVIDER";
+//		}
+//		alert(commandCode);
+//
+//	} else {
+//		messgae = "please select a course provider type";
+//	}
 	
-	if (status == true) {
-		alert("true");
-		$
-				.ajax({
-					url : '/AdminController',
-					method : 'POST',
-					data : {
-						'CCO' : commandCode
-					},
-					dataType : "json",
-					async : false,
-					success : function(response) {
+	var form = $('#basicForm');
+	var formDate = $(form).serialize();
+		$.ajax({
+			url : '/AdminController',
+			method : 'POST',
+			data : formData ,
+			dataType : "json",
+			async : false,
+			success : function(response) {
 
-						if (response !== undefined && response !== null) {
-							window.message = response.userMessage;
+				if (response !== undefined && response !== null) {
+					message = response.userMessage;
+					var registerId = response.registerId;
+					
+					alert("success" + message + registerId);
+					document.getElementById("usermessage").html = message;
+					// document.getElementById("logoPanel").style.display
+					// = "block";
+					// document.getElementById("basicForm").style.display
+					// = "none";
 
-							alert("success" + window.message);
-							document.getElementById("usermessage").html = "success";
-							// document.getElementById("logoPanel").style.display
-							// = "block";
-							// document.getElementById("basicForm").style.display
-							// = "none";
-
-						}
-					},
-				});
-	}
+				}
+			},
+		});
 }
