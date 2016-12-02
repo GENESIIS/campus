ede879d85db404ffff1afff423080c367a77dba6
dushanthacb <dushantha@genesiis.com>
2016-12-02 14:54:06 +0530

    C18-student-signup-without-using-third-party-application-dn isStringContainsAlphaNumericAndUnderscore() created for username length validation

--- campus.jar/src/com/genesiis/campus/validation/PrevalentValidation.java
+++ campus.jar/src/com/genesiis/campus/validation/PrevalentValidation.java
@@ -6,10 +6,14 @@ package com.genesiis.campus.validation;
 //20161123 DN c10-contacting-us-page-MP isValidPhoneNumber() regular expression changed to 
 //cater more phone number styles with spaces in between.
 //20161124 DN c10-contacting-us-page-MP  changed the regular expression to accept only +(2 digit)(9-digit)
-//20161128 DN isValidPhoneNumber(),validateEmail() and isNotEmpty() methods changed to use trim() on passing parameter.
+//20161128 DN C18-student-signup-without-using-third-party-application-test-dn isValidPhoneNumber(),validateEmail() and isNotEmpty()
+//methods changed to use trim() on passing parameter.
+//20161202 DN C18-student-signup-without-using-third-party-application-test-dn isStringContainsAlphaNumericAndUnderscore() created
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+
 
 
 import org.apache.log4j.Logger;
@@ -95,9 +99,7 @@ public class PrevalentValidation implements Validatory {
 		String spacelessString = numberToValidate.replaceAll("\\s+","");
 			if((numberToValidate!=null) | (numberToValidate.trim()!="")){
 				Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^(00\\d{2}|\\+\\d{2}|0)?\\d{9}$");
-				//Pattern VALID_EMAIL_ADDRESS_REGEX_LESS = Pattern.compile("^\\d{7}$");
 				Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(spacelessString);
-				//Matcher matcherLess = VALID_EMAIL_ADDRESS_REGEX_LESS.matcher(spacelessString);
 				validPhoneNumber= (matcher.find());
 				
 				if(!validPhoneNumber){
@@ -109,6 +111,30 @@ public class PrevalentValidation implements Validatory {
 			}
 			return validPhoneNumber;
 	}
+	
+	/**
+	 * isStringContainsAlphaNumericAndUnderscore method checks if the 
+	 * parameter string contains alphanumeric and _ combination with 
+	 * minimum length as agreed. there is no restriction for maximum 
+	 * length that could be.
+	 * @param testableString
+	 * @param requiredMinimumLength
+	 * @return boolean if the test passes else false
+	 * @throws Exception
+	 */
+	
+	@Override
+	public boolean isStringContainsAlphaNumericAndUnderscore(String testableString, int requiredMinimumLength)throws Exception{
+		boolean isValidString=false;
+		if(testableString!=""|testableString!=null){
+			Pattern validPattern = Pattern.compile("^([a-zA-Z]+)([a-zA-Z0-9_]){"+requiredMinimumLength+",}$");
+			Matcher matcher = validPattern.matcher(testableString);
+			isValidString = matcher.find();
+		}
+		throwCustomError(isValidString," User Name Contains Bad Characters Or Less Than Required Length!");
+		return isValidString;
+	}
+	
 	
 	/*
 	 * throwCustomError class accepts two parameters and based on the test falsity Exception
