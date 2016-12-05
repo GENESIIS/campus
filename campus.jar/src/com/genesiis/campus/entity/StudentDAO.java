a85eecaef809dd7dec1d3d568a13da6cddaeb356
pabodhaW <pabodha@genesiis.com>
2016-12-05 22:45:28 +0530

    c26-add-student-details: update(Connection con, Object object) method to update student personal details.

--- campus.jar/src/com/genesiis/campus/entity/StudentDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/StudentDAO.java
@@ -3,6 +3,7 @@ package com.genesiis.campus.entity;
 //20161121 MM c25-student-login-create-dashboard-MP-mm INIT - Initialised file
 //20161122 MM c25-student-login-create-dashboard-MP-mm Added code to retrieve more columns from the result set
 //20161122 MM c25-student-login-create-dashboard-MP-mm Fixed logger class import issue
+//20161205 PN c26-add-student-details: update(Connection con, Object object) method to update student personal details. 
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -178,10 +179,64 @@ public class StudentDAO implements ICrud {
 	}
 
 	@Override
-	public int update(Object object, Connection conn) throws SQLException,
+	public int update(Object object, Connection con) throws SQLException,
 			Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		Student student = (Student) object;
+		Connection conn = con;
+		PreparedStatement stmt = null;
+		int isUpdated = 0;
+
+		try {
+			conn = ConnectionManager.getConnection();
+			String query ="UPDATE [CAMPUS].[STUDENT] SET [FIRSTNAME] = ?, [MIDDLENAME] = ?, "
+					+ "[LASTNAME] = ?, [DATEOFBIRTH] = ?, [GENDER] = ?, [EMAIL] = ?, [LANDPHONECOUNTRYCODE] = ?, "
+					+ "[LANDPHONENO] = ?, [MOBILEPHONECOUNTRYCODE] = ?, [MOBILEPHONENETWORKCODE] = ?, [MOBILEPHONENO] = ?, "
+					+ "[DESCRIPTION] = ?, [FACEBOOKURL] = ?, [TWITTERURL] = ?, [MYSPACEURL] = ?, [LINKEDINURL] = ?, "
+					+ "[INSTAGRAMURL] = ?, [VIBERNUMBER] = ?, [WHATSAPPNUMBER] = ?, [ADDRESS1] = ?, [TOWN] = ?, "
+					+ "[MODON]=(getdate()), [MODBY] = ? "
+					+ "WHERE [CODE] = ?;";
+
+			stmt = conn.prepareStatement(query);	
+			stmt.setString(1, student.getFirstName());
+			stmt.setString(2, student.getMiddleName());
+			stmt.setString(3, student.getLastName());
+			stmt.setDate(4, student.getDateOfBirth());
+			stmt.setInt(5, student.getGender());
+			stmt.setString(6, student.getEmail());
+			stmt.setString(7, student.getLandPhoneCountryCode());
+			stmt.setString(8, student.getLandPhoneNo());
+			stmt.setString(9, student.getLandPhoneCountryCode());
+			stmt.setString(10, student.getMobilePhoneNo().substring(0, 3));
+			stmt.setString(11, student.getMobilePhoneNo().substring(3, student.getMobilePhoneNo().length()-1));
+			stmt.setString(12, student.getDescription());
+			stmt.setString(13, student.getFacebookUrl());
+			stmt.setString(14, student.getTwitterUrl());
+			stmt.setString(15, student.getMySpaceUrl());
+			stmt.setString(16, student.getLinkedInUrl());
+			stmt.setString(17, student.getInstagramUrl());
+			stmt.setString(18, student.getViberNumber());
+			stmt.setString(19, student.getWhatsAppNumber());
+			stmt.setString(20, student.getAddress1());
+			stmt.setString(21, student.getTown());
+			stmt.setString(22, student.getModBy());
+			stmt.setInt(23, student.getCode());		
+			stmt.executeUpdate();
+			isUpdated = 1;
+		} catch (SQLException sqlException) {
+			Log.info("update(Object object): SQLE " + sqlException.toString());
+			throw sqlException;
+		} catch (Exception e) {
+			Log.info("update(Object object): E " + e.toString());
+			throw e;
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		}
+		return isUpdated;
 	}
 
 	@Override
