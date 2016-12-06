809ba46016ceefaff1acc3d589b63268d6559a37
dushanthacb <dushantha@genesiis.com>
2016-12-06 18:00:10 +0530

    C18-student-signup-without-using-third-party-application DISPLAY_TOWN_DATA added

--- campus.jar/src/com/genesiis/campus/factory/StudentCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/StudentCmdFactory.java
@@ -4,7 +4,7 @@ package com.genesiis.campus.factory;
 // 	to the class and bound with CmdSignUpWithoutThirdParty.java
 //20161205 DN C18-student-signup-without-using-third-party-application-dn add DISPLAY_PREREQUISITE_DATA
 //and bind CmdListPrereqSignUpWithoutThirdParty()
-
+//20161205 DN C18-student-signup-without-using-third-party-application-dn DISPLAY_TOWN_DATA added
 
 import com.genesiis.campus.command.CmdListPrereqSignUpWithoutThirdParty;
 import com.genesiis.campus.command.CmdSignUpWithoutThirdParty;
@@ -16,7 +16,7 @@ public class StudentCmdFactory implements ICmdFactory{
 	static {
 	map.put(Operation.SIGN_UP_WO_THRD_PARTY, new CmdSignUpWithoutThirdParty());	
 	map.put(Operation.DISPLAY_PREREQUISITE_DATA, new CmdListPrereqSignUpWithoutThirdParty());
-	
+	map.put(Operation.DISPLAY_TOWN_DATA,new CmdListPrereqSignUpWithoutThirdParty());
 	}
 
 	@Override
@@ -28,6 +28,9 @@ public class StudentCmdFactory implements ICmdFactory{
 			command = map.get(o);
 			break;
 		case DISPLAY_PREREQUISITE_DATA:
+			command = map.get(o);
+			break;
+		case DISPLAY_TOWN_DATA:
 			command = map.get(o);
 			break;
 		default:
