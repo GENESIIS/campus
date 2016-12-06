c7040e5f64cbfa5f5993cd96e19b7c5fe93175d9
pabodhaW <pabodha@genesiis.com>
2016-12-06 22:11:33 +0530

    c26-add-student-details modified execute() method to get student's existing skill data.

--- campus.jar/src/com/genesiis/campus/command/CmdGetSkillDetails.java
+++ campus.jar/src/com/genesiis/campus/command/CmdGetSkillDetails.java
@@ -1,7 +1,8 @@
 package com.genesiis.campus.command;
 
 //20161206 PN c26-add-student-details INIT CmdGetSkillDetails.java
-//        PN c26-add-student-details implement execute() method.
+//         PN c26-add-student-details implement execute() method.
+//		   PN c26-add-student-details modified execute() method to get student's existing skill data.
 
 import java.sql.SQLException;
 import java.util.Collection;
@@ -9,18 +10,24 @@ import java.util.Collection;
 import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.entity.SkillDAO;
+import com.genesiis.campus.entity.StudentSkillDAO;
 import com.genesiis.campus.util.IDataHelper;
 
 public class CmdGetSkillDetails implements ICommand{
 
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException, Exception {
+		int StudentCode = 1; // This needs to be assign from the session.
 		
 		ICrud skillDao = new SkillDAO();
+		ICrud studentSkillDao =  new StudentSkillDAO();
 		
 		Collection<Collection<String>> skillCollection = skillDao.getAll();
 		view.setCollection(skillCollection);
-
+		
+		Collection<Collection<String>> stskillCollection = studentSkillDao.findById(StudentCode);
+		helper.setAttribute("stskillCollection", stskillCollection);
+		
 		return view;
 	}
 
