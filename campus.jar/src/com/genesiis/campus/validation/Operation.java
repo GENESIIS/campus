1c83299a7cd436fb2612198068c95fbac7103ae9
Dumani Jayasinghe <dumani@genesiis.com>
2016-12-06 21:34:57 +0530

    c52-report-banner-statistics-MP-dj add SEARCH_VIEW_BANNER_STATISTICS entry

--- campus.jar/src/com/genesiis/campus/validation/Operation.java
+++ campus.jar/src/com/genesiis/campus/validation/Operation.java
@@ -1,6 +1,6 @@
 package com.genesiis.campus.validation;
-//20161127 DJ c51-report-courses-by-course-provider-MP-dj add SEARCH_VIEW_COURSES_BY_COURSE_PROVIDER 
-//20161127 DJ c51-report-courses-by-course-provider-MP-dj add REPORT_COURSES_BY_COURSE_PROVIDER entry 
+
+//20161206 DJ c52-report-banner-statistics-MP-dj add SEARCH_VIEW_BANNER_STATISTICS entry 
 
 /**
  * Operation class facilitate in binding the CCO
@@ -9,8 +9,7 @@ package com.genesiis.campus.validation;
 public enum Operation {	
 	
 	BAD_OPERATION("BO", ResponseType.INAPPLICABLE, "commandNotFound.jsp"),
-	SEARCH_VIEW_COURSES_BY_COURSE_PROVIDER("SEARCH_VIEW_COURSES_BY_COURSE_PROVIDER",ResponseType.JSON," "),
-	REPORT_COURSES_BY_COURSE_PROVIDER("REPORT_COURSES_BY_COURSE_PROVIDER",ResponseType.JSP,"dist/partials/coursesByCourseProviderView.jsp");
+	SEARCH_VIEW_BANNER_STATISTICS("SEARCH_VIEW_BANNER_STATISTICS",ResponseType.JSON," ");
 			
 	private final String commandString;
 	private final ResponseType responseType;
