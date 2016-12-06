273466d6fe69a609c21bbb988c81d57020ab6f59
Dumani Jayasinghe <dumani@genesiis.com>
2016-12-06 21:58:05 +0530

    c52-report-banner-statistics-MP-dj created BannerStatDAO.java

---
+++ campus.jar/src/com/genesiis/campus/entity/BannerStatDAO.java
@@ -0,0 +1,75 @@
+package com.genesiis.campus.entity;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Collection;
+
+//DJ 20161206 c52-report-banner-statistics-MP-dj created BannerStatDAO.java
+
+import org.apache.log4j.Logger;
+
+public class BannerStatDAO implements ICrud {
+	
+	static org.apache.log4j.Logger log = Logger.getLogger(BannerStatDAO.class.getName());
+
+	@Override
+	public int add(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<Collection<String>> findById(Object code)
+			throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Collection<String>> getAll() throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int add(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<Collection<String>> findById(Object object,
+			Connection conn) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
