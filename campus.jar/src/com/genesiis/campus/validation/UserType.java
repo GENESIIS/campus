6d8985c302766e5ab29527272f0a0e3578ce808f
jayani1991 <jayani@genesiis.com>
2016-12-06 14:57:55 +0530

    c39-add-course-provider UserType.java enum class created

---
+++ campus.jar/src/com/genesiis/campus/validation/UserType.java
@@ -0,0 +1,17 @@
+package com.genesiis.campus.validation;
+
+//20161206 JH c39-add-course-provider UserType.java created
+
+/**
+ * UserType enum class used to handle default user types of 
+ * the application.
+ */
+
+public enum UserType {
+
+	ADMIN,
+	FEATURED_COURSE_PROVIDER,
+	TUTOR,
+	STUDENT;
+	
+}
