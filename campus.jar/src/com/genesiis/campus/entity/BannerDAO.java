64f650d0ad20f296d514f8ce240c05c76f969f57
Dumani Jayasinghe <dumani@genesiis.com>
2016-12-06 22:28:24 +0530

    c52-report-banner-statistics-MP-dj Initiate getAll() method

--- campus.jar/src/com/genesiis/campus/entity/BannerDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/BannerDAO.java
@@ -1,12 +1,19 @@
 package com.genesiis.campus.entity;
 
 //DJ 20161206 c52-report-banner-statistics-MP-dj created BannerDAO.java
+//DJ 20161206 c52-report-banner-statistics-MP-dj Initiate getAll() method
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import org.apache.log4j.Logger;
+
+import com.genesiis.campus.util.ConnectionManager;
+import com.genesiis.campus.util.DaoHelper;
 
 public class BannerDAO  implements ICrud{
 	
@@ -40,8 +47,31 @@ public class BannerDAO  implements ICrud{
 	@Override
 	public Collection<Collection<String>> getAll() throws SQLException,
 			Exception {
-		// TODO Auto-generated method stub
-		return null;
+
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet resultSet =null;
+		Collection<Collection<String>> allBannerList = new ArrayList<Collection<String>>();
+
+		try {
+			
+			conn=ConnectionManager.getConnection();			
+			final StringBuilder sb=new StringBuilder();
+			
+			stmt=conn.prepareStatement(sb.toString());
+			resultSet=stmt.executeQuery();		
+			
+			
+		} catch (SQLException sqlException) {
+			log.info("getAll() sqlException" + sqlException.toString());
+			throw sqlException;
+		} catch (Exception e) {
+			log.info("getAll() Exception" + e.toString());
+			throw e;
+		} finally {
+			DaoHelper.cleanup(conn, stmt, resultSet);
+		}
+		return allBannerList;
 	}
 
 	@Override
