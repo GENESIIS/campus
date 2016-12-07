ec3bf6385b7d254cfb1d709da476d6f098e68373
pabodhaW <pabodha@genesiis.com>
2016-12-07 12:43:41 +0530

    c26-add-student-details modified execute() method to get student's existing interest data and all interest data.

--- campus.jar/src/com/genesiis/campus/command/CmdGetSkillDetails.java
+++ campus.jar/src/com/genesiis/campus/command/CmdGetSkillDetails.java
@@ -3,13 +3,16 @@ package com.genesiis.campus.command;
 //20161206 PN c26-add-student-details INIT CmdGetSkillDetails.java
 //         PN c26-add-student-details implement execute() method.
 //		   PN c26-add-student-details modified execute() method to get student's existing skill data.
+//20161207 PN c26-add-student-details modified execute() method to get student's existing interest data and all interest data..
 
 import java.sql.SQLException;
 import java.util.Collection;
 
 import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.InterestDAO;
 import com.genesiis.campus.entity.SkillDAO;
+import com.genesiis.campus.entity.StudentInterestDAO;
 import com.genesiis.campus.entity.StudentSkillDAO;
 import com.genesiis.campus.util.IDataHelper;
 
@@ -21,12 +24,20 @@ public class CmdGetSkillDetails implements ICommand{
 		
 		ICrud skillDao = new SkillDAO();
 		ICrud studentSkillDao =  new StudentSkillDAO();
+		ICrud interestDao = new InterestDAO();
+		ICrud studentInterestDao =  new StudentInterestDAO();
 		
 		Collection<Collection<String>> skillCollection = skillDao.getAll();
 		view.setCollection(skillCollection);
 		
 		Collection<Collection<String>> stskillCollection = studentSkillDao.findById(StudentCode);
 		helper.setAttribute("stskillCollection", stskillCollection);
+		
+		Collection<Collection<String>> interestCollection = interestDao.getAll();
+		helper.setAttribute("interestCollection", interestCollection);
+		
+		Collection<Collection<String>> stinterestCollection = studentInterestDao.findById(StudentCode);
+		helper.setAttribute("stinterestCollection", stinterestCollection);
 		
 		return view;
 	}
