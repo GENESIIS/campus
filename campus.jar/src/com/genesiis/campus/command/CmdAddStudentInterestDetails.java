916285cb8863bf2bd3d1dc876a997fe6e6c95b8f
pabodhaW <pabodha@genesiis.com>
2016-12-07 16:22:32 +0530

    c26-add-student-details INIT CmdAddStudentInterestDetails.java. Implemented execute() method.

---
+++ campus.jar/src/com/genesiis/campus/command/CmdAddStudentInterestDetails.java
@@ -0,0 +1,104 @@
+package com.genesiis.campus.command;
+
+//20161207 PN c26-add-student-details INIT CmdAddStudentInterestDetails.java. Implemented execute() method.
+//		   PN c26-add-student-details: modified execute() method by adding status messages.
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.genesiis.campus.entity.ICrud;
+import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.StudentInterestDAO;
+import com.genesiis.campus.entity.model.StudentInterest;
+import com.genesiis.campus.util.ConnectionManager;
+import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.validation.SystemMessage;
+import com.genesiis.campus.validation.Validator;
+
+public class CmdAddStudentInterestDetails implements ICommand {
+	static Logger log = Logger.getLogger(CmdAddStudentInterestDetails.class.getName());
+
+	@Override
+	public IView execute(IDataHelper helper, IView view) throws SQLException, Exception {
+		// This needs to be assign from the session.
+		int StudentCode = 1;
+		String[] oldStudentInterest = helper.getParameter("oldStudentInterest").split(",");
+		String[] newStudentInterest = helper.getParameter("newStudentInterest").split(",");
+		String message = "";
+
+		ICrud interestDao = new StudentInterestDAO();
+		Connection connection = ConnectionManager.getConnection();
+		try {
+			// Commit false till the updations/additions successfully
+			// completed.
+			connection.setAutoCommit(false);
+
+			if (oldStudentInterest.length > newStudentInterest.length) {
+				log.info("Delete Diff.");
+				List diff = Validator.subtract(Arrays.asList(oldStudentInterest), Arrays.asList(newStudentInterest));
+				for (Object object : diff) {
+					log.info("object " + object);
+					StudentInterest interest = new StudentInterest();
+					interest.setStudent(StudentCode);
+					interest.setInterest(Integer.parseInt(object.toString()));
+					interestDao.delete(interest, connection);
+				}
+			} else if (oldStudentInterest.length < newStudentInterest.length) {
+				log.info("Add Diff.");
+				List diff = Validator.subtract(Arrays.asList(newStudentInterest), Arrays.asList(oldStudentInterest));
+				for (Object object : diff) {
+					log.info("object " + object);
+					StudentInterest interest = new StudentInterest();
+					interest.setStudent(StudentCode);
+					interest.setInterest(Integer.parseInt(object.toString()));
+					interest.setCrtBy("USER");
+					interestDao.add(interest, connection);
+				}
+			} else {
+				List diff = Validator.subtract(Arrays.asList(oldStudentInterest), Arrays.asList(newStudentInterest));
+				for (Object object : diff) {
+					log.info("old object " + object);
+					StudentInterest interest = new StudentInterest();
+					interest.setStudent(StudentCode);
+					interest.setInterest(Integer.parseInt(object.toString()));
+					interestDao.delete(interest, connection);
+				}
+
+				List diff1 = Validator.subtract(Arrays.asList(newStudentInterest), Arrays.asList(oldStudentInterest));
+				for (Object object : diff1) {
+					log.info("new object " + object);
+					StudentInterest interest = new StudentInterest();
+					interest.setStudent(StudentCode);
+					interest.setInterest(Integer.parseInt(object.toString()));
+					interest.setCrtBy("USER");
+					interestDao.add(interest, connection);
+				}
+			}
+			message = SystemMessage.SUCCESS.message();
+			// Commit if all the updations/additions successfully completed.
+			connection.commit();
+
+			Collection<Collection<String>> studentInterestCollection = interestDao.findById(StudentCode);
+			view.setCollection(studentInterestCollection);
+		} catch (SQLException sqle) {
+			message = SystemMessage.ERROR.message();
+			log.error("execute() : sqle" + sqle.toString());
+			throw sqle;
+		} catch (Exception e) {
+			message = SystemMessage.ERROR.message();
+			log.error("execute() : e" + e.toString());
+			throw e;
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+		helper.setAttribute("InterestChangesStatus", message);
+		return view;
+	}
+}
