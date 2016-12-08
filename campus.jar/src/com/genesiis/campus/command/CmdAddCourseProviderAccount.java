319ce8f17d3e7ca20df86df1eb22331748a6fc93
jayani1991 <jayani@genesiis.com>
2016-12-09 01:26:39 +0530

    c39-add-course-provider CmdAddCourseProviderAccount.java created and coding wip

---
+++ campus.jar/src/com/genesiis/campus/command/CmdAddCourseProviderAccount.java
@@ -0,0 +1,32 @@
+package com.genesiis.campus.command;
+
+//20161208 JH c39-add-course-provider CmdAddCourseProviderAccount.java created
+
+import java.sql.SQLException;
+
+import com.genesiis.campus.entity.ICrud;
+import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.UsernameDAO;
+import com.genesiis.campus.util.IDataHelper;
+
+import org.apache.log4j.Logger;
+
+public class CmdAddCourseProviderAccount implements ICommand{
+	static org.apache.log4j.Logger log = Logger.getLogger(CmdAddCourseProviderAccount.class.getName());
+	
+	public IView execute(IDataHelper helper, IView view) throws SQLException,
+			Exception {
+
+
+	String action = 	helper.getParameter("action");
+	
+	if(action.equalsIgnoreCase("USERNAME_VALIDATION")){//used when validating a username
+		ICrud usernameDAO = new UsernameDAO();
+	}
+	else if(action.equalsIgnoreCase("PREFIX_VALIDATION")){//used when validating prefix
+		
+	}
+		return view;
+	}
+
+}
