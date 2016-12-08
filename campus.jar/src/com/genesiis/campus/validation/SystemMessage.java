fc11a4b69561a8b8c7599ade1b4df53e100643d3
jayani1991 <jayani@genesiis.com>
2016-12-09 02:47:37 +0530

    c39-add-course-provider added USERNAME_VALID, USERNAME_INVALID to the SystemMessage enum

--- campus.jar/src/com/genesiis/campus/validation/SystemMessage.java
+++ campus.jar/src/com/genesiis/campus/validation/SystemMessage.java
@@ -3,14 +3,16 @@ package com.genesiis.campus.validation;
 //20160811 PN x25-list-employees SystemMessage enum Created
 //20161024 DN c10-contacting-us extracted to campus project
 //20161201 JH c39-add-course-provider removed unwanted enums
+//20161208 JH c39-add-course-provider added USERNAME_VALID, USERNAME_INVALID
 
 public enum SystemMessage {	
 	ADDED("Details added successfully."), 
 	ERROR("Unsuccessful."), 	
 	UNKNOWN(""),
 	SUCCESS("Successfull"),
-	NOTADDED("Problem when inserting data."), 	
-;
+	NOTADDED("Problem when inserting data."),
+	USERNAME_VALID("valid username"),
+	USERNAME_INVALID("already exist the username");
 
 	
 	private String message;
