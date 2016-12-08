06f6a52d019354a16d26defa5081f45705ef2405
jayani1991 <jayani@genesiis.com>
2016-12-09 01:27:09 +0530

    c39-add-course-provider UsernameDAO.java created

---
+++ campus.jar/src/com/genesiis/campus/entity/UsernameDAO.java
@@ -0,0 +1,54 @@
+package com.genesiis.campus.entity;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Collection;
+
+//20161208 JH c39-add-course-provider UsernameDAO.java created
+
+public class UsernameDAO implements ICrud{
+
+	public int add(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int update(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int delete(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Collection findById(Object code) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection getAll() throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int add(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int update(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int delete(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
