3bfebafa1658351df59cecfd947a2a1a191abe9a
jayani1991 <jayani@genesiis.com>
2016-12-09 16:41:21 +0530

    c39-add-course-provider added TownDAO.java as a reusable component to list towns for a selected country

---
+++ campus.jar/src/com/genesiis/campus/entity/TownDAO.java
@@ -0,0 +1,142 @@
+package com.genesiis.campus.entity;
+
+//20161029 PN c11-criteria-based-filter-search implemented getAll() method for retrieve existing details
+//20161205 PN c26-add-student-details: implemented findById() method for retrieve towns for given country code.
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.log4j.Logger;
+
+import com.genesiis.campus.util.ConnectionManager;
+
+public class TownDAO implements ICrud {
+	static Logger log = Logger.getLogger(TownDAO.class.getName());
+
+	@Override
+	public int add(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<Collection<String>> findById(Object code)
+			throws SQLException, Exception {
+		int countryCode = (Integer) code;
+		final Collection<Collection<String>> allTownList = new ArrayList<Collection<String>>();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+
+		try {
+			conn = ConnectionManager.getConnection();
+			String query = "SELECT [CODE],[NAME],[DISTRICT] FROM [CAMPUS].[TOWN] WHERE [COUNTRY] = ?;";
+
+			stmt = conn.prepareStatement(query);
+			stmt.setInt(1, countryCode);
+			final ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				final ArrayList<String> singleTownList = new ArrayList<String>();
+				singleTownList.add(rs.getString("CODE"));
+				singleTownList.add(rs.getString("NAME"));
+				singleTownList.add(rs.getString("DISTRICT"));
+
+				final Collection<String> singleTownCollection = singleTownList;
+				allTownList.add(singleTownCollection);
+			}
+		} catch (SQLException sqlException) {
+			log.info("getAll(): SQLE " + sqlException.toString());
+			throw sqlException;
+		} catch (Exception e) {
+			log.info("getAll(): E " + e.toString());
+			throw e;
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		}
+
+		return allTownList;
+	}
+
+	@Override
+	public Collection<Collection<String>> getAll() throws SQLException,
+			Exception {
+		final Collection<Collection<String>> allTownList = new ArrayList<Collection<String>>();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+
+		try {
+			conn = ConnectionManager.getConnection();
+			String query = "SELECT [CODE],[NAME],[DESCRIPTION],[IMAGE],[ISACTIVE] FROM [CAMPUS].[Town] WHERE [ISACTIVE] = 1;";
+
+			stmt = conn.prepareStatement(query);
+			final ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				final ArrayList<String> singleTownList = new ArrayList<String>();
+				singleTownList.add(rs.getString("CODE"));
+				singleTownList.add(rs.getString("NAME"));
+				singleTownList.add(rs.getString("DESCRIPTION"));
+
+				final Collection<String> singleTownCollection = singleTownList;
+				allTownList.add(singleTownCollection);
+			}
+		} catch (SQLException sqlException) {
+			log.info("getAll(): SQLE " + sqlException.toString());
+			throw sqlException;
+		} catch (Exception e) {
+			log.info("getAll(): E " + e.toString());
+			throw e;
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		}
+		return allTownList;
+	}
+
+	@Override
+	public int add(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
