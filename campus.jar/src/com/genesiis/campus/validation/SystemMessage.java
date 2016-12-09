a58ddb7234f24235fc61b3233ec08d68802b52b4
jayani1991 <jayani@genesiis.com>
2016-12-09 15:29:08 +0530

    c39-add-course-provider added EMPTY_USERNAME to SystemMessage enum class

--- campus.jar/src/com/genesiis/campus/validation/SystemMessage.java
+++ campus.jar/src/com/genesiis/campus/validation/SystemMessage.java
@@ -4,6 +4,7 @@ package com.genesiis.campus.validation;
 //20161024 DN c10-contacting-us extracted to campus project
 //20161201 JH c39-add-course-provider removed unwanted enums
 //20161208 JH c39-add-course-provider added USERNAME_VALID, USERNAME_INVALID
+//20161209 JH c39-add-course-provider added EMPTY_USERNAME
 
 public enum SystemMessage {	
 	ADDED("Details added successfully."), 
@@ -12,8 +13,8 @@ public enum SystemMessage {
 	SUCCESS("Successfull"),
 	NOTADDED("Problem when inserting data."),
 	USERNAME_VALID("valid username"),
-	USERNAME_INVALID("already exist the username");
-
+	USERNAME_INVALID("already exist the username"),
+	EMPTY_USERNAME("Please give a username");
 	
 	private String message;
 
