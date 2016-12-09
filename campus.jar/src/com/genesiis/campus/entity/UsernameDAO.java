2fd832a71637eb0586a22c6f85251de5e6b427f4
jayani1991 <jayani@genesiis.com>
2016-12-09 12:12:46 +0530

    c39-add-course-provider UsernameDAO class findById method coding wip

--- campus.jar/src/com/genesiis/campus/entity/UsernameDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/UsernameDAO.java
@@ -1,12 +1,18 @@
 package com.genesiis.campus.entity;
 
+//20161208 JH c39-add-course-provider UsernameDAO.java created
+//20161209 JH c39-add-course-provider findById method coding 
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Collection;
 
-//20161208 JH c39-add-course-provider UsernameDAO.java created
+import org.apache.log4j.Logger;
 
-public class UsernameDAO implements ICrud{
+public class UsernameDAO implements ICrud {
+	static org.apache.log4j.Logger log = Logger.getLogger(UsernameDAO.class
+			.getName());
 
 	public int add(Object object) throws SQLException, Exception {
 		// TODO Auto-generated method stub
@@ -23,8 +29,31 @@ public class UsernameDAO implements ICrud{
 		return 0;
 	}
 
+	/**
+	 * findById method used to get the course provider details using the course
+	 * provider
+	 */
 	public Collection findById(Object code) throws SQLException, Exception {
-		// TODO Auto-generated method stub
+
+		Connection conn = null;
+		PreparedStatement preparedStatement = null;
+
+		try {
+			final Collection<Collection<String>> usernameCollection = null;
+
+			String query = "SELECT * FROM ";
+
+		} catch (Exception exception) {
+			log.error("findById method Exception " + exception.toString());
+			throw exception;
+		} finally {
+			if (conn != null) {
+				conn.close();
+			}
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+		}
 		return null;
 	}
 
