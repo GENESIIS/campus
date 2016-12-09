f8d7347caa6a1b534abef2417f382fba8e391bd0
jayani1991 <jayani@genesiis.com>
2016-12-09 15:49:39 +0530

    c39-add-course-provider CmdAddCourseProviderAccount command class coding wip

--- campus.jar/src/com/genesiis/campus/command/CmdAddCourseProviderAccount.java
+++ campus.jar/src/com/genesiis/campus/command/CmdAddCourseProviderAccount.java
@@ -7,31 +7,53 @@ import java.util.Collection;
 
 import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
-import com.genesiis.campus.entity.FeaturedProviderUsernameDAO;
+import com.genesiis.campus.entity.CourseProviderUsernameDAO;
 import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.validation.SystemMessage;
+import com.genesiis.campus.validation.Validator;
 
 import org.apache.log4j.Logger;
+import org.hibernate.validator.internal.xml.ValidatedByType;
 
-public class CmdAddCourseProviderAccount implements ICommand{
-	static org.apache.log4j.Logger log = Logger.getLogger(CmdAddCourseProviderAccount.class.getName());
-	
+public class CmdAddCourseProviderAccount implements ICommand {
+	static org.apache.log4j.Logger log = Logger
+			.getLogger(CmdAddCourseProviderAccount.class.getName());
+
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
 
+		final String action = null; //
+		SystemMessage message = null;
+		Validator validator = new Validator();
+		
+		if (!validator.isEmpty(helper.getParameter("action"))) {
 
-	String action = 	helper.getParameter("action");
-	
-	if(action.equalsIgnoreCase("USERNAME_VALIDATION")){//used when validating a username
-		ICrud usernameDAO = new FeaturedProviderUsernameDAO();
-		String username = helper.getParameter("username");
-		
-		  usernameDAO.findById(username);
+			if (action.equalsIgnoreCase("USERNAME_VALIDATION")) {// used when
+																	// validating
+																	// a
+																	// username
+				ICrud usernameDAO = new CourseProviderUsernameDAO();
+				String username = helper.getParameter("username");
 
+				final Collection<Collection<String>> userAccountCollectin = usernameDAO
+						.findById(username);
+				if (userAccountCollectin.size() > 0) {// username exist
+					message = SystemMessage.USERNAME_INVALID;
+				} else {
+					message = SystemMessage.USERNAME_VALID;
+				}
+
+			} else if (action.equalsIgnoreCase("PREFIX_VALIDATION")) {// used
+																		// when
+																		// validating
+																		// prefix
+
+			}
+		}else{
+			message = SystemMessage.EMPTY_USERNAME;
+		}
 		
-	}
-	else if(action.equalsIgnoreCase("PREFIX_VALIDATION")){//used when validating prefix
-		
-	}
+		helper.setAttribute("message", message.message());
 		return view;
 	}
 
