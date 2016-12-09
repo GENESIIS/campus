d8e4e61c7363056413ebd0d4b31d9d8152c5e420
jayani1991 <jayani@genesiis.com>
2016-12-09 13:22:07 +0530

    c39-add-course-provider findById method modified: removed query used to get userType in FeatureCourseProviderDAO

--- campus.jar/src/com/genesiis/campus/entity/FeaturedCourseProviderDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/FeaturedCourseProviderDAO.java
@@ -7,6 +7,7 @@ package com.genesiis.campus.entity;
 //20161129 JH c39-add-course-provider CourseProviderDAO class renamed as FeaturedCourseProviderDAO
 //20161202 JH c39-add-course-provider add method modified
 //20161208 JH c39-add-course-provider error fixed and code ReFactored
+//20161209 JH c39-add-course-provider findById method modified: removed query used to get userType
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -24,92 +25,88 @@ import com.genesiis.campus.util.ConnectionManager;
 import com.genesiis.campus.validation.AccountType;
 import com.genesiis.campus.validation.UserType;
 
-public class FeaturedCourseProviderDAO implements ICrud{
-	
-	static Logger log = Logger.getLogger(FeaturedCourseProviderDAO.class.getName());
+public class FeaturedCourseProviderDAO implements ICrud {
+
+	static Logger log = Logger.getLogger(FeaturedCourseProviderDAO.class
+			.getName());
 
 	/**
-	 * add method used to create a new featured course provider record. 
+	 * add method used to create a new featured course provider record.
+	 * 
 	 * @author JH
-	 * @param object type of hash map
+	 * @param object
+	 *            type of hash map
 	 * @return int to indicate success or fail
 	 */
 	@Override
 	public int add(Object object) throws SQLException, Exception {
-	
+
 		Connection conn = null;
 		PreparedStatement preparedStatement = null;
 		PreparedStatement preparedStatement2 = null;
-		PreparedStatement preparedStatement3 = null;
 		int status = 0;
 		int generatedKey = 0;
-		
-		try{
-			HashMap map =  (HashMap)(object);
-			
+
+		try {
+			HashMap map = (HashMap) (object);
+
 			CourseProvider courseProvider = new CourseProvider();
 			CourseProviderAccount courseProviderAccount = new CourseProviderAccount();
-			
-			courseProvider =(CourseProvider) map.get("provider");
+
+			courseProvider = (CourseProvider) map.get("provider");
 			courseProviderAccount = (CourseProviderAccount) map.get("account");
-			
-			
-			
+
 			conn = ConnectionManager.getConnection();
 			conn.setAutoCommit(false);
-					
-			
+
 			/**
-			 * provider query used to insert data into course provider table. 
+			 * provider query used to insert data into course provider table.
 			 */
 			String provider = "INSERT INTO [CAMPUS].[COURSEPROVIDER](UNIQUEPREFIX ,SHORTNAME, NAME, DESCRIPTION, GENERALEMAIL,"
-			+" COURSEINQUIRYEMAIL, LANDPHONECOUNTRYCODE, LANDPHONEAREACODE ,LANDPHONENO ,LANDPHONE2NO ,"
-			+" FAXNO ,MOBILEPHONECOUNTRYCODE, MOBILEPHONENETWORKCODE, MOBILEPHONENO, SPECIALITY ,WEBLINK, FACEBOOKURL, TWITTERURL, MYSPACEURL , LINKEDINURL, INSTAGRAMURL ,"
-            +" VIBERNUMBER, WHATSAPPNUMBER, EXPIRATIONDATE, ADDRESS1, ADDRESS2, ADDRESS3, ACCOUNTTYPE, "
-            +" HEADOFFICETOWN, ISTUTORRELATED, ISADMINALLOWED, COURSEPROVIDERSTATUS, COURSEPROVIDERTYPE, "
-            +" PRINCIPAL, CRTON, CRTBY, MODON, MODBY )"
-            +" VALUES ( ?, ?, ? , ? , ?, ?, ? , ?, ?, ?, "
-            +" ? ,?, ?, ? , ? , ?, ?, ? , ?, ?, ?, ?, ?, "
-            +" ?, ?, ? , ? , ?, ?, ? , ?, ?, ?, ?, getDate(), ?, getDate(),? )";
-			
+					+ " COURSEINQUIRYEMAIL, LANDPHONECOUNTRYCODE, LANDPHONEAREACODE ,LANDPHONENO ,LANDPHONE2NO ,"
+					+ " FAXNO ,MOBILEPHONECOUNTRYCODE, MOBILEPHONENETWORKCODE, MOBILEPHONENO, SPECIALITY ,WEBLINK, FACEBOOKURL, TWITTERURL, MYSPACEURL , LINKEDINURL, INSTAGRAMURL ,"
+					+ " VIBERNUMBER, WHATSAPPNUMBER, EXPIRATIONDATE, ADDRESS1, ADDRESS2, ADDRESS3, ACCOUNTTYPE, "
+					+ " HEADOFFICETOWN, ISTUTORRELATED, ISADMINALLOWED, COURSEPROVIDERSTATUS, COURSEPROVIDERTYPE, "
+					+ " PRINCIPAL, CRTON, CRTBY, MODON, MODBY )"
+					+ " VALUES ( ?, ?, ? , ? , ?, ?, ? , ?, ?, ?, "
+					+ " ? ,?, ?, ? , ? , ?, ?, ? , ?, ?, ?, ?, ?, "
+					+ " ?, ?, ? , ? , ?, ?, ? , ?, ?, ?, ?, getDate(), ?, getDate(),? )";
+
 			/**
 			 * account query used to insert data into course provider account
 			 * table. Takes course provider code generated in 'provider' query
 			 * as an input to this query.
 			 */
-			
+
 			String account = "INSERT INTO [CAMPUS].[COURSEPROVIDERACCOUNT](NAME, USERNAME, PASSWORD, DESCRIPTION, ISACTIVE, COURSEPROVIDER,"
 					+ " USERTYPE ,CRTON, CRTBY, MODON, MODBY) VALUES(  ?, ?, ?, ?, ?, ?, ?, getDate(), ?, getDate(), ?)";
-			
-			
-			/**
-			 * getUserType query used to get the code of default course provider user type.
-			 * USERTYPESTRING is a new column added to the table to identify the default user 
-			 * types with the support of 'USERTYPE' enum class
-			 */
-			String getUserType = "SELECT CODE FROM [CAMPUS].[USERTYPE] WHERE COURSEPROVIDER = -1 AND USERTYPESTRING = ?";
 
 			preparedStatement = conn.prepareStatement(provider,
 					PreparedStatement.RETURN_GENERATED_KEYS);
 
-
-//			Date d1 = new Date();
-//			java.sql.Date sqlDate = new java.sql.Date(d1.getTime());
+			// Date d1 = new Date();
+			// java.sql.Date sqlDate = new java.sql.Date(d1.getTime());
 			preparedStatement.setString(1, courseProvider.getUniquePrefix());
 			preparedStatement.setString(2, courseProvider.getShortName());
 			preparedStatement.setString(3, courseProvider.getName());
 			preparedStatement.setString(4, courseProvider.getDescription());
 			preparedStatement.setString(5, courseProvider.getGeneralEmail());
-			preparedStatement.setString(6, courseProvider.getCourseInquiryEmail());
-			preparedStatement.setString(7, courseProvider.getLandPhoneCountryCode());
-			preparedStatement.setString(8, courseProvider.getLandPhoneAreaCode());
+			preparedStatement.setString(6,
+					courseProvider.getCourseInquiryEmail());
+			preparedStatement.setString(7,
+					courseProvider.getLandPhoneCountryCode());
+			preparedStatement.setString(8,
+					courseProvider.getLandPhoneAreaCode());
 			preparedStatement.setString(9, courseProvider.getLandPhoneNo());
 			preparedStatement.setString(10, courseProvider.getLandPhpneNo2());
 			preparedStatement.setString(11, courseProvider.getFaxNo());
-			preparedStatement.setString(12,courseProvider.getMobilePhoneCountryCode());
-			preparedStatement.setString(13, courseProvider.getMobilePhoneNetworkCode());
-			preparedStatement.setString(14, courseProvider.getMobilePhoneNumber());
-			preparedStatement.setString(15,courseProvider.getSpeciality());
+			preparedStatement.setString(12,
+					courseProvider.getMobilePhoneCountryCode());
+			preparedStatement.setString(13,
+					courseProvider.getMobilePhoneNetworkCode());
+			preparedStatement.setString(14,
+					courseProvider.getMobilePhoneNumber());
+			preparedStatement.setString(15, courseProvider.getSpeciality());
 			preparedStatement.setString(16, courseProvider.getWeblink());
 			preparedStatement.setString(17, courseProvider.getFacebookURL());
 			preparedStatement.setString(18, courseProvider.getTwitterURL());
@@ -117,7 +114,7 @@ public class FeaturedCourseProviderDAO implements ICrud{
 			preparedStatement.setString(20, courseProvider.getLinkedinURL());
 			preparedStatement.setString(21, courseProvider.getInstagramURL());
 			preparedStatement.setString(22, courseProvider.getViberNumber());
-			preparedStatement.setString(23,courseProvider.getWhatsappNumber());
+			preparedStatement.setString(23, courseProvider.getWhatsappNumber());
 			preparedStatement.setDate(24, courseProvider.getExpirationDate());
 			preparedStatement.setString(25, courseProvider.getAddress1());
 			preparedStatement.setString(26, courseProvider.getAddress2());
@@ -126,72 +123,64 @@ public class FeaturedCourseProviderDAO implements ICrud{
 			preparedStatement.setInt(29, courseProvider.getHeadOffice());
 			preparedStatement.setBoolean(30, courseProvider.isTutorRelated());
 			preparedStatement.setBoolean(31, courseProvider.isAdminAllowed());
-			preparedStatement.setInt(32, courseProvider.getCourseProviderStatus());
-			preparedStatement.setInt(33, courseProvider.getCourseProviderType());
-			preparedStatement.setInt(34, courseProvider.getCourseProviderType());
+			preparedStatement.setInt(32,
+					courseProvider.getCourseProviderStatus());
+			preparedStatement
+					.setInt(33, courseProvider.getCourseProviderType());
+			preparedStatement
+					.setInt(34, courseProvider.getCourseProviderType());
 			preparedStatement.setInt(35, courseProvider.getPrincipal());
 			preparedStatement.setString(36, courseProvider.getCrtBy());
 			preparedStatement.setString(37, courseProvider.getModBy());
 
-			
 			preparedStatement2 = conn.prepareStatement(account);
 			preparedStatement2.setString(1, courseProviderAccount.getName());
-			preparedStatement2.setString(2, courseProviderAccount.getUsername());
-			preparedStatement2.setString(3, courseProviderAccount.getPassword());
-			preparedStatement2.setString(4, courseProviderAccount.getDescription());
+			preparedStatement2
+					.setString(2, courseProviderAccount.getUsername());
+			preparedStatement2
+					.setString(3, courseProviderAccount.getPassword());
+			preparedStatement2.setString(4,
+					courseProviderAccount.getDescription());
 			preparedStatement2.setBoolean(5, courseProviderAccount.isActive());
 			preparedStatement2.setString(8, courseProviderAccount.getCrtBy());
 			preparedStatement2.setString(9, courseProviderAccount.getModBy());
-			
-			
-			preparedStatement3 = conn.prepareStatement(getUserType);
-			preparedStatement3.setString(1, UserType.FEATURED_COURSE_PROVIDER.getUserType());
-			
-			ResultSet userTypeRS = preparedStatement3.executeQuery();
-			
-			if(userTypeRS != null){//valid default user type
-				int userTypeId = Integer.parseInt(userTypeRS.getString("CODE"));
-				
-				if(userTypeId !=0 ){
-					
-					status = preparedStatement.executeUpdate();
-					log.info(".........." + status);
-					
-					ResultSet rs = preparedStatement.getGeneratedKeys();
-					
-					if(rs.next()){
-						generatedKey = rs.getInt(1);
-						status = 1;
-						log.info(">>>>>>>>>>>>>>>    " + generatedKey);
-						preparedStatement2.setInt(6, generatedKey);
-						preparedStatement2.setInt(7, userTypeId);
-						
-					status = preparedStatement2.executeUpdate();
-					}
-				}
+
+			status = preparedStatement.executeUpdate();
+			log.info(".........." + status);
+
+			ResultSet rs = preparedStatement.getGeneratedKeys();
+
+			if (rs.next()) {
+				generatedKey = rs.getInt(1);
+				status = 1;
+				log.info(">>>>>>>>>>>>>>>    " + generatedKey);
+				preparedStatement2.setInt(6, generatedKey);
+				preparedStatement2.setInt(7, 2);
+			//	preparedStatement2.setInt(7, userTypeId);
+
+				status = preparedStatement2.executeUpdate();
 			}
-	
+
 			conn.commit();
-			
-			
-		}catch(SQLException sqlException){
+
+		} catch (SQLException sqlException) {
 			log.error("add method SQL Exception " + sqlException.toString());
 			conn.rollback();
 			throw sqlException;
-			
-		}catch (Exception exception) {
+
+		} catch (Exception exception) {
 			log.error("add method Exception " + exception.toString());
 			conn.rollback();
 			throw exception;
-		}finally{
-			if(conn != null){
+		} finally {
+			if (conn != null) {
 				conn.setAutoCommit(true);
 				conn.close();
 			}
-			if(preparedStatement != null){
+			if (preparedStatement != null) {
 				preparedStatement.close();
 			}
-			if(preparedStatement2 != null) {
+			if (preparedStatement2 != null) {
 				preparedStatement2.close();
 			}
 		}
