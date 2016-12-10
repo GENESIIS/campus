bec62599739ff63b9b5ce4ada227e9791d1b627e
Dumani Jayasinghe <dumani@genesiis.com>
2016-12-10 22:35:10 +0530

    c52-report-banner-statistics-MP-dj  Implement getAll() method

--- campus.jar/src/com/genesiis/campus/entity/PageDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/PageDAO.java
@@ -1,10 +1,20 @@
 package com.genesiis.campus.entity;
 
+//DJ 20161210 c52-report-banner-statistics-MP-dj  created PageDAO.java
+//DJ 20161210 c52-report-banner-statistics-MP-dj  Implement getAll() method
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 
-//DJ 20161210 c52-report-banner-statistics-MP-dj  created PageDAO.java
+import com.genesiis.campus.util.ConnectionManager;
+import com.genesiis.campus.util.DaoHelper;
+import com.genesiis.campus.validation.ApplicationStatus;
+
+
 
 public class PageDAO implements ICrud {
 
@@ -32,12 +42,45 @@ public class PageDAO implements ICrud {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	/**
+	 * Retrieve Page details(page code and page name) result set
+	 * 
+	 * @author DJ
+	 * @return Collection
+	 */
 	@Override
 	public Collection<Collection<String>> getAll() throws SQLException,
 			Exception {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet resultSet = null;
+		final Collection<Collection<String>> pageList = new ArrayList<Collection<String>>();
+		try {
+			conn=ConnectionManager.getConnection();
+			String sql="SELECT PAGE.CODE AS PAGECODE, PAGE.NAME AS PAGENAME FROM [CAMPUS].[PAGE] PAGE WHERE PAGE.ISACTIVE=?";
+			
+			stmt=conn.prepareStatement(sql);
+			stmt.setInt(1, ApplicationStatus.INACTIVE.getStatusValue());
+			resultSet= stmt.executeQuery();	
+			while (resultSet.next()) {
+				final ArrayList<String> singlePageList = new ArrayList<String>();
+				singlePageList.add(resultSet.getString("PAGECODE"));
+				singlePageList.add(resultSet.getString("PAGENAME"));				
+				pageList.add(singlePageList);
+			}
+		
+		} catch (SQLException sqlException) {
+			log.info("getAll() sqlException" + sqlException.toString());
+			throw sqlException;
+		} catch (Exception e) {
+			log.info("getAll() Exception" + e.toString());
+			throw e;
+		} finally {
+			DaoHelper.cleanup(conn, stmt, resultSet);
+		}
+		return pageList;
+		
 	}
 
 	@Override
