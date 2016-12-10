1ae3d7430170b1c698162d3cf49a2ee541860700
Dumani Jayasinghe <dumani@genesiis.com>
2016-12-10 19:02:24 +0530

    c52-report-banner-statistics-MP-dj created Page.java

---
+++ campus.jar/src/com/genesiis/campus/entity/model/Page.java
@@ -0,0 +1,38 @@
+package com.genesiis.campus.entity.model;
+
+//DJ 20161210 c52-report-banner-statistics-MP-dj created Page.java
+
+public class Page {
+	
+	private int code;	
+	private String name;
+	private String description;
+	private boolean isActive;
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	
+	
+
+}
