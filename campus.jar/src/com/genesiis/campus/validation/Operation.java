d7854527e7a45701432da98a6f611f8ccd9f7be2
Dumani Jayasinghe <dumani@genesiis.com>
2016-12-10 19:03:53 +0530

    c52-report-banner-statistics-MP-dj add REPORT_BANNER_STATISTICS entry

--- campus.jar/src/com/genesiis/campus/validation/Operation.java
+++ campus.jar/src/com/genesiis/campus/validation/Operation.java
@@ -1,6 +1,7 @@
 package com.genesiis.campus.validation;
 
 //20161206 DJ c52-report-banner-statistics-MP-dj add SEARCH_VIEW_BANNER_STATISTICS entry 
+//20161210 DJ c52-report-banner-statistics-MP-dj add REPORT_BANNER_STATISTICS entry 
 
 /**
  * Operation class facilitate in binding the CCO
@@ -9,8 +10,9 @@ package com.genesiis.campus.validation;
 public enum Operation {	
 	
 	BAD_OPERATION("BO", ResponseType.INAPPLICABLE, "commandNotFound.jsp"),
-	SEARCH_VIEW_BANNER_STATISTICS("SEARCH_VIEW_BANNER_STATISTICS",ResponseType.JSON," ");
-			
+	SEARCH_VIEW_BANNER_STATISTICS("SEARCH_VIEW_BANNER_STATISTICS",ResponseType.JSON," "),
+	REPORT_BANNER_STATISTICS("REPORT_BANNER_STATISTICS",ResponseType.JSON," ");
+	
 	private final String commandString;
 	private final ResponseType responseType;
 	private final String viewJspUrl;
