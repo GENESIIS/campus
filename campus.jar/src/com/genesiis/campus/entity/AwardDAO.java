1e14fe16bbb9301aef65f1c26123e62505d14cdb
pabodhaW <pabodha@genesiis.com>
2016-12-15 12:48:15 +0530

    CAM-28: INIT AwardDAO.java class and implemented add() method.

---
+++ campus.jar/src/com/genesiis/campus/entity/AwardDAO.java
@@ -0,0 +1,100 @@
+package com.genesiis.campus.entity;
+
+//20161215 PN CAM-28: INIT AwardDAO.java class and implemented add() method.
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.log4j.Logger;
+
+import com.genesiis.campus.command.CmdGetSchoolEducationData;
+import com.genesiis.campus.util.ConnectionManager;
+
+public class AwardDAO implements ICrud{
+	static Logger log = Logger.getLogger(AwardDAO.class.getName());
+	@Override
+	public int add(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<Collection<String>> findById(Object code) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Collection<String>> getAll() throws SQLException, Exception {
+		final Collection<Collection<String>> allAwardList = new ArrayList<Collection<String>>();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+
+		try {
+			conn = ConnectionManager.getConnection();
+			String query = "SELECT [CODE],[SHORTTITLE],[LONGTITLE] FROM [CAMPUS].[AWARD];";
+
+			stmt = conn.prepareStatement(query);
+			final ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				final ArrayList<String> singleAwardList = new ArrayList<String>();
+				singleAwardList.add(rs.getString("CODE"));
+				singleAwardList.add(rs.getString("SHORTTITLE"));
+				singleAwardList.add(rs.getString("LONGTITLE"));
+
+				final Collection<String> singleAwardCollection = singleAwardList;
+				allAwardList.add(singleAwardCollection);
+			}
+		} catch (SQLException sqlException) {
+			log.error("getAll(): SQLE " + sqlException.toString());
+			throw sqlException;
+		} catch (Exception e) {
+			log.error("getAll(): E " + e.toString());
+			throw e;
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		}
+		return allAwardList;
+	}
+
+	@Override
+	public int add(Object object, Connection conn) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object, Connection conn) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object, Connection conn) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
