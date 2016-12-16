64d8ab28d5d6f72b98ed9540c3686c9c0fb55a89
Anuradha Silva <anuradha@genesiis.com>
2016-12-16 17:31:30 +0530

    C19-student-login-without-using-third-party-application-test-as message handling

--- campus.war/web/dist/js/login.js
+++ campus.war/web/dist/js/login.js
@@ -1,55 +1,70 @@
 /**
- * 20161122 AS C19-student-login-without-using-third-party-application-test-as login.js class created.
- * 20161206 AS C19-student-login-without-using-third-party-application-test-as added remember checkbox. 
+ * 20161122 AS C19-student-login-without-using-third-party-application-test-as
+ * login.js class created. 20161206 AS
+ * C19-student-login-without-using-third-party-application-test-as added
+ * remember checkbox.
  */
 var theNewScript = document.createElement("script");
 theNewScript.type = "text/javascript";
 theNewScript.src = "/dist/js/institute/validation/validation.js";
 
-function studentLogin(){
-	
+function studentLogin() {
+
 	var username = $("#email").val();
 	var password = $("#password").val();
 	var remember = $("#remember").prop('checked');
-	
+
 	var usernametb = isempty(username);
 	var passtb = isempty(password);
-	
-	if (usernametb == false) {
-		document.getElementById('fullNametbError').innerHTML = "  ** Email can not be Empty.";
-		flag = false;
-	}  if (passtb == false) {
-		document.getElementById('emailtbError').innerHTML = "   ** Password can not be Empty.";
-		flag = false;
-	}
-	
-	if((username != null) && (password != null)){
-		alert(username +"-----"+password +remember)
-		var jsonData = {
-				"userKey" : username,
-				"password" : password,
-				"remember" : remember
-		};
-		$.ajax({
-			type : "POST",
-			url : '../../LoginController',
-			data : {
-				jsonData : JSON.stringify(jsonData),
-				CCO : "SLOG"
-				
-			},
-			dataType : "json",
-			success : function(response) {
 
-				document.getElementById('messsage').innerHTML = response.result;
-				
-			},
-			error : function(e) {
-				 alert("Error " + e);
-				console.log(e);
-			}
-			
-		});
-		
+	if (usernametb == false) {
+		document.getElementById('emailtbError').innerHTML = "  ** Email can not be Empty.";
+		flag = false;
+		return false;
 	}
+	if (passtb == false) {
+		document.getElementById('passtbError').innerHTML = "   ** Password can not be Empty.";
+		return false;
+	}
+
+	if ((username != null) && (password != null)) {
+		alert(username + "-----" + password + remember)
+		var jsonData = {
+			"userKey" : username,
+			"password" : password,
+			"remember" : remember
+		};
+		$
+				.ajax({
+					type : "POST",
+					url : '../../LoginController',
+					data : {
+						jsonData : JSON.stringify(jsonData),
+						CCO : "SLOG"
+
+					},
+					dataType : "json",
+					success : function(response) {
+						alert(response['message']);
+						// window.location.href =
+						// '/dist/partials/student/student-dashboard.jsp';
+						document.getElementById('messsage').innerHTML = response.result;
+						document.getElementById('errorMesssage').innerHTML = response['message'];
+						
+					
+						
+					},
+					error : function(e) {
+						alert("Error " + e);
+						console.log(e);
+					}
+
+				});
+
+	}
+}
+
+
+function lastLoginDateTime( DateTimeString){
+	
 }
