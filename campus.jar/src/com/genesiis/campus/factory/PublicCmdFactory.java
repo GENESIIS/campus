413fdeda9a3b2390015ce60c12f6d16798ff1ecf
Miyuru-Genesiis <miyuru@genesiis.com>
2016-12-17 03:35:26 +0530

    c2-integrate-google-banners Removed map element and case block related to LOAD_BANNER_PAGE, that was there for testing purposes during development of the issue

--- campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
@@ -1,16 +1,16 @@
 //20161116 MM c2-integrate-google-banners Added map element with key LIST_GOOGLE_ADVERTS enum
 //20161116 MM c2-integrate-google-banners Changed map element with key LIST_GOOGLE_ADVERTS to LOAD_BANNER_PAGE
+//20161217 MM c2-integrate-google-banners Removed map element and case block related to LOAD_BANNER_PAGE, 
+//				that was there for testing purposes during development of the issue
 
 package com.genesiis.campus.factory;
 
 import com.genesiis.campus.command.CmdAddBannerStat;
-import com.genesiis.campus.command.CmdLoadBannerPage;
 import com.genesiis.campus.command.ICommand;
 import com.genesiis.campus.validation.Operation;
 
 public class PublicCmdFactory implements ICmdFactory {
 	static {
-		map.put(Operation.LOAD_BANNER_PAGE, new CmdLoadBannerPage());
 		map.put(Operation.ADD_BANNER_STAT, new CmdAddBannerStat());
 	}
 
@@ -20,9 +20,6 @@ public class PublicCmdFactory implements ICmdFactory {
 		Operation o = Operation.BAD_OPERATION;
 		o = Operation.getOperation(cco);
 		switch (o) {
-			case LOAD_BANNER_PAGE:
-				command = map.get(o);
-				break;
 			case ADD_BANNER_STAT:
 				command = map.get(o);
 				break;
