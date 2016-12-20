ba1b714f9f41a32f73ed057cd1eb04db9d185b04
jayani1991 <jayani@genesiis.com>
2016-12-20 16:39:44 +0530

    c39-add-course-provider JavaScript coding to display countries unsing ajax : completed

--- campus.war/web/dist/js/admin/load-provider-registration-page.js
+++ campus.war/web/dist/js/admin/load-provider-registration-page.js
@@ -3,6 +3,9 @@
  * 
  */
 
+
+window.countryCollection = null;
+
 $(document).ready(function() {
 	arrangeUI();
 });
@@ -12,46 +15,81 @@ function arrangeUI() {
 
 }
 
+window.onload = function() {
+	getProviderPageLoadData();
+};
+
+function getProviderPageLoadData() {
+
+	$.ajax({
+		url : '/AdminController',
+		method : 'POST',
+		data : {
+			'CCO' : 'LIST_PROVIDER_REGISTRATION_PAGE'
+		},
+		dataType : "json",
+		async : false,
+		success : function(response) {
+
+			if (response !== undefined && response !== null) {
+				window.countryCollection = response.requestData;
+				displayProviderCountries();
+			}
+		},
+	});
+}
+
+function displayProviderCountries() {
+	var countryCollection = window.countryCollection;
+	var singleCountryElement = '';
+
+	singleCountryElement += '<select><option>--Default--</option>';
+	if (countryCollection !== undefined & countryCollection !== null) {
+		$
+				.each(
+						countryCollection,
+						function(index, value) {
+							singleCountryElement += '<option>';
+							//singleCountryElement += '<button type="submit" name="CCO" id="CCO" class="btn btn-info navbar-btn" value="LIST_CATEGORY_LANDING_PAGE">'
+							//		+ value[1] + '</button>';
+							singleCountryElement +=  value[1] ;
+							singleCountryElement += '</option>';
+
+						});
+	}
+	singleCountryElement += '</select>';
+	var countryNames = $("#country-List");
+	countryNames.html(singleCountryElement);
+}
+
 function getProviderType() {
 
 	var status = false;
 	var radioValue = $("input[name='featured-oneoff']:checked").val();
 	var statusValue = $("input[name='providerStatus']:checked").val();
-//	if (radioValue != "") {
-//		if (radioValue == "featured") {
-//			commandCode = "ADD_FEATURED_COURSE_PROVIDER";
-//		}
-//		if (radioValue == "one-off") {
-//			commandCode = "ADD_ONE_OFF_COURSE_PROVIDER";
-//		}
-//		alert(commandCode);
-//
-//	} else {
-//		messgae = "please select a course provider type";
-//	}
-	
+
 	var form = $('#basicForm');
 	var formDate = $(form).serialize();
-		$.ajax({
-			url : '/AdminController',
-			method : 'POST',
-			data : formData ,
-			dataType : "json",
-			async : false,
-			success : function(response) {
+	$.ajax({
+		url : '/AdminController',
+		method : 'POST',
+		data : formData,
+		dataType : "json",
+		async : false,
+		success : function(response) {
 
-				if (response !== undefined && response !== null) {
-				//	message = response.userMessage;
-					var registerId = response.result;
-					
-					alert("success"  + registerId);
-					document.getElementById("usermessage").html = message;
-					// document.getElementById("logoPanel").style.display
-					// = "block";
-					// document.getElementById("basicForm").style.display
-					// = "none";
+			if (response !== undefined && response !== null) {
+				// message = response.userMessage;
+				var registerId = response.result;
 
-				}
-			},
-		});
+				alert("success" + registerId);
+				document.getElementById("usermessage").html = message;
+				// document.getElementById("logoPanel").style.display
+				// = "block";
+				// document.getElementById("basicForm").style.display
+				// = "none";
+
+			}
+		},
+	});
 }
