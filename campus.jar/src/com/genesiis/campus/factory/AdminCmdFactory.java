8f38442950eee9a016ca3f5b52494439c2cc5f26
jayani1991 <jayani@genesiis.com>
2016-12-20 10:02:45 +0530

    c39-add-course-provider added new map for LIST_PROVIDER_REGISTRATION_PAGE in AdminCmdFactory

--- campus.jar/src/com/genesiis/campus/factory/AdminCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/AdminCmdFactory.java
@@ -6,12 +6,14 @@ package com.genesiis.campus.factory;
 //20161201 JH c39-add-course-provider added new map for ADD_ONE_OFF_COURSE_PROVIDER
 //20161208 JH c39-add-course-provider added new map for COURSE_PROVIDER_VALIDATION
 //20161209 JH c39-add-course-provider COURSE_PROVIDER_VALIDATION map modified
+//20161220 JH c39-add-course-provider added new map for LIST_PROVIDER_REGISTRATION_PAGE
 
 import com.genesiis.campus.command.CmdCourseProviderAccountValidate;
 import com.genesiis.campus.command.CmdAddFeaturedProvider;
 import com.genesiis.campus.command.CmdAddOneOffProvider;
 import com.genesiis.campus.command.CmdListCategories;
 import com.genesiis.campus.command.CmdListCategoryLandingPage;
+import com.genesiis.campus.command.CmdListCourseProviderRegisterPage;
 import com.genesiis.campus.command.ICommand;
 import com.genesiis.campus.validation.Operation;
 
@@ -24,7 +26,7 @@ public class AdminCmdFactory implements ICmdFactory{
 		map.put(Operation.LIST_CATEGORY_DATA, new CmdListCategories());
 		map.put(Operation.ADD_FEATURED_COURSE_PROVIDER, new CmdAddFeaturedProvider());
 		map.put(Operation.COURSE_PROVIDER_VALIDATION, new CmdCourseProviderAccountValidate());
-
+		map.put(Operation.LIST_PROVIDER_REGISTRATION_PAGE, new CmdListCourseProviderRegisterPage());
 	}
 
 	@Override
@@ -39,6 +41,9 @@ public class AdminCmdFactory implements ICmdFactory{
 			command = map.get(o);
 			break;
 		case COURSE_PROVIDER_VALIDATION:
+			command = map.get(o);
+			break;
+		case LIST_PROVIDER_REGISTRATION_PAGE:
 			command = map.get(o);
 			break;
 		default:
