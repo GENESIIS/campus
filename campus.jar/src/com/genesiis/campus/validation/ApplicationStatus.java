cdd76a0e2a1e11e8f177622f7849fd2350bf90e0
Dumani Jayasinghe <dumani@genesiis.com>
2016-12-21 12:13:12 +0530

    DJ c51-report-courses-by-course-provider-MP-dj Implement generic method getApplicationStatus()

--- campus.jar/src/com/genesiis/campus/validation/ApplicationStatus.java
+++ campus.jar/src/com/genesiis/campus/validation/ApplicationStatus.java
@@ -1,6 +1,7 @@
 package com.genesiis.campus.validation;
 
 //20161124 JH c7-higher-education-landing-page-MP ApplicationStatus.java created
+//20161221 DJ c51-report-courses-by-course-provider-MP-dj Implement generic method getApplicationStatus()
 
 /**
 * ApplicationStatus enum created to manage all the status values
@@ -26,6 +27,28 @@ public enum ApplicationStatus {
 	 */
 	public int getStatusValue() {
 		return statusValue;
+	}
+	
+	/**
+	 * Generic method for map the status.Able to call from whole campus application
+	 * @author DJ
+	 * @param selected status Value string
+	 * @return applicationStatus
+	 * @throws Exception
+	 */
+	public static int getApplicationStatus(String statusValue){
+		int applicationStatus=0;		
+		if(statusValue.equalsIgnoreCase("ACTIVE")){
+			applicationStatus = ApplicationStatus.ACTIVE.getStatusValue();
+		}
+		if(statusValue.equalsIgnoreCase("INACTIVE")){
+			applicationStatus = ApplicationStatus.INACTIVE.getStatusValue();
+		}
+		if(statusValue.equalsIgnoreCase("PENDING") ){
+			applicationStatus = ApplicationStatus.PENDING.getStatusValue();
+		}
+		
+		return applicationStatus;
 	}
 }
 
