5b19c2f4650b3e683241cd2e6b7e5968e4f76dde
Miyuru-Genesiis <miyuru@genesiis.com>
2016-12-27 11:11:34 +0530

    c5-corporate-training-landing-page - Modified code to add map element and switch case item for LIST_STUDENT_RECOMMENDED_PROGRAMMES

--- campus.jar/src/com/genesiis/campus/factory/StudentCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/StudentCmdFactory.java
@@ -3,15 +3,18 @@ package com.genesiis.campus.factory;
 //20161121 MM c5-corporate-training-landing-page - INIT - Initialised file
 //20161122 MM c5-corporate-training-landing-page - Modified code to add map element 
 //				and switch case item for LIST_STUDENT_DASHBOARD
+//20161227 MM c5-corporate-training-landing-page - Modified code to add map element 
+//				and switch case item for LIST_STUDENT_RECOMMENDED_PROGRAMMES
 
 import com.genesiis.campus.command.CmdListStudentDashboardDetails;
-
+import com.genesiis.campus.command.CmdListStudentRecommendedProgrammes;
 import com.genesiis.campus.command.ICommand;
 import com.genesiis.campus.validation.Operation;
 
 public class StudentCmdFactory implements ICmdFactory {
 	static {
 		map.put(Operation.LIST_STUDENT_DASHBOARD, new CmdListStudentDashboardDetails());
+		map.put(Operation.LIST_STUDENT_RECOMMENDED_PROGRAMMES, new CmdListStudentRecommendedProgrammes());
 	}
 
 	@Override
@@ -21,6 +24,9 @@ public class StudentCmdFactory implements ICmdFactory {
 		o = Operation.getOperation(cco);
 		switch (o) {
 			case LIST_STUDENT_DASHBOARD:
+				command = map.get(o);
+				break;
+			case LIST_STUDENT_RECOMMENDED_PROGRAMMES:
 				command = map.get(o);
 				break;
 			default:
