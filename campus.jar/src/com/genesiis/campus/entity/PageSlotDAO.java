3e59f1bb42d987db8d4901ab69a8595c30fe953c
Dumani Jayasinghe <dumani@genesiis.com>
2016-12-27 15:26:29 +0530

    c52-report-banner-statistics-MP- Load pageslot

--- campus.jar/src/com/genesiis/campus/entity/PageSlotDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/PageSlotDAO.java
@@ -1,7 +1,7 @@
 package com.genesiis.campus.entity;
 
 //DJ 20161210 c52-report-banner-statistics-MP-dj  Implement  PageSlotDAO.java
-//DJ 20161210 c52-report-banner-statistics-MP-dj  Implement  getAll() method
+//DJ 20161210 c52-report-banner-statistics-MP-dj  Implement  findById() method
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -13,6 +13,7 @@ import java.util.Collection;
 import com.genesiis.campus.util.ConnectionManager;
 import com.genesiis.campus.util.DaoHelper;
 import com.genesiis.campus.validation.ApplicationStatus;
+import com.genesiis.campus.validation.UtilityHelper;
 
 import org.apache.log4j.Logger;
 
@@ -52,14 +53,18 @@ public class PageSlotDAO implements ICrud {
 		PreparedStatement stmt=null;
 		ResultSet resultSet=null;
 		final Collection<Collection<String>> pageSlotList = new ArrayList<Collection<String>>();
-
+		int pageCode=0;
+		if(UtilityHelper.isNotEmptyObject(code)){
+			 pageCode=Integer.valueOf((String) code);			
+		}
+        
 		try {
 			conn=ConnectionManager.getConnection();
 			final StringBuilder sb = new StringBuilder(
 					"SELECT SLOT.CODE AS PAGESLOTCODE , SLOT.NAME AS PAGESLOTNAME FROM [CAMPUS].[PAGESLOT] SLOT WHERE");
 			sb.append(" SLOT.PAGE= ? AND SLOT.ISACTIVE= ?");
 			stmt=conn.prepareStatement(sb.toString());
-			stmt.setInt(1, 1);
+			stmt.setInt(1, pageCode);
 			stmt.setInt(2, ApplicationStatus.ACTIVE.getStatusValue());
 			resultSet= stmt.executeQuery();	
 			while (resultSet.next()) {
