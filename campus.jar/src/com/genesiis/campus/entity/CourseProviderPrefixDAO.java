7483d3a7de7da54c233161bfc4ec107fd1e7259f
jayani1991 <jayani@genesiis.com>
2016-12-27 13:45:59 +0530

    c39-add-course-provider course provider prefix DAO class coding

--- campus.jar/src/com/genesiis/campus/entity/CourseProviderPrefixDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/CourseProviderPrefixDAO.java
@@ -34,39 +34,39 @@ public class CourseProviderPrefixDAO implements ICrud {
 		return 0;
 	}
 
+	/**
+	 * findById() method used to get the get course provider details using the prefix.
+	 * @param 
+	 */
 	public Collection findById(Object code) throws SQLException, Exception {
 		String query = " SELECT * FROM [CAMPUS].[COURSEPROVIDER] WHERE UNIQUEPREFIX = ? ";
 
 		Connection conn = null;
 		PreparedStatement preparedStatement = null;
-		final Collection<Collection<String>> prefixCollection = null;
+		final Collection<Collection<String>> prefixCollection = new ArrayList<Collection<String>>(); 
 		ResultSet rs = null;
 
 		try {
 			final CourseProvider courseProvider = (CourseProvider) code;
 			conn = ConnectionManager.getConnection();
 			preparedStatement = conn.prepareStatement(query);
+			String prefix = courseProvider.getUniquePrefix();
 			preparedStatement.setString(1, courseProvider.getUniquePrefix());
 
 			rs = preparedStatement.executeQuery();
 
-			if (rs != null) {
 				while (rs.next()) {
 					final ArrayList<String> singleProviderList = new ArrayList<String>();
 
 					singleProviderList.add(rs.getString("CODE"));
-					singleProviderList.add(rs.getString("UNIQUEPREFIX"));
-					singleProviderList.add(rs.getString("SHORTNAME"));
-					singleProviderList.add(rs.getString("PASSWORD"));
-					singleProviderList.add(rs.getString("EMAIL"));
-					singleProviderList.add(rs.getString("DESCRIPTION"));
-					singleProviderList.add(rs.getString("COURSEPROVIDER"));
+					log.info(">>>>>>>>>>>>>> number of elements " + rs.getString("CODE"));
+					//singleProviderList.add(rs.getString("UNIQUEPREFIX"));
+				//	singleProviderList.add(rs.getString("SHORTNAME"));
 
-					final Collection<String> singleproviderColleciton = singleProviderList;
-					prefixCollection.add(singleproviderColleciton);
+					final Collection<String> singleProviderColleciton = singleProviderList;
+					prefixCollection.add(singleProviderColleciton);
 				}
 
-			}
 
 		} catch (SQLException sqlException) {
 
