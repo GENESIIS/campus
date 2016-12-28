418df0d266d9bdfc6bee850738f57cf9a0ba1c2d
dushanthacb <dushantha@genesiis.com>
2016-12-28 17:46:40 +0530

    CAM 47: create the package for the servlet

---
+++ campus.war/src/com/genesiis/campus/tutorController/TutorController.java
@@ -0,0 +1,44 @@
+package com.genesiis.campus.tutorController;
+
+import com.genesiis.campus.controller.CampusController;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TutorController
+ */
+@WebServlet("/TutorController")
+public class TutorController extends CampusController {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public TutorController() {
+    	 super();
+    }
+    
+   
+	public void init() throws ServletException {
+
+	}
+
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		super.doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		super.doPost(request, response);
+	}
+
+}
