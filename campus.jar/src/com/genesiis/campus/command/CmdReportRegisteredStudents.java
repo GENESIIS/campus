545cf35a18ecaa7c1475ecb358fc133cee173db2
Dumani Jayasinghe <dumani@genesiis.com>
2016-12-28 16:44:33 +0530

    c53-report-registered-students-MP-dj Implement execute method

--- campus.jar/src/com/genesiis/campus/command/CmdReportRegisteredStudents.java
+++ campus.jar/src/com/genesiis/campus/command/CmdReportRegisteredStudents.java
@@ -3,6 +3,7 @@ package com.genesiis.campus.command;
 //DJ 20161228 c53-report-registered-students-MP-dj created CmdReportRegisteredStudents.java
 
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.View;
 import com.genesiis.campus.util.IDataHelper;
 
 import org.apache.log4j.Logger;
@@ -13,12 +14,27 @@ import java.sql.SQLException;
 public class CmdReportRegisteredStudents implements ICommand {
 	
 	static Logger log = Logger.getLogger(CmdReportCoursesByCourseProvider.class.getName());
-
+	/**
+	 * @author DJ
+	 * @param helper
+	 * @param iView
+	 * @return iView
+	 * @throws Exception
+	 */
 	@Override
-	public IView execute(IDataHelper helper, IView view) throws SQLException,
+	public IView execute(IDataHelper helper, IView iView) throws SQLException,
 			Exception {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+
+			iView = new View();
+			String cco = helper.getParameter("CCO");
+
+		} catch (Exception exception) {
+			log.error("execute() : Exception " + exception.toString());
+			throw exception;
+		}
+		return iView;
+
 	}
 
 }
