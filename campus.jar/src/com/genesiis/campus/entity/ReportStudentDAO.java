e3d0cf84e694046d72ffd61db3205afb5f2ad2a1
Dumani Jayasinghe <dumani@genesiis.com>
2016-12-29 06:44:39 +0530

    c53-report-registered-students-MP Initiate ReportStudentDAO and create findById()

---
+++ campus.jar/src/com/genesiis/campus/entity/ReportStudentDAO.java
@@ -0,0 +1,112 @@
+package com.genesiis.campus.entity;
+
+
+//DJ 20161229 c53-report-registered-students-MP-dj created ReportStudentDAO.java
+
+import com.genesiis.campus.entity.model.Programme;
+import com.genesiis.campus.util.ConnectionManager;
+import com.genesiis.campus.util.DaoHelper;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import org.apache.log4j.Logger;
+
+
+
+public class ReportStudentDAO  implements ICrud{
+	
+	static org.apache.log4j.Logger log = Logger.getLogger(ReportStudentDAO.class
+			.getName());
+
+	@Override
+	public int add(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<Collection<String>> findById(Object code)
+			throws SQLException, Exception {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet resultSet = null;
+		final Collection<Collection<String>> registeredStudentList = new ArrayList<Collection<String>>();
+		Programme programme = new Programme();
+
+		try {
+			conn = ConnectionManager.getConnection();
+			final StringBuilder sb = new StringBuilder("");
+			stmt = conn.prepareStatement(sb.toString());
+			stmt.setInt(1, programme.getCourseProvider());
+			
+			
+			resultSet= stmt.executeQuery();			
+			while (resultSet.next()) {
+			}
+
+		} catch (SQLException sqlException) {
+			log.info("findById() sqlException" + sqlException.toString());
+			throw sqlException;
+		} catch (Exception e) {
+			log.info("findById() Exception" + e.toString());
+			throw e;
+		} finally {
+			DaoHelper.cleanup(conn, stmt, resultSet);
+		}
+		return registeredStudentList;
+			
+		
+	}
+
+	@Override
+	public Collection<Collection<String>> getAll() throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int add(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<Collection<String>> findById(Object object,
+			Connection conn) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
