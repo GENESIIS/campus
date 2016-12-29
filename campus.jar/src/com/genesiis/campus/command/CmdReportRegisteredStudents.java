83bf9931cf04893fc26f22ce2a96768eb63a6160
Dumani Jayasinghe <dumani@genesiis.com>
2016-12-29 06:46:24 +0530

    c53-report-registered-students-MP Identify the command and created generateReportResults()

--- campus.jar/src/com/genesiis/campus/command/CmdReportRegisteredStudents.java
+++ campus.jar/src/com/genesiis/campus/command/CmdReportRegisteredStudents.java
@@ -2,13 +2,25 @@ package com.genesiis.campus.command;
 
 //DJ 20161228 c53-report-registered-students-MP-dj created CmdReportRegisteredStudents.java
 
+import com.genesiis.campus.entity.CourseProviderDAO;
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.ProgrammeDAO;
+import com.genesiis.campus.entity.ReportStudentDAO;
 import com.genesiis.campus.entity.View;
+import com.genesiis.campus.entity.model.Programme;
+import com.genesiis.campus.entity.model.Student;
 import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.validation.Operation;
+import com.genesiis.campus.validation.UtilityHelper;
 
 import org.apache.log4j.Logger;
 
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Date;
 
 
 public class CmdReportRegisteredStudents implements ICommand {
@@ -26,8 +38,20 @@ public class CmdReportRegisteredStudents implements ICommand {
 			Exception {
 		try {
 
-			iView = new View();
+			iView=new View();
 			String cco = helper.getParameter("CCO");
+			
+			switch (Operation.getOperation(cco)) {
+			case SEARCH_VIEW_REGISTERED_STUDENTS:
+				final Collection<Collection<String>> providerList = new CourseProviderDAO().getAll();
+				iView.setCollection(providerList);
+				break;
+			case REPORT_REGISTERED_STUDENTS:
+				generateReportResults(helper);
+				break;
+			default:
+				break;
+			}
 
 		} catch (Exception exception) {
 			log.error("execute() : Exception " + exception.toString());
@@ -35,6 +59,33 @@ public class CmdReportRegisteredStudents implements ICommand {
 		}
 		return iView;
 
+	}
+	
+	/** Identify input search parameters and retrieve particular  result set according to search criteria.
+	 * @author DJ
+	 * @param helper
+	 * @throws Exception
+	 */
+	private void generateReportResults(IDataHelper helper) {
+		String startDateString = helper.getParameter("startDate");
+		String endDateString = helper.getParameter("endDate");
+		final Student student = new Student();
+		try {
+			final DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+			/*if (UtilityHelper.isNotEmpty(startDateString)) {
+				student.setDisplayStartDate(df.parse((startDateString)));
+			}
+			if (UtilityHelper.isNotEmpty(endDateString)) {
+				student.setExpiryDate((Date) df.parse((endDateString)));
+			}*/
+
+		} catch (ParseException parseException) {
+			log.error("execute() : ParseException "	+ parseException.toString());
+			throw parseException;
+		}
+		final Collection<Collection<String>> registeredStudentList = new ReportStudentDAO().findById(student);
+		helper.setAttribute("registeredStudentList", registeredStudentList);
+		
 	}
 
 }
