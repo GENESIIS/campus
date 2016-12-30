1eea2a892e555bf8b7b948774bf5e60eebcd9beb
Dumani Jayasinghe <dumani@genesiis.com>
2016-12-31 01:20:56 +0530

    c52-report-banner-statistics-MP-dj Implementing populateResultTable()

--- campus.war/web/dist/js/report/banner-statistics.js
+++ campus.war/web/dist/js/report/banner-statistics.js
@@ -8,27 +8,25 @@ $(document).ready(function() {
 		},
 		dataType : "json",
 		success : function(response) {
-			getAjaxProviderData(response);
+			loadReportBannerStatisticsView(response);
 		},
 		error : function(jqXHR, exception) {
-			var msg = 0;
-			if (jqXHR.status === 0) {
-				msg = 'Not connect.\n Verify Network.';
-			} else if (jqXHR.status == 404) {
-				msg = 'Requested page not found. [404]';
-			} else if (jqXHR.status == 500) {
-				msg = 'Internal Server Error [500].';
-			} else if (exception === 'timeout') {
-				msg = 'Time out error.';
-			} else {
-				msg = 'Internal error is occurred. Please try again.';
-			}
-			alert(msg);
+			errorCodeGeneration(jqXHR, exception);
 		}
 	});
 
 	$('#pagelist').bind('change', function(event) {		
-		var pageCode = $('#pagelist').val();
+		var pageName = $('#pagelist').val();
+		var pageCode = 0;
+		
+		var option = $('#pageName').find('option');
+		for (var i = 0; i < option.length; i++) {
+			$('#pageName').find('option')[i].outerHTML;
+			if (option[i].text == pageName) {
+				pageCode = option[i].attributes[0].value;
+				break;
+			}
+		}
 
 		$.ajax({
 			url : '../../ReportController',
@@ -41,19 +39,7 @@ $(document).ready(function() {
 				getPageWisePageSlots(response);
 			},
 			error : function(jqXHR, exception) {
-				var msg = 0;
-				if (jqXHR.status === 0) {
-					msg = 'Not connect.\n Verify Network.';
-				} else if (jqXHR.status == 404) {
-					msg = 'Requested page not found. [404]';
-				} else if (jqXHR.status == 500) {
-					msg = 'Internal Server Error [500].';
-				} else if (exception === 'timeout') {
-					msg = 'Time out error.';
-				} else {
-					msg = 'Internal error is occurred. Please try again.';
-				}
-				alert(msg);
+				errorCodeGeneration(jqXHR, exception);
 			}
 		});
 
@@ -72,19 +58,7 @@ $(document).ready(function() {
 				getPageSlotWiseAdvertiser(response);
 			},
 			error : function(jqXHR, exception) {
-				var msg = 0;
-				if (jqXHR.status === 0) {
-					msg = 'Not connect.\n Verify Network.';
-				} else if (jqXHR.status == 404) {
-					msg = 'Requested page not found. [404]';
-				} else if (jqXHR.status == 500) {
-					msg = 'Internal Server Error [500].';
-				} else if (exception === 'timeout') {
-					msg = 'Time out error.';
-				} else {
-					msg = 'Internal error is occurred. Please try again.';
-				}
-				alert(msg);
+				errorCodeGeneration(jqXHR, exception);
 			}
 		});
 
@@ -100,11 +74,11 @@ $(document).ready(function() {
 
 });
 
-function getAjaxProviderData(response) {
+function loadReportBannerStatisticsView(response) {
 	var htmlstr = "";
 	$.each(response.result, function(index, value) {
 		if (value != null && value.length > 0) {
-			htmlstr += '<option value="' + value[0] + '">' + value[1]
+			htmlstr += '<option val="' + value[0] + '">' + value[1]
 					+ '</option>';
 		}
 
@@ -165,7 +139,7 @@ function loadResultSet(event){
 			populateResultTable(response);
 		},
 		error : function(jqXHR, exception) {
-			
+			errorCodeGeneration(jqXHR, exception);			
 		}
 		});
 	
@@ -183,12 +157,22 @@ function populateResultTable(response) {
 	headertr += '<td>' + callerPage + '</td>';
 	headertr += '<td>' + date + '</td>';
 	coursesListTable.append(headertr);
-	var count = 1;
+	var totalResultCount=0;
 
 	$.each(response.bannerStatDetails, function(index, value) {
 		$('#resultSetDiv').show();
 		if (value != null && value.length > 0) {
-
+			totalResultCount ++;
+			var code = value[0].toString();
+			var name = value[1].toString();
+			var des = value[2].toString();
+						
+			var tr = '<tr>' ;
+			tr += '<td>' + totalResultCount + '</td>';
+			tr += '<td>' + code  + '</td>';
+			tr += '<td>' + name  + '</td>';
+			tr += '<td>' + des  + '</td>';			
+			coursesListTable.append(tr);	
 		}
 
 	});
@@ -201,5 +185,21 @@ function clearParameters(event){
 	$('#bannerList').val(""); 	
 	$('#startdate').val(" "); 
 	$('#enddate').val(" "); 	
+}
+
+function errorCodeGeneration(jqXHR, exception){
+	var msg = '';
+	   if (jqXHR.status === 0) {
+         msg = 'Not connect.\n Verify Network.';
+     } else if (jqXHR.status == 404) {
+         msg = 'Requested page not found. [404]';
+     } else if (jqXHR.status == 500) {
+         msg = 'Internal Server Error [500].';
+     } else if (exception === 'timeout') {
+         msg = 'Time out error.';
+     }  else {
+         msg = 'Uncaught Error.\n' + jqXHR.responseText;
+     }	        
+ alert(msg);	
 }
 
