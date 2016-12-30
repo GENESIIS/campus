5d26e60be1baff2c7d745c192afd6dda06b35ba6
Dumani Jayasinghe <dumani@genesiis.com>
2016-12-30 15:20:13 +0530

    c51-report-courses-by-course-provider-MP Renamed DTO as searchDTO

--- campus.jar/src/com/genesiis/campus/entity/model/ProgrammeSearchDTO.java
+++ campus.jar/src/com/genesiis/campus/entity/model/ProgrammeSearchDTO.java
@@ -1,62 +1,63 @@
 package com.genesiis.campus.entity.model;
 
+//DJ 20161230 c51-report-courses-by-course-provider-MP-dj renamed as ProgrammeSearchDTO.java
+
 import java.util.Date;
 
-
-
-public class Programme {
-	private int code;
-	private String name;
-	private int courseProvider;
+public class ProgrammeSearchDTO {
+	private int programmeCode;
+	private int courseProviderCode;
 	private Date displayStartDate;
 	private Date expiryDate;
 	private int providerStatus;
 	private int programmeStatus;
+
 	public int getCode() {
-		return code;
+		return programmeCode;
 	}
+
 	public void setCode(int code) {
-		this.code = code;
+		this.programmeCode = code;
 	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+
 	public int getCourseProvider() {
-		return courseProvider;
+		return courseProviderCode;
 	}
+
 	public void setCourseProvider(int courseProvider) {
-		this.courseProvider = courseProvider;
+		this.courseProviderCode = courseProvider;
 	}
+
 	public Date getDisplayStartDate() {
 		return displayStartDate;
 	}
+
 	public void setDisplayStartDate(Date displayStartDate) {
 		this.displayStartDate = displayStartDate;
 	}
+
 	public Date getExpiryDate() {
 		return expiryDate;
 	}
+
 	public void setExpiryDate(Date expiryDate) {
 		this.expiryDate = expiryDate;
 	}
-	
+
 	public int getProviderStatus() {
 		return providerStatus;
 	}
+
 	public void setProviderStatus(int providerStatus) {
 		this.providerStatus = providerStatus;
 	}
+
 	public int getProgrammeStatus() {
 		return programmeStatus;
 	}
+
 	public void setProgrammeStatus(int programmeStatus) {
 		this.programmeStatus = programmeStatus;
 	}
-	
-	
-	
 
 }
