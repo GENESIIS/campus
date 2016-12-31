3d05a03bf391aaab4664030da835b6e96863a867
jayani1991 <jayani@genesiis.com>
2016-12-31 10:39:31 +0530

    c39-add-course-provider case LIST_PROVIDER_REGISTRATION_PAGE modified in CmdListCourseProviderRegisterPage

--- campus.jar/src/com/genesiis/campus/command/CmdListCourseProviderRegisterPage.java
+++ campus.jar/src/com/genesiis/campus/command/CmdListCourseProviderRegisterPage.java
@@ -3,6 +3,7 @@ package com.genesiis.campus.command;
 //20161209 JH c39-add-course-provider CmdListCourseProviderRegisterPage.java created
 //20161220 JH c39-add-course-provider execute method coding: added switch case
 //20161229 JH c39-add-course-provider added new switch case LIST_COUESE_PROVIDER_TYPES 
+//20161230 JH c39-add-course-provider case LIST_PROVIDER_REGISTRATION_PAGE modified 
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ import java.util.Collection;
 import org.apache.log4j.Logger;
 
 import com.genesiis.campus.entity.Country2DAO;
+import com.genesiis.campus.entity.CourseProviderTypeDAO;
 import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.entity.TownDAO;
@@ -28,14 +30,19 @@ public class CmdListCourseProviderRegisterPage implements ICommand {
 		Collection<Collection<String>> requestData = new ArrayList<Collection<String>>();
 		Collection<Collection<String>> countryCollection = new ArrayList<Collection<String>>();
 		Collection<Collection<String>> townCollection = new ArrayList<Collection<String>>();
+		Collection<Collection<String>> providerTypeCollection = new ArrayList<Collection<String>>();
 		try {
-
 			Operation op = Operation.getOperation(helper.getCommandCode());
 			switch (op) {
 			case LIST_PROVIDER_REGISTRATION_PAGE:
 				ICrud country2Dao = new Country2DAO();
 				countryCollection = country2Dao.getAll();
 				helper.setAttribute("countryArrayList", countryCollection);
+				ICrud providerTypeDAO = new CourseProviderTypeDAO();
+
+				//get course provider types
+				providerTypeCollection = providerTypeDAO.getAll();
+				helper.setAttribute("providerTypes", providerTypeCollection);
 				break;
 
 			case DISPLAY_TOWN_DATA:
@@ -44,8 +51,6 @@ public class CmdListCourseProviderRegisterPage implements ICommand {
 				int countryId = Integer.parseInt(helper.getParameter("country"));
 				townCollection = townDao.findById(countryId);
 				helper.setAttribute("townArrayList", townCollection);
-				break;
-			case LIST_COUESE_PROVIDER_TYPES:
 				break;
 			default:
 				Collection<String> signUpdata = new ArrayList<String>();
