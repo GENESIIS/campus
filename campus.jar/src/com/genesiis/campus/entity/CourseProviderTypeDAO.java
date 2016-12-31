b39a4b1fa758aa0f1b03c1c7a0a48ab03d45a6ff
jayani1991 <jayani@genesiis.com>
2016-12-31 10:40:45 +0530

    c39-add-course-provider getAll() method modified in CourseProviderTypeDAO

--- campus.jar/src/com/genesiis/campus/entity/CourseProviderTypeDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/CourseProviderTypeDAO.java
@@ -1,14 +1,21 @@
 package com.genesiis.campus.entity;
 
+//20161230 JH c39-add-course-provider CourseProviderTypeDAO.java created
+//20161231 JH c39-add-course-provider getAll() method modified
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 
-//20161230 JH c39-add-course-provider CourseProviderTypeDAO.java created
+import org.apache.log4j.Logger;
 
-public class CourseProviderTypeDAO implements ICrud{
+import com.genesiis.campus.util.ConnectionManager;
+
+public class CourseProviderTypeDAO implements ICrud {
+	static Logger log = Logger.getLogger(CourseProviderTypeDAO.class.getName());
 
 	public int add(Object object) throws SQLException, Exception {
 		// TODO Auto-generated method stub
@@ -31,24 +38,45 @@ public class CourseProviderTypeDAO implements ICrud{
 	}
 
 	public Collection getAll() throws SQLException, Exception {
-		Connection userTypeConnection = null;
-		PreparedStatement prepaire = null;
-		ResultSet userCode = null;
-		
-		try{
+		Connection conn = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;		
+		Collection<Collection<String>> providerTypeCollection = new ArrayList<Collection<String>>();
+
+		try {
+
+			String courseProviderTypes = "SELECT * FROM [CAMPUS].[COURSEPROVIDERTYPE] WHERE ISACTIVE = 1 ";
 			
-			String courseProviderTypes = "SELECT * FROM [CAMPUS].[COURSEPROVIDERTYPE] ";			
-		}catch (SQLException sqle) {
+			conn = ConnectionManager.getConnection();
+			preparedStatement = conn.prepareStatement(courseProviderTypes);
+
+			resultSet = preparedStatement.executeQuery();
+
+			while(resultSet.next()){
+				final Collection<String> singleProviderType = new ArrayList<String>();
+				
+				singleProviderType.add(resultSet.getString("CODE"));
+				singleProviderType.add(resultSet.getString("NAME"));
+				singleProviderType.add(resultSet.getString("DESCRIPTION"));
+				
+				providerTypeCollection.add(singleProviderType);				
+			}
+			
+		} catch (SQLException sqle) {
 			log.error("findById(): SQLException " + sqle.toString());
 			throw sqle;
 		} catch (Exception exp) {
 			log.error("findById(): Wxcepption" + exp.toString());
 			throw exp;
-		} finally{
-			if (prepaire != null)
-				prepaire.close();
+		} finally {
+			if(resultSet != null){
+				resultSet.close();
+			}if (preparedStatement != null)
+				preparedStatement.close();
+		}if(conn != null){
+			conn.close();
 		}
-		return null;
+		return providerTypeCollection;
 	}
 
 	public int add(Object object, Connection conn) throws SQLException,
