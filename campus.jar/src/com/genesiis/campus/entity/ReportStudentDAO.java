40c7818fb97add9606c3c3596bfc0c69f762c5bd
Dumani Jayasinghe <dumani@genesiis.com>
2017-01-02 15:47:14 +0530

    c53-report-registered-students-MP Implement the result set

--- campus.jar/src/com/genesiis/campus/entity/ReportStudentDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/ReportStudentDAO.java
@@ -3,8 +3,11 @@ package com.genesiis.campus.entity;
 
 //DJ 20161229 c53-report-registered-students-MP-dj created ReportStudentDAO.java
 
+import com.genesiis.campus.entity.model.Student;
 import com.genesiis.campus.util.ConnectionManager;
 import com.genesiis.campus.util.DaoHelper;
+import com.genesiis.campus.util.DataHelper;
+import com.genesiis.campus.validation.UtilityHelper;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -12,6 +15,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+
 import org.apache.log4j.Logger;
 
 
@@ -40,24 +44,34 @@ public class ReportStudentDAO  implements ICrud{
 	}
 
 	@Override
-	public Collection<Collection<String>> findById(Object code)
+	public Collection<Collection<String>> findById(Object studentDTO)
 			throws SQLException, Exception {
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		ResultSet resultSet = null;
+		Student student=new Student();
 		final Collection<Collection<String>> registeredStudentList = new ArrayList<Collection<String>>();
-		 
+		 if(UtilityHelper.isNotEmptyObject(studentDTO)){
+			  student=(Student)studentDTO;			 
+		 }
 
 		try {
 			conn = ConnectionManager.getConnection();
-			final StringBuilder sb = new StringBuilder("SELECT STUDENT.CODE,  CONCAT(STUDENT.FIRSTNAME,' ' ,STUDENT.MIDDLENAME,' ' ,STUDENT.LASTNAME) AS STUDENTNAME, STUDENT.CRTON AS REGISTEREDDATE, STUDENT.LASTLOGGEDINDATE ");
-			sb.append(" FROM CAMPUS.STUDENT STUDENT WHERE STUDENT.ACCOUNTTYPE=?");
+			final StringBuilder sb = new StringBuilder("SELECT STUDENT.CODE AS STUDENTCODE,  CONCAT(STUDENT.FIRSTNAME,' ' ,STUDENT.MIDDLENAME,' ' ,STUDENT.LASTNAME) AS STUDENTNAME, STUDENT.ISACTIVE AS STUDENTSTATUS, STUDENT.CRTON AS REGISTEREDDATE, ");
+			sb.append("STUDENT.LASTLOGGEDINDATE AS LASTLOGGEDINDATE  FROM CAMPUS.STUDENT STUDENT WHERE STUDENT.ACCOUNTTYPE=?");
 			stmt = conn.prepareStatement(sb.toString());
-			stmt.setInt(1, 1);
+			stmt.setInt(1,student.getAccountType());
 			
 			
 			resultSet= stmt.executeQuery();			
 			while (resultSet.next()) {
+				final ArrayList<String> singleProvider = new ArrayList<String>();
+				singleProvider.add(resultSet.getString("STUDENTCODE"));				
+				singleProvider.add(resultSet.getString("STUDENTNAME"));	
+				singleProvider.add(resultSet.getString("STUDENTSTATUS"));
+				singleProvider.add(resultSet.getString("REGISTEREDDATE"));
+				singleProvider.add(resultSet.getString("LASTLOGGEDINDATE"));
+				registeredStudentList.add(singleProvider);
 			}
 
 		} catch (SQLException sqlException) {
