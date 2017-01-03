ab92fbb4387e87a49e840e7e21283ad2c3eafaf6
jayani1991 <jayani@genesiis.com>
2017-01-03 21:10:51 +0530

    JH c39-add-course-provider removed if condition for PublicControllder servlet from FactoryProducer class

--- campus.jar/src/com/genesiis/campus/factory/FactoryProducer.java
+++ campus.jar/src/com/genesiis/campus/factory/FactoryProducer.java
@@ -1,25 +1,23 @@
 package com.genesiis.campus.factory;
 
 //20161123 JH c39-add-course-provider added conditions for AdminController servlet
+//20170103 JH c39-add-course-provider removed if condition for PublicControllder servlet
 
 /**
- * FactoryProducer class bares the responsibility of producing 
- * the Factory  
- *
+ * FactoryProducer class bares the responsibility of producing the Factory
+ * 
  */
 
 public class FactoryProducer {
 	/**
-	 * getFactory method provides the IAbstractCmdFactory  
-	 * @param String choice decides the command factory associated with the
-	 * choice
+	 * getFactory method provides the IAbstractCmdFactory
+	 * 
+	 * @param String
+	 *            choice decides the command factory associated with the choice
 	 * @return ICmdFactory
 	 */
 	public static ICmdFactory getFactory(String choice) {
-		if (choice.equalsIgnoreCase("/PublicController")) {
-			return new PublicCmdFactory();
-
-		} else if (choice.equalsIgnoreCase("/AdminController")) {
+		if (choice.equalsIgnoreCase("/AdminController")) {
 			return new AdminCmdFactory();
 		}
 
