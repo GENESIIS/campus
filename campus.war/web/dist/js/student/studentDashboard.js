70bba35acdf09ba36dbb8a77713abd3d886af377
Miyuru-Genesiis <miyuru@genesiis.com>
2017-01-05 06:26:33 +0530

    c25-student-dashboard-MP - Added code to hide recommended programmes and institutes sections from view when no data is available for them; disabled the display of section that shows recent activity

--- campus.war/web/dist/js/student/studentDashboard.js
+++ campus.war/web/dist/js/student/studentDashboard.js
@@ -11,6 +11,8 @@
   * 			feedback messages to the user in case of error or absence of data
   * 20161230 MM c25-student-create-dashboard-MP - Added code to make Ajax call to retrieve data on 
   * 			student recent activities
+  * 20170105 MM c25-student-dashboard-MP - Added code to hide recommended programmes and institutes sections from view 
+  * 			when no data is available for them; disabled the display of section that shows recent activity
   * 
   */ 
 
@@ -80,32 +82,37 @@ function assignContentToCarousel() {
 //	alert("inside assignContentToCaraousel()");
 	
 	var programmeCollectionFetched = window.programmeCollectionFetched;
-	
-	var carouselInnerDiv = $('.carousel-inner');
-	var carouselItemsHtml = '';
-	var currentProgramme = null;
-	for (var i = 0; i < programmeCollectionFetched.length; i++) {
 		
-		currentProgramme = programmeCollectionFetched[i];
+	if (programmeCollectionFetched.length > 0) {
+		var carouselInnerDiv = $('.carousel-inner');
+		var carouselItemsHtml = '';
+		var currentProgramme = null;
 		
-		carouselItemsHtml += '<div class="item';
-		if (i == 1) {
-			carouselItemsHtml += ' active';
+		for (var i = 0; i < programmeCollectionFetched.length; i++) {
+			
+			currentProgramme = programmeCollectionFetched[i];
+			
+			carouselItemsHtml += '<div class="item';
+			if (i == 1) {
+				carouselItemsHtml += ' active';
+			}
+			carouselItemsHtml += '">\
+		        <blockquote>\
+		    <div class="row">\
+		        <div class="col-sm-8 col-sm-offset-2">';
+			carouselItemsHtml += '<label>' + currentProgramme[1] + '</label>';
+			carouselItemsHtml += '<p>' + currentProgramme[4] + '</p>';
+			carouselItemsHtml += '<small>' + currentProgramme[17] + '</small>';
+			carouselItemsHtml += '</div>\
+			    </div>\
+				</blockquote>\
+				</div>';	
 		}
-		carouselItemsHtml += '">\
-	        <blockquote>\
-	    <div class="row">\
-	        <div class="col-sm-8 col-sm-offset-2">';
-		carouselItemsHtml += '<label>' + currentProgramme[1] + '</label>';
-		carouselItemsHtml += '<p>' + currentProgramme[4] + '</p>';
-		carouselItemsHtml += '<small>' + currentProgramme[17] + '</small>';
-		carouselItemsHtml += '</div>\
-		    </div>\
-			</blockquote>\
-			</div>';
+		
+		carouselInnerDiv.html(carouselItemsHtml);
+	} else {
+		$('.w-rec-courses').hide();
 	}
-	
-	carouselInnerDiv.html(carouselItemsHtml);	
 }
 
 
@@ -116,72 +123,77 @@ function constructInstituteListing() {
 	var courseProviderLogoPath = window.courseProviderLogoPathFetched;
 	
 	var instituteULElement = $('div.rec-institute ul');
-	var instituteListHtml = '';
-	var currentInstitute = null;
-	var courseProviderImage = null;
-	var imageSuffix = '_small';
-	var imageExtension = '.jpg';
-	var imageSuffixAndExtension = imageSuffix + imageExtension;
-	for (var i = 0; i < instituteCollectionFetched.length; i++) {
-		
-		currentInstitute = instituteCollectionFetched[i];
-		var currentCourseProviderCode = currentInstitute[0];
-		var currentCourseProviderShortName = currentInstitute[1];
-		courseProviderImage = courseProviderLogoPath + '/' + currentCourseProviderCode + '/' + currentCourseProviderCode + imageSuffixAndExtension;
-		
-		instituteListHtml += '<li class="col-lg-12 col-sm-12 col-md-12">\
-        <div class="inst-logo col-lg-3 col-sm-12 col-md-4">\
-			<img height="60px" width="180px" src="' + courseProviderImage + '" alt="' + currentCourseProviderShortName + ' logo">\
-			</div>\
-        <a href="javascript:" class="col-lg-9 col-sm-12 col-md-8" data-institute-code="' + currentCourseProviderCode + '">' + currentCourseProviderShortName + ' - ' + currentInstitute[2] + '</a>\
-    </li>';
-	}
 	
-	instituteULElement.html(instituteListHtml);	
-	
-	getStudentActivities();
-}
-
-function getStudentActivities () {
-	$.ajax({
-		url : '/StudentController',
-		method : 'POST',
-		data : {
-			'CCO' : 'LIST_STUDENT_RECENT_ACTIVITIES'
-		},
-		dataType : "json",
-		async : false,
-		success : function(response) {
+	if (instituteCollectionFetched.length > 0) {	
+		var instituteListHtml = '';
+		var currentInstitute = null;
+		var courseProviderImage = null;
+		var imageSuffix = '_small';
+		var imageExtension = '.jpg';
+		var imageSuffixAndExtension = imageSuffix + imageExtension;
+		for (var i = 0; i < instituteCollectionFetched.length; i++) {
 			
-			if (response !== undefined && response !== null) {
-				window.studentActivitiesFetched = response.result;
-			}			
-		},
-		error : function(response) {
-			alert("Ajax error");
-			var messageList = [];
-			messageList.push("There was an error retrieving data to display from the server.");
-			messageList.push("Please check that you are connected to the Internet and that you are sending the correct data.");
-			showMessages(messageList, window.messageType.ERROR);
+			currentInstitute = instituteCollectionFetched[i];
+			var currentCourseProviderCode = currentInstitute[0];
+			var currentCourseProviderShortName = currentInstitute[1];
+			courseProviderImage = courseProviderLogoPath + '/' + currentCourseProviderCode + '/' + currentCourseProviderCode + imageSuffixAndExtension;
+			
+			instituteListHtml += '<li class="col-lg-12 col-sm-12 col-md-12">\
+	        <div class="inst-logo col-lg-3 col-sm-12 col-md-4">\
+				<img height="60px" width="180px" src="' + courseProviderImage + '" alt="' + currentCourseProviderShortName + ' logo">\
+				</div>\
+	        <a href="javascript:" class="col-lg-9 col-sm-12 col-md-8" data-institute-code="' + currentCourseProviderCode + '">' + currentCourseProviderShortName + ' - ' + currentInstitute[2] + '</a>\
+	    </li>';
 		}
-	});	
-};
-
-function constructActivityListing() {
-	
-	var studentActivities = window.studentActivitiesFetched;
-	
-	var activityULElement = $('div.widget-content ul.ul-activity');
-	var activityListHtml = '';
-
-	for (var i = 0; i < studentActivities.length; i++) {
 		
-		currentActivity = studentActivities[i];
-	
-		activityListHtml += '<li>Applied to BSc in Software Engineering at "Esoft Metro Campus" <br><span class="act-time">~ 20 March 2012 - Now </span></li>';
+		instituteULElement.html(instituteListHtml);	
+	} else {
+		$('.w-rec-institute').hide();
 	}
 	
-	activityULElement.html(instituteListHtml);	
-	
-	getStudentActivities();
+//	getStudentActivities();
 }
+
+//function getStudentActivities () {
+//	$.ajax({
+//		url : '/StudentController',
+//		method : 'POST',
+//		data : {
+//			'CCO' : 'LIST_STUDENT_RECENT_ACTIVITIES'
+//		},
+//		dataType : "json",
+//		async : false,
+//		success : function(response) {
+//			
+//			if (response !== undefined && response !== null) {
+//				window.studentActivitiesFetched = response.result;
+//			}			
+//		},
+//		error : function(response) {
+//			alert("Ajax error");
+//			var messageList = [];
+//			messageList.push("There was an error retrieving data to display from the server.");
+//			messageList.push("Please check that you are connected to the Internet and that you are sending the correct data.");
+//			showMessages(messageList, window.messageType.ERROR);
+//		}
+//	});	
+//};
+//
+//function constructActivityListing() {
+//	
+//	var studentActivities = window.studentActivitiesFetched;
+//	
+//	var activityULElement = $('div.widget-content ul.ul-activity');
+//	var activityListHtml = '';
+//
+//	for (var i = 0; i < studentActivities.length; i++) {
+//		
+//		currentActivity = studentActivities[i];
+//	
+//		activityListHtml += '<li>Applied to BSc in Software Engineering at "Esoft Metro Campus" <br><span class="act-time">~ 20 March 2012 - Now </span></li>';
+//	}
+//	
+//	activityULElement.html(instituteListHtml);	
+//	
+//	getStudentActivities();
+//}
