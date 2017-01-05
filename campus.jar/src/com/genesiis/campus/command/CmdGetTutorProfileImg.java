5d2e6b1ca29bba92e0da1986f2c27f477c337617
dushanthacb <dushantha@genesiis.com>
2017-01-05 14:53:02 +0530

    c47-tutor-add-tutor-information-upload-image-dn introduced getExistingImageFilePathFromTheStorage() getProfilePictureIfExisitElseDefault()

--- campus.jar/src/com/genesiis/campus/command/CmdGetTutorProfileImg.java
+++ campus.jar/src/com/genesiis/campus/command/CmdGetTutorProfileImg.java
@@ -1,12 +1,16 @@
 package com.genesiis.campus.command;
 //20170103 DN c47-tutor-add-tutor-information-upload-image-dn create the CmdGetTutorProfileImg.java class
+//20170105 DN c47-tutor-add-tutor-information-upload-image-dn introduced getExistingImageFilePathFromTheStorage()
+//getProfilePictureIfExisitElseDefault()
 import java.sql.SQLException;
 
 import org.apache.log4j.Logger;
 
 import com.genesiis.campus.util.FileUtility;
+import com.genesiis.campus.util.ImageUtility;
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.validation.SystemConfig;
 
 
 /**
@@ -17,18 +21,82 @@ import com.genesiis.campus.util.IDataHelper;
  */
 public class CmdGetTutorProfileImg implements ICommand {
 
-	static Logger log = Logger.getLogger(CmdGetTutorProfileImg.class.getName());
+	static final Logger log = Logger.getLogger(CmdGetTutorProfileImg.class.getName());
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
-		FileUtility utility = new FileUtility();
+		try{
+			return getExistingImageFilePathFromTheStorage(helper,view);
+			
+		} catch (SQLException sqle) {
+			log.error("execute():SQLExceptiion :"+sqle.toString());
+			throw sqle;
+		} catch (Exception exe) {
+			log.error("execute():Exception :"+exe.toString());
+			throw exe;
+		}
+	}
+	
+	
+	private IView getExistingImageFilePathFromTheStorage(IDataHelper helper, IView view) throws Exception{
+		
+		//###########################################################################################
+		// TUTOR CODE-GENDER HAS TO BE OBTAINED FROM SESSION - This section of code vulnerable to
+		// change once the session parameters are set.	
+		//###########################################################################################
+		
 		Integer tutorCodeFromSession = (Integer) helper.getSession(false).getAttribute("userid");
-		int tutorCode = (tutorCodeFromSession!=null)?tutorCodeFromSession:1; //TUTOR CODE HAS TO BE OBTAINED FROM SESSION
-		// have to set the absolute path
-		boolean isFileExists = utility.isFileExists("", Integer.toString(tutorCode));
-		
-		
+		int tutorCode = (tutorCodeFromSession!=null)?tutorCodeFromSession:1;
+		String gender = (String) helper.getSession(false).getAttribute("gender");
+		gender = (gender!=null||gender!="")?gender:"M";
+		try{
+			String profilePicture= getProfilePictureIfExisitElseDefault(tutorCode,gender);
+			helper.setAttribute("profilePicture", profilePicture);
+		}catch (Exception exp){
+			log.error("getExistingImageFilePathFromTheStorage(): Exception :"+exp.toString());
+			throw exp;
+		}
 		return view;
 	}
+	
+	
+	private String getProfilePictureIfExisitElseDefault(int tutorCode,String gender) throws Exception{
+		try{
+			String tutorWarFileImagePath = ImageUtility
+					.getImageUploadPath(SystemConfig.TUTOR_PROFILE_IMAGE_PATH)+"/username_"+tutorCode;
+			String tutorAbsoluteImagePath = ImageUtility
+					.getImageUploadPath(SystemConfig.TUTOR_DEFAULT_PROFILE_IMAGE_ABSOLUTE_PATH)+"/username_"+tutorCode;
+			String tutorWarDefaultFileImagePath = ImageUtility
+					.getImageUploadPath(SystemConfig.TUTOR_DEFAULT_PROFILE_IMAGE_WAR_PATH);
+			String tutorAbsoluteDefaultImagePath =ImageUtility
+					.getImageUploadPath(SystemConfig.TUTOR_DEFAULT_PROFILE_IMAGE_ABSOLUTE_PATH);
+			
+			FileUtility utility = new FileUtility();
+			String pictureName = "";
+			if(utility.isFileExists(tutorAbsoluteImagePath, Integer.toString(tutorCode))){
+				pictureName = tutorWarFileImagePath+"/"+utility.getImageName();
+			}else{
+				if(gender.equalsIgnoreCase("m")){
+					pictureName=tutorWarDefaultFileImagePath+"/male_tutor.jpg";
+				}else{
+					pictureName=tutorWarDefaultFileImagePath+"/female_tutor.jpg";
+				}
+			}
+			
+			return pictureName;
+		}catch (Exception exp){
+			log.error("getProfilePictureIfExisitElseDefault(): Exception :"+exp.toString());
+			throw exp;
+		}
+		
+	
+		
+	}
+	
+	
+	
+	
+	
+	
 
 }
