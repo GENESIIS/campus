65167e271f6a96291cd68025ba9f03553269ee58
dushanthacb <dushantha@genesiis.com>
2017-01-06 17:23:49 +0530

    c47-tutor-add-tutor-information-upload-image-dn UPLOAD_SUBMITED_TUTOR_IMAGE to the map and getCommand()

--- campus.jar/src/com/genesiis/campus/factory/TutorCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/TutorCmdFactory.java
@@ -1,9 +1,11 @@
 package com.genesiis.campus.factory;
 
 //20170103 DN CAM:47 tutor: add tutor information : upload image created UPLOAD_EXISTING_PROFILE_PIC in  getCommand()
+//20170106 DN CAM:47 UPLOAD_SUBMITED_TUTOR_IMAGE to the map and getCommand()
 
 
 import com.genesiis.campus.command.CmdGetTutorProfileImg;
+import com.genesiis.campus.command.CmdUploadTutorImage;
 import com.genesiis.campus.command.ICommand;
 import com.genesiis.campus.validation.Operation;
 
@@ -11,7 +13,7 @@ public class TutorCmdFactory implements ICmdFactory {
 	static {
 	
 		map.put(Operation.UPLOAD_EXISTING_TUTOR_PROFILE_PIC, new CmdGetTutorProfileImg());
-		
+		map.put(Operation.UPLOAD_SUBMITED_TUTOR_IMAGE, new CmdUploadTutorImage());
 	}
 
 	@Override
@@ -21,6 +23,9 @@ public class TutorCmdFactory implements ICmdFactory {
 		o = Operation.getOperation(cco);
 		switch (o) {
 		case UPLOAD_EXISTING_TUTOR_PROFILE_PIC:
+			command = map.get(o);
+			break;
+		case UPLOAD_SUBMITED_TUTOR_IMAGE:
 			command = map.get(o);
 			break;
 		default:
