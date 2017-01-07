30936d6e57ff2358e05718dca31593b99f56a929
Dumani Jayasinghe <dumani@genesiis.com>
2017-01-07 18:30:41 +0530

    c6-list-available-institutes-on-the-view-MP-dj Initiated ProgrammeICrud.java

---
+++ campus.jar/src/com/genesiis/campus/entity/ProgrammeICrud.java
@@ -0,0 +1,11 @@
+package com.genesiis.campus.entity;
+
+//20170107 c6-list-available-institutes-on-the-view Initiated ProgrammeICrud.java
+
+/**The class  {@code ProgrammeICrud} is a form of Interface class.
+ * The Interface {@code ProgrammeICrud} has precise control over programme dao level manipulations. 
+ *  @author dumani DJ   
+ */
+public interface ProgrammeICrud extends ICrud {
+
+}
