62b370d3343bd88c41f3407327ab094dabd58cf0
Dumani Jayasinghe <dumani@genesiis.com>
2017-01-07 18:31:45 +0530

    c6-list-available-institutes-on-the-view-MP-dj refactored CourseProviderICrud.java

--- campus.jar/src/com/genesiis/campus/entity/CourseProviderICrud.java
+++ campus.jar/src/com/genesiis/campus/entity/CourseProviderICrud.java
@@ -9,6 +9,10 @@ import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
+/**The class  {@code CourseProviderICrud} is a form of Interface class.
+ * The Interface {@code CourseProviderICrud} has precise control over course provider dao level manipulations. 
+ *  @author dumani DJ   
+ */
 public interface CourseProviderICrud extends ICrud{
 		
 	Collection<Collection<String>> findTopViewedProviders(CourseProviderSearchDTO provider) throws SQLException,Exception;
@@ -18,5 +22,4 @@ public interface CourseProviderICrud extends ICrud{
 	Collection<Collection<String>> findFilterdCourseProviders(CourseProviderSearchDTO provider) throws SQLException,Exception;
 	
 	List<CourseProviderResultDTO> getCategoryWiseTypes(Integer categoryCode)throws SQLException,Exception;
-
 }
