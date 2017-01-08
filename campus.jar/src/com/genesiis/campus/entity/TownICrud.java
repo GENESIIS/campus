de96759507e6a9e49177125b537c8d5622014517
Dumani Jayasinghe <dumani@genesiis.com>
2017-01-08 12:20:12 +0530

    c6-list-available-institutes-on-the-view Declaring getAllDistricts() methods

--- campus.jar/src/com/genesiis/campus/entity/TownICrud.java
+++ campus.jar/src/com/genesiis/campus/entity/TownICrud.java
@@ -1,11 +1,15 @@
 package com.genesiis.campus.entity;
 
 //20170107 c6-list-available-institutes-on-the-view created TownICrud.java
+//20170108 c6-list-available-institutes-on-the-view Declaring getAllDistricts() methods
+
+import java.sql.SQLException;
+import java.util.Collection;
 
 /**The class  {@code TownICrud} is a form of Interface class.
  * The Interface {@code TownICrud} has precise control over district, town dao level manipulations. 
  *  @author dumani DJ   
  */
 public interface TownICrud extends ICrud {
-
+	public Collection<Collection<String>> getAllDistricts() throws SQLException,Exception;
 }
