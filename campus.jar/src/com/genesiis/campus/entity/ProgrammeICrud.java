c1a921e8af64019cd9bcb98c6c2f7c40571f2f15
Dumani Jayasinghe <dumani@genesiis.com>
2017-01-08 12:19:15 +0530

    c6-list-available-institutes-on-the-view Declaring new methods

--- campus.jar/src/com/genesiis/campus/entity/ProgrammeICrud.java
+++ campus.jar/src/com/genesiis/campus/entity/ProgrammeICrud.java
@@ -1,6 +1,10 @@
 package com.genesiis.campus.entity;
 
 //20170107 c6-list-available-institutes-on-the-view Initiated ProgrammeICrud.java
+//20170108 c6-list-available-institutes-on-the-view Declaring new methods
+
+import java.sql.SQLException;
+import java.util.Collection;
 
 /**The class  {@code ProgrammeICrud} is a form of Interface class.
  * The Interface {@code ProgrammeICrud} has precise control over programme dao level manipulations. 
@@ -8,11 +12,14 @@ package com.genesiis.campus.entity;
  */
 public interface ProgrammeICrud extends ICrud {
 	
-	getAllMajors();
-	getAllCategories();
-	getAllLevels();
-	getAllSemesters();
-	getAllProgrammeInterests;
+	public Collection<Collection<String>> getAllMajors()throws SQLException,Exception;
 	
+	public Collection<Collection<String>> getAllCategories()throws SQLException,Exception;
+	
+	public Collection<Collection<String>> getAllLevels()throws SQLException,Exception;
+	
+	public Collection<Collection<String>> getAllSemesters()throws SQLException,Exception;
+	
+	public Collection<Collection<String>> getAllProgrammeInterests()throws SQLException,Exception;	
 
 }
