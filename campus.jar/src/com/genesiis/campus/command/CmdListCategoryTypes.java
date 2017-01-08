b8f34d277a24b6c1f188806687b71c0e29b72530
Dumani Jayasinghe <dumani@genesiis.com>
2017-01-08 15:31:32 +0530

    c6-list-available-institutes-on-the-view  Adjust dao calls to new Icrud interfaces

--- campus.jar/src/com/genesiis/campus/command/CmdListCategoryTypes.java
+++ campus.jar/src/com/genesiis/campus/command/CmdListCategoryTypes.java
@@ -1,13 +1,17 @@
 package com.genesiis.campus.command;
 
 //DJ 20161124 c17-provider-criteria-based-filter-search created CmdListCategoryTypes.java
-//DJ 20161125 c17-provider-criteria-based-filter-search execute method implementation 
+//DJ 20161125 c17-provider-criteria-based-filter-search execute method implementation
+//DJ 20170108 c6-list-available-institutes-on-the-view  Adjust dao calls to new Icrud interfaces
 
+import com.genesiis.campus.entity.CourseProviderICrud;
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.ProgrammeICrud;
 import com.genesiis.campus.entity.dao.CourseProviderDAO;
 import com.genesiis.campus.entity.dao.CourseProviderTypeDAO;
 import com.genesiis.campus.entity.dao.LevelDAO;
 import com.genesiis.campus.entity.dao.MajorDAO;
+import com.genesiis.campus.entity.dao.ProgrammeDAO;
 import com.genesiis.campus.entity.model.CourseProviderResultDTO;
 import com.genesiis.campus.util.IDataHelper;
 import com.genesiis.campus.validation.UtilityHelper;
@@ -36,7 +40,7 @@ public class CmdListCategoryTypes implements ICommand {
 			Exception {
 		int categoryCode = 0;
 		try {
-			// List categorywise types
+			//List categorywise types
 			String categoryCodeString = helper.getParameter("categoryCode");
 			if (UtilityHelper.isNotEmpty(categoryCodeString)) {
 				if (UtilityHelper.isInteger(categoryCodeString)) {
@@ -44,8 +48,8 @@ public class CmdListCategoryTypes implements ICommand {
 				}
 			}
 			if (categoryCode > 0) {
-
-				final List<CourseProviderResultDTO> categoryTypes = new CourseProviderDAO().getCategoryWiseTypes(categoryCode);
+				final CourseProviderICrud courseProviderDAO=new CourseProviderDAO();
+				final List<CourseProviderResultDTO> categoryTypes =courseProviderDAO.getCategoryWiseTypes(categoryCode);
 
 				final Set<Integer> cpTypeCodeSet = new HashSet<>();
 				final Set<Integer> majorCodeSet = new HashSet<>();
@@ -55,23 +59,22 @@ public class CmdListCategoryTypes implements ICommand {
 					cpTypeCodeSet.add(dto.getCourserProviderType());
 					majorCodeSet.add(dto.getMajor());
 					levelCodeSet.add(dto.getLevel());
-				}
-
+				}			
 				// List course provider types for the drop down
 				if (cpTypeCodeSet != null && !cpTypeCodeSet.isEmpty()) {
-					final Collection<Collection<String>> cpTypeList = new CourseProviderTypeDAO().findCPTypesByCodes(cpTypeCodeSet);
+					final Collection<Collection<String>> cpTypeList = courseProviderDAO.findCPTypesByCPTypeCodes(cpTypeCodeSet);
 					helper.setAttribute("cpTypeList", cpTypeList);
 				}
-
+				final ProgrammeICrud programmeDAO=new ProgrammeDAO();
 				// List Majors for the drop down
 				if (majorCodeSet != null && !majorCodeSet.isEmpty()) {
-					final Collection<Collection<String>> majorList = new MajorDAO().findMajorsByMajorCodes(majorCodeSet);
+					final Collection<Collection<String>> majorList =programmeDAO.findMajorsByMajorCodes(majorCodeSet);
 					helper.setAttribute("majorList", majorList);
 				}
 
 				// List Levels for the drop down
 				if (levelCodeSet != null && !levelCodeSet.isEmpty()) {
-					final Collection<Collection<String>> levelList = new LevelDAO().findLevelsByLevelCodes(levelCodeSet);
+					final Collection<Collection<String>> levelList = programmeDAO.findLevelsByLevelCodes(levelCodeSet);
 					helper.setAttribute("levelList", levelList);
 				}
 			}
