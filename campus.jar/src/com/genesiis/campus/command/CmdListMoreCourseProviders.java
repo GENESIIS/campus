ba717547fe63b1600ef8b4fe6744e9d2f3afa483
Dumani Jayasinghe <dumani@genesiis.com>
2017-01-08 15:37:11 +0530

    c6-list-available-institutes-on-the-view Adjust dao call to new icrud interfaces

--- campus.jar/src/com/genesiis/campus/command/CmdListMoreCourseProviders.java
+++ campus.jar/src/com/genesiis/campus/command/CmdListMoreCourseProviders.java
@@ -5,12 +5,14 @@ package com.genesiis.campus.command;
 //DJ 20161030 c6-list-available-institutes-on-the-view identified get all institutes
 //DJ 20161110 c6-list-available-institutes-on-the-view identified get all and category wise course providers
 //DJ 20161123 c6-list-available-institutes-on-the-view add SystemConfig.PROVIDER_LOGO_PATH
+//DJ 20170108 c6-list-available-institutes-on-the-view Adjust dao call to new icrud interfaces
 
 import com.genesiis.campus.entity.CourseProviderICrud;
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.entity.dao.CategoryDAO;
 import com.genesiis.campus.entity.dao.CourseProviderDAO;
 import com.genesiis.campus.entity.dao.DistrictDAO;
+import com.genesiis.campus.entity.dao.ProgrammeDAO;
 import com.genesiis.campus.entity.model.CourseProviderSearchDTO;
 import com.genesiis.campus.util.IDataHelper;
 import com.genesiis.campus.validation.SystemConfig;
@@ -26,16 +28,15 @@ public class CmdListMoreCourseProviders implements ICommand {
 			.getName());
 	
 	/**
-	 * @author DJ
-	 * @param helper
-	 * @param view
-	 * @return List course provides.Identify the categorycode and retrieve appropriate course providers.
-	 * @throws Exception
+	 * @author dumani DJ
+	 * @param helper IDataHelper Object
+	 * @param iview IView Object
+	 * @return List course provides.Identify the categorycode and retrieve appropriate course providers. 
 	 */
 	@Override
 	public IView execute(IDataHelper helper, IView iview) throws SQLException,
 			Exception {
-		final CourseProviderICrud  providerDAO = new CourseProviderDAO();		
+				
 		String contextDeployLogoPath=SystemConfig.PROVIDER_LOGO_PATH.getValue1();
 		try {
 			int categoryCode = 0;
@@ -47,6 +48,7 @@ public class CmdListMoreCourseProviders implements ICommand {
 					provider.setCategory(categoryCode);
 				}				
 			}
+			final CourseProviderICrud  providerDAO = new CourseProviderDAO();
 			if (categoryCode > 0) {
 				final Collection<Collection<String>> categoryWiseCourseProviders = providerDAO.findById(provider);
 				iview.setCollection(categoryWiseCourseProviders);
@@ -54,12 +56,12 @@ public class CmdListMoreCourseProviders implements ICommand {
 				final Collection<Collection<String>> allCourseProviders = providerDAO.getAll();
 				iview.setCollection(allCourseProviders);
 			}
-			//List Category data for the drop down			
-			final Collection<Collection<String>> categoryList=new CategoryDAO().getAll();
+			//List Category data for the drop down		
+			final Collection<Collection<String>> categoryList=new ProgrammeDAO().getAllCategories();
 			helper.setAttribute("categoryList", categoryList);
 			
 			//List location for the drop down
-			final Collection<Collection<String>> districtList=new DistrictDAO().getAll();
+			final Collection<Collection<String>> districtList=new DistrictDAO().getAllDistricts();
 			helper.setAttribute("districtList", districtList);			
 			
 			helper.setAttribute("contextDeployLogoPath", contextDeployLogoPath);
