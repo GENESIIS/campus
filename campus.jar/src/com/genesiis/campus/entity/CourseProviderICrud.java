15df56e1e5e6e7fb9e6f27c4388286f57d40887a
Dumani Jayasinghe <dumani@genesiis.com>
2017-01-08 11:47:25 +0530

    c6-list-available-institutes-on-the-view Declaring new methods

--- campus.jar/src/com/genesiis/campus/entity/CourseProviderICrud.java
+++ campus.jar/src/com/genesiis/campus/entity/CourseProviderICrud.java
@@ -1,6 +1,7 @@
 package com.genesiis.campus.entity;
 
 //20170107 c6-list-available-institutes-on-the-view created CourseProviderICrud.java
+//20170108 c6-list-available-institutes-on-the-view Declaring new methods 
 
 import com.genesiis.campus.entity.model.CourseProviderResultDTO;
 import com.genesiis.campus.entity.model.CourseProviderSearchDTO;
@@ -13,8 +14,25 @@ import java.util.List;
  * The Interface {@code CourseProviderICrud} has precise control over course provider dao level manipulations. 
  *  @author dumani DJ   
  */
-public interface CourseProviderICrud extends ICrud{
-		
+public interface CourseProviderICrud extends ICrud{	
+	
+   //Retrieve all the fields of course provider table
+	public Collection<Collection<String>> getAllCourseProviders()throws SQLException,Exception;
+	
+	//Retrieve basic fields of course provider table
+	public Collection<Collection<String>> getLightAllCourseProviders()throws SQLException,Exception;
+	
+	//Retrieve a course provider details by course provider code
+	public Collection<Collection<String>> getCourseProviderById(Integer courseProviderCode)throws SQLException,Exception;
+	
+	 //Retrieve all the fields of Course Provider Type table
+	public Collection<Collection<String>> getAllCourseProviderTypes()throws SQLException,Exception;
+	
+	//TODO:For Future benefits
+	/*public int addCourseProvider(CourseProvider courseProvider) throws SQLException, Exception;
+	
+	public int updateCourseProvider(CourseProvider courseProvider) throws SQLException, Exception;*/
+	
 	public Collection<Collection<String>> findTopViewedProviders(CourseProviderSearchDTO provider) throws SQLException,Exception;
 	
 	public Collection<Collection<String>> findTopRatedProviders(CourseProviderSearchDTO provider) throws SQLException,Exception;
@@ -23,6 +41,6 @@ public interface CourseProviderICrud extends ICrud{
 	
 	public List<CourseProviderResultDTO> getCategoryWiseTypes(Integer categoryCode)throws SQLException,Exception;
 	
-	public Collection<Collection<String>> findById(CourseProviderSearchDTO provider) throws SQLException,
-	Exception;
+	
+	
 }
