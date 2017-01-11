cedc4240afc37c6918f680f9b2d0e8138cdb6d99
Dumani Jayasinghe <dumani@genesiis.com>
2017-01-11 09:19:03 +0530

    c52-report-banner-statistics-MP-dj Implemented getAllPages() , getAllPageSlotByPageCode() methods

--- campus.jar/src/com/genesiis/campus/entity/dao/BannerDAOImpl.java
+++ campus.jar/src/com/genesiis/campus/entity/dao/BannerDAOImpl.java
@@ -1,11 +1,16 @@
 package com.genesiis.campus.entity.dao;
 
 //20170111 DJ c52-report-banner-statistics-MP-dj Initiated BannerDAOImpl.java
+//20170111 DJ c52-report-banner-statistics-MP-dj Implemented getAllPages() , getAllPageSlotByPageCode() methods.
 
+import com.genesiis.campus.command.CmdReportBannerStatistics;
 import com.genesiis.campus.entity.BannerICrud;
 import com.genesiis.campus.util.ConnectionManager;
 import com.genesiis.campus.util.DaoHelper;
 import com.genesiis.campus.validation.ApplicationStatus;
+import com.genesiis.campus.validation.UtilityHelper;
+
+import org.apache.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -15,7 +20,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class BannerDAOImpl implements BannerICrud{
-
+	static Logger log = Logger.getLogger(BannerDAOImpl.class.getName());
 	@Override
 	public int add(Object object) throws SQLException, Exception {
 		// TODO Auto-generated method stub
@@ -104,22 +109,55 @@ public class BannerDAOImpl implements BannerICrud{
 			}
 		
 		} catch (SQLException sqlException) {
-			log.info("getAll() sqlException" + sqlException.toString());
+			log.info("getAllPages() sqlException" + sqlException.toString());
 			throw sqlException;
 		} catch (Exception e) {
-			log.info("getAll() Exception" + e.toString());
+			log.info("getAllPages() Exception" + e.toString());
 			throw e;
 		} finally {
 			DaoHelper.cleanup(conn, stmt, resultSet);
 		}
 		return pageList;
 	}
-
+	/**
+	 * Retrieve active Page Slots result set by page code.
+	 * @param 
+	 * @author DJ
+	 * @return Collection
+	 */
 	@Override
 	public Collection<Collection<String>> getAllPageSlotByPageCode(int pageCode)
 			throws SQLException, Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn=null;
+		PreparedStatement stmt=null;
+		ResultSet resultSet=null;
+		final Collection<Collection<String>> pageSlotList = new ArrayList<Collection<String>>();		       
+		try {			
+			conn=ConnectionManager.getConnection();
+			final StringBuilder sb = new StringBuilder(
+					"SELECT SLOT.CODE AS PAGESLOTCODE , SLOT.NAME AS PAGESLOTNAME FROM [CAMPUS].[PAGESLOT] SLOT WHERE");
+			sb.append(" SLOT.PAGE= ? AND SLOT.ISACTIVE= ?");
+			stmt=conn.prepareStatement(sb.toString());
+			stmt.setInt(1, pageCode);
+			stmt.setInt(2, ApplicationStatus.ACTIVE.getStatusValue());
+			resultSet= stmt.executeQuery();	
+			while (resultSet.next()) {
+				final ArrayList<String> singleSlot = new ArrayList<String>();
+				singleSlot.add(resultSet.getString("PAGESLOTCODE"));
+				singleSlot.add(resultSet.getString("PAGESLOTNAME"));				
+				pageSlotList.add(singleSlot);
+			}			
+
+		} catch (SQLException sqlException) {
+			log.info("getAllPageSlotByPageCode() sqlException" + sqlException.toString());
+			throw sqlException;
+		} catch (Exception e) {
+			log.info("getAllPageSlotByPageCode() Exception" + e.toString());
+			throw e;
+		} finally {
+			DaoHelper.cleanup(conn, stmt, resultSet);
+		}
+		return pageSlotList;
 	}
 
 	@Override
