18ee10301ad3a1e9788b2f2053736946576b8f75
Dumani Jayasinghe <dumani@genesiis.com>
2017-01-12 23:01:01 +0530

    c123-general-filter-search-course-provider-MP-dj created CmdGeneralFilterSearch.java

---
+++ campus.jar/src/com/genesiis/campus/command/CmdGeneralFilterSearch.java
@@ -0,0 +1,20 @@
+package com.genesiis.campus.command;
+
+//DJ 20170112 c123-general-filter-search-course-provider-MP-dj created CmdGeneralFilterSearch.java
+
+import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.util.IDataHelper;
+
+import java.sql.SQLException;
+
+
+public class CmdGeneralFilterSearch implements ICommand{
+
+	@Override
+	public IView execute(IDataHelper helper, IView view) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
