f922f6e65e1bcb5fc28073271d5ccba7bfc08f95
Dumani Jayasinghe <dumani@genesiis.com>
2017-01-13 08:43:20 +0530

    c123-general-filter-search-course-provider-MP-dj add FILTER_SEARCH_COURSE_PROVIDERS

--- campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
@@ -4,10 +4,12 @@ package com.genesiis.campus.factory;
 //20161031 DJ c6-list-available-institutes-on-the-view amended by inserting LIST_TOP_COURSE_PROVIDERS entry to the map
 //20161103 DJ c6-list-available-institutes-on-the-view amended by inserting LIST_ALL_COURSE_PROVIDERS entry to the map
 //20161121 DJ c17-provider-criteria-based-filter-search by inserting LIST_FILTER_SEARCH_COURSE_PROVIDERS entry to the map
-//20161124 DJ c17-provider-criteria-based-filter-search by inserting LIST_FILTER_SEARCH_CATEGORY_TYPES entry to the map
+//20161124 DJ c17-provider-criteria-based-filter-search by inserting LIST_FILTER_SEARCH_CATEGORY_TYPES entry to the map.
+//20170113 DJ c123-general-filter-search-course-provider-MP-dj add FILTER_SEARCH_COURSE_PROVIDERS.
 
 
 import com.genesiis.campus.command.CmdCourseProviderFilterSearch;
+import com.genesiis.campus.command.CmdGeneralFilterSearch;
 import com.genesiis.campus.command.CmdListCategoryTypes;
 import com.genesiis.campus.command.CmdListMoreCourseProviders;
 import com.genesiis.campus.command.CmdListTopCourseProviders;
@@ -22,6 +24,7 @@ public class PublicCmdFactory implements ICmdFactory {
 		map.put(Operation.LIST_ALL_COURSE_PROVIDERS, new CmdListMoreCourseProviders());
 		map.put(Operation.LIST_FILTER_SEARCH_COURSE_PROVIDERS, new CmdCourseProviderFilterSearch());
 		map.put(Operation.LIST_FILTER_SEARCH_CATEGORY_TYPES, new CmdListCategoryTypes());
+		map.put(Operation.FILTER_SEARCH_COURSE_PROVIDERS, new CmdGeneralFilterSearch());
 		
 	}
 	
@@ -40,6 +43,9 @@ public class PublicCmdFactory implements ICmdFactory {
 			command = map.get(o);
 			break;
 		case LIST_FILTER_SEARCH_CATEGORY_TYPES:
+			command = map.get(o);
+			break;
+		case FILTER_SEARCH_COURSE_PROVIDERS:
 			command = map.get(o);
 			break;
 		default:
