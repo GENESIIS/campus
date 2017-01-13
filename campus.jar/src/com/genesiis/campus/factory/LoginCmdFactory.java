ed299c5e1501fbbd7707c0da91330622a7d30cd3
Anuradha Silva <anuradha@genesiis.com>
2017-01-13 15:11:46 +0530

    CAM-21 logout command class added

--- campus.jar/src/com/genesiis/campus/factory/LoginCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/LoginCmdFactory.java
@@ -1,6 +1,8 @@
 package com.genesiis.campus.factory;
 //20161123 AS C19-student-login-without-using-third-party-application-test-as LoginCmdFactory class created.
+//20170113 AS C21 student-logout-clear-session-details-update-logout-data-as CmdStudentLogout command added.
 import com.genesiis.campus.command.CmdStudentLogin;
+import com.genesiis.campus.command.CmdStudentLogout;
 import com.genesiis.campus.command.ICommand;
 import com.genesiis.campus.validation.Operation;
 
@@ -9,6 +11,7 @@ public class LoginCmdFactory implements ICmdFactory{
 	private ICommand command = null;
 	static {	
 		map.put(Operation.STUDENT_LOGIN, new CmdStudentLogin());	
+		map.put(Operation.STUDENT_LOGOUT, new CmdStudentLogout());	
 	}
 	
 	@Override
@@ -18,6 +21,9 @@ public class LoginCmdFactory implements ICmdFactory{
 		switch (o) {
 		
 		case STUDENT_LOGIN:
+			command = map.get(o);
+			break;
+		case STUDENT_LOGOUT:
 			command = map.get(o);
 			break;
 		default:
