edfb9f4b8303f8dc0b5e9d5e7c89704593800254
Dumani Jayasinghe <dumani@genesiis.com>
2017-01-13 09:26:29 +0530

    c123-general-filter-search-course-provider-MP-dj Implementing execute() method

--- campus.jar/src/com/genesiis/campus/command/CmdGeneralFilterSearch.java
+++ campus.jar/src/com/genesiis/campus/command/CmdGeneralFilterSearch.java
@@ -2,19 +2,45 @@ package com.genesiis.campus.command;
 
 //DJ 20170112 c123-general-filter-search-course-provider-MP-dj created CmdGeneralFilterSearch.java
 
+import com.genesiis.campus.entity.CourseProviderICrud;
+import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.dao.CourseProviderDAOImpl;
 import com.genesiis.campus.util.IDataHelper;
 
 import java.sql.SQLException;
+import java.util.List;
 
 
 public class CmdGeneralFilterSearch implements ICommand{
-
+	
+	/**	 * 
+	 * @author dumani DJ
+	 * @param helper IDataHelper Object
+	 * @param view IView Object
+	 * @return General filter search information.
+	 * @throws Exception
+	 */
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			CourseProviderICrud courseProviderICrud=new CourseProviderDAOImpl();
+			//TODO:Identify radio button click-Course providers
+			String keyWord="icbt";			
+			
+			//Do wild card search on key word
+			List result=courseProviderICrud.wildCardSearchOnCourseProvider();
+			
+			//Find particular course providers
+			//find Course providers
+			
+		} catch (Exception exception) {
+			log.error("execute() :Exception  " + exception);			
+			throw exception;
+		}
+		
+		return view;
 	}
 
 }
