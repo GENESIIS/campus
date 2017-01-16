d6739d3c2ca02765abafa7e3760175ccaab525d5
Dumani Jayasinghe <dumani@genesiis.com>
2017-01-16 16:51:59 +0530

    c123-general-filter-search-course-provider-MP-dj Ajax implementation

--- campus.war/web/dist/js/filterSearch/general-filter-search.js
+++ campus.war/web/dist/js/filterSearch/general-filter-search.js
@@ -1,22 +1,21 @@
 //DJ 20170116 c123-general-filter-search-course-provider-MP-dj Identify general filter search entities. 
 
 $(document).ready(function() {
-	alert("Test");
-	
-	$('#addSearchData').on('click', function(event) {
-		alert("addSearchData");
+	//Identifies the search button action
+	$('#addSearchData').on('click', function(event) {	
+		var keyWordString=$("#keyWord").val();	
+		var selectedType= $('input[name=cpRadio]:checked').val()
 		
-		$(this).val();
-			var keyWordString=" ";
 		$.ajax({
 			url : '../../PublicController',
 			data : {
-				CCO : 'FILTER_SEARCH_COURSE_PROVIDERS',
-				keyWordString : keyWordString
+				CCO : 'GENERAL_FILTER_SEARCH_COURSE_PROVIDERS',
+				keyWordString : keyWordString,
+				selectedType:selectedType
 			},
 			dataType : "json",
 			success : function(response) {
-				
+				getProviderCodeList(response);
 			},
 			error : function(jqXHR, exception) {			
 				var msg = '';
@@ -35,5 +34,43 @@ $(document).ready(function() {
 			}
 		});
 	});
+	
+	//Populate course provider code List and pass code list for course provider filter search view.
+	function getProviderCodeList(response) {
+		var cpCodeList = response.codeList;
+		//if (cpCodeList != null && cpCodeList.length > 0) {
+			
+			var product_id=1;
+			window.location.replace("/dist/partials/viewMoreCourseProviders.jsp?cpCodeList=" + cpCodeList);
+			
+		/*	$.ajax({
+						url : '../../PublicController',
+						data : {
+							CCO : 'LIST_ALL_COURSE_PROVIDERS',
+							cpCodeList : cpCodeList
+						},
+						dataType : "json",
+						success : function(response) {
+							getInitialPageResults(null, response);
+							return true;
+						},
+						error : function(jqXHR, exception) {
+							var msg = '';
+							if (jqXHR.status === 0) {
+								msg = 'Not connect.\n Verify Network.';
+							} else if (jqXHR.status == 404) {
+								msg = 'Requested page not found. [404]';
+							} else if (jqXHR.status == 500) {
+								msg = 'Internal Server Error [500].';
+							} else if (exception === 'timeout') {
+								msg = 'Time out error.';
+							} else {
+								msg = 'Internal error is occurred. Please try again.';
+							}
+							alert(msg);
+						}
+					});*/
+		//}
+	}
 
 });
