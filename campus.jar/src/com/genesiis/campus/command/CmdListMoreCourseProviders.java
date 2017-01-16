a7d58fc6d6f98f039238b79e15f256299ec11a20
Dumani Jayasinghe <dumani@genesiis.com>
2017-01-16 15:39:23 +0530

    c123-general-filter-search-course-provider-MP-dj created Enhanced execute() method with course provider code list.

--- campus.jar/src/com/genesiis/campus/command/CmdListMoreCourseProviders.java
+++ campus.jar/src/com/genesiis/campus/command/CmdListMoreCourseProviders.java
@@ -6,6 +6,7 @@ package com.genesiis.campus.command;
 //DJ 20161110 c6-list-available-institutes-on-the-view identified get all and category wise course providers
 //DJ 20161123 c6-list-available-institutes-on-the-view add SystemConfig.PROVIDER_LOGO_PATH
 //DJ 20170108 c6-list-available-institutes-on-the-view Adjust dao call to new icrud interfaces
+//DJ 20170116 c123-general-filter-search-course-provider-MP-dj created Enhanced execute() method with course provider code list.
 
 import com.genesiis.campus.entity.CourseProviderICrud;
 import com.genesiis.campus.entity.IView;
@@ -21,7 +22,9 @@ import com.genesiis.campus.validation.UtilityHelper;
 import org.apache.log4j.Logger;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * The class {@code CmdListMoreCourseProviders} is a form of Command class.It is created for the purpose of handling commands of
@@ -47,13 +50,20 @@ public class CmdListMoreCourseProviders implements ICommand {
 		try {
 			int categoryCode = 0;
 			final CourseProviderSearchDTO providerSearchDTO = new CourseProviderSearchDTO();
-			String categoryCodeString = helper.getParameter("categoryCode");
-			String cpCodeList = helper.getParameter("cpCodeList");
+			String categoryCodeString = helper.getParameter("categoryCode");			
+			String [] cpCodeList=helper.getParameterValues("cpCodeList[]");
 			if (UtilityHelper.isNotEmpty(categoryCodeString)) {
 				if (UtilityHelper.isInteger(categoryCodeString)) {
 					categoryCode = Integer.parseInt(categoryCodeString);
 					providerSearchDTO.setCategory(categoryCode);
 				}				
+			}
+			final List<Integer> courseProviderCodeList = new ArrayList<Integer>();
+			if (cpCodeList != null && cpCodeList.length > 0) {			
+				for (String code : cpCodeList) {
+					courseProviderCodeList.add(Integer.valueOf(code));
+				}
+				providerSearchDTO.setCourseProviderCodeList(courseProviderCodeList);				
 			}
 			providerSearchDTO.setCourseProviderStatus(ApplicationStatus.ACTIVE.getStatusValue());
 			final CourseProviderICrud  providerDAO = new CourseProviderDAOImpl();
