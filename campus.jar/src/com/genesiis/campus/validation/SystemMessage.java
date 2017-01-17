5446c9ba039d53153ef625cb466bdba19bd88dd9
chinthakacw <chinthaka@genesiis.com>
2017-01-17 16:56:35 +0530

    c36-add-tutor-details-added EMAIL_USED - cw

--- campus.jar/src/com/genesiis/campus/validation/SystemMessage.java
+++ campus.jar/src/com/genesiis/campus/validation/SystemMessage.java
@@ -4,6 +4,7 @@ package com.genesiis.campus.validation;
 //20161024 DN c10-contacting-us extracted to campus project
 //20170109 c36-add-tutor-details-added few more enum fields cw
 //20170111 c36-add-tutor-details-added USERNAME_LENGTH, WEBLINKERROR, FACEBOOKERROR, LINKEDINERROR, TWITTERERROR, INSTAGRAMERROR, MYSPACEERROR, WHATSAPPERROR, VIBERERROR cw
+//20170117 c36-add-tutor-details-added EMAIL_USED CW
 
 public enum SystemMessage {	
 	ADDED("Details added successfully."), 
@@ -29,6 +30,7 @@ public enum SystemMessage {
 	MYSPACEERROR("MySpace link error."),	
 	WHATSAPPERROR("WhatsApp number error."),	
 	VIBERERROR("Viber error."),	
+	EMAIL_USED("E mail already used to create a tutor profile."),
 	EMPTYFIELD("Please fill required data");
 
 	
