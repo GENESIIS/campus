abddb7ef9468bc2e866953837dd22b939b01b918
jayani1991 <jayani@genesiis.com>
2017-01-17 17:00:20 +0530

    c133-admin-list-tutors list tutors and exception handling in CmdAdminListTutors command class wip

--- campus.jar/src/com/genesiis/campus/command/CmdAdminListTutors.java
+++ campus.jar/src/com/genesiis/campus/command/CmdAdminListTutors.java
@@ -1,6 +1,7 @@
 package com.genesiis.campus.command;
 
 //20170117 JH c133-admin-list-tutors CmdAdminListTutors.java created
+//20170117 JH c133-admin-list-tutors list tutors and exception handling 
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.entity.TutorDAO;
 import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.validation.SystemMessage;
 
 import org.apache.log4j.Logger;
 
@@ -22,15 +24,29 @@ public class CmdAdminListTutors implements ICommand{
 
 		ICrud tutorDAO = new TutorDAO();
 		Collection<Collection<String>> tutorCollection = new ArrayList<Collection<String>>();
+		SystemMessage systemMessage = SystemMessage.NO_DATA;
 
 		try {
 			 tutorCollection = tutorDAO.getAll();
-				log.info(tutorCollection.size());
-				view.setCollection(tutorCollection);
-		} catch (Exception exception) {
+			 
+			 if(tutorCollection.size() >0){
+					view.setCollection(tutorCollection);
+			 }else{
+				systemMessage = SystemMessage.NO_DATA; 
+			 }
+
+		}  catch (SQLException sqlException) {
+			log.error("execute(IDataHelper, IView) : Exception"
+					+ sqlException.toString());
+			systemMessage = SystemMessage.ERROR;
+			throw sqlException;
+		}catch (Exception exception) {
 			log.error("execute(IDataHelper, IView) : Exception"
 					+ exception.toString());
+			systemMessage = SystemMessage.ERROR;
 			throw exception;
+		}finally{
+			helper.setAttribute("userMessage", systemMessage.message());
 		}
 
 		return view;
