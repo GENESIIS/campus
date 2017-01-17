7bf5aa7b904f6033ed019f692a6cdd728abe8b32
jayani1991 <jayani@genesiis.com>
2017-01-17 15:34:49 +0530

    c133-admin-list-tutors command class coding to get all tutors wip

--- campus.jar/src/com/genesiis/campus/command/CmdAdminListTutors.java
+++ campus.jar/src/com/genesiis/campus/command/CmdAdminListTutors.java
@@ -3,8 +3,12 @@ package com.genesiis.campus.command;
 //20170117 JH c133-admin-list-tutors CmdAdminListTutors.java created
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.TutorDAO;
 import com.genesiis.campus.util.IDataHelper;
 
 import org.apache.log4j.Logger;
@@ -16,18 +20,20 @@ public class CmdAdminListTutors implements ICommand{
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
 
-		
-try{
+		ICrud tutorDAO = new TutorDAO();
+		Collection<Collection<String>> tutorCollection = new ArrayList<Collection<String>>();
 
-	
-	
-	
-}catch(Exception exception){
-	log.error("execute(IDataHelper, IView) : Exception" + exception.toString());
-	throw exception;
-}
-		
-		return null;
+		try {
+			 tutorCollection = tutorDAO.getAll();
+				log.info(tutorCollection.size());
+				view.setCollection(tutorCollection);
+		} catch (Exception exception) {
+			log.error("execute(IDataHelper, IView) : Exception"
+					+ exception.toString());
+			throw exception;
+		}
+
+		return view;
 	}
 
 }
