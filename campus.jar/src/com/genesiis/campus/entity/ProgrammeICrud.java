4940ec403b26a36e51c9a3923d971bdb074c012d
Dumani Jayasinghe <dumani@genesiis.com>
2017-01-23 09:36:59 +0530

    c124-general-filter-search-programme Declaring wildCardSearchOnProgrammes(final ProgrammeSearchDTO searchDTO)

--- campus.jar/src/com/genesiis/campus/entity/ProgrammeICrud.java
+++ campus.jar/src/com/genesiis/campus/entity/ProgrammeICrud.java
@@ -3,6 +3,9 @@ package com.genesiis.campus.entity;
 //20170107 c6-list-available-institutes-on-the-view Initiated ProgrammeICrud.java
 //20170108 c6-list-available-institutes-on-the-view Declaring new methods
 //DJ 20170118 c124-general-filter-search-programme Declaring wildCardSearchOnProgrammes().
+//DJ 20170123 c124-general-filter-search-programme Declaring wildCardSearchOnProgrammes(final ProgrammeSearchDTO searchDTO).
+
+import com.genesiis.campus.entity.model.ProgrammeSearchDTO;
 
 import java.sql.SQLException;
 import java.util.Collection;
@@ -30,5 +33,7 @@ public interface ProgrammeICrud extends ICrud {
 	public Collection<Collection<String>> findLevelsByLevelCodes(Set<Integer> levelCodeSet)throws SQLException,Exception;
 	
 	public Set<Integer> wildCardSearchOnProgrammes(String keyWord) throws SQLException, Exception;
+	
+	public Collection<Collection<String>> wildCardSearchOnProgrammes(final ProgrammeSearchDTO searchDTO)throws SQLException, Exception;
 
 }
