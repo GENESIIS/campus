9a82884629e95d924a5cd7111b2eb67791cb8d19
jayani1991 <jayani@genesiis.com>
2017-01-23 15:25:15 +0530

    c135-admin-tutor-keyword-search created CmdAdminSearchTutors.java command class

---
+++ campus.jar/src/com/genesiis/campus/command/CmdAdminSearchTutors.java
@@ -0,0 +1,20 @@
+package com.genesiis.campus.command;
+
+//20170123 JH c135-admin-tutor-keyword-search created CmdAdminSearchTutors.java
+
+import java.sql.SQLException;
+
+import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.util.IDataHelper;
+
+public class CmdAdminSearchTutors implements ICommand{
+
+	@Override
+	public IView execute(IDataHelper helper, IView view) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
