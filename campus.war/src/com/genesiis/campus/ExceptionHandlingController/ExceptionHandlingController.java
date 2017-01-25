ceacc8e9f9419a3fe095b0195f741899f9ccb61b
pabodhaW <pabodha@genesiis.com>
2017-01-25 22:04:14 +0530

    CAM-72: removed processError(HttpServletRequest request, HttpServletResponse response) method.

--- campus.war/src/com/genesiis/campus/ExceptionHandlingController/ExceptionHandlingController.java
+++ campus.war/src/com/genesiis/campus/ExceptionHandlingController/ExceptionHandlingController.java
@@ -2,7 +2,7 @@ package com.genesiis.campus.ExceptionHandlingController;
 
 //20170111 PN CAM-72: INIT ExceptionHandlingController.java servlet. Implemented processError() method. - WIP
 //20170111 PN CAM-72: modified processError() method to extend CampusController servlet.
-
+//20170125 PN CAM-72: removed processError(HttpServletRequest request, HttpServletResponse response) method.
 import java.io.IOException;
 import java.io.PrintWriter;
 import org.apache.log4j.Logger;
@@ -27,7 +27,7 @@ public class ExceptionHandlingController extends CampusController {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		super.doPost(request, response);
+		this.doPost(request, response);
 	}
 
 	/**
@@ -39,39 +39,4 @@ public class ExceptionHandlingController extends CampusController {
 		super.doPost(request, response);
 	}
 
-	private void processError(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		// Analyze the servlet exception
-		Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
-		Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-		String servletName = (String) request.getAttribute("javax.servlet.error.servlet_name");
-		if (servletName == null) {
-			servletName = "Unknown";
-		}
-		String requestUri = (String) request.getAttribute("javax.servlet.error.request_uri");
-		if (requestUri == null) {
-			requestUri = "Unknown";
-		}
-
-		// Set response content type
-		response.setContentType("text/html");
-
-		PrintWriter out = response.getWriter();
-		out.write("<html><head><title>Exception/Error Details</title></head><body>");
-		if (statusCode != 500) {
-			out.write("<h3>Error Details</h3>");
-			out.write("<strong>Status Code</strong>:" + statusCode + "<br>");
-			out.write("<strong>Requested URI</strong>:" + requestUri);
-		} else {
-			out.write("<h3>Exception Details</h3>");
-			out.write("<ul><li>Servlet Name:" + servletName + "</li>");
-			out.write("<li>Exception Name:" + throwable.getClass().getName() + "</li>");
-			out.write("<li>Requested URI:" + requestUri + "</li>");
-			out.write("<li>Exception Message:" + throwable.getMessage() + "</li>");
-			out.write("</ul>");
-		}
-
-		out.write("<br><br>");
-		out.write("<a href=\"index.html\">Home Page</a>");
-		out.write("</body></html>");
-	}
 }
