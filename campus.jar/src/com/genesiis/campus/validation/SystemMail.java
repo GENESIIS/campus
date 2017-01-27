ca63ebbb1671035db7d878121b8e438d49dd6981
chinthakacw <chinthaka@genesiis.com>
2017-01-27 22:48:26 +0530

    c126-formatting-un-formatted-email-tutor-signup-cw - add attributes to the Enum - cw

--- campus.jar/src/com/genesiis/campus/validation/SystemMail.java
+++ campus.jar/src/com/genesiis/campus/validation/SystemMail.java
@@ -2,7 +2,7 @@ package com.genesiis.campus.validation;
 
 //20170105 CW c98-send-email-at-tutor-signup created the initial SystemMail.java version
 //20170125 CW c125-un-formatted-email-sending-tutor-signup-add comments to the Enum - cw
-
+//20170127 CW c126-formatting-un-formatted-email-tutor-signup-cw - add attributes to the Enum
 
 /**
  * SystemMail enum handles the content of the email to be sent
@@ -13,7 +13,10 @@ public enum SystemMail {
 
 	SEND_EMAIL_TUTOR_SIGNUP_BODY1("Registration Confirmation", "Dear Sir/Madam", "You have successfully registered with the campus.lk."),
 	SEND_EMAIL_TUTOR_SIGNUP_BODY2("Registration Confirmation", "Dear Sir/Madam", "Your details are waiting for the administration approval. On admin approval you will receive an email, confirming your information with campus.lk."),
-	SEND_EMAIL_TUTOR_SIGNUP_BODY3("Registration Confirmation", "Dear Sir/Madam", "If you didn't sign up for this account, or you are having trouble with your account, please contact us at support@campus.lk and we will be happy to help you.");
+	SEND_EMAIL_TUTOR_SIGNUP_BODY3("Registration Confirmation", "Dear Sir/Madam", "If you didn't sign up for this account, or you are having trouble with your account, please contact us at support@campus.lk and we will be happy to help you."),
+	SEND_EMAIL_TUTOR_SIGNUP_COMPLEMENTORY_CLOSE1("", "", "Campus.lk Support Team"),
+	SEND_EMAIL_TUTOR_SIGNUP_COMPLEMENTORY_CLOSE2("", "", "http://www.campus.lk"),
+	SEND_EMAIL_TUTOR_SIGNUP_COMPLEMENTORY_CLOSE3("", "", "Your user ID: ");
 	
 	private final String subject;
 	private final String title;
