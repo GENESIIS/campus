3f45bef8994ef8dd7d39d593905a99d6af257f82
Dumani Jayasinghe <dumani@genesiis.com>
2017-01-31 11:17:57 +0530

    c53-report-registered-students-MP-dj created StudentSearchResultDTO.java

---
+++ campus.jar/src/com/genesiis/campus/entity/model/StudentSearchResultDTO.java
@@ -0,0 +1,17 @@
+package com.genesiis.campus.entity.model;
+
+//DJ 20170131 c53-report-registered-students-MP-dj created StudentSearchResultDTO.java
+
+import java.util.Date;
+
+public class StudentSearchResultDTO {
+	
+	private int studentCode;
+	private String studentName;		
+	private int studentStatus;
+	private String StudentInterest;
+	private String town;	
+	private Date registeredDate;
+	private Date lastLoginDate;
+
+}
