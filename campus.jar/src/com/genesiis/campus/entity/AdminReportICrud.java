fe36651c1baabb2c3608415dee8736482a67d465
Dumani Jayasinghe <dumani@genesiis.com>
2017-01-31 11:48:08 +0530

    C53 Changed the return type to List <StudentSearchResultDTO> in  getRegisteredStudentReport(StudentSearchDTO searchDTO)

--- campus.jar/src/com/genesiis/campus/entity/AdminReportICrud.java
+++ campus.jar/src/com/genesiis/campus/entity/AdminReportICrud.java
@@ -5,9 +5,11 @@ package com.genesiis.campus.entity;
 
 import com.genesiis.campus.entity.model.BannerStatSearchDTO;
 import com.genesiis.campus.entity.model.StudentSearchDTO;
+import com.genesiis.campus.entity.model.StudentSearchResultDTO;
 
 import java.sql.SQLException;
 import java.util.Collection;
+import java.util.List;
 
 /**The class  {@code AdminReportICrud} is a form of Interface class.
  * The Interface {@code AdminReportICrud} has precise control over admin report generation dao level manipulations. 
@@ -17,6 +19,6 @@ public interface AdminReportICrud extends ICrud{
 	
 	Collection<Collection<String>> getBannerStatisticReport(BannerStatSearchDTO searchDTO ) throws SQLException,Exception;
 	
-	Collection<Collection<String>> getRegisteredStudentReport(StudentSearchDTO searchDTO ) throws SQLException,Exception;
+	List <StudentSearchResultDTO> getRegisteredStudentReport(StudentSearchDTO searchDTO ) throws SQLException,Exception;
 
 }
