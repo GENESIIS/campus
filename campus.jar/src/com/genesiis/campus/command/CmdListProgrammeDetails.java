ed8d36adf13f4594ac344e533a512911beccd59a
Dumani Jayasinghe <dumani@genesiis.com>
2017-02-02 16:22:41 +0530

    c138-add-basic-programme-MP-dj Initiate CmdListProgrammeDetails.java

---
+++ campus.jar/src/com/genesiis/campus/command/CmdListProgrammeDetails.java
@@ -0,0 +1,25 @@
+package com.genesiis.campus.command;
+
+//20170202 DJ c138-add-basic-programme-MP-dj Initiate CmdListProgrammeDetails.java
+
+import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.util.IDataHelper;
+
+import java.sql.SQLException;
+
+/**
+ * The class {@code CmdListProgrammeDetails} is a form of Command class.It is created for the purpose of handling commands of
+ * add programme details. 
+ * @author dumani DJ
+ *
+*/
+public class CmdListProgrammeDetails  implements ICommand {
+
+	@Override
+	public IView execute(IDataHelper helper, IView view) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
