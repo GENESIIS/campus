0322457e06d1c9bf7ccecf02cc51f12c2d981d35
Dumani Jayasinghe <dumani@genesiis.com>
2017-02-02 17:10:48 +0530

    c138-add-basic-programme-MP-dj Initiate Dao call for master data view

--- campus.jar/src/com/genesiis/campus/command/CmdListProgrammeDetails.java
+++ campus.jar/src/com/genesiis/campus/command/CmdListProgrammeDetails.java
@@ -1,11 +1,21 @@
 package com.genesiis.campus.command;
 
 //20170202 DJ c138-add-basic-programme-MP-dj Initiate CmdListProgrammeDetails.java
+//20170202 DJ c138-add-basic-programme-MP-dj Initiate Dao call for master data view
 
+import com.genesiis.campus.entity.CourseProviderICrud;
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.ProgrammeICrud;
+import com.genesiis.campus.entity.dao.CourseProviderDAOImpl;
+import com.genesiis.campus.entity.dao.ProgrammeDAOImpl;
+import com.genesiis.campus.entity.model.CourseProviderSearchDTO;
 import com.genesiis.campus.util.IDataHelper;
 
+import org.apache.log4j.Logger;
+
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Map;
 
 /**
  * The class {@code CmdListProgrammeDetails} is a form of Command class.It is created for the purpose of handling commands of
@@ -14,12 +24,45 @@ import java.sql.SQLException;
  *
 */
 public class CmdListProgrammeDetails  implements ICommand {
-
+	static Logger log = Logger.getLogger(CmdListProgrammeDetails.class.getName());
+	/**
+	 * @author DJ
+	 * @param helper
+	 * @param view
+	 * @return 
+	 * @throws Exception
+	 */
 	@Override
-	public IView execute(IDataHelper helper, IView view) throws SQLException,
+	public IView execute(IDataHelper helper, IView iView) throws SQLException,
 			Exception {
-		// TODO Auto-generated method stub
-		return null;
+		final CourseProviderICrud providerDAO = new CourseProviderDAOImpl();	
+		final ProgrammeICrud programmeDAO = new ProgrammeDAOImpl();	
+		
+		try {
+			final CourseProviderSearchDTO providerSearchDTO = new CourseProviderSearchDTO();
+			final Map<String, String[]> searchParamCollection = helper.getParameterMap();			
+			
+				final Collection<Collection<String>> courseProviderSearchResults = providerDAO.getLightAllCourseProviders();
+				iView.setCollection(courseProviderSearchResults);
+				
+				final Collection<Collection<String>> allCategories= programmeDAO.getAllCategories();
+				helper.setAttribute("allCategories",allCategories);
+				
+				final Collection<Collection<String>> allMajors= programmeDAO.getAllMajors();
+				helper.setAttribute("allMajors",allMajors);
+				
+				final Collection<Collection<String>> allLevels= programmeDAO.getAllLevels();
+				helper.setAttribute("allLevels",allLevels);
+				
+				final Collection<Collection<String>> allClassTypes= programmeDAO.getAllClassTypes();
+				helper.setAttribute("allClassTypes",allClassTypes);			
+			
+			
+		} catch (Exception exception) {
+			log.error("execute() : Exception " + exception);			
+			throw exception;
+		}		
+		return iView;
 	}
 
 }
