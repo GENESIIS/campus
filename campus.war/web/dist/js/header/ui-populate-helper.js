66a8f319e4764d1db415404301581f2bb6e79fd3
pabodhaW <pabodha@genesiis.com>
2017-02-03 16:57:46 +0530

    CAM-137: added landing page category set displaying JavaScript in here.

--- campus.war/web/dist/js/header/ui-populate-helper.js
+++ campus.war/web/dist/js/header/ui-populate-helper.js
@@ -6,7 +6,7 @@
 //20161117 JH c7-higher-education-landing-page-MP displayCategories() method created
 //20161125 JH c7-higher-education-landing-page-MP solve servlet path error in common header loading error
 //20161205 JH c7-higher-education-landing-page-MP modify javascript function to load category data without onload function
-
+//20170203 PN CAM-137: added landing page category set displaying JavaScript in here.
 /**
  * This method is to load category details
  */
@@ -60,5 +60,36 @@ function displayCategories() {
     singleCategoryElement += '</ul>';
     var categoryNames = $("#category-list");
     categoryNames.html(singleCategoryElement);
+    
+    var count = 0 ;
+	var mainCategoryList = $("#mainCategoryList");
+	mainCategoryList.find('li').remove();
+	$.each(categoryCollectionList, function(index, value) {
+		mainCategoryList.append('<li>'+
+                '<div class="category-box clearfix">'+
+                '<div class="category-name text-center clearfix">'+
+                    '<a href="javascript:">'+value[1].toString()+'</a>'+
+                '</div>'+
+                '<div class="course-info clearfix">'+
+                    '<div class="image pull-left">'+
+                        '<img class="center-block" src="../../education/general/category/image/'+value[0].toString()+'.jpg" alt="">'+
+                    '</div>'+
+                    '<div class="description pull-right">'+
+                        '<p class="text-justify">'+value[2].toString().replace(/##/g , ",")+'</p>'+
+                        '<a href="javascript:" class="pull-right">Show More >> </a>'+
+                    '</div>'+
+                '</div>'+
+            '</div>'+
+        '</li>');
+		count++;
+	});
+}
 
+/**
+ * This method is to display available main categories within the Landing page.
+ * @param response
+ * @returns
+ */
+function displayCategoryDetails(response){
+	
 }
