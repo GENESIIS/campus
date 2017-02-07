04d941309fd0d5dba1a1951b86ac6f1dab7f1a57
chinthakacw <chinthaka@genesiis.com>
2017-02-07 16:55:21 +0530

    c38-view-update-tutor-profile- re-organize the import statements - cw

--- campus.jar/src/com/genesiis/campus/command/CmdLoadCountry.java
+++ campus.jar/src/com/genesiis/campus/command/CmdLoadCountry.java
@@ -2,15 +2,16 @@ package com.genesiis.campus.command;
 
 //20161122 CM c36-add-tutor-information INIT CmdLoadTownDetails.java
 //20161122 CM c36-add-tutor-information Modified execute()method. 
-//20170130 CW c36-add-tutor-information re-organise the import statements.
-
-import java.sql.SQLException;
-import java.util.Collection;
+//20170207 CW c38-view-update-tutor-profile- re-organize the import statements.
 
 import com.genesiis.campus.entity.CountryDAO;
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.util.IDataHelper;
+
 import org.apache.log4j.Logger;
+
+import java.sql.SQLException;
+import java.util.Collection;
 
 public class CmdLoadCountry implements ICommand {
 	static Logger log = Logger.getLogger(CmdLoadCountry.class.getName());
