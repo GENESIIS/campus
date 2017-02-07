462eac6be74128704ffc05834242a02fe518ca2c
Dumani Jayasinghe <dumani@genesiis.com>
2017-02-07 09:59:52 +0530

    c124-general-filter-search-programme-MP-dj Add Line spaces

--- campus.jar/src/com/genesiis/campus/entity/dao/ProgrammeDAOImpl.java
+++ campus.jar/src/com/genesiis/campus/entity/dao/ProgrammeDAOImpl.java
@@ -90,24 +90,23 @@ public class ProgrammeDAOImpl implements ProgrammeICrud{
 	 * @return Collection 
 	 */
 	@Override
-	public Collection<Collection<String>> getAllCategories()
-			throws SQLException, Exception {
-		Connection conn=null;
-		PreparedStatement stmt=null;
-		ResultSet rs=null;
-		final Collection<Collection<String>> allCategoryList=new ArrayList<Collection<String>>();
+	public Collection<Collection<String>> getAllCategories() throws SQLException, Exception {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		final Collection<Collection<String>> allCategoryList = new ArrayList<Collection<String>>();
 		try {
-			conn=ConnectionManager.getConnection();
-			String sql="SELECT CAT.CODE AS CATEGORYCODE , CAT.NAME AS CATEGORYNAME FROM [CAMPUS].CATEGORY CAT WHERE CAT.ISACTIVE=? ";
-			
-			stmt=conn.prepareStatement(sql.toString());
+			conn = ConnectionManager.getConnection();
+			String sql = "SELECT CAT.CODE AS CATEGORYCODE , CAT.NAME AS CATEGORYNAME FROM [CAMPUS].CATEGORY CAT WHERE CAT.ISACTIVE=? ";
+
+			stmt = conn.prepareStatement(sql.toString());
 			stmt.setInt(1, ApplicationStatus.ACTIVE.getStatusValue());
-			rs=stmt.executeQuery();
-			
-			while (rs.next()) {				
+			rs = stmt.executeQuery();
+
+			while (rs.next()) {
 				final ArrayList<String> singleCategory = new ArrayList<String>();
-				singleCategory.add(rs.getString("CATEGORYCODE"));				
-				singleCategory.add(rs.getString("CATEGORYNAME"));				
+				singleCategory.add(rs.getString("CATEGORYCODE"));
+				singleCategory.add(rs.getString("CATEGORYNAME"));
 				allCategoryList.add(singleCategory);
 			}
 		} catch (SQLException sqlException) {
@@ -119,9 +118,9 @@ public class ProgrammeDAOImpl implements ProgrammeICrud{
 		} finally {
 			DaoHelper.cleanup(conn, stmt, rs);
 		}
-		
 		return allCategoryList;
-	}	
+	}
+	
 	/**
 	 * Get all major list details by major code set
 	 * @param majorCodeSet
@@ -130,14 +129,14 @@ public class ProgrammeDAOImpl implements ProgrammeICrud{
 	 */
 	@Override
 	public Collection<Collection<String>> findMajorsByMajorCodes(Set<Integer> majorCodeSet) throws SQLException, Exception {
-		Connection conn=null;
-		PreparedStatement stmt=null;
-		ResultSet rs=null;
-		final Collection<Collection<String>> allMajorList=new ArrayList<Collection<String>>();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		final Collection<Collection<String>> allMajorList = new ArrayList<Collection<String>>();
 		try {
 			conn = ConnectionManager.getConnection();
-			final StringBuilder sb =new StringBuilder(" SELECT MAJOR.[CODE] AS MAJORCODE,MAJOR.[NAME] AS MAJORNAME FROM [CAMPUS].[MAJOR] MAJOR ");
-			sb.append(" WHERE MAJOR.ISACTIVE = ? AND MAJOR.CODE IN( " );
+			final StringBuilder sb = new StringBuilder(" SELECT MAJOR.[CODE] AS MAJORCODE,MAJOR.[NAME] AS MAJORNAME FROM [CAMPUS].[MAJOR] MAJOR ");
+			sb.append(" WHERE MAJOR.ISACTIVE = ? AND MAJOR.CODE IN( ");
 			boolean doneOne = false;
 			for (Integer code : majorCodeSet) {
 				if (doneOne) {
@@ -146,7 +145,7 @@ public class ProgrammeDAOImpl implements ProgrammeICrud{
 				sb.append(code);
 				doneOne = true;
 			}
-			sb.append(")" );
+			sb.append(")");
 
 			stmt = conn.prepareStatement(sb.toString());
 			stmt.setInt(1, ApplicationStatus.ACTIVE.getStatusValue());
@@ -168,7 +167,6 @@ public class ProgrammeDAOImpl implements ProgrammeICrud{
 		} finally {
 			DaoHelper.cleanup(conn, stmt, rs);
 		}
-		
 		return allMajorList;
 	}
 	
@@ -180,14 +178,14 @@ public class ProgrammeDAOImpl implements ProgrammeICrud{
 	 */
 	@Override
 	public Collection<Collection<String>> findLevelsByLevelCodes(Set<Integer> levelCodeSet) throws SQLException, Exception {
-		Connection conn=null;
-		PreparedStatement stmt=null;
-		ResultSet rs=null;
-		final Collection<Collection<String>> allLevelList=new ArrayList<Collection<String>>();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		final Collection<Collection<String>> allLevelList = new ArrayList<Collection<String>>();
 		try {
-			conn=ConnectionManager.getConnection();			
-			final StringBuilder sb =new StringBuilder("SELECT LEVEL.CODE AS LEVELCODE , LEVEL.NAME AS LEVELNAME FROM [CAMPUS].LEVEL LEVEL  ");
-			sb.append(" WHERE LEVEL.ISACTIVE=? AND LEVEL.CODE IN (");
+			conn = ConnectionManager.getConnection();
+			final StringBuilder sb = new StringBuilder("SELECT LEVEL.CODE AS LEVELCODE , LEVEL.NAME AS LEVELNAME FROM [CAMPUS].LEVEL LEVEL  ");
+			sb.append(" WHERE LEVEL.ISACTIVE=? AND LEVEL.CODE IN ( ");
 			boolean doneOne = false;
 			for (Integer code : levelCodeSet) {
 				if (doneOne) {
@@ -196,15 +194,15 @@ public class ProgrammeDAOImpl implements ProgrammeICrud{
 				sb.append(code);
 				doneOne = true;
 			}
-			sb.append(")" );
-			stmt=conn.prepareStatement(sb.toString());
+			sb.append(" )");
+			stmt = conn.prepareStatement(sb.toString());
 			stmt.setInt(1, ApplicationStatus.ACTIVE.getStatusValue());
-		    rs=stmt.executeQuery();
-			
-			while (rs.next()) {				
+			rs = stmt.executeQuery();
+
+			while (rs.next()) {
 				final ArrayList<String> singleLevel = new ArrayList<String>();
-				singleLevel.add(rs.getString("LEVELCODE"));				
-				singleLevel.add(rs.getString("LEVELNAME"));				
+				singleLevel.add(rs.getString("LEVELCODE"));
+				singleLevel.add(rs.getString("LEVELNAME"));
 				allLevelList.add(singleLevel);
 			}
 		} catch (SQLException sqlException) {
@@ -216,7 +214,7 @@ public class ProgrammeDAOImpl implements ProgrammeICrud{
 		} finally {
 			DaoHelper.cleanup(conn, stmt, rs);
 		}
-		
+
 		return allLevelList;
 	}
 
@@ -228,26 +226,26 @@ public class ProgrammeDAOImpl implements ProgrammeICrud{
 	 */
 	@Override
 	public Set<Integer> wildCardSearchOnProgrammes(String keyWord)throws SQLException, Exception {
-		Connection conn=null;
-		PreparedStatement stmt=null;
-		ResultSet rs=null;
-		final Set<Integer> programmeCodeSet=new HashSet<Integer>();		
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		final Set<Integer> programmeCodeSet = new HashSet<Integer>();
 		try {
-			conn=ConnectionManager.getConnection();			
-			final StringBuilder sb =new StringBuilder("SELECT PROG.CODE AS PROGCODE  FROM CAMPUS.PROGRAMME PROG WHERE ");
+			conn = ConnectionManager.getConnection();
+			final StringBuilder sb = new StringBuilder("SELECT PROG.CODE AS PROGCODE  FROM CAMPUS.PROGRAMME PROG WHERE ");
 			sb.append(" ( PROG.NAME LIKE ?");
 			sb.append(" OR PROG.DESCRIPTION LIKE ?");
-			sb.append(" OR PROG.EMAIL LIKE ? )");			
-			sb.append(" AND PROGRAMMESTATUS = ? ");			
-			
-			stmt=conn.prepareStatement(sb.toString());
+			sb.append(" OR PROG.EMAIL LIKE ? )");
+			sb.append(" AND PROGRAMMESTATUS = ? ");
+
+			stmt = conn.prepareStatement(sb.toString());
 			stmt.setString(1, keyWord);
 			stmt.setString(2, keyWord);
 			stmt.setString(3, keyWord);
 			stmt.setInt(4, ApplicationStatus.ACTIVE.getStatusValue());
-		    rs=stmt.executeQuery();
-		    while (rs.next()) {				
-		    	programmeCodeSet.add(Integer.valueOf(rs.getString("PROGCODE")));				
+			rs = stmt.executeQuery();
+			while (rs.next()) {
+				programmeCodeSet.add(Integer.valueOf(rs.getString("PROGCODE")));
 			}
 		} catch (SQLException sqlException) {
 			log.info("wildCardSearchOnProgrammes() sqlException" + sqlException.toString());
@@ -257,8 +255,8 @@ public class ProgrammeDAOImpl implements ProgrammeICrud{
 			throw e;
 		} finally {
 			DaoHelper.cleanup(conn, stmt, rs);
-		}		
-		return programmeCodeSet;	
+		}
+		return programmeCodeSet;
 	}
 	
 	/**
@@ -269,24 +267,24 @@ public class ProgrammeDAOImpl implements ProgrammeICrud{
 	 * @return allProgrammeList Collection<Collection<String>>
 	 */
 	public Collection<Collection<String>> wildCardSearchOnProgrammes(final ProgrammeSearchDTO searchDTO)throws SQLException, Exception {
-		Connection conn=null;
-		PreparedStatement stmt=null;
-		ResultSet rs=null;
-		final Collection<Collection<String>> allProgrammeList=new ArrayList<Collection<String>>();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		final Collection<Collection<String>> allProgrammeList = new ArrayList<Collection<String>>();
 		
 		try {
 			conn=ConnectionManager.getConnection();
-			final StringBuilder sb =new StringBuilder("SELECT PROG.CODE, PROG.NAME, CAST(PROG.[DESCRIPTION] as NVARCHAR(max)) AS [DESCRIPTION],PROG.DURATION  ,PROG.[ENTRYREQUIREMENTS] ,PROG.[COUNSELORNAME] ,");
+			final StringBuilder sb = new StringBuilder("SELECT PROG.CODE, PROG.NAME, CAST(PROG.[DESCRIPTION] as NVARCHAR(max)) AS [DESCRIPTION],PROG.DURATION  ,PROG.[ENTRYREQUIREMENTS] ,PROG.[COUNSELORNAME] ,");
 			sb.append(" PROG.[COUNSELORPHONE] ,PROG.[DISPLAYSTARTDATE] ,PROG.[EXPIRYDATE] ,PROG.[PROGRAMMESTATUS] ,PROG.[COURSEPROVIDER] ,PROG.[MAJOR] ,PROG.[CATEGORY],PROG.[LEVEL] ,PROG.[CLASSTYPE], ");
 			sb.append(" PROVIDER.[NAME] as PROVIDER, PROVIDER.[UNIQUEPREFIX],PROVIDER.[CODE] as [CPCODE] , PROVIDER.[WEBLINK],ISNULL(MIN(INTAKE.[FEE]),0.00) as COST");
 			sb.append(" FROM CAMPUS.PROGRAMME PROG ");
 			sb.append(" INNER JOIN  CAMPUS.COURSEPROVIDER PROVIDER  ON PROG.COURSEPROVIDER=PROVIDER.CODE");
 			sb.append(" LEFT JOIN [CAMPUS].[INTAKE] INTAKE ON INTAKE.PROGRAMME=PROG.CODE");
 			sb.append(" WHERE 1=1");
-			if(searchDTO.getProgrammeStatus()>=0){
+			if (searchDTO.getProgrammeStatus() >= 0) {
 				sb.append(" AND  PROG.PROGRAMMESTATUS = ").append(searchDTO.getProgrammeStatus());
 			}
-			if(searchDTO.getKeyWordString()!=null && !searchDTO.getKeyWordString().isEmpty() ){
+			if (searchDTO.getKeyWordString() != null && !searchDTO.getKeyWordString().isEmpty()) {
 				sb.append(" AND ( PROG.NAME LIKE ?");
 				sb.append(" OR PROG.DESCRIPTION LIKE ?");
 				sb.append(" OR PROG.EMAIL LIKE ? )");
@@ -296,14 +294,14 @@ public class ProgrammeDAOImpl implements ProgrammeICrud{
 			sb.append(" PROG.[COUNSELORPHONE] ,PROG.[DISPLAYSTARTDATE] ,PROG.[EXPIRYDATE] ,PROG.[PROGRAMMESTATUS] ,PROG.[COURSEPROVIDER] ,PROG.[MAJOR] ,PROG.[CATEGORY] ,");
 			sb.append(" PROG.[LEVEL] ,PROG.[CLASSTYPE], PROVIDER.[NAME], PROVIDER.[UNIQUEPREFIX], PROVIDER.[CODE] , PROVIDER.[WEBLINK]");
 			
-			stmt=conn.prepareStatement(sb.toString());
+			stmt = conn.prepareStatement(sb.toString());
 			if (searchDTO.getKeyWordString() != null
 					&& !searchDTO.getKeyWordString().isEmpty()) {
 				stmt.setString(1, searchDTO.getKeyWordString());
 				stmt.setString(2, searchDTO.getKeyWordString());
 				stmt.setString(3, searchDTO.getKeyWordString());
 			}
-		    rs=stmt.executeQuery();		    
+			rs = stmt.executeQuery();	    
 		    
 		    while (rs.next()) {
 				final ArrayList<String> singleProgrammeList = new ArrayList<String>();
