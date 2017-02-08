dae956e1b0c720c534bdef8ba4a2dfdd50181f07
Miyuru-Genesiis <miyuru@genesiis.com>
2017-02-08 10:25:53 +0530

    c111-display-banners-on-jsp-load - Organised imports

--- campus.jar/src/com/genesiis/campus/command/CmdListBannersOnAsyncRequest.java
+++ campus.jar/src/com/genesiis/campus/command/CmdListBannersOnAsyncRequest.java
@@ -1,18 +1,16 @@
 //20170208 MM c111-display-banners-on-jsp-load - INIT class; implemented execute(IDataHelper, IView)
+//20170208 MM c111-display-banners-on-jsp-load - Organised imports
 
 package com.genesiis.campus.command;
+
+import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.validation.BannerData;
+import com.sun.istack.internal.logging.Logger;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
-import com.genesiis.campus.entity.BannerStatDAO;
-import com.genesiis.campus.entity.IView;
-import com.genesiis.campus.entity.model.BannerStat;
-import com.genesiis.campus.util.IDataHelper;
-import com.genesiis.campus.validation.BannerData;
-import com.genesiis.campus.validation.Validator;
-import com.sun.istack.internal.logging.Logger;
 
 public class CmdListBannersOnAsyncRequest implements ICommand {
 
