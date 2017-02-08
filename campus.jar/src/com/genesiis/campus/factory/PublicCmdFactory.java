128c38a9e896aba23173aaab24ed642b1a8d09db
Miyuru-Genesiis <miyuru@genesiis.com>
2017-02-09 03:13:47 +0530

    c111-display-banners-on-jsp-load - Added map element and case clause for Operation.LIST_BANNERS enum

--- campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
@@ -2,16 +2,19 @@
 //20161116 MM c2-integrate-google-banners Changed map element with key LIST_GOOGLE_ADVERTS to LOAD_BANNER_PAGE
 //20161217 MM c2-integrate-google-banners Removed map element and case block related to LOAD_BANNER_PAGE, 
 //				that was there for testing purposes during development of the issue
+//20170208 MM c111-display-banners-on-jsp-load - Added map element and case clause for Operation.LIST_BANNERS enum
 
 package com.genesiis.campus.factory;
 
 import com.genesiis.campus.command.CmdAddBannerStat;
+import com.genesiis.campus.command.CmdListBannersOnAsyncRequest;
 import com.genesiis.campus.command.ICommand;
 import com.genesiis.campus.validation.Operation;
 
 public class PublicCmdFactory implements ICmdFactory {
 	static {
 		map.put(Operation.ADD_BANNER_STAT, new CmdAddBannerStat());
+		map.put(Operation.LIST_BANNERS, new CmdListBannersOnAsyncRequest());
 	}
 
 	@Override
@@ -21,6 +24,9 @@ public class PublicCmdFactory implements ICmdFactory {
 		o = Operation.getOperation(cco);
 		switch (o) {
 			case ADD_BANNER_STAT:
+				command = map.get(o);
+				break;
+			case LIST_BANNERS:
 				command = map.get(o);
 				break;
 			default:
