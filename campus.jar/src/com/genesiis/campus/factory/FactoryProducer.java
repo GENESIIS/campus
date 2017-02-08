985a68a3976a92ada4c0f1a69e038044f13f16ab
dushanthacb <dushantha@genesiis.com>
2017-02-08 16:55:59 +0530

    c131-admin-manage-banner-upload-banner-image-dn added /AdminController in getFactory().

--- campus.jar/src/com/genesiis/campus/factory/FactoryProducer.java
+++ campus.jar/src/com/genesiis/campus/factory/FactoryProducer.java
@@ -3,6 +3,7 @@ package com.genesiis.campus.factory;
 //20161025 DN c10-contacting-us create the initial version of FactoryProducer.java
 //20170102 DN CAM: 47 added TutorController if else branch
 //20161130 PN c27-upload-user-image: removed return value for choice - "/PublicController".
+//20170208 DN c131-admin-manage-banner-upload-banner-image-dn  added /AdminController in getFactory().
 
 import com.genesiis.campus.command.CmdGenerateEmail;
 import org.apache.log4j.Logger;
@@ -30,6 +31,8 @@ public class FactoryProducer {
 			return new TutorCmdFactory();
 		} else if (choice.equalsIgnoreCase("/StudentController")) {
 			return new StudentCmdFactory();
+		} else if (choice.equalsIgnoreCase("/AdminController")) {
+			return new AdminCmdFactory();
 		}
 		return null;
 	}
