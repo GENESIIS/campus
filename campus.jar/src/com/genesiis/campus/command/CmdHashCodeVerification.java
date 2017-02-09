0b00c3fba7e8cc91ac7098214dda8bd187d71da7
Anuradha Rushmitha <anuradha@genesiis.com>
2017-02-10 00:49:01 +0530

    CAM-22 created for verify hashcode

---
+++ campus.jar/src/com/genesiis/campus/command/CmdHashCodeVerification.java
@@ -0,0 +1,17 @@
+package com.genesiis.campus.command;
+//201700209 AS C22 forgot password, CmdHashCodeVerification command class created
+import java.sql.SQLException;
+
+import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.util.IDataHelper;
+
+public class CmdHashCodeVerification implements ICommand{
+
+	@Override
+	public IView execute(IDataHelper helper, IView view) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
