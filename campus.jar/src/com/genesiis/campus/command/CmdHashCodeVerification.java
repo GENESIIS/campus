8756516c2791df5ec7f1eddda41c58080706b719
Anuradha Silva <anuradha@genesiis.com>
2017-02-13 17:36:13 +0530

    c22 command class coding WIP

--- campus.jar/src/com/genesiis/campus/command/CmdHashCodeVerification.java
+++ campus.jar/src/com/genesiis/campus/command/CmdHashCodeVerification.java
@@ -1,17 +1,62 @@
 package com.genesiis.campus.command;
 //201700209 AS C22 forgot password, CmdHashCodeVerification command class created
 import java.sql.SQLException;
+import java.util.Collection;
 
+import org.apache.log4j.Logger;
+
+import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.StudentEmailVerificationDAO;
+import com.genesiis.campus.entity.model.Student;
 import com.genesiis.campus.util.IDataHelper;
+import com.google.gson.Gson;
 
 public class CmdHashCodeVerification implements ICommand{
-
+	static Logger log = Logger.getLogger(CmdHashCodeVerification.class.getName());
+	private Student data;
+	private Collection<Collection<String>> dataCollection = null;
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String gsonData = helper.getParameter("jsonData");
+		data = getStudentdetails(gsonData);
+		StudentEmailVerificationDAO studentEmailvarification = new StudentEmailVerificationDAO();
+		dataCollection = studentEmailvarification.verifyHashCode(data);
+		
+		
+		
+		return view;
+	}
+	
+	
+	/**
+	 * extract data from json object and assign to Student object
+	 * object
+	 * 
+	 * @author anuradha
+	 * @param gsonData
+	 * @return Student object
+	 */
+
+	private Student getStudentdetails(String gsonData) {
+		Student student = (Student) extractFromJason(gsonData);
+		return student;
+	}
+
+	public Object extractFromJason(String gsonData) {
+		Gson gson = new Gson();
+		String message = "";
+		Student student = null;
+		try {
+			student = gson.fromJson(gsonData, Student.class);
+
+		} catch (Exception e) {
+			log.error("extractFromJason(): " + e.toString());
+			throw e;
+		}
+		return student;
 	}
 
 }
