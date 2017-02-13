40344226f682489676022085fe33771059dee0eb
Anuradha Silva <anuradha@genesiis.com>
2017-02-13 17:35:28 +0530

    CAM-22 command class modification completed

--- campus.jar/src/com/genesiis/campus/command/CmdEmailVarification.java
+++ campus.jar/src/com/genesiis/campus/command/CmdEmailVarification.java
@@ -34,74 +34,93 @@ public class CmdEmailVarification implements ICommand {
 	String lastName = "";
 	String email = "";
 	String uname = "";
-	String SecuredHash="";
+	String code ="";
+	String securedHash = "";
 	private String emailTitle = "Reset password instructions";
 	private ArrayList<String> recieversEmailAddreses;
 	private String sendersEmail = "dushantha@genesiis.com";
 	private String emailBody = "Someone has requested a link to change your password. You can do this through the Link below.";
 
-
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
-		try{ 
-		int status;
-		IEmailComposer resetPasswordEmailComposer = new ResetPasswordInstructionEmailDispenser();
-		String gsonData = helper.getParameter("jsonData");
-		data = getStudentdetails(gsonData);
-		boolean validEmail = LoginValidator.validateEmail(data.getEmail());
-		if (validEmail) {
-			ICrud emailVarifyDAO = new StudentEmailVerificationDAO();
-			dataCollection = emailVarifyDAO.findById(data);
-			// log.info(dataCollection);
+		try {
+			int status = -3;
+			IEmailComposer resetPasswordEmailComposer = new ResetPasswordInstructionEmailDispenser();
+			String gsonData = helper.getParameter("jsonData");
+			data = getStudentdetails(gsonData);
+			boolean validEmail = LoginValidator.validateEmail(data.getEmail());
+			if (validEmail) {
+				ICrud emailVarifyDAO = new StudentEmailVerificationDAO();
+				dataCollection = emailVarifyDAO.findById(data);
+				// log.info(dataCollection);
 
-			for (Collection<String> collection : dataCollection) {
-				Object[] array = collection.toArray();
-				result = (String) array[0];
-				firstName = (String) array[0];
-				lastName = (String) array[1];
-				email = (String) array[2];
-				uname = (String) array[3];
-			}
+				for (Collection<String> collection : dataCollection) {
+					Object[] array = collection.toArray();
+					result = (String) array[0];
+					firstName = (String) array[0];
+					lastName = (String) array[1];
+					email = (String) array[2];
+					uname = (String) array[3];
+					code = (String) array[4] ;
+				}
 
-			if (result.equalsIgnoreCase(SystemMessage.INVALID_EMAIL.message())) {
-				message = SystemMessage.INVALID_EMAIL.message();
-				log.info(message + "okkkkkkkk");
+				if (result.equalsIgnoreCase(SystemMessage.INVALID_EMAIL
+						.message())) {
+					message = SystemMessage.INVALID_EMAIL.message();
+					log.info(message + "okkkkkkkk");
+				} else {
+					data.setFirstName(firstName);
+					data.setLastName(lastName);
+					data.setEmail(email);
+					data.setCode(Integer.parseInt(code));
+					
+					recieversEmailAddreses = new ArrayList<String>();
+					recieversEmailAddreses.add(email);
+					HashCodeBuilder hashBuilder = new HashCodeBuilder();
+					securedHash = hashBuilder.createHash(firstName, lastName);
+					data.setHashCode(securedHash);
+					
+					int updateData = emailVarifyDAO.update(data);
+					 if(updateData >0){
+						 message = SystemMessage.HASHCODES.message();
+						 resetPasswordEmailComposer.setEnvironment(firstName + " "
+									+ lastName, sendersEmail, recieversEmailAddreses,
+									emailTitle, emailBody + " " + securedHash,
+									securedHash);
+
+							resetPasswordEmailComposer
+									.setGeneralEmail(resetPasswordEmailComposer
+											.formatEmailInstance(addSpecificContentToOriginalMailBody()));
+							status = this.sendMail(resetPasswordEmailComposer);
+						 
+					 }else{
+						 message = SystemMessage.HASHCODEUNS.message();
+					 }
+					 
+					
+					helper.setAttribute("message", systemMessage(status));
+
+				}
 			} else {
-				log.info(firstName + " " + lastName);
-				recieversEmailAddreses = new ArrayList<String>();
-				recieversEmailAddreses.add(email);
-				HashCodeBuilder hashBuilder = new HashCodeBuilder();
-				SecuredHash = hashBuilder.createHash(firstName, lastName);
-				
-				resetPasswordEmailComposer.setEnvironment(firstName+" "+lastName, sendersEmail, recieversEmailAddreses, emailTitle, emailBody+" "+SecuredHash, SecuredHash);
-				
-				resetPasswordEmailComposer.setGeneralEmail(resetPasswordEmailComposer.formatEmailInstance(
-						 addSpecificContentToOriginalMailBody())); 
-				status=this.sendMail(resetPasswordEmailComposer);
-				helper.setAttribute("message", systemMessage(status));
-				
+				message = SystemMessage.INVALID_EMAIL.message();
 			}
-		} else {
-			message = SystemMessage.INVALID_EMAIL.message();
-		}
-		}catch (SQLException sexp) {
-			log.error("execute(): SQLException "+ sexp.toString());
+		} catch (SQLException sexp) {
+			log.error("execute(): SQLException " + sexp.toString());
 			throw sexp;
-		} catch (IllegalArgumentException ilexp){
-			log.error("execute(): IllegalArgumentException"+ ilexp.toString());
+		} catch (IllegalArgumentException ilexp) {
+			log.error("execute(): IllegalArgumentException" + ilexp.toString());
 			throw ilexp;
 		} catch (Exception exp) {
-			log.error("execute():Exception "+ exp.toString());
+			log.error("execute():Exception " + exp.toString());
 			throw exp;
 		}
-		
-		
+
 		return view;
 	}
 
 	/**
-	 * extract data from json object and assign to StudentProgrammeInquiry
+	 * extract data from json object and assign to Student object
 	 * object
 	 * 
 	 * @author anuradha
@@ -127,89 +146,93 @@ public class CmdEmailVarification implements ICommand {
 		}
 		return student;
 	}
-	
-	
+
 	/*
 	 * sendMail() method intended to dispense the email
+	 * 
 	 * @author DN
-	 * @return int -3 fail sending email 3 sent email successfully 
+	 * 
+	 * @return int -3 fail sending email 3 sent email successfully
+	 * 
 	 * @throws MessagingException in any case dispensing email fails
 	 */
-	private int sendMail(IEmailComposer emailComposer)  {
-		int MAIL_SENT_STATUS=3;
-		try{ 
-			if(emailComposer.getGeneralEmail()== null){
+	private int sendMail(IEmailComposer emailComposer) {
+		int MAIL_SENT_STATUS = 3;
+		try {
+			if (emailComposer.getGeneralEmail() == null) {
 				Exception exp = new Exception("IEmail is not created ");
-				log.error("sendMail(): Exception"+exp.toString());
+				log.error("sendMail(): Exception" + exp.toString());
 				throw exp;
 			}
-				
-			emailComposer.setEmailDispenser(
-					new EmailDispenser(emailComposer.getGeneralEmail())
-					);
+
+			emailComposer.setEmailDispenser(new EmailDispenser(emailComposer
+					.getGeneralEmail()));
 			emailComposer.getEmailDispenser().emailDispense();
-		} catch (IllegalArgumentException illearg){
-			log.error("sendMail():IllegalArgumentException "+illearg.toString());
-			 MAIL_SENT_STATUS= -3;
+		} catch (IllegalArgumentException illearg) {
+			log.error("sendMail():IllegalArgumentException "
+					+ illearg.toString());
+			MAIL_SENT_STATUS = -3;
 		} catch (MessagingException msexp) {
-			log.error("sendMail():MessagingException "+msexp.toString());
-		 MAIL_SENT_STATUS= -3;
+			log.error("sendMail():MessagingException " + msexp.toString());
+			MAIL_SENT_STATUS = -3;
 		} catch (NullPointerException msexp) {
-			log.error("sendMail():NullPointerException "+msexp.toString());
-		 MAIL_SENT_STATUS= -3;
-		} catch(Exception exp){
-			 MAIL_SENT_STATUS= -3;
-		}
-		finally{
+			log.error("sendMail():NullPointerException " + msexp.toString());
+			MAIL_SENT_STATUS = -3;
+		} catch (Exception exp) {
+			MAIL_SENT_STATUS = -3;
+		} finally {
 			return MAIL_SENT_STATUS;
 		}
-		
+
 	}
-	
+
 	/*
 	 * addSpecificContentToOriginalMailBody() formats the original details with
 	 * users credentials e.g email, contact number, full name
+	 * 
 	 * @param originalMailBody String the original message that the user send to
 	 * the SMPT mail server
 	 */
-	private String addSpecificContentToOriginalMailBody(){
-		StringBuilder result = new StringBuilder();	
+	private String addSpecificContentToOriginalMailBody() {
+		StringBuilder result = new StringBuilder();
 		result.append(System.getProperty("line.separator"));
 		result.append(System.getProperty("line.separator"));
-	//	result.append("Account credentials are as follows. ");
+		// result.append("Account credentials are as follows. ");
 		result.append(System.getProperty("line.separator"));
 		result.append(System.getProperty("line.separator"));
-//		result.append("User Name :"+partialStudent.getUserName());		
+		// result.append("User Name :"+partialStudent.getUserName());
 		result.append(System.getProperty("line.separator"));
-//		result.append("Pass word :"+partialStudent.getPassWord());
-		result.append(System.getProperty("line.separator"));
-		result.append(System.getProperty("line.separator"));
-//		result.append("It's so much pleasure to have you with us. Have a good day!");
+		// result.append("Pass word :"+partialStudent.getPassWord());
 		result.append(System.getProperty("line.separator"));
 		result.append(System.getProperty("line.separator"));
-//		result.append(SystemMessage.SUPERADMIN_NAME.message());
+		// result.append("It's so much pleasure to have you with us. Have a good day!");
+		result.append(System.getProperty("line.separator"));
+		result.append(System.getProperty("line.separator"));
+		// result.append(SystemMessage.SUPERADMIN_NAME.message());
 		return result.toString();
-		
+
 	}
-	
+
 	/*
-	 * systemMessage() handles the system Messages according to
-	 * the state of the status passed in
-	 * @return String the message
-	 * @param status 3 request submitted successfully.
-	 * @param status -3 request submission fails.
+	 * systemMessage() handles the system Messages according to the state of the
+	 * status passed in
 	 * 
+	 * @return String the message
+	 * 
+	 * @param status 3 request submitted successfully.
+	 * 
+	 * @param status -3 request submission fails.
 	 */
-	private String systemMessage(int status){
+	private String systemMessage(int status) {
 		String message = SystemMessage.UNKNOWN.message();
-		switch(status){		
+		switch (status) {
 		case 3:
-			message =SystemMessage.MAIL_SUCCESS.message();
+			message = SystemMessage.MAIL_SUCCESS.message();
 			break;
 		case -3:
-			message =SystemMessage.MAIL_UNSUCCESS.message();
+			message = SystemMessage.MAIL_UNSUCCESS.message();
 			break;
-		default:			
+		default:
 			break;
 		}
 		return message;
