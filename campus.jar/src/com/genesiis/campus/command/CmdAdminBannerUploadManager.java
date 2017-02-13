61db71b53f3cf20e5c3fc46e062062181b55230b
dushanthacb <dushantha@genesiis.com>
2017-02-13 13:17:58 +0530

    c131-admin-manage-banner-upload-banner-image-dn created the initial class stub

---
+++ campus.jar/src/com/genesiis/campus/command/CmdAdminBannerUploadManager.java
@@ -0,0 +1,17 @@
+package com.genesiis.campus.command;
+
+import java.sql.SQLException;
+
+import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.util.IDataHelper;
+
+public class CmdAdminBannerUploadManager implements ICommand {
+
+	@Override
+	public IView execute(IDataHelper helper, IView view) throws SQLException,
+			Exception {
+		
+		return null;
+	}
+
+}
