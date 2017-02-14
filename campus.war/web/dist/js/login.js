58f75ba359fbb01fb5894d738d4a8311f5a100d9
Anuradha Silva <anuradha@genesiis.com>
2017-02-14 17:49:05 +0530

    CAM-22 reset password function AJAX call WIP

--- campus.war/web/dist/js/login.js
+++ campus.war/web/dist/js/login.js
@@ -236,6 +236,12 @@ function verifyCode() {
 			dataType : "json",
 			success : function(response) {
 				
+				if (response['errorMessage'] == "Your Varification code is invalid. Please try again ! " || response['errorMessage'] == "Verification code has been Expired!") {
+					alert(response['errorMessage']);
+					document.getElementById('verifyMesssage').innerHTML = response['errorMessage'];
+				} else if(response['errorMessage'] == "Your Verification code valid !") {
+					window.location.href = response['pageURL'];
+				}
 			},
 			error : function(response,error,errorThrown) {
 				alert("Error " + error);
@@ -260,4 +266,114 @@ function verifyCode() {
 
 		});
 	}
+}
+// password changed ajax function 
+function changedPassword(){
+	var code = $("#verifyCode").val();
+	var password = $("#verifiemail").val();
+	var paaswordEmpty = isempty(password);
+	
+	//code filed validation error messages handling
+	if (!(paaswordEmpty)) {
+		document.getElementById('emailtbError').innerHTML = "  ** Verify Code can not be Empty.";
+		flag = false;
+		return false;
+	}
+	
+	
+	if (code != null) {
+		var jsonData = {
+			"code" : code,
+			"password":password
+		};
+		
+		$.ajax({
+			type : "POST",
+			url : '/LoginController',
+			data : {
+				jsonData : JSON.stringify(jsonData),
+				CCO : "RESETPASS"
+
+			},
+			dataType : "json",
+			success : function(response) {
+				if (response['message'] === "") {
+					window.location.href = response['pageURL'];
+				} else {
+					document.getElementById('errorMesssage').innerHTML = response['message'];
+				}
+			},
+			error : function(response,error,errorThrown) {
+				alert("Error " + error);
+				console.log(error);
+				 var msg = '';
+			      if (response.status === 0) {
+			          msg = 'Not connect.\n Verify Network.';
+			      } else if (response.status == 404) {
+			          msg = 'Requested page not found. [404]';
+			      } else if (response.status == 500) {
+			          msg = 'Internal Server Error [500].';
+			      } else if (error === 'parsererror') {
+			          msg = 'Requested JSON parse failed.';
+			      } else if (error === 'timeout') {
+			          msg = 'Time out error.';
+			      } else if (error === 'abort') {
+			          msg = 'Ajax request aborted.';
+			      } else {
+			          msg = 'Uncaught Error.\n' + response.responseText;
+			      }
+			}
+
+		});
+	}
+}
+
+/**
+ * validateSignUpWoThirdPartyPageEmbedData() validates all the current  critical fields
+ * placed on /dist/partials/signUpWoThirdParty.jsp page. It's the custom field validator
+ * dedicated for the page above.
+ * @author dushantha DN
+ * @returns {Boolean}
+ */
+function validatePasswordResetData(){
+	var validationPass = true;
+	
+	if (!(isFieldFilled(isStringHasValiCharsAndLength($('#passWord').val(),/^([a-zA-Z0-9]+)([a-zA-Z0-9_]+){7,}$/g),"Check Field Contains Invalid Characters Or Should Be > 7 Characters and ","passWordError"))) {
+		return !validationPass;
+	}else if (!(isFieldFilled(isempty($('#passWord').val()),"Password Field","passWordError"))) {
+		return !validationPass;
+	} else if (!(isFieldFilled(isempty($('#confrmpsw').val()),"Confirm Password Field","confPassWordError"))){
+		return !validationPass;
+	} else if(!(isFieldFilled(passwordAndConfirmPassword($('#passWord').val(),$('#confrmpsw').val()),"PassWords Does Not Match ,The Field(s)","confPassWordError"))){
+		return !validationPass;
+	}
+		return validationPass;
+	
+}
+
+/**
+ * convertPassWordToString() method displays the pass word to text
+ * @author dushantha DN 
+ * @param checkboxId check box id which used to toggle the command
+ * @param passWordElementId password field element id 
+ * @param confirmWordElementId confirming field element id
+ * @returns void
+ */
+
+function convertPassWordToString(checkboxId,passWordElementId,confirmWordElementId){
+	var value =($('#'+checkboxId).is(':checked'))?"text":"password";
+	$('#'+passWordElementId).attr("type",value);
+	$('#'+confirmWordElementId).attr("type",value);
+}
+
+/**
+ * clearAllFields willclear all the data input text fields
+ * once it'scalled
+ */
+function clearAllFields(){
+	
+	$('#showpasscheckbox').prop('checked', false);
+	var value = $('#showpasscheckbox').is(':checked')?"text":"password";
+	$('#passWord').attr("type",value);
+	$('#confrmpsw').attr("type",value);
 }
