4ef064ebf00c0e0ca9f2155ea78183c1c4d00f44
Anuradha Rushmitha <anuradha@genesiis.com>
2017-02-16 12:25:12 +0530

    CAM-22 user data extract and bind to url done

--- campus.war/web/dist/js/login.js
+++ campus.war/web/dist/js/login.js
@@ -57,25 +57,25 @@ function studentLogin() {
 						}
 
 					},
-					error : function(response,error,errorThrown) {
+					error : function(response, error, errorThrown) {
 						alert("Error " + error);
 						console.log(error);
-						 var msg = '';
-					      if (response.status === 0) {
-					          msg = 'Not connect.\n Verify Network.';
-					      } else if (response.status == 404) {
-					          msg = 'Requested page not found. [404]';
-					      } else if (response.status == 500) {
-					          msg = 'Internal Server Error [500].';
-					      } else if (error === 'parsererror') {
-					          msg = 'Requested JSON parse failed.';
-					      } else if (error === 'timeout') {
-					          msg = 'Time out error.';
-					      } else if (error === 'abort') {
-					          msg = 'Ajax request aborted.';
-					      } else {
-					          msg = 'Uncaught Error.\n' + response.responseText;
-					      }
+						var msg = '';
+						if (response.status === 0) {
+							msg = 'Not connect.\n Verify Network.';
+						} else if (response.status == 404) {
+							msg = 'Requested page not found. [404]';
+						} else if (response.status == 500) {
+							msg = 'Internal Server Error [500].';
+						} else if (error === 'parsererror') {
+							msg = 'Requested JSON parse failed.';
+						} else if (error === 'timeout') {
+							msg = 'Time out error.';
+						} else if (error === 'abort') {
+							msg = 'Ajax request aborted.';
+						} else {
+							msg = 'Uncaught Error.\n' + response.responseText;
+						}
 					}
 
 				});
@@ -83,7 +83,7 @@ function studentLogin() {
 	}
 }
 
-// reset error message labels 
+// reset error message labels
 function resetLoginLabels() {
 
 	$("#errorMesssage").text("");
@@ -108,46 +108,54 @@ function studentLogout() {
 			success : function(response) {
 				$(window).scrollTop(0);
 				$('#logout-popup').modal('show');
-				 
-				setTimeout( function(){
-					window.location.href = response['pageURL']; //this name may have to change depend on actual location of the page "Student Login or public index page"
-					}, 5000);
-				
-				
+
+				setTimeout(function() {
+					window.location.href = response['pageURL']; // this name may
+																// have to
+																// change depend
+																// on actual
+																// location of
+																// the page
+																// "Student
+																// Login or
+																// public index
+																// page"
+				}, 5000);
+
 			},
-			error : function(response,error,errorThrown) {
+			error : function(response, error, errorThrown) {
 				alert("Error " + error);
 				console.log(error);
-				 var msg = '';
-			      if (response.status === 0) {
-			          msg = 'Not connect.\n Verify Network.';
-			      } else if (response.status == 404) {
-			          msg = 'Requested page not found. [404]';
-			      } else if (response.status == 500) {
-			          msg = 'Internal Server Error [500].';
-			      } else if (error === 'parsererror') {
-			          msg = 'Requested JSON parse failed.';
-			      } else if (error === 'timeout') {
-			          msg = 'Time out error.';
-			      } else if (error === 'abort') {
-			          msg = 'Ajax request aborted.';
-			      } else {
-			          msg = 'Uncaught Error.\n' + response.responseText;
-			      }
+				var msg = '';
+				if (response.status === 0) {
+					msg = 'Not connect.\n Verify Network.';
+				} else if (response.status == 404) {
+					msg = 'Requested page not found. [404]';
+				} else if (response.status == 500) {
+					msg = 'Internal Server Error [500].';
+				} else if (error === 'parsererror') {
+					msg = 'Requested JSON parse failed.';
+				} else if (error === 'timeout') {
+					msg = 'Time out error.';
+				} else if (error === 'abort') {
+					msg = 'Ajax request aborted.';
+				} else {
+					msg = 'Uncaught Error.\n' + response.responseText;
+				}
 			}
 
 		});
 	}
 }
 
-//forget password function
+// forget password function
 function forgotPassword() {
 	var userEmail = $("#verifiemail").val();
-	
+
 	var emailempty = isempty(userEmail);
 	var valEmail = isValidEmailFormat(userEmail);
-	
-	//email filed validation error messages handling
+
+	// email filed validation error messages handling
 	if (!(emailempty)) {
 		document.getElementById('emailtbError').innerHTML = "  ** Email can not be Empty.";
 		flag = false;
@@ -158,231 +166,279 @@ function forgotPassword() {
 		flag = false;
 		return false;
 	}
-	
+
 	if (userEmail != null) {
 		var jsonData = {
 			"email" : userEmail
-			
+
 		};
-		
-		$.ajax({
-			type : "POST",
-			url : '/LoginController',
-			data : {
-				jsonData : JSON.stringify(jsonData),
-				CCO : "EMAILV"
 
-			},
-			dataType : "json",
-			success : function(response) {
-				document.getElementById('messsage').innerHTML = response['message'];
-				setTimeout( function(){
-					$('#verifications-popup').modal('show'); 
-					}, 5000);
-			},
-			error : function(response,error,errorThrown) {
-				alert("Error " + error);
-				console.log(error);
-				 var msg = '';
-			      if (response.status === 0) {
-			          msg = 'Not connect.\n Verify Network.';
-			      } else if (response.status == 404) {
-			          msg = 'Requested page not found. [404]';
-			      } else if (response.status == 500) {
-			          msg = 'Internal Server Error [500].';
-			      } else if (error === 'parsererror') {
-			          msg = 'Requested JSON parse failed.';
-			      } else if (error === 'timeout') {
-			          msg = 'Time out error.';
-			      } else if (error === 'abort') {
-			          msg = 'Ajax request aborted.';
-			      } else {
-			          msg = 'Uncaught Error.\n' + response.responseText;
-			      }
-			}
+		$
+				.ajax({
+					type : "POST",
+					url : '/LoginController',
+					data : {
+						jsonData : JSON.stringify(jsonData),
+						CCO : "EMAILV"
 
-		});
+					},
+					dataType : "json",
+					success : function(response) {
+						document.getElementById('messsage').innerHTML = response['message'];
+						setTimeout(function() {
+							$('#verifications-popup').modal('show');
+						}, 5000);
+					},
+					error : function(response, error, errorThrown) {
+						alert("Error " + error);
+						console.log(error);
+						var msg = '';
+						if (response.status === 0) {
+							msg = 'Not connect.\n Verify Network.';
+						} else if (response.status == 404) {
+							msg = 'Requested page not found. [404]';
+						} else if (response.status == 500) {
+							msg = 'Internal Server Error [500].';
+						} else if (error === 'parsererror') {
+							msg = 'Requested JSON parse failed.';
+						} else if (error === 'timeout') {
+							msg = 'Time out error.';
+						} else if (error === 'abort') {
+							msg = 'Ajax request aborted.';
+						} else {
+							msg = 'Uncaught Error.\n' + response.responseText;
+						}
+					}
+
+				});
 	}
 }
-// Verify hash code 
+// Verify hash code
 
 function verifyCode() {
 	var code = $("#verifyCode").val();
 	var email = $("#verifiemail").val();
 	var codeEmpty = isempty(code);
-	
-	//code filed validation error messages handling
+
+	// code filed validation error messages handling
 	if (!(codeEmpty)) {
 		document.getElementById('emailtbError').innerHTML = "  ** Verify Code can not be Empty.";
 		flag = false;
 		return false;
 	}
-	
-	
+
 	if (code != null) {
 		var jsonData = {
 			"hashCode" : code,
-			"email":email
+			"email" : email
 		};
-		
-		$.ajax({
-			type : "POST",
-			url : '/LoginController',
-			data : {
-				jsonData : JSON.stringify(jsonData),
-				CCO : "HASHV"
 
-			},
-			dataType : "json",
-			success : function(response) {
-				var counter = 0;
-				if (response['errorMessage'] == "Your Varification code is invalid. Please try again ! " || response['errorMessage'] == "Verification code has been Expired!") {
-					alert(response['errorMessage']);
-					document.getElementById('verifyMesssage').innerHTML = response['errorMessage'];
-				} else if(response['errorMessage'] == "Your Verification code valid !") {
-					window.location.href = response['pageURL'];
-				}else{
-					
-					var resultData = response.result;
-					alert(resultData);
-					$.each(response.result, function(index, value) {
-    					var res = value.toString();
-    					var data = res.split(",");
-    					counter++;});
-					alert(data[1].toString());
-				}
-			},
-			error : function(response,error,errorThrown) {
-				alert("Error " + error);
-				console.log(error);
-				 var msg = '';
-			      if (response.status === 0) {
-			          msg = 'Not connect.\n Verify Network.';
-			      } else if (response.status == 404) {
-			          msg = 'Requested page not found. [404]';
-			      } else if (response.status == 500) {
-			          msg = 'Internal Server Error [500].';
-			      } else if (error === 'parsererror') {
-			          msg = 'Requested JSON parse failed.';
-			      } else if (error === 'timeout') {
-			          msg = 'Time out error.';
-			      } else if (error === 'abort') {
-			          msg = 'Ajax request aborted.';
-			      } else {
-			          msg = 'Uncaught Error.\n' + response.responseText;
-			      }
-			}
+		$
+				.ajax({
+					type : "POST",
+					url : '/LoginController',
+					data : {
+						jsonData : JSON.stringify(jsonData),
+						CCO : "HASHV"
 
-		});
+					},
+					dataType : "json",
+					success : function(response) {
+						var counter = 0;
+						if (response['errorMessage'] == "Your Varification code is invalid. Please try again ! "
+								|| response['errorMessage'] == "Verification code has been Expired!") {
+							alert(response['errorMessage']);
+							document.getElementById('verifyMesssage').innerHTML = response['errorMessage'];
+						} else if (response['errorMessage'] == "Your Verification code valid !") {
+							window.location.href = response['pageURL'];
+						} else {
+
+							var firstName = "";
+							var lastName = "";
+							var email = "";
+							var scode = "";
+							var resultData = response.result;
+							
+							$.each(response.result, function(index, value) {
+								var res = value.toString();
+								var data = res.split(",");
+								counter++;
+								alert(data[4].toString());
+								firstName = data[0].toString();
+								lastName = data[1].toString();
+								email = data[2].toString();
+								scode = data[4].toString();
+							});
+							var encode = hashEncode(scode);
+							//var decode = hashDecose(encode);
+							//data binding to URL 
+							var pageURL = firstName+"&"+lastName+"&"+email+"&"+encode;
+						
+							window.location.href = response['pageURL']+"?uData&"+pageURL;
+						}
+					},
+					error : function(response, error, errorThrown) {
+						alert("Error " + error);
+						console.log(error);
+						var msg = '';
+						if (response.status === 0) {
+							msg = 'Not connect.\n Verify Network.';
+						} else if (response.status == 404) {
+							msg = 'Requested page not found. [404]';
+						} else if (response.status == 500) {
+							msg = 'Internal Server Error [500].';
+						} else if (error === 'parsererror') {
+							msg = 'Requested JSON parse failed.';
+						} else if (error === 'timeout') {
+							msg = 'Time out error.';
+						} else if (error === 'abort') {
+							msg = 'Ajax request aborted.';
+						} else {
+							msg = 'Uncaught Error.\n' + response.responseText;
+						}
+					}
+
+				});
 	}
 }
-// password changed ajax function 
-function changedPassword(){
+// String encode to Hash code
+function hashEncode(data) {
+	// Define the string
+	var string = data;
+
+	// Encode the String
+	var encodedString = btoa(string);
+	console.log(encodedString);
+	return encodedString;
+}
+// String Hash code decode to Sting
+function hashDecose(data) {
+	// Decode the String
+	var decodedString = atob(data);
+	console.log(decodedString); // Outputs: "Hello World!"
+	return decodedString;
+}
+
+// password changed ajax function
+function changedPassword() {
 	var code = $("#verifyCode").val();
 	var password = $("#verifiemail").val();
 	var paaswordEmpty = isempty(password);
-	
-	//code filed validation error messages handling
+
+	// code filed validation error messages handling
 	if (!(paaswordEmpty)) {
 		document.getElementById('emailtbError').innerHTML = "  ** Verify Code can not be Empty.";
 		flag = false;
 		return false;
 	}
-	
-	
+
 	if (code != null) {
 		var jsonData = {
 			"code" : code,
-			"password":password
+			"password" : password
 		};
-		
-		$.ajax({
-			type : "POST",
-			url : '/LoginController',
-			data : {
-				jsonData : JSON.stringify(jsonData),
-				CCO : "RESETPASS"
 
-			},
-			dataType : "json",
-			success : function(response) {
-				if (response['message'] === "") {
-					window.location.href = response['pageURL'];
-				} else {
-					document.getElementById('errorMesssage').innerHTML = response['message'];
-				}
-			},
-			error : function(response,error,errorThrown) {
-				alert("Error " + error);
-				console.log(error);
-				 var msg = '';
-			      if (response.status === 0) {
-			          msg = 'Not connect.\n Verify Network.';
-			      } else if (response.status == 404) {
-			          msg = 'Requested page not found. [404]';
-			      } else if (response.status == 500) {
-			          msg = 'Internal Server Error [500].';
-			      } else if (error === 'parsererror') {
-			          msg = 'Requested JSON parse failed.';
-			      } else if (error === 'timeout') {
-			          msg = 'Time out error.';
-			      } else if (error === 'abort') {
-			          msg = 'Ajax request aborted.';
-			      } else {
-			          msg = 'Uncaught Error.\n' + response.responseText;
-			      }
-			}
+		$
+				.ajax({
+					type : "POST",
+					url : '/LoginController',
+					data : {
+						jsonData : JSON.stringify(jsonData),
+						CCO : "RESETPASS"
 
-		});
+					},
+					dataType : "json",
+					success : function(response) {
+						if (response['message'] === "") {
+							window.location.href = response['pageURL'];
+						} else {
+							document.getElementById('errorMesssage').innerHTML = response['message'];
+						}
+					},
+					error : function(response, error, errorThrown) {
+						alert("Error " + error);
+						console.log(error);
+						var msg = '';
+						if (response.status === 0) {
+							msg = 'Not connect.\n Verify Network.';
+						} else if (response.status == 404) {
+							msg = 'Requested page not found. [404]';
+						} else if (response.status == 500) {
+							msg = 'Internal Server Error [500].';
+						} else if (error === 'parsererror') {
+							msg = 'Requested JSON parse failed.';
+						} else if (error === 'timeout') {
+							msg = 'Time out error.';
+						} else if (error === 'abort') {
+							msg = 'Ajax request aborted.';
+						} else {
+							msg = 'Uncaught Error.\n' + response.responseText;
+						}
+					}
+
+				});
 	}
 }
 
 /**
- * validateSignUpWoThirdPartyPageEmbedData() validates all the current  critical fields
- * placed on /dist/partials/signUpWoThirdParty.jsp page. It's the custom field validator
- * dedicated for the page above.
+ * validateSignUpWoThirdPartyPageEmbedData() validates all the current critical
+ * fields placed on /dist/partials/signUpWoThirdParty.jsp page. It's the custom
+ * field validator dedicated for the page above.
+ * 
  * @author dushantha DN
  * @returns {Boolean}
  */
-function validatePasswordResetData(){
+function validatePasswordResetData() {
 	var validationPass = true;
-	
-	if (!(isFieldFilled(isStringHasValiCharsAndLength($('#passWord').val(),/^([a-zA-Z0-9]+)([a-zA-Z0-9_]+){7,}$/g),"Check Field Contains Invalid Characters Or Should Be > 7 Characters and ","passWordError"))) {
+
+	if (!(isFieldFilled(
+			isStringHasValiCharsAndLength($('#passWord').val(),
+					/^([a-zA-Z0-9]+)([a-zA-Z0-9_]+){7,}$/g),
+			"Check Field Contains Invalid Characters Or Should Be > 7 Characters and ",
+			"passWordError"))) {
 		return !validationPass;
-	}else if (!(isFieldFilled(isempty($('#passWord').val()),"Password Field","passWordError"))) {
+	} else if (!(isFieldFilled(isempty($('#passWord').val()), "Password Field",
+			"passWordError"))) {
 		return !validationPass;
-	} else if (!(isFieldFilled(isempty($('#confrmpsw').val()),"Confirm Password Field","confPassWordError"))){
+	} else if (!(isFieldFilled(isempty($('#confrmpsw').val()),
+			"Confirm Password Field", "confPassWordError"))) {
 		return !validationPass;
-	} else if(!(isFieldFilled(passwordAndConfirmPassword($('#passWord').val(),$('#confrmpsw').val()),"PassWords Does Not Match ,The Field(s)","confPassWordError"))){
+	} else if (!(isFieldFilled(passwordAndConfirmPassword($('#passWord').val(),
+			$('#confrmpsw').val()), "PassWords Does Not Match ,The Field(s)",
+			"confPassWordError"))) {
 		return !validationPass;
 	}
-		return validationPass;
-	
+	return validationPass;
+
 }
 
 /**
  * convertPassWordToString() method displays the pass word to text
- * @author dushantha DN 
- * @param checkboxId check box id which used to toggle the command
- * @param passWordElementId password field element id 
- * @param confirmWordElementId confirming field element id
+ * 
+ * @author dushantha DN
+ * @param checkboxId
+ *            check box id which used to toggle the command
+ * @param passWordElementId
+ *            password field element id
+ * @param confirmWordElementId
+ *            confirming field element id
  * @returns void
  */
 
-function convertPassWordToString(checkboxId,passWordElementId,confirmWordElementId){
-	var value =($('#'+checkboxId).is(':checked'))?"text":"password";
-	$('#'+passWordElementId).attr("type",value);
-	$('#'+confirmWordElementId).attr("type",value);
+function convertPassWordToString(checkboxId, passWordElementId,
+		confirmWordElementId) {
+	var value = ($('#' + checkboxId).is(':checked')) ? "text" : "password";
+	$('#' + passWordElementId).attr("type", value);
+	$('#' + confirmWordElementId).attr("type", value);
 }
 
 /**
- * clearAllFields willclear all the data input text fields
- * once it'scalled
+ * clearAllFields willclear all the data input text fields once it'scalled
  */
-function clearAllFields(){
-	
+function clearAllFields() {
+
 	$('#showpasscheckbox').prop('checked', false);
-	var value = $('#showpasscheckbox').is(':checked')?"text":"password";
-	$('#passWord').attr("type",value);
-	$('#confrmpsw').attr("type",value);
+	var value = $('#showpasscheckbox').is(':checked') ? "text" : "password";
+	$('#passWord').attr("type", value);
+	$('#confrmpsw').attr("type", value);
 }
