a0e6368189b66524f74b45fb6158f965665a2e34
Dumani Jayasinghe <dumani@genesiis.com>
2017-02-17 14:25:53 +0530

    c138-add-basic-programme-MP-dj Implemented isPatternMatch(), isFromDateAfterTodate(), isValidEmailFormat(),isThisDateValid()

--- campus.jar/src/com/genesiis/campus/validation/UtilityHelper.java
+++ campus.jar/src/com/genesiis/campus/validation/UtilityHelper.java
@@ -2,12 +2,24 @@ package com.genesiis.campus.validation;
 
 //20161030 DJ c6-list-available-institutes-on-the-view INIT UtilityHelper class. implemented isNotEmpty() method
 //20161030 DJ c6-list-available-institutes-on-the-view INIT UtilityHelper class. implemented isInteger() method
+//20170217 DJ c138-add-basic-programme-MP-dj Implemented isPatternMatch(), isFromDateAfterTodate(), isValidEmailFormat(),isThisDateValid().
+
+import org.apache.log4j.Logger;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 
 /**
  * @author DJ Utility Helper class to  server side validate. Class name can be change when merging to master branch.
  * @since 20161030
  */
 public class UtilityHelper {
+	
+	static Logger log = Logger.getLogger(FormValidator.class.getName());	
+	static String dateFormat="yyyy-MM-dd";
 	
 	/**
 	 * @author DJ
@@ -46,6 +58,51 @@ public class UtilityHelper {
 		} catch (NumberFormatException e) {
 			return false;
 		} 
+		return true;
+	}
+	
+	public static boolean isFromDateAfterTodate(String commencementDate,
+			String expirationDate) throws ParseException {
+		final SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+		sdf.setLenient(false);
+		try {
+			// if not valid, it will throw ParseException
+			final Date commencement = sdf.parse(commencementDate);
+			final Date expiration = sdf.parse(expirationDate);
+
+			if (commencement.after(expiration)) {
+				 return true;
+			}
+
+		} catch (ParseException e) {
+			log.error("validateProgrammeDetails() : ParseException " + e.toString());	
+			throw e;
+		}
+		return false;
+	}
+
+	public static boolean isPatternMatch(String sourceString,String regularExpression) {
+		return (sourceString.matches(regularExpression)?true:false);
+	}
+
+	
+	public static boolean isValidEmailFormat(String email) {				
+		String reg = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";		
+		return isPatternMatch(email,reg);	    
+	}
+	
+	public static boolean isThisDateValid(String dateToValidate) {		
+		final SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+		sdf.setLenient(false);
+		
+		try {
+			// if not valid, it will throw ParseException
+			final Date date = sdf.parse(dateToValidate);			
+
+		} catch (ParseException e) {
+			log.error("isThisDateValid() : ParseException " + e.toString());
+			return false;
+		}
 		return true;
 	}
 
