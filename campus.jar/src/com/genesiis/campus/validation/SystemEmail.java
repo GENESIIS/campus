5997693d6578ffc36b14ada0f24fd0db1f65fada
chinthakacw <chinthaka@genesiis.com>
2017-02-20 00:09:15 +0530

    c103-send-email-tutor-status-change-cw - add attributes to the Enum SystemEmail.java

--- campus.jar/src/com/genesiis/campus/validation/SystemEmail.java
+++ campus.jar/src/com/genesiis/campus/validation/SystemEmail.java
@@ -5,6 +5,7 @@ package com.genesiis.campus.validation;
 //20170127 CW c126-formatting-un-formatted-email-tutor-signup-cw - add attributes to the Enum
 //20170128 cw c126-formatting-un-formatted-email-tutor-signup-cw - modified attribute name
 //20170128 cw c126-formatting-un-formatted-email-tutor-signup-cw - rename the enum name from SystemMail to SystemEmail.
+//20170219 CW c103-send-email-tutor-status-change-cw - add attributes to the Enum
 
 /**
  * SystemMail enum handles the content of the email to be sent
@@ -18,7 +19,9 @@ public enum SystemEmail {
 	SEND_EMAIL_TUTOR_SIGNUP_BODY3("Registration Confirmation", "Dear Sir/Madam", "If you didn't sign up for this account, or you are having trouble with your account, please contact us at support@campus.lk and we will be happy to help you."),
 	SEND_EMAIL_TUTOR_SIGNUP_COMPLEMENTARY_CLOSE1("", "", "Campus.lk Support Team"),
 	SEND_EMAIL_TUTOR_SIGNUP_COMPLEMENTARY_CLOSE2("", "", "http://www.campus.lk"),
-	SEND_EMAIL_TUTOR_SIGNUP_COMPLEMENTARY_CLOSE3("", "", "Your user ID: ");
+	SEND_EMAIL_TUTOR_SIGNUP_COMPLEMENTARY_CLOSE3("", "", "Your user ID: "),
+	SEND_EMAIL_ADMIN_TUTOR_UPDATE_BODY1("Update Confirmation", "Dear Sir/Madam","Your registration details are verified by the campus.lk site admin & you are now"),
+	SEND_EMAIL_ADMIN_TUTOR_UPDATE_BODY2("Update Confirmation", "Dear Sir/Madam","Tutor of campus.lk website.");
 	
 	private final String subject;
 	private final String title;
