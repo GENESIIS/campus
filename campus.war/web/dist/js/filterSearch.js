7d036075252d521603a325c1e1fbfead31e6acfc
Dumani Jayasinghe <dumani@genesiis.com>
2017-02-20 06:54:30 +0530

    c123-general-filter-search-course-provider-MP-dj  Merge latest UI same as cam-124

--- campus.war/web/dist/js/filterSearch.js
+++ campus.war/web/dist/js/filterSearch.js
@@ -1,8 +1,12 @@
-/*
-	Created by Dimuthu Kalyanaratne on 02/02/2017.
-*/
 
+//Created by Dimuthu Kalyanaratne on 02/02/2017.
+//20170220 DJ c123-general-filter-search-course-provider-MP-dj  Merge latest UI same as cam-124
 	$( document ).ready(function() {
+	
+		/*
+		 	* Course search panel related JS
+		 	* DK Dimuthu Kalyanaratne
+		*/
 		
 		$(".filter-boxes").css({"display": "none", "opacity": "0"}); 
 		
@@ -32,7 +36,13 @@
 			$(this).show().css('opacity', 1).slideUp(slideOpts).animate({ opacity: 0 }, fadeOpts);
 		};
 		
-		$("#course-search, .panel-collapse-ico").click(function(event){
+		
+		/*
+		 	* Course filter search panel open for the focus event of the course search input
+		 	* DK Dimuthu Kalyanaratne
+		*/		
+		
+		$("#course-search").focus(function(event){
 			if( $(".filter-boxes").is(':visible')){
 
 					$(".filter-boxes").hideUp("slow");
@@ -45,8 +55,28 @@
 			return false;
 		});
 		
-		$(document).click(function(event) { 
-			if(!$(event.target).closest(".search-area").length) {
+		
+		/*
+		 	* Course filter search panel open for the click event of drop down arrow
+		 	* DK Dimuthu Kalyanaratne
+		*/
+		
+		$(".panel-collapse-ico").click(function(event){
+			if( $(".filter-boxes").is(':visible')){
+
+					$(".filter-boxes").hideUp("slow");
+					$(".rotate").toggleClass("down")  ; 
+	
+			}else{
+				$(".filter-boxes").showDown("slow");
+				$(".rotate").toggleClass("down")  ; 
+			}		
+			return false;
+		});
+		
+		$('.main-screen').click(function(event) { 
+			if(!$(event.target).closest(".bar-holder").length) {
+				$("#course-search").val('Search : Program, Course, or Career');  
 				if($(".filter-boxes").is(":visible")) {
 					$(".filter-boxes").hideUp("slow");
 					$(".rotate").toggleClass("down")  
@@ -54,24 +84,96 @@
 			}        
 		});
 		
+		
+		/*
+		 	* General Filter search button click action.
+		 	* DJ dumani
+		*/
 		$('#addSearchData').click(function(e){
 			
-			var keyWordString = " ";
-			var selectedType = " ";
-			keyWordString = $("#course-search").val();
-			selectedType = $('input[name="courseOpt"]:checked').val();
-
-			if (selectedType == 'CPROVIDER') {
-				window.location
-						.assign("/dist/partials/viewMoreCourseProviders.jsp?keyWord="
-								+ keyWordString + "&selectedType=" + selectedType);
-			} else {
-				alert("Will load programes \n Will be implemented in cam-124");
-			}
+			var courseSearchTxt = $("#course-search").val();
 			
-			e.preventDefault();	
+			//if( courseSearchTxt != "Search : Program, Course, or Career"){
+			if( $("#searchFcuntion").val() != "filterSearch"){
+							
+				var keyWordString = " ";
+				var selectedType = " ";
+				
+				keyWordString = $("#course-search").val();
+				selectedType = $('input[name="courseOpt"]:checked').val();
+	
+				if (selectedType == 'CPROVIDER') {
+					alert("Will load course providers \n implemented in cam-123");				
+				} else {
+					window.location.assign("/dist/partials/courses.jsp?keyWord=" + keyWordString);
+				}
+
+				
+			}else{
+					
+				e.preventDefault();
+				return false;
+				
+			}
 
 		});
 		
-	});
+		
+		/*
+		 	* Submit form data pn "Enter"
+		 	* DK Dimuthu Kalyanaratne
+		*/
 
+		$("input#course-search").on('keyup', function (e) {
+			
+			if(e.which == 13) {
+				
+				if(!this.value == "" || !this.value == "Search : Program, Course, or Career"){
+					
+					var keyWordString = " ";
+					var selectedType = " ";
+					
+					keyWordString = $("#course-search").val();
+					selectedType = $('input[name="courseOpt"]:checked').val();
+		
+					if (selectedType == 'CPROVIDER') {
+						alert("Will load course providers \n implemented in cam-123");				
+					} else {
+						window.location.assign("/dist/partials/courses.jsp?keyWord=" + keyWordString);
+					}
+				
+				}else{
+					
+					e.preventDefault();
+				    return false;
+					
+				}	
+				
+			}
+							
+		});
+
+
+		
+
+		/*
+		 	* Auto clear for course search field
+		 	* DK Dimuthu Kalyanaratne
+		*/
+		
+		$("input#course-search").focus(function() {		  
+			 if(this.value == "Search : Program, Course, or Career") {
+				 $(this).val("");
+			 }
+		}).blur(function() {
+			if (this.value == "") {
+				$(this).val("Search : Program, Course, or Career");
+			}
+		});
+		
+		
+		//Set default check as programme title 
+		$("input[name=courseOpt][value='PROGRAMME']").prop("checked",true);
+
+		
+	});
