cb97733bcb30414a8c398c48c7a4b9c81ebbe045
dushanthacb <dushantha@genesiis.com>
2017-02-21 12:54:33 +0530

    c131-admin-manage-banner-upload-banner-image-dn created getAll() by moving the method from FeaturedCourseProviderDAO class

--- campus.jar/src/com/genesiis/campus/entity/AdvertiserDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/AdvertiserDAO.java
@@ -2,14 +2,24 @@ package com.genesiis.campus.entity;
 
 /*
  * 20170203 DN c131-admin-manage-banner-upload-banner-image-dn created the initial class stub
- * 
+ * 20170221 DN c131-admin-manage-banner-upload-banner-image-dn created getAll()
  */
 
+import com.genesiis.campus.util.ConnectionManager;
+import com.genesiis.campus.util.DaoHelper;
+
+import org.apache.log4j.Logger;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class AdvertiserDAO implements ICrud {
+	static Logger log = Logger.getLogger(AdvertiserDAO.class
+			.getName());
 
 	@Override
 	public int add(Object object) throws SQLException, Exception {
@@ -36,11 +46,46 @@ public class AdvertiserDAO implements ICrud {
 		return null;
 	}
 
+	/**
+	 * getAll() method bears the responsibility of extracting all the course providers
+	 * who are featured and status been 1
+	 * 
+	 */
 	@Override
 	public Collection<Collection<String>> getAll() throws SQLException,
 			Exception {
-		// TODO Auto-generated method stub
-		return null;
+		// Extract all the course providers who are featured providers and status is one
+				StringBuilder CourseProvidersSQL =new StringBuilder("SELECT [CODE] ,[NAME]  FROM [CAMPUS].[ADVERTISER]");
+				CourseProvidersSQL.append("WHERE [ISACTIVE] = 1");		
+				String allCourseProviderSQL = CourseProvidersSQL.toString();
+				Connection courseProviderConnection =null;
+				PreparedStatement preparedStatement = null;
+				ResultSet resultSet = null;
+				
+				try{
+					courseProviderConnection =  ConnectionManager.getConnection();
+					preparedStatement =courseProviderConnection.prepareStatement(allCourseProviderSQL);
+					resultSet = preparedStatement.executeQuery();
+					final Collection<Collection<String>> outerWrapper = new ArrayList<Collection<String>>();
+					
+					while(resultSet.next()){
+						final ArrayList<String>courseProviderRecord = new ArrayList<String>();
+						courseProviderRecord.add(resultSet.getString("CODE"));
+						courseProviderRecord.add(resultSet.getString("NAME"));
+						outerWrapper.add(courseProviderRecord);
+					}
+					
+					return outerWrapper;
+					
+				}catch (SQLException sqle){
+					log.error("getAll() :SQLException"+sqle.toString());
+					throw sqle;
+				} catch (Exception exp) {
+					log.error("getAll() :Exception"+exp.toString());
+					throw exp;
+				} finally{
+					DaoHelper.cleanup(courseProviderConnection, preparedStatement, resultSet);
+				}
 	}
 
 	@Override
