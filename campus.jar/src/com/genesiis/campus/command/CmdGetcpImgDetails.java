4a992657de18fe288e1b64b4aa0e03ee6da132b3
pabodhaW <pabodha@genesiis.com>
2017-02-21 13:49:35 +0530

    CAM-48: implementing execute() method to get cp images related config data.

---
+++ campus.jar/src/com/genesiis/campus/command/CmdGetcpImgDetails.java
@@ -0,0 +1,43 @@
+/**
+ * 20170221 PN CAM-48: INIT CmdGetcpImgDetails.java class and implementing execute() method to get cp images related config data.
+ */
+package com.genesiis.campus.command;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.log4j.Logger;
+
+import com.genesiis.campus.entity.ICrud;
+import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.SystemConfigDAO;
+import com.genesiis.campus.util.IDataHelper;
+
+/**
+ * @author Pabodha Narmadani
+ *
+ */
+public class CmdGetcpImgDetails implements ICommand {
+	static Logger log = Logger.getLogger(CmdGetcpImgDetails.class.getName());
+	int courseProviderCode = 1; // This needs to be assign from the request
+								// later.
+
+	@Override
+	public IView execute(IDataHelper helper, IView view) throws SQLException, Exception {
+		ICrud sysConf = new SystemConfigDAO();
+		Collection<Collection<String>> sysConfCollection = new ArrayList<Collection<String>>();
+		try {
+			sysConfCollection = sysConf.getAll();
+			view.setCollection(sysConfCollection);
+		} catch (SQLException sqle) {
+			log.info("execute() : sqle" + sqle.toString());
+			throw sqle;
+		} catch (Exception e) {
+			log.info("execute() : e" + e.toString());
+			throw e;
+		}
+		return view;
+	}
+
+}
