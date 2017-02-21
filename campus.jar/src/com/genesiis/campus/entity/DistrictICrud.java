5e2fd4a0380da9b623487490b81ba05246be6d1e
Dumani Jayasinghe <dumani@genesiis.com>
2017-02-21 14:26:15 +0530

    c145-add-enhanced-programme created DistrictICrud.java

---
+++ campus.jar/src/com/genesiis/campus/entity/DistrictICrud.java
@@ -0,0 +1,17 @@
+package com.genesiis.campus.entity;
+
+//20170221 c145-add-enhanced-programme created DistrictICrud.java
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+/**The class  {@code DistrictICrud} is a form of Interface class.
+ * The Interface {@code DistrictICrud} has precise control over district, town dao level manipulations.
+ * 
+ *  Interface declare methods related to District table and tables around it, join directly and meaningfully.
+ *  For an example; methods related Town, DSD etc. 
+ *  @author dumani DJ   
+ */
+public interface DistrictICrud extends ICrud {
+	//public Collection<Collection<String>> getAllDistricts() throws SQLException,Exception;
+}
