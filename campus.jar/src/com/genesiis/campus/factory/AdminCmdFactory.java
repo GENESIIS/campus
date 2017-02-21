def81fd6e11d20ba308b7cc8f0fda9d4ae566fa5
pabodhaW <pabodha@genesiis.com>
2017-02-21 15:59:27 +0530

    CAM-48: added a attribute into static block HashMap to map CmdcpImgUpload command class.

--- campus.jar/src/com/genesiis/campus/factory/AdminCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/AdminCmdFactory.java
@@ -1,16 +1,17 @@
 package com.genesiis.campus.factory;
 
-import com.genesiis.campus.command.CmdGetcpImgDetails;
-
 //20170221 PN CAM-48: INIT AdminCmdFactory.java. This will redirect servlet request into course provider image upload usecase' command classes.
 
 import com.genesiis.campus.command.ICommand;
 import com.genesiis.campus.validation.Operation;
+import com.genesiis.campus.command.CmdGetcpImgDetails;
+import com.genesiis.campus.command.CmdcpImgUpload;
 
 public class AdminCmdFactory implements ICmdFactory {
 	private ICommand command = null;
 	static {
 		map.put(Operation.GET_CP_IMAGE_DETAILS, new CmdGetcpImgDetails());
+		map.put(Operation.UPLOAD_CP_IMAGE, new CmdcpImgUpload());
 	}
 
 	@Override
