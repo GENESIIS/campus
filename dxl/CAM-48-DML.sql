ce5a629b13a0f44f5b2e4710eda3ec77a6311f4b
pabodhaW <pabodha@genesiis.com>
2017-02-21 10:03:22 +0530

    CAM-48: SQL queries modified by changing insertion values.

--- dxl/CAM-48-DML.sql
+++ dxl/CAM-48-DML.sql
@@ -7,10 +7,10 @@ SELECT * FROM CAMPUS.SYSTEMCONFIG;
 --Insertion queries--
 INSERT INTO CAMPUS.SYSTEMCONFIG
 (SYSTEMCONFIGCODE, DESCRIPTION, VALUE1, VALUE2, VALUE3, SORTKEY, CRTON, CRTBY, MODON, MODBY)
-VALUES(('CP_SMALL_LOGO'), 'The logo to be display on institute serch page. 84 x 39 Px', 'C:\sdb\ctxdeploy\education.war\provider\logo', '3276', '0.13', ((1)), (getdate()), ('CAM-48'), (getdate()), (''));
+VALUES(('CP_SMALL_LOGO'), 'The logo to be display on institute serch page. 84 x 39 Px', 'C:\sdb\ctxdeploy\education.war\provider\logo', '84', '39', ((1)), (getdate()), ('CAM-48'), (getdate()), (''));
 INSERT INTO CAMPUS.SYSTEMCONFIG
 (SYSTEMCONFIGCODE, DESCRIPTION, VALUE1, VALUE2, VALUE3, SORTKEY, CRTON, CRTBY, MODON, MODBY)
-VALUES(('CP_LARGE_LOGO'), 'The logo to be display on corse details preview. Size 134 x 75 Px.', 'C:\sdb\ctxdeploy\education.war\provider\logo', '10050', '0.04', ((1)), (getdate()), ('CAM-48'), (getdate()), (''));
+VALUES(('CP_LARGE_LOGO'), 'The logo to be display on corse details preview. Size 134 x 75 Px.', 'C:\sdb\ctxdeploy\education.war\provider\logo', '134', '75', ((1)), (getdate()), ('CAM-48'), (getdate()), (''));
 INSERT INTO CAMPUS.SYSTEMCONFIG
 (SYSTEMCONFIGCODE, DESCRIPTION, VALUE1, VALUE2, VALUE3, SORTKEY, CRTON, CRTBY, MODON, MODBY)
-VALUES(('CP_HEADER_IMAGE'), 'The image is to display on miniweb heager.', 'C:\sdb\ctxdeploy\education.war\provider\logo', '', '1', ((1)), (getdate()), ('CAM-48'), (getdate()), (''));
+VALUES(('CP_HEADER_IMAGE'), 'The image is to display on miniweb heager.', 'C:\sdb\ctxdeploy\education.war\provider\logo', '', '', ((1)), (getdate()), ('CAM-48'), (getdate()), (''));
