27d92268b2215dec3a238b793b3f99d5c367e1b4
Dumani Jayasinghe <dumani@genesiis.com>
2017-02-21 14:26:47 +0530

    c145-add-enhanced-programme created DistrictDAOImpl.java

---
+++ campus.jar/src/com/genesiis/campus/entity/dao/DistrictDAOImpl.java
@@ -0,0 +1,77 @@
+package com.genesiis.campus.entity.dao;
+
+import com.genesiis.campus.entity.DistrictICrud;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Collection;
+
+//20170221 DJ c145-add-enhanced-programme created DistrictDAOImpl.java
+
+/**The class  {@code DistrictDAOImpl} is a form of DAO class.
+ * The Interface {@code DistrictDAOImpl} has precise control over district and town dao level manipulations. 
+ *  @author dumani DJ   
+ */
+public class DistrictDAOImpl  implements DistrictICrud{
+
+	@Override
+	public int add(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<Collection<String>> findById(Object code)
+			throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Collection<String>> getAll() throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int add(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object object, Connection conn) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<Collection<String>> findById(Object object,
+			Connection conn) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}	
+
+}
