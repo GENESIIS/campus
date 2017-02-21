39138014c33a917f8f767fad6e51e20cca014234
Dumani Jayasinghe <dumani@genesiis.com>
2017-02-21 21:11:19 +0530

    c145-add-enhanced-programme-MP-dj Implementing getCourseProviderTown(int providerCode)

--- campus.jar/src/com/genesiis/campus/entity/dao/DistrictDAOImpl.java
+++ campus.jar/src/com/genesiis/campus/entity/dao/DistrictDAOImpl.java
@@ -3,9 +3,13 @@ package com.genesiis.campus.entity.dao;
 //20170221 DJ c145-add-enhanced-programme created DistrictDAOImpl.java
 
 import com.genesiis.campus.entity.DistrictICrud;
+import com.genesiis.campus.util.ConnectionManager;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 
 
@@ -79,7 +83,21 @@ public class DistrictDAOImpl  implements DistrictICrud{
 	@Override
 	public Collection<Collection<String>> getCourseProviderTown(int providerCode)
 			throws SQLException, Exception {
-		// TODO Auto-generated method stub
+		Connection conn = null;
+		PreparedStatement  stmt = null;
+		ResultSet resultSet =null;		
+		
+		try {				
+			conn=ConnectionManager.getConnection();
+			String sb = "SELECT CODE, TOWN  FROM CAMPUS.COURSEPROVIDERTOWN TOWN WHERE TOWN.COURSEPROVIDER = ";
+			stmt = conn.prepareStatement(sb.toString());			
+			
+			log.debug("SQL : " + sb.toString());
+			resultSet= stmt.executeQuery();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		return null;
 	}
 
