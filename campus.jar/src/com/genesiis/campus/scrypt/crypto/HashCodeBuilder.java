a6a129447372e2cf4d79b6c38f54e88fd35c179d
Anuradha Rushmitha <anuradha@genesiis.com>
2017-02-22 15:11:41 +0530

    CAM-22 code review issues fixed, header comments updated and removed unwanted code comments

--- campus.jar/src/com/genesiis/campus/scrypt/crypto/HashCodeBuilder.java
+++ campus.jar/src/com/genesiis/campus/scrypt/crypto/HashCodeBuilder.java
@@ -1,36 +1,40 @@
 //20170206 AS CAM-22 HashCodeBuilder created for testing purpose. 
 package com.genesiis.campus.scrypt.crypto;
 
-/**
- *
- * @author anuradha
- */
- 
-public class HashCodeBuilder {
-	 
-	public String createHash(String firstname, String lastname){
-		 String originalHash = firstname+lastname;
-		 String generatedSecuredHash = SCryptUtil.scrypt(originalHash, 16, 16, 16);
-		 System.out.println(generatedSecuredHash);
-	
-		// boolean matched = SCryptUtil.check("Anuradhasilva", "$s0$41010$ffwJmgi4Kcxgz3LqB1CRIA==$KyiEoXhpNKozpTCD/2Wn31EiHaUgL8qzMCxIjFfNl3U=");
-	    // System.out.println(matched);
-		 return generatedSecuredHash;
-	}
-	
-	
-	
-		 public void checkHash(){
-	        String originalPassword = "password";
-	        String generatedSecuredPasswordHash = SCryptUtil.scrypt(originalPassword, 16, 16, 16);
-	        System.out.println(generatedSecuredPasswordHash);
-	         
-	        boolean matched = SCryptUtil.check("password", generatedSecuredPasswordHash);
-	        System.out.println(matched);
-	         
-	        matched = SCryptUtil.check("passwordno", generatedSecuredPasswordHash);
-	        System.out.println(matched);
-	    }
+import org.apache.log4j.Logger;
+import com.genesiis.campus.util.ResetPasswordInstructionEmailDispenser;
 
-	
+public class HashCodeBuilder {
+	static Logger log = Logger.getLogger(HashCodeBuilder.class.getName());
+
+	/**
+	 * Pass the users first name and last name and encode to one string.
+	 * 
+	 * @param string
+	 * @param string
+	 * @return string (generated hash code )
+	 */
+
+	public String createHash(String firstname, String lastname) {
+		String originalHash = firstname + lastname;
+		String generatedSecuredHash = SCryptUtil.scrypt(originalHash, 16, 16,
+				16);
+		log.info(generatedSecuredHash);
+		return generatedSecuredHash;
+	}
+
+	public void checkHash() {
+		String originalPassword = "password";
+		String generatedSecuredPasswordHash = SCryptUtil.scrypt(
+				originalPassword, 16, 16, 16);
+		System.out.println(generatedSecuredPasswordHash);
+
+		boolean matched = SCryptUtil.check("password",
+				generatedSecuredPasswordHash);
+		System.out.println(matched);
+
+		matched = SCryptUtil.check("passwordno", generatedSecuredPasswordHash);
+		System.out.println(matched);
+	}
+
 }
