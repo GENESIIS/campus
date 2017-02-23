d5a3d5eea2d8b1797e8c2175fe4845af50f00fca
chinthakacw <chinthaka@genesiis.com>
2017-02-23 16:56:56 +0530

    c36-add-tutor-information re-organise the import statements in CmdCheckEmail.java - cw

--- campus.jar/src/com/genesiis/campus/command/CmdCheckEmail.java
+++ campus.jar/src/com/genesiis/campus/command/CmdCheckEmail.java
@@ -5,17 +5,19 @@ package com.genesiis.campus.command;
 //20170126 CW c36-add-tutor-information modified the execute ()method return value.
 //20170130 CW c36-add-tutor-information re-organise the import statements.
 //20170215 CW c38-view-update-tutor-profile Add class comment 
-
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collection;
+//20170223 CW c36-add-tutor-information re-organise the import statements. 
 
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.entity.TutorEmailDAO;
 import com.genesiis.campus.entity.model.Tutor;
 import com.genesiis.campus.util.IDataHelper;
 import com.genesiis.campus.validation.Validator;
+
 import org.apache.log4j.Logger;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * this class used to compare user given email address with the 
