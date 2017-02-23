e520ba52869c83bfafba6b3cbd26bd0d7e3ba6a7
chinthakacw <chinthaka@genesiis.com>
2017-02-23 23:14:43 +0530

    c38-view-update-tutor-profile-cw execute() method to validate for null tutor code in CmdViewTutorProfile.java- cw

--- campus.jar/src/com/genesiis/campus/command/CmdViewTutorProfile.java
+++ campus.jar/src/com/genesiis/campus/command/CmdViewTutorProfile.java
@@ -2,12 +2,14 @@ package com.genesiis.campus.command;
 
 //20161220 CW c38-view-update-tutor-profile-cw INIT CmdViewTutorProfile.java
 //20170207 CW c38-view-update-tutor-profile-cw execute() method
-//20170223 CW c36-add-tutor-information re-organise the import statements.
+//20170223 CW c38-add-tutor-information re-organise the import statements.
+//20170223 CW c38-view-update-tutor-profile-cw execute() method to validate for null tutor code
 
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.entity.TutorDAO;
 import com.genesiis.campus.entity.model.Tutor;
 import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.validation.SystemMessage;
 import com.genesiis.campus.validation.Validator;
 
 import org.apache.log4j.Logger;
@@ -34,11 +36,14 @@ public class CmdViewTutorProfile implements ICommand {
 			final TutorDAO tutorDAO = new TutorDAO();
 			final Tutor tutor = new Tutor();
 			Collection<Collection<String>> tutorViewCollection = new ArrayList<Collection<String>>();
+			
 			tutorCode = helper.getParameter("tutorCode");
-			tutor.setCode(Integer.parseInt(tutorCode));
 			
 			if (Validator.isNotEmpty(tutorCode)){
+				tutor.setCode(Integer.parseInt(tutorCode));
 				tutorViewCollection = tutorDAO.findById(tutor);
+			}else{
+				message = SystemMessage.INCORRECTTUTOR.message();
 			}
 			
 			view.setCollection(tutorViewCollection);			
