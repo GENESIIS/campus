97d0c76578d4ba62d469df68b1fdf42c05379245
Anuradha Rushmitha <anuradha@genesiis.com>
2017-02-24 13:59:16 +0530

    CAM-22 code review issues fixed, unwanted comments removed and changed the imports oder

--- campus.jar/src/com/genesiis/campus/command/CmdStudentLogout.java
+++ campus.jar/src/com/genesiis/campus/command/CmdStudentLogout.java
@@ -2,13 +2,15 @@ package com.genesiis.campus.command;
 
 //20170118 AS CAM-21 CmdStudentLogout command class created. 
 //20170130 AS CAM-21 code review modification done. 
+//20170224 AS C22 Changed imports oder. 
+
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.entity.StudentLoginDAO;
 import com.genesiis.campus.entity.model.Student;
 import com.genesiis.campus.util.IDataHelper;
 import com.genesiis.campus.validation.SystemMessage;
-import com.google.gson.Gson;
 
+import com.google.gson.Gson;
 import org.apache.log4j.Logger;
 
 import java.sql.SQLException;
@@ -48,25 +50,26 @@ public class CmdStudentLogout implements ICommand {
 
 			if (status > 0) {
 				Cookie[] cookies = helper.getRequest().getCookies();
-		    	if(cookies != null){
-		    	for(Cookie cookie : cookies){
-		    		if(cookie.getName().equals("JSESSIONID")){
-		    			System.out.println("JSESSIONID="+cookie.getValue());
-		    			break;
-		    		}
-		    	}
-		    	}
-				
-				
-				HttpSession curentSession = helper.getRequest().getSession(false);
-				
-				if(curentSession != null){
+				if (cookies != null) {
+					for (Cookie cookie : cookies) {
+						if (cookie.getName().equals("JSESSIONID")) {
+							System.out.println("JSESSIONID="
+									+ cookie.getValue());
+							break;
+						}
+					}
+				}
+
+				HttpSession curentSession = helper.getRequest().getSession(
+						false);
+
+				if (curentSession != null) {
 					curentSession.removeAttribute("user");
 					curentSession.removeAttribute("userCode");
 					curentSession.removeAttribute("currentUserData");
 					curentSession.invalidate();
 
-				message = SystemMessage.LOGOUTSUCCESSFULL.message();
+					message = SystemMessage.LOGOUTSUCCESSFULL.message();
 				}
 			} else {
 				message = SystemMessage.LOGOUTUNSUCCESSFULL.message();
