7d592eb35d1ca6b36297de1dd9f5ceac56c82810
Anuradha Rushmitha <anuradha@genesiis.com>
2017-02-24 14:15:04 +0530

    CAM-22 Header comments updated

--- campus.jar/src/com/genesiis/campus/scrypt/jni/SysLibraryLoader.java
+++ campus.jar/src/com/genesiis/campus/scrypt/jni/SysLibraryLoader.java
@@ -1,4 +1,4 @@
-//20170206 AS CAM-22 implemented Scrypt encode decode plugging classes. 
+//20170206 AS CAM-22 this class implemented from Scrypt encode decode plugging classes
 
 package com.genesiis.campus.scrypt.jni;
 
@@ -6,7 +6,7 @@ package com.genesiis.campus.scrypt.jni;
  * A native library loader that simply invokes {@link System#loadLibrary}. The shared
  * library path and filename are platform specific.
  *
- * @author Will Glozer
+ * 
  */
 public class SysLibraryLoader implements LibraryLoader {
     /**
