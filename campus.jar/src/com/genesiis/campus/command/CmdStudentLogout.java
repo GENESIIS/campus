f0027fdfc4291d4599f6cb8ea6f87f2ffc370199
Anuradha Silva <anuradha@genesiis.com>
2017-02-28 18:49:59 +0530

    CAM-21 command class midification completed.

--- campus.jar/src/com/genesiis/campus/command/CmdStudentLogout.java
+++ campus.jar/src/com/genesiis/campus/command/CmdStudentLogout.java
@@ -2,6 +2,7 @@ package com.genesiis.campus.command;
 
 //20170118 AS CAM-21 CmdStudentLogout command class created. 
 //20170130 AS CAM-21 code review modification done. 
+//20170228 AS C22-  checked current Session User and is there already account logout shows the message and  redirect to index page.
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.entity.StudentLoginDAO;
 import com.genesiis.campus.entity.model.Student;
@@ -26,20 +27,22 @@ public class CmdStudentLogout implements ICommand {
 
 	private Student loggedStudent;
 	String pageURL = "/index.jsp";
-	
+
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
-		HttpSession  curentSession = helper.getRequest().getSession(false);
 		String message = SystemMessage.LOGOUTUNSUCCESSFULL.message();
-		
 		try {
-			if (curentSession == null) {
-				log.info("allready logout");
-				pageURL = "http://www.campus.dev:8080/dist/partials/error/error-content.jsp";
-			} else {
+			HttpSession curentSession = helper.getRequest().getSession(false);
+			
+			String currentSessionUser = (String) curentSession
+					.getAttribute("currentSessionUser");
+			if (currentSessionUser != null) {
+				curentSession.removeAttribute("user");
+				curentSession.removeAttribute("userCode");
+				curentSession.removeAttribute("currentUserData");
+				curentSession.invalidate();
 
-				log.info("not logout yet");
 				String gsonData = helper.getParameter("jsonData");
 				loggedStudent = getStudentdetails(gsonData);
 
@@ -66,23 +69,16 @@ public class CmdStudentLogout implements ICommand {
 						}
 					}
 
-					
-
-					if (curentSession != null) {
-						curentSession.removeAttribute("user");
-						curentSession.removeAttribute("userCode");
-						curentSession.removeAttribute("currentUserData");
-						curentSession.invalidate();
-						
-
-						message = SystemMessage.LOGOUTSUCCESSFULL.message();
-					}
+					message = SystemMessage.LOGOUTSUCCESSFULL.message();
+					log.info(" logout : " + message);
 				} else {
 					message = SystemMessage.LOGOUTUNSUCCESSFULL.message();
 				}
-			}
 
-			
+			} else {
+				pageURL = "http://www.campus.dev:8080/dist/partials/error/error-content.jsp";
+				log.info("allready logout");
+			}
 
 		} catch (Exception e) {
 			log.error("CmdStudentLogout():  Exception" + e.toString());
