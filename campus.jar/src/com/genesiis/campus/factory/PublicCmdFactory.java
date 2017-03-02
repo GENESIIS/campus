beb7110220e0cd9fece36725114d2f3ec8b4cb2f
jayani1991 <jayani@genesiis.com>
2017-03-02 17:49:35 +0530

    c96-public-list-all-tutors added case PUBLIC_LIST_ALL_TUTORS to PublicCmdFactory class

--- campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
@@ -1,6 +1,7 @@
 package com.genesiis.campus.factory;
 
 //20161025 PN c11-criteria-based-filter-search INIT PublicCmdFactory.java
+//20170302 JH c96-public-list-all-tutors added case PUBLIC_LIST_ALL_TUTORS
 
 import com.genesiis.campus.command.ICommand;
 import com.genesiis.campus.validation.Operation;
@@ -18,6 +19,9 @@ public class PublicCmdFactory implements ICmdFactory {
 		o = Operation.getOperation(cco);
 		switch (o) {
 		
+		case PUBLIC_LIST_ALL_TUTORS:
+			command = map.get(o);
+			break;
 		default:
 			break;
 		}
