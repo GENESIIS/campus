13aa8637449ae3403829d5414a589d915c1b4b0d
dushanthacb <dushantha@genesiis.com>
2017-03-03 09:14:40 +0530

    c131-admin-manage-banner-upload-banner-image-dn declare compareDates(Date date,Date otherDate) throws Exception method signature.

--- campus.jar/src/com/genesiis/campus/validation/Validatory.java
+++ campus.jar/src/com/genesiis/campus/validation/Validatory.java
@@ -1,19 +1,27 @@
 package com.genesiis.campus.validation;
 
 //20161111 DN c10-contacting-us-MP created the Validatory.java interface
+//20170303 DN c131-admin-manage-banner-upload-banner-image-dn typo in doc comment
+//         has been corrected
+//20170303 DN c131-admin-manage-banner-upload-banner-image-dn declare 
+//         compareDates(Date date,Date otherDate) throws Exception method signature.
 
-/**
- * Validatory interface provides the necessary contracts 
- * for validating and asserting the passed values
- * @author DN
- *
- */
+
+
+import java.util.Date;
+
+	/**
+	 * Validatory interface provides the necessary contracts 
+	 * for validating and asserting the passed values
+	 * @author DN
+	 *
+	 */
 
 public interface Validatory {
 	
 	/**
 	 * isNotEmpty  method validate if the passed String is
-	 * null ot empty.
+	 * null or empty.
 	 * @author DN
 	 * @param text to be tested for null or empty
 	 * @return boolean if "text" contains an alpha numeric value
@@ -25,7 +33,7 @@ public interface Validatory {
 	/**
 	 * validateEmail method test for known email patterns.
 	 * If the argument in test is confirm to valid email
-	 * format method successfully pass the test
+	 * format ,method successfully passes the test
 	 * @author DN
 	 * @param email String 
 	 * @return boolean if email confirms to the email format
@@ -44,7 +52,7 @@ public interface Validatory {
 	 */
 	public boolean isInteger(String value) throws Exception;
 	/**
-	 * isInteger() tests if the passed parameter is an Float
+	 * isFloat() tests if the passed parameter is an Float
 	 * @author DN
 	 * @param value String value representing the Float and
 	 * required to validate
@@ -77,4 +85,18 @@ public interface Validatory {
 	
 	public boolean isStringContainsAlphaNumericAndUnderscore(String testableString,
 			int requiredMinimumLength) throws Exception;
+	
+	/**
+	 * compareDates() compares two dates respectively <I>date</I> and <i>otherDate</i>
+	 *  and returns an integer based on the comparison 
+	 * date > otherDate  then , returns +ve int
+	 * date < otherDate then , returns -ve int
+	 * date == otherDate then, returns 0
+	 * @param date : java.util.Date the initial date
+	 * @param otherDate : java.util.Date the secondary date 
+	 * @return int value as explained above
+	 * @throws Exception 
+	 */
+	public int compareDates(Date date,Date otherDate) throws Exception;
+	
 }
