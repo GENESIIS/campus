371183a3b712183b4398018fef75daa64b7c67a0
Anuradha Rushmitha <anuradha@genesiis.com>
2017-03-03 09:08:08 +0530

    CAM-142 Session validation backend

--- campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
@@ -2,13 +2,19 @@ package com.genesiis.campus.factory;
 
 //20161026 DN c10-contacting-us-page amended by inserting CONTACT_US_PUBLC entry to the map
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import com.genesiis.campus.command.CmdListCategories;
 import com.genesiis.campus.command.ICommand;
+import com.genesiis.campus.util.DataHelper;
+import com.genesiis.campus.util.IDataHelper;
 import com.genesiis.campus.validation.Operation;
 
 public class PublicCmdFactory implements ICmdFactory {
 
 	private ICommand command = null;
+	HttpSession session;
 	static {	
 		
 		map.put(Operation.LIST_CATEGORY_DATA, new CmdListCategories());
@@ -17,6 +23,7 @@ public class PublicCmdFactory implements ICmdFactory {
 
 	@Override
 	public ICommand getCommand(String cco) {
+		boolean status = sessionValidation(helper);
 		Operation o = Operation.BAD_OPERATION;
 		o = Operation.getOperation(cco);
 		switch (o) {
@@ -29,6 +36,20 @@ public class PublicCmdFactory implements ICmdFactory {
 			break;
 		}
 		return command;
+	}
+	//Session validation from backend side. 
+	public boolean sessionValidation(){
+		// HttpServletRequest request = ServletActionContext.getRequest();  
+		IDataHelper helper = new DataHelper(request);
+		session = helper.getRequest().getSession(false);
+		String currentSessionUser = (String) session.getAttribute("currentSessionUser");
+	
+		if (currentSessionUser == null) {
+			
+		}else{
+			
+		}
+		return false;
 	}
 
 }
