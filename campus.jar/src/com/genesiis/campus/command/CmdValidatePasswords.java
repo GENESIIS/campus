39ba28620f5fc6ce2674fa6dfe3ebdc0ed47e8c5
chinthakacw <chinthaka@genesiis.com>
2017-03-05 22:57:02 +0530

    c37-tutor-update-tutor-profile-cw modified execute method to validate password fields properly using Validator.isValidPassword method in CmdValidatePasswords.java

--- campus.jar/src/com/genesiis/campus/command/CmdValidatePasswords.java
+++ campus.jar/src/com/genesiis/campus/command/CmdValidatePasswords.java
@@ -1,16 +1,17 @@
 package com.genesiis.campus.command;
 
 //20170303 c37-tutor-update-tutor-profile-cw INIT CmdValidatePasswords.java
+//20170305 c37-tutor-update-tutor-profile-cw modified execute method to validate password fields properly using Validator.isValidPassword method
 
 import com.genesiis.campus.entity.IView;
-import com.genesiis.campus.entity.TutorEmailDAO;
-import com.genesiis.campus.entity.model.Tutor;
+//import com.genesiis.campus.entity.TutorEmailDAO;
+//import com.genesiis.campus.entity.model.Tutor;
 import com.genesiis.campus.util.IDataHelper;
 import com.genesiis.campus.validation.Validator;
 
 import org.apache.log4j.Logger;
 
-import java.sql.SQLException;
+//import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -23,30 +24,19 @@ public class CmdValidatePasswords implements ICommand {
 	static Logger log = Logger.getLogger(CmdValidatePasswords.class.getName());
 
 	@Override
-	public IView execute(IDataHelper helper, IView view) throws SQLException, Exception {
+	public IView execute(IDataHelper helper, IView view) throws Exception {
 
 		String message = "";
-		boolean valid = false;
-		
-		Collection<Collection<String>> tutorCollection= new ArrayList<Collection<String>>();
-		
 		try {
+				
+			boolean valid = false;
+			valid = Validator.isValidPassword(helper);
 			
-			valid = Validator.isNotEmpty(helper.getParameter("email"));
-			
-			if (valid){
-				final Tutor tutor = new Tutor();
-				tutor.setEmailAddress(helper.getParameter("email"));
-				tutorCollection = new TutorEmailDAO().findById(tutor);		
+			if(valid){
+				message = "TRUE";
+			}else{
+				message = "FALSE";
 			}
-			
-			if (tutorCollection.isEmpty()) {
-				message = "1"; // email does not exist
-			} else {
-				message = "0"; // email Already exists
-			}
-			
-			view.setCollection(tutorCollection);
 			
 		} catch (Exception exception) {
 			log.error("execute() : Exception " + exception.toString());
