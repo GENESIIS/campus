e984ff803e7e712a80bf2e5bd9e354dff6fc3012
jayani1991 <jayani@genesiis.com>
2017-03-06 13:24:18 +0530

    c96-public-list-all-tutors get tutor profile image path using SystemConfig enum in command class

--- campus.jar/src/com/genesiis/campus/command/CmdPublicListTutors.java
+++ campus.jar/src/com/genesiis/campus/command/CmdPublicListTutors.java
@@ -1,11 +1,13 @@
 package com.genesiis.campus.command;
 
-//20170228 JH c96-publicListTuors INIT CmdPublicListTutors.java
+//20170228 JH c96-public-list-all-tutors INIT CmdPublicListTutors.java
+//20170306 JH c96-public-list-all-tutors get tutor profile image path using SystemConfig enum
 
 import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.entity.PublicTutorDAO;
 import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.validation.SystemConfig;
 import com.genesiis.campus.validation.SystemMessage;
 
 import org.apache.log4j.Logger;
@@ -35,7 +37,7 @@ public class CmdPublicListTutors implements ICommand{
 				message = SystemMessage.NODATA;
 			}
 			view.setCollection(tutorCollection);
-			
+
 			
 		}catch(SQLException sqlException){
 			log.error("execute() SQLException : "+ sqlException.toString());
@@ -44,7 +46,9 @@ public class CmdPublicListTutors implements ICommand{
 			log.error("execute() Exception : "+ exception.toString());
 			throw exception;
 		}finally{
+			final String tutorImagePath = SystemConfig.TUTOR_PROFILE_IMAGE_PATH.getValue1();
 			helper.setAttribute("userMessage", message.toString());
+			helper.setAttribute("tutorProfileImagePath", tutorImagePath);
 		}
 		
 		
