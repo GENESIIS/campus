fa2e84b3daed97f0212e24198b3bf859bdcb36b2
jayani1991 <jayani@genesiis.com>
2017-03-06 05:58:12 +0530

    c96-public-list-all-tutors removed unwanted lines and display tutor image with static system config file path in javascript file

--- campus.war/web/dist/js/public/public-list-tutor-helper.js
+++ campus.war/web/dist/js/public/public-list-tutor-helper.js
@@ -1,11 +1,12 @@
 /**
  * 20170302 JH c96 public-list-tutor-helper.js created to help list tutors for the public
+ * 20170306 JH c96 removed unwanted lines and display tutor image with static system config file path
  */
 
 window.tutorList = null;
 
 window.onload = function() {
-	DisplayTutorTable();
+	listPublicTutors();
 };
 
 function listPublicTutors(){
@@ -26,6 +27,7 @@ function listPublicTutors(){
 				    $('#example tbody').on('click', 'tr', function () {
 				    	   var tutorCode = $(this).find("td:first").html();
 				    	   
+				    	   alert("will direct to tutor public profile. and the tutor is " + tutorCode);
 				    	   var form = document.createElement('form');
 				    	   form.method = 'post';
 				    	   form.action = '/dist/partials/public/display-tutors.jsp';
@@ -41,74 +43,17 @@ function listPublicTutors(){
 		},
 	});
 }
-//
-//function DisplayTutorTable(){
-//	var t =  $('#example').DataTable();
-//	var tutors = window.tutorList;
-//	var rowCount = 0;
-//	t.clear().draw();
-//
-//	if (tutors !== undefined & tutors !== null) {
-//		$
-//				.each(
-//						tutors,
-//						function(index, value) {
-//
-////							var res = value.toString();
-////							var data = res.split(",");
-//							// rowCount++;
-//							var value11 = null;
-//							if (value[11] == 1) {
-//								value11 = ' <span class="glyphicon glyphicon-ok" style="color:green;"></span>';
-//							} else if (value[11] == 0) {
-//								value11 = ' <span class="glyphicon glyphicon-info-sign" style="color:blue;"></span>';
-//							} else if (value[11] == 2) {
-//								value11 = ' <span class="glyphicon glyphicon-remove" style="color:red;"></span>';
-//							}
-//							
-//							var value17 = null; 
-//							if( value[17] == 1){
-//								value17 = ' <span style="color:green;">Active</span></td>';
-//							}else if( value[17] == 0){
-//								value17 = ' <span style="color:red;">Inactive </span></td>';
-//							}else if( value[17] == 2){
-//								value17 = ' <span style="color:blue;">Pending </span></td>';
-//							}else if( value[17] == 3){
-//								value17 = ' <span style="color:red;">Expired </span></td>';
-//							}
-//							
-//t.row.add(
-//									[
-//
-//											value[0],
-//											value[1] + '&nbsp;' + value[2]
-//													+ '&nbsp;' + value[3],
-//											value[4],
-//											value[5],
-//											value[6] + value[7] + '&nbsp;'
-//													+ value[8],
-//											value[6] + value[9] + '&nbsp;'
-//													+ value[10],
-//											value[15],
-//											value[16],
-//											value11,
-//											value17
-//
-//									]).draw(false);
-//
-//			
-//		});
-//	}
-//	
-//}
-//
-
 
 function DisplayTutorTable(){
 	var t =  $('#example').DataTable();
 	var tutors = window.tutorList;
 	var rowCount = 0;
+	var tutorImagePath = "education/tutor/profile/";
+	var fileSeparator = "/";
+	var extension = ".jpg";
 	t.clear().draw();
+	var imageFile = null;
+	var defaultImage  = '/education/tutor/profile/pfr-image.jpg';
 
 	if (tutors !== undefined & tutors !== null) {
 		$
@@ -116,15 +61,16 @@ function DisplayTutorTable(){
 						tutors,
 						function(index, value) {
 
-//							var res = value.toString();
-//							var data = res.split(",");
-							// rowCount++;
+							 rowCount++;
 							var value1 = null;
-							value1 = '  <img src="../../i/public/tutors/pfr-image.jpg" alt="">';
+							
+							imageFile = fileSeparator + tutorImagePath + value[0] + fileSeparator + value[0]+extension;
+							value1 = '  <img src="' + imageFile + '" alt="" onerror="this.src = ' + defaultImage + '">';
 	
 t.row.add(
 									[
-
+									 
+rowCount,
 											value1,
 											value[1] + '&nbsp;' + value[2]
 													+ '&nbsp;' + value[3],
