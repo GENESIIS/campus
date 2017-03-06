f9c555c377c4d73b1604c9e5c604a93eb37075cd
jayani1991 <jayani@genesiis.com>
2017-03-06 11:56:09 +0530

    c96-public-list-all-tutors created new javascript method selectTutorRecord(code) to pass tutor code

--- campus.war/web/dist/js/public/public-list-tutor-helper.js
+++ campus.war/web/dist/js/public/public-list-tutor-helper.js
@@ -1,6 +1,6 @@
 /**
  * 20170302 JH c96 public-list-tutor-helper.js created to help list tutors for the public
- * 20170306 JH c96 removed unwanted lines and display tutor image with static system config file path
+ * 20170306 JH c96 removed unwanted lines and display tutor image with static system config file path, added new method selectTutorRecord(code)
  */
 
 window.tutorList = null;
@@ -9,6 +9,11 @@ window.onload = function() {
 	listPublicTutors();
 };
 
+/**
+ * Creates a the database call to retrieve list of tutors 
+ * @returns
+ * @author JH
+ */
 function listPublicTutors(){
 	$.ajax({
 		url : '/PublicController',
@@ -24,36 +29,50 @@ function listPublicTutors(){
 				window.tutorList = response.result;
 
 				DisplayTutorTable();
-				    $('#example tbody').on('click', 'tr', function () {
-				    	   var tutorCode = $(this).find("td:first").html();
-				    	   
-				    	   alert("will direct to tutor public profile. and the tutor is " + tutorCode);
-				    	   var form = document.createElement('form');
-				    	   form.method = 'post';
-				    	   form.action = '/dist/partials/public/display-tutors.jsp';
-				    	   var input = document.createElement('input');
-				    	   input.type = 'text';
-				           input.name = 'tutorCode';
-				           input.value = tutorCode;
-				           form.appendChild(input);
-				           form.submit();
-				    	   
-				    });
+			//	    $('#example tbody').on('click', 'tr', function () {
+
 			}
 		},
 	});
 }
 
+/**
+ * used to select a specific record of a tutor and pass the value to the next page
+ * @param code
+ * @returns
+ * @author JH
+ */
+function selectTutorRecord(code){
+	  var tutorCode = $(this).find("td:first").html();
+	   
+	   alert("will direct to tutor public profile. and the tutor is " + tutorCode);
+	   var form = document.createElement('form');
+	   form.method = 'post';
+	   form.action = '/dist/partials/public/display-tutors.jsp';
+	   var input = document.createElement('input');
+	   input.type = 'text';
+      input.name = 'tutorCode';
+      input.value = tutorCode;
+      form.appendChild(input);
+      form.submit();
+}
+
+
+/**
+ * Used to generate the tutor table using datatables
+ * @returns
+ * @author JH
+ */
 function DisplayTutorTable(){
 	var t =  $('#example').DataTable();
 	var tutors = window.tutorList;
 	var rowCount = 0;
-	var tutorImagePath = "education/tutor/profile/";
+	var tutorImagePath = "education/tutor/pro_image/";
 	var fileSeparator = "/";
 	var extension = ".jpg";
 	t.clear().draw();
 	var imageFile = null;
-	var defaultImage  = '/education/tutor/profile/pfr-image.jpg';
+	var defaultImage  = '/education/tutor/pro_image/default.jpg';
 
 	if (tutors !== undefined & tutors !== null) {
 		$
