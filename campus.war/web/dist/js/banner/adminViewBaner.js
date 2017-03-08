ca6a62a8e6a9f67db20330b2bd8751ee3c4d0574
dushanthacb <dushantha@genesiis.com>
2017-03-08 09:41:24 +0530

    c81-admin-manage-banner-add-and-view-banner-dn created the js initial file

---
+++ campus.war/web/dist/js/banner/adminViewBaner.js
@@ -0,0 +1,8 @@
+/**
+ * 20170308 DN c81-admin-manage-banner-add-and-view-banner-dn initial adminViewBanner.js.
+ *  
+ */
+
+var theNewScript = document.createElement("script");
+theNewScript.type = "text/javascript";
+theNewScript.src = "../../dist/js/institute/validation/validation.js";
