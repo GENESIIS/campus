ca0250ef7ef0519829f98e367ecc0af0f8cc2f97
jayani1991 <jayani@genesiis.com>
2017-03-08 12:22:57 +0530

    c96-public-list-all-tutors remove repeating records of the tutor from the tutorCollection wip

--- campus.jar/src/com/genesiis/campus/command/CmdPublicListTutors.java
+++ campus.jar/src/com/genesiis/campus/command/CmdPublicListTutors.java
@@ -2,6 +2,7 @@ package com.genesiis.campus.command;
 
 //20170228 JH c96-public-list-all-tutors INIT CmdPublicListTutors.java
 //20170306 JH c96-public-list-all-tutors get tutor profile image path using SystemConfig enum, show error messages
+//20170308 JH c96-public-list-all-tutors remove repeating records of the tutor from the tutorCollection
 
 import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
@@ -13,7 +14,10 @@ import com.genesiis.campus.validation.SystemMessage;
 import org.apache.log4j.Logger;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author JH
@@ -35,9 +39,14 @@ public class CmdPublicListTutors implements ICommand{
 			
 			if(tutorCollection.size() == 0 ){
 				message = SystemMessage.NODATA;
+			}else{
+				Map categoryMap = new HashMap<String, ArrayList<String>>();
+				for(Collection<String> singleList : tutorCollection){
+					
+				}
 			}
 			view.setCollection(tutorCollection);
-
+			
 			
 		}catch(SQLException sqlException){
 			message = SystemMessage.ERROR;
