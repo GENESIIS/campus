796ef23a169b2a30375c4643751c6e4847d895a2
Anuradha Silva <anuradha@genesiis.com>
2017-03-08 16:40:54 +0530

    CAM-142 session validation handling methods removed

--- campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
@@ -1,14 +1,13 @@
 package com.genesiis.campus.factory;
 
 //20161026 DN c10-contacting-us-page amended by inserting CONTACT_US_PUBLC entry to the map
+//20170308 AS c142 removed session validation method 
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import com.genesiis.campus.command.CmdListCategories;
 import com.genesiis.campus.command.ICommand;
-import com.genesiis.campus.util.DataHelper;
-import com.genesiis.campus.util.IDataHelper;
+
 import com.genesiis.campus.validation.Operation;
 
 public class PublicCmdFactory implements ICmdFactory {
@@ -23,7 +22,7 @@ public class PublicCmdFactory implements ICmdFactory {
 
 	@Override
 	public ICommand getCommand(String cco) {
-		boolean status = sessionValidation(helper);
+	
 		Operation o = Operation.BAD_OPERATION;
 		o = Operation.getOperation(cco);
 		switch (o) {
@@ -37,19 +36,6 @@ public class PublicCmdFactory implements ICmdFactory {
 		}
 		return command;
 	}
-	//Session validation from backend side. 
-	public boolean sessionValidation(){
-		// HttpServletRequest request = ServletActionContext.getRequest();  
-		IDataHelper helper = new DataHelper(request);
-		session = helper.getRequest().getSession(false);
-		String currentSessionUser = (String) session.getAttribute("currentSessionUser");
-	
-		if (currentSessionUser == null) {
-			
-		}else{
-			
-		}
-		return false;
-	}
+
 
 }
