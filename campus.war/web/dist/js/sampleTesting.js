3c77d00292c3ba4058ec3ee3b96d074b7be90c37
Anuradha Silva <anuradha@genesiis.com>
2017-03-09 17:55:43 +0530

    CAM-142 added for testing WIP

---
+++ campus.war/web/dist/js/sampleTesting.js
@@ -0,0 +1,31 @@
+/**
+ * 20170309 AS CAM-142 sampleTesting.js created for testing 
+ */
+
+function ajaxTesting(){
+	var testData = "testing data";
+	
+	var jsonData = {
+			"testData" : testData
+		};
+		$.ajax({
+
+			url : '/StudentController',
+			method : 'POST',
+			data : {
+				jsonData : JSON.stringify(jsonData),
+				CCO : "TEST_AJAX"
+				
+			},
+			dataType : "json",
+			success : function(response) {
+				alert(response.result);
+				window.location = response.url;
+			},
+			error : function(e) {
+				 alert("Error " + e);
+				console.log(e);
+			}
+		});
+	
+}
