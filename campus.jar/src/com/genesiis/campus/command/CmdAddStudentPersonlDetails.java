5c35237980d6b38b139c915776fefa6cfe5ab627
pabodhaW <pabodha@genesiis.com>
2017-03-10 15:28:31 +0530

    CAM-150: CAM-150: execute() method modified by adding validation to country and town values.

--- campus.jar/src/com/genesiis/campus/command/CmdAddStudentPersonlDetails.java
+++ campus.jar/src/com/genesiis/campus/command/CmdAddStudentPersonlDetails.java
@@ -5,6 +5,7 @@ package com.genesiis.campus.command;
 //20170105 PN CAM-28: edit user information: execute() method code modified with improved connection property management.
 //20170117 PN CAM-28: dao method call moved into try block.
 //20170309 PN CAM-150: execute() method modified to add AddressLine1, AddressLine2 and AddressLine3 separately. set 'studentDetails' collection to a view object to pass into the front end.
+//20170310 PN CAM-150: execute() method modified by adding validation to country and town values.
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -13,9 +14,11 @@ import java.util.Map;
 import java.sql.Connection;
 import org.apache.log4j.Logger;
 
+import com.genesiis.campus.entity.Country2DAO;
 import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.entity.StudentDAO;
+import com.genesiis.campus.entity.TownDAO;
 import com.genesiis.campus.entity.model.Student;
 import com.genesiis.campus.util.ConnectionManager;
 import com.genesiis.campus.util.IDataHelper;
@@ -45,63 +48,79 @@ public class CmdAddStudentPersonlDetails implements ICommand {
 
 		try {
 			data = gson.fromJson(helper.getParameter("jsonData"), Student.class);
-			data.setCode(StudentCode);
-			data.setCrtBy("USER");
-			data.setModBy("USER");
 
-			// Set incoming data to view collection.
-			studentData.add(data.getFirstName());
-			studentData.add(data.getMiddleName());
-			studentData.add(data.getLastName());
-			studentData.add(String.valueOf(data.getDateOfBirth()));
-			studentData.add(data.getDescription());
-			studentData.add(data.getMobilePhoneNo());
-			studentData.add(data.getLandPhoneNo());
-			studentData.add(data.getAddress1());
-			studentData.add(data.getAddress2());
-			studentData.add(data.getAddress3());
-			studentData.add(data.getTown());
-			studentData.add(data.getEmail());
-			studentData.add(data.getFacebookUrl());
-			studentData.add(data.getTwitterUrl());
-			studentData.add(data.getLinkedInUrl());
-			studentData.add(data.getInstagramUrl());
-			studentData.add(data.getMySpaceUrl());
-			studentData.add(data.getWhatsAppNumber());
-			studentData.add(data.getViberNumber());
-			studentData.add(Integer.toString(data.getGender()));
-			studentData.add(data.getLandPhoneCountryCode());
-			studentDataCollection.add(studentData);
-			view.setCollection(studentDataCollection);
+			if (Validator.isNotEmpty(data.getTown()) && Validator.isNotEmpty(data.getLandPhoneCountryCode())) {
+				log.info("data.getTown(): "+data.getTown());
+				boolean isCountryExists = Country2DAO.isCountryExists(Integer.parseInt(data.getLandPhoneCountryCode()));
+				boolean isTownExists = TownDAO.isTownExists(Integer.parseInt(data.getTown()));
+				
+				if (Country2DAO.isCountryExists(Integer.parseInt(data.getLandPhoneCountryCode())) && TownDAO.isTownExists(Integer.parseInt(data.getTown()))) {
+					data.setCode(StudentCode);
+					data.setCrtBy("USER");
+					data.setModBy("USER");
 
-			// Validate incoming data and set it into a HashMap.
-			Map<String, Boolean> map = Validator.validaPersonalData(data);
-			// Check if the given data is valid.
-			boolean isValid = true;
-			for (String text : map.keySet()) {
-				if (!map.get(text)) {
-					isValid = false;
-					helper.setAttribute("studentPersonalStatus", map);
-					message = SystemMessage.INVALID_INFORMATION.message();
-					break;
+					// Set incoming data to view collection.
+					studentData.add(data.getFirstName());
+					studentData.add(data.getMiddleName());
+					studentData.add(data.getLastName());
+					studentData.add(String.valueOf(data.getDateOfBirth()));
+					studentData.add(data.getDescription());
+					studentData.add(data.getMobilePhoneNo());
+					studentData.add(data.getLandPhoneNo());
+					studentData.add(data.getAddress1());
+					studentData.add(data.getAddress2());
+					studentData.add(data.getAddress3());
+					studentData.add(data.getTown());
+					studentData.add(data.getEmail());
+					studentData.add(data.getFacebookUrl());
+					studentData.add(data.getTwitterUrl());
+					studentData.add(data.getLinkedInUrl());
+					studentData.add(data.getInstagramUrl());
+					studentData.add(data.getMySpaceUrl());
+					studentData.add(data.getWhatsAppNumber());
+					studentData.add(data.getViberNumber());
+					studentData.add(Integer.toString(data.getGender()));
+					studentData.add(data.getLandPhoneCountryCode());
+					studentDataCollection.add(studentData);
+					view.setCollection(studentDataCollection);
+
+					// Validate incoming data and set it into a HashMap.
+					Map<String, Boolean> map = Validator.validaPersonalData(data);
+					// Check if the given data is valid.
+					boolean isValid = true;
+					for (String text : map.keySet()) {
+						if (!map.get(text)) {
+							isValid = false;
+							helper.setAttribute("studentPersonalStatus", map);
+							message = SystemMessage.INVALID_INFORMATION.message();
+							break;
+						}
+					}
+
+					// Only if data is valid DAO method will fire
+					if (isValid) {
+						connection = ConnectionManager.getConnection();
+						// Commit false till the updations/additions
+						// successfully
+						// completed.
+						connection.setAutoCommit(false);
+						int rowId = studentDao.update(data, connection);
+						message = SystemMessage.UPDATED.message();
+						if (rowId == 0) {
+							rowId = studentDao.add(data, connection);
+							message = SystemMessage.ADDED.message();
+						}
+						// Commit if all the updations/additions successfully
+						// completed.
+						connection.commit();
+					}
+				} else {
+					message = SystemMessage.INVALID_LOCALE_DETAILS.message();
 				}
+			} else {
+				message = SystemMessage.INVALID_LOCALE_DETAILS.message();
 			}
 
-			// Only if data is valid DAO method will fire
-			if (isValid) {
-				connection = ConnectionManager.getConnection();
-				// Commit false till the updations/additions successfully
-				// completed.
-				connection.setAutoCommit(false);
-				int rowId = studentDao.update(data, connection);
-				message = SystemMessage.UPDATED.message();
-				if (rowId == 0) {
-					rowId = studentDao.add(data, connection);
-					message = SystemMessage.ADDED.message();
-				}
-				// Commit if all the updations/additions successfully completed.
-				connection.commit();
-			}
 			studentDetails = studentDao.findById(data);
 		} catch (SQLException sqle) {
 			connection.rollback();
