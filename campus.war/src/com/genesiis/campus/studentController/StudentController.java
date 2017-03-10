7a21d8fc391411b52194bd51b21f6631ee835df0
Anuradha Silva <anuradha@genesiis.com>
2017-03-10 17:19:34 +0530

    CAM-142 Session validation codes removed

--- campus.war/src/com/genesiis/campus/studentController/StudentController.java
+++ campus.war/src/com/genesiis/campus/studentController/StudentController.java
@@ -3,22 +3,15 @@ package com.genesiis.campus.studentController;
 //20161122 MM c5-corporate-training-landing-page - INIT - Initialised file
 //20170308 AS C142 Session validation handling and testing 
 import com.genesiis.campus.controller.CampusController;
-import com.genesiis.campus.util.DataHelper;
-import com.genesiis.campus.util.IDataHelper;
-import com.genesiis.campus.validation.ResponseType;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
 
-import javax.activation.DataHandler;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class PublicController
@@ -27,7 +20,7 @@ import javax.servlet.http.HttpSession;
 public class StudentController extends CampusController {
 	static Logger log = Logger.getLogger(StudentController.class.getName());
 	private static final long serialVersionUID = 1L;
-	HttpSession session;
+	
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
@@ -51,36 +44,8 @@ public class StudentController extends CampusController {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		//Session validation from backend side. 
-
-			session = request.getSession(false);
-			String currentSessionUser = (String) session.getAttribute("currentSessionUser");
-			IDataHelper helper = new DataHelper(request, response);
-			
-			if (currentSessionUser == null) {
-				String cco = helper.getCommandCode();
-				ResponseType responseType = helper.getResponseType(cco);
-				if(ResponseType.JSP.equals(responseType)){
-					response.sendRedirect("http://www.campus.dev:8080/dist/partials/error/error-content.jsp");
-				}else if(ResponseType.JSON.equals(responseType)){
-				//	JSONObject jobj = new JSONObject();
-			//		JsonObject myObj = new JsonObject();
-			//		Gson gson = new Gson();
-					String urlToRedirect = "http://www.campus.dev:8080/dist/partials/error/error-content.jsp";
-				//	myObj.add("url",urlToRedirect);
-				//	response.getWriter().write(myObj.("url",urlToRedirect));
-					response.setIntHeader("Refresh", 0);
-				//	response.setHeader("Location", "http://www.campus.dev:8080/dist/partials/error/error-content.jsp");
-			
-					 response.getWriter().write("{ url: '" + urlToRedirect + "' }");
-				}
-				
-			}else{
+	
 				super.doPost(request, response);
-			}
-
-
-		
-		
+	
 	}
 }
