c5a19c8f855f24bcb46fe18a571773061bc0c2b5
chinthakacw <chinthaka@genesiis.com>
2017-03-11 21:14:54 +0530

    c147-tutor-reset-password-cw modified execute method to get the data from the database for given email in CmdTutorEmailVerification.java

--- campus.jar/src/com/genesiis/campus/command/CmdTutorEmailVerification.java
+++ campus.jar/src/com/genesiis/campus/command/CmdTutorEmailVerification.java
@@ -1,8 +1,13 @@
 package com.genesiis.campus.command;
 
+import com.genesiis.campus.entity.ICrud;
+
 //20170307 CW c147-tutor-reset-password-cw CmdTutorEmailVerification class created.
+//20170311 CW c147-tutor-reset-password-cw modified execute method to get the data from the database for given email
 
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.TutorEmailVerificationDAO;
+import com.genesiis.campus.entity.model.Tutor;
 import com.genesiis.campus.util.IDataHelper;
 import com.genesiis.campus.util.mail.GenerateEmail;
 import com.genesiis.campus.validation.LoginValidator;
@@ -11,6 +16,8 @@ import com.genesiis.campus.validation.SystemMessage;
 import org.apache.log4j.Logger;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class CmdTutorEmailVerification implements ICommand {
 	
@@ -20,8 +27,8 @@ public class CmdTutorEmailVerification implements ICommand {
 	public IView execute(IDataHelper helper, IView view) throws SQLException, Exception {
 
 		int status = -3;
-		String message = SystemMessage.INVALID_EMAIL.message();
-		String result = "";
+		String message = null;//SystemMessage.INVALID_EMAIL.message();
+		//String result = "";
 		String firstName = "";
 		String lastName = "";
 		String email = "";
@@ -32,45 +39,48 @@ public class CmdTutorEmailVerification implements ICommand {
 			//IEmailComposer resetPasswordEmailComposer = new ResetPasswordInstructionEmailDispenser();
 			
 			GenerateEmail resetPasswordEmailComposer = new GenerateEmail();
-			String gsonData = helper.getParameter("jsonData");
+			Tutor tutor = new Tutor();
+			tutor.setEmailAddress(helper.getParameter("email"));
+/*			String gsonData = helper.getParameter("jsonData");
 			System.out.println("verifyemail = "+helper.getParameter("verifyemail"));
-/*			data = getStudentdetails(gsonData);
-			boolean validEmail = LoginValidator.validateEmail(data.getEmail());
-z			if (validEmail) {
-				ICrud emailVarifyDAO = new StudentEmailVerificationDAO();
-				dataCollection = emailVarifyDAO.findById(data);
+			data = getStudentdetails(gsonData);*/
+			boolean validEmail = LoginValidator.validateEmail(tutor.getEmailAddress());
+			if (validEmail) {
+				ICrud emailVarifyDAO = new TutorEmailVerificationDAO();
+				Collection<Collection<String>> emailCollection = new ArrayList<Collection<String>>();
+				emailCollection = emailVarifyDAO.findById(tutor);
 
-				for (Collection<String> collection : dataCollection) {
-					Object[] array = collection.toArray();
-					if (array[0].equals(SystemMessage.INVALID_EMAIL.message())) {
-						result = (String) array[0];
-					} else {
-
-						firstName = (String) array[0];
-						lastName = (String) array[1];
-						email = (String) array[2];
-						uname = (String) array[3];
-						code = (String) array[4];
+				for (Collection<String> collection : emailCollection) {
+					if(collection != null && !collection.isEmpty()){
+						Object[] array = collection.toArray();
+						/*if (array[0].equals(SystemMessage.INVALID_EMAIL.message())) {
+							result = (String) array[0];
+						} else {*/	
+							firstName = (String) array[0];
+							lastName = (String) array[1];
+							email = (String) array[2];
+							uname = (String) array[3];
+							code = (String) array[4];
+					}else{
+						message = SystemMessage.INVALID_EMAIL.message();
 					}
 				}
 
-				if (result.equalsIgnoreCase(SystemMessage.INVALID_EMAIL
-						.message())) {
-					message = SystemMessage.INVALID_EMAIL.message();
-
-				} else {
-					data.setFirstName(firstName);
-					data.setLastName(lastName);
-					data.setEmail(email);
-					data.setCode(Integer.parseInt(code));
+				if (message.isEmpty()){  // && message.equalsIgnoreCase(SystemMessage.INVALID_EMAIL.message())) {
+			/*		message = SystemMessage.INVALID_EMAIL.message();
+				} else {*/
+					tutor.setFirstName(firstName);
+					tutor.setLastName(lastName);
+					tutor.setEmailAddress(email);
+					tutor.setCode(Integer.parseInt(code));
 
 					recieversEmailAddreses = new ArrayList<String>();
 					recieversEmailAddreses.add(email);
 					HashCodeBuilder hashBuilder = new HashCodeBuilder();
 					securedHash = hashBuilder.createHash(firstName, lastName);
-					data.setHashCode(securedHash);
+					tutor.setHashCode(securedHash);
 
-					int updateData = emailVarifyDAO.update(data);
+					int updateData = emailVarifyDAO.update(tutor);
 					if (updateData > 0) {
 						message = SystemMessage.HASHCODES.message();
 						resetPasswordEmailComposer.setEnvironment(firstName
@@ -98,7 +108,7 @@ z			if (validEmail) {
 			throw sexp;
 		} catch (IllegalArgumentException ilexp) {
 			log.error("execute(): IllegalArgumentException" + ilexp.toString());
-			throw ilexp;*/
+			throw ilexp;
 		} catch (Exception exp) {
 			log.error("execute():Exception " + exp.toString());
 			throw exp;
