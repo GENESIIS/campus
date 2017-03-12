40efaa7c7df93292330e9eac4faf3319d4fa54a1
chinthakacw <chinthaka@genesiis.com>
2017-03-12 20:54:17 +0530

    c147-tutor-reset-password-cw add systemMessage method to send the message properly & modified execute method to send the email with hashcode in CmdTutorEmailVerification.java

--- campus.jar/src/com/genesiis/campus/command/CmdTutorEmailVerification.java
+++ campus.jar/src/com/genesiis/campus/command/CmdTutorEmailVerification.java
@@ -1,13 +1,15 @@
 package com.genesiis.campus.command;
 
-import com.genesiis.campus.entity.ICrud;
-
 //20170307 CW c147-tutor-reset-password-cw CmdTutorEmailVerification class created.
 //20170311 CW c147-tutor-reset-password-cw modified execute method to get the data from the database for given email
+//20170312 CW c147-tutor-reset-password-cw add systemMessage method to send the message properly
+				// modified execute method to send the email with hashcode 
 
+import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.entity.TutorEmailVerificationDAO;
 import com.genesiis.campus.entity.model.Tutor;
+import com.genesiis.campus.scrypt.crypto.HashCodeBuilder;
 import com.genesiis.campus.util.IDataHelper;
 import com.genesiis.campus.util.mail.GenerateEmail;
 import com.genesiis.campus.validation.LoginValidator;
@@ -27,23 +29,16 @@ public class CmdTutorEmailVerification implements ICommand {
 	public IView execute(IDataHelper helper, IView view) throws SQLException, Exception {
 
 		int status = -3;
-		String message = null;//SystemMessage.INVALID_EMAIL.message();
-		//String result = "";
+		String message = null;
 		String firstName = "";
 		String lastName = "";
 		String email = "";
 		String uname = "";
 		String code = "";
 		try {
-
-			//IEmailComposer resetPasswordEmailComposer = new ResetPasswordInstructionEmailDispenser();
-			
-			GenerateEmail resetPasswordEmailComposer = new GenerateEmail();
+			GenerateEmail tutorResetPasswordEmail = new GenerateEmail();
 			Tutor tutor = new Tutor();
 			tutor.setEmailAddress(helper.getParameter("email"));
-/*			String gsonData = helper.getParameter("jsonData");
-			System.out.println("verifyemail = "+helper.getParameter("verifyemail"));
-			data = getStudentdetails(gsonData);*/
 			boolean validEmail = LoginValidator.validateEmail(tutor.getEmailAddress());
 			if (validEmail) {
 				ICrud emailVarifyDAO = new TutorEmailVerificationDAO();
@@ -53,52 +48,34 @@ public class CmdTutorEmailVerification implements ICommand {
 				for (Collection<String> collection : emailCollection) {
 					if(collection != null && !collection.isEmpty()){
 						Object[] array = collection.toArray();
-						/*if (array[0].equals(SystemMessage.INVALID_EMAIL.message())) {
-							result = (String) array[0];
-						} else {*/	
-							firstName = (String) array[0];
-							lastName = (String) array[1];
-							email = (String) array[2];
-							uname = (String) array[3];
-							code = (String) array[4];
+						tutor.setFirstName((String) array[0]);
+						tutor.setLastName((String) array[1]);
+						tutor.setEmailAddress((String) array[2]);
+						tutor.setUsername((String) array[3]);
+						
+						code = (String) array[4];
+						if(code != null){
+							tutor.setCode(Integer.parseInt(code));
+						}						
 					}else{
 						message = SystemMessage.INVALID_EMAIL.message();
 					}
 				}
 
-				if (message.isEmpty()){  // && message.equalsIgnoreCase(SystemMessage.INVALID_EMAIL.message())) {
-			/*		message = SystemMessage.INVALID_EMAIL.message();
-				} else {*/
-					tutor.setFirstName(firstName);
-					tutor.setLastName(lastName);
-					tutor.setEmailAddress(email);
-					tutor.setCode(Integer.parseInt(code));
-
-					recieversEmailAddreses = new ArrayList<String>();
-					recieversEmailAddreses.add(email);
+				if (message == null){
 					HashCodeBuilder hashBuilder = new HashCodeBuilder();
-					securedHash = hashBuilder.createHash(firstName, lastName);
+					String securedHash = hashBuilder.createHash(tutor.getFirstName(), tutor.getLastName());
 					tutor.setHashCode(securedHash);
 
 					int updateData = emailVarifyDAO.update(tutor);
 					if (updateData > 0) {
 						message = SystemMessage.HASHCODES.message();
-						resetPasswordEmailComposer.setEnvironment(firstName
-								+ " " + lastName, sendersEmail,
-								recieversEmailAddreses, emailTitle, emailBody
-										+ " " + securedHash, securedHash);
-
-						resetPasswordEmailComposer
-								.setGeneralEmail(resetPasswordEmailComposer
-										.formatEmailInstance(addSpecificContentToOriginalMailBody()));
-						status = this.sendMail(resetPasswordEmailComposer);
-
+						status = tutorResetPasswordEmail.sendTutorResetPasswordVerificationEmail(tutor.getFirstName(), 
+								tutor.getLastName()	, tutor.getEmailAddress(), tutor.getUsername(), tutor.getHashCode());
 					} else {
 						message = SystemMessage.HASHCODEUNS.message();
 					}
-
 					message = systemMessage(status);
-
 				}
 			} else {
 				message = SystemMessage.INVALID_EMAIL.message();
@@ -115,6 +92,31 @@ public class CmdTutorEmailVerification implements ICommand {
 		}
 		helper.setAttribute("message", message);
 		return view;
+	}
+	
+	/*
+	 * systemMessage() handles the system Messages according to the state of the
+	 * status passed in
+	 * 
+	 * @return String the message
+	 * 
+	 * @param status 3 request submitted successfully.
+	 * 
+	 * @param status -3 request submission fails.
+	 */
+	private String systemMessage(int status) {
+		String message = SystemMessage.UNKNOWN.message();
+		switch (status) {
+		case 3:
+			message = SystemMessage.MAIL_SUCCESS.message();
+			break;
+		case -3:
+			message = SystemMessage.MAIL_UNSUCCESS.message();
+			break;
+		default:
+			break;
+		}
+		return message;
 	}
 
 }
