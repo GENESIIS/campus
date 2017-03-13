197fa07ba5536bc888ed9f964ac19749b43a3371
chinthakacw <chinthaka@genesiis.com>
2017-03-13 13:55:52 +0530

    c148-tutor-verify-hashcode-reset-password-cw add TUTOR_HASH_VERIFICATION command in TutorCmdFactory.java

--- campus.jar/src/com/genesiis/campus/factory/TutorCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/TutorCmdFactory.java
@@ -1,29 +1,20 @@
 package com.genesiis.campus.factory;
 
 //20161121 CM c36-add-tutor-information INIT TutorCmdFactory.java
-//20161121 CM c36-add-tutor-information Modified getCommand() method. 
-//20161122 CM c36-add-tutor-information Modified getCommand() method. 
-//20161123 CM c36-add-tutor-information Modified getCommand() method. 
-//20170116 CM c36-add-tutor-information add getCommand() add LOAD_TUTOR_WITH_DUMMY_DATA element. 
-//20170116 CM c36-add-tutor-information removed getCommand() add LOAD_TUTOR_WITH_DUMMY_DATA element. 
-//20170117 CM c36-add-tutor-information Modified getCommand() method. 
-//20170116 CM c36-add-tutor-information removed CmdCheckUsername element.
-//20170207 CW c38-view-update-tutor-profile removed CmdCheckUsername element.
-//20170207 CW c38-view-update-tutor-profile removed ADD_TUTOR_PROFILE_DETAILS element
-//20170227 CW c37-tutor-update-tutor-profile-cw add VIEW_TUTOR_UPDATE_TUTOR_DETAILS & TUTOR_UPDATE_TUTOR element
-//20170303 CW c37-tutor-update-tutor-profile-cw add CHECK_PASSWORDS
 //20170308 CW c147-tutor-reset-password-cw Class copied from c37-tutor-update-tutor-profile-cw & removed unwanted commands
 				//c147 added CHECK_EMAIL command & TUTOR_EMAIL_VERIFICATION command.
 //20170308 CW c147-tutor-reset-password-cw removed CHECK_EMAIL command
+//20170313 CW c148-tutor-verify-hashcode-reset-password-cw add TUTOR_HASH_VERIFICATION command
 
-import com.genesiis.campus.command.CmdCheckEmail;
 import com.genesiis.campus.command.CmdTutorEmailVerification;
+import com.genesiis.campus.command.CmdTutorHashVerification;
 import com.genesiis.campus.command.ICommand;
 import com.genesiis.campus.validation.Operation;
 
 public class TutorCmdFactory implements ICmdFactory {
 	static {
 		map.put(Operation.TUTOR_EMAIL_VERIFICATION, new CmdTutorEmailVerification());
+		map.put(Operation.TUTOR_HASH_VERIFICATION, new CmdTutorHashVerification());
 	}
 	
 	@Override
