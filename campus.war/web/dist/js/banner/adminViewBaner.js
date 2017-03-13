4d2800806a7244f6691096d8483ffe5bff3170e0
dushanthacb <dushantha@genesiis.com>
2017-03-13 13:22:42 +0530

    c81-admin-manage-banner-add-and-view-banner-dn tbale id is provided

--- campus.war/web/dist/js/banner/adminViewBaner.js
+++ campus.war/web/dist/js/banner/adminViewBaner.js
@@ -3,6 +3,10 @@
  * 			 implemented loadBanners()
  * 20170310 DN c81-admin-manage-banner-add-and-view-banner-dn populateBannerTable() implemented.
  *  		success: section of the ajax call of loadBanners() method has been modified. 
+ * 20170313 DN c81-admin-manage-banner-add-and-view-banner-dn include a button to edit record in populateBannerTable()
+ * 			and indeces  of the response array that retrieves the element has been changed. Multiple line comments are added 
+ * 			to the same method. Add the "/" to the url variable populateBannerTable() method.
+ * 
  */
 
 var theNewScript = document.createElement("script");
@@ -10,6 +14,7 @@ theNewScript.type = "text/javascript";
 theNewScript.src = "../../dist/js/institute/validation/validation.js";
 
 var bannerArray ="";
+var adminControllerUrl = '../../../AdminController';
 
 $(document).ready(function(){
 	
@@ -21,15 +26,16 @@ $(document).ready(function(){
  * loadBanners() method sends the request to the server wrapping the 
  * filtering credentials in a ajax call. The method then populates the
  * the table with extracted records from the repository.
- * If none of the criteria information has been sent to the backend,
+ * If none of the criteria information has been sent to the back-end,
  * the default selected field values will be extracted from the front end
  * and all the banner records for active advertisers will be populated.
  */
+
 function loadBanners(){
 	
 	$.ajax({
 		type:'POST',
-		url:'',
+		url:adminControllerUrl,
 		data:{
 			CCO					:"ADDISBNRS", //Admin Display Banners
 			commencingDate		:$('#startDate').val(),
@@ -39,13 +45,20 @@ function loadBanners(){
 		dataType:'json',
 		success:function(response){
 			// call the function that populates the table based on supplied data
-			if(response.result.length===0)
-				displayLabelMessage('messagePopUp','displayLabel','green',"No records to display !");
-			populateBannerTable(response.result,response.bannerWarPath);
+			// if the server reply is a success
+			
+			if(response.successCode===1){
+				
+				if(response.result === "NO-DATA")
+					displayLabelMessage('messagePopUp','displayLabel','green',"No records to display !");
+				populateBannerTable(response.result,response.bannerWarPath);
+			}
+			
+			displayLabelMessage('messagePopUp','displayLabel','green',response.message);
 			
 		},
 		error:function(allBanners,error,errorThrown){
-			var msg = ajaxErorMessage(pageSlots,error,errorThrown);
+			var msg = ajaxCallErorMessage(allBanners,error,errorThrown);
 			displayLabelMessage('messagePopUp','displayLabel','red',msg);
 		},
 		complete: function(response,status){
@@ -65,16 +78,18 @@ function loadBanners(){
  * placed in the page dynamically. 
  * @param allBannerRecords : is a array of arrays that comes from the 
  * 		  server. The inner array contains data of single row which is
- * 		  extracted from the repository.
- * @returns null if the Array of array is 
+ * 		  extracted from the repository. If the array does not contain any data
+ * 		  it holds "NO-DATA" string.
+ * @returns null if the Array of array is empty else the interested table will be
+ * 			populated.
  */
 function populateBannerTable(allBannerRecords,bannerWarPath){
 	
-	if(allBannerRecords.length === 0){
+	if(allBannerRecords === "NO-DATA"){
 		return null;
 	}
 		
-	
+	$("#bannerDisplaytbl tr").remove();
 	// assigning all the banner records to the global variable.
 	bannerArray = allBannerRecords;
 	var bannerImageWarPath = bannerWarPath;
@@ -94,16 +109,24 @@ function populateBannerTable(allBannerRecords,bannerWarPath){
 			
 			var BannerCode = aRow[0];
 			var imageName =aRow[1];
-			var bannerActivateDate=aRow[1];
-			var bannerDeactivateDate = null;
-			var url =bannerImageWarPath+"/"+imageName;
-			var markUp = "<tr id='rowId"+rowNumber+"'><td> From : "+bannerActivateDate+" |To : "+bannerDeactivateDate+" <br><br>";
+			var bannerActivateDate=aRow[8];
+			var bannerDeactivateDate = aRow[9];
+			var imgeNameComponent = aRow[1].split(".");
+			
+			/*
+			 * path to the image is agreed to be '/education/banner/143/143.jpg etc
+			 * thus if there is any change to the folder structure should result a change to
+			 * bellow url path too.
+			 */
+			var url ="/"+bannerImageWarPath+"/"+imgeNameComponent[0]+"/"+imageName; // 
+			
+			var markUp = "<tr id='rowId"+rowNumber+"'><td> "+BannerCode+" From : "+bannerActivateDate+" |To : "+bannerDeactivateDate+" <br><br>";
 					markUp = markUp +"<form  method='POST'>"+ //action='urltogo' should be specified when needs to edit the banner record.
-										"<input type='submit' name="" class='editRow' id='CCO' value='ADMEDTBNR'>" +//ADMIN EDIT BANNER
+										"<button type='submit' name='CCO' class='editRow' id='CCO' value='ADMEDTBNR'>Edit The Record</button>" +//ADMIN EDIT BANNER
 										"<input type='hidden' id='bnrHidden"+rowNumber+"' name='bnrHidden"+rowNumber+"' value='"+BannerCode+"'>"+ 
 									 "</form></td>";
 					markUp = markUp +"<td><img id='bnnerImage"+rowNumber+"'src='"+url+"' alt='banner-Image' style='width:200px;hight:60px'></td></tr>" ;
-				
+			jQuery("table").css('overflow-y','auto');
 			jQuery("table tbody").append(markUp);
 			
 		});
@@ -111,21 +134,21 @@ function populateBannerTable(allBannerRecords,bannerWarPath){
 }
 
 /**
- * ajaxErorMessage
+ * ajaxCallErorMessage
  * @author DJ,DN re engineered 
  * @param response response that comes from the server
  * @error error  error message comes from the server when ajax call fails
  * @errorThrown actual error thrown once ajax response fails
  */
 
-function ajaxErorMessage(response,error,errorThrown){
+function ajaxCallErorMessage(response,error,errorThrown){
 	  var msg = '';
-    if (response.status === 0) {
-        msg = 'The XMLHttpRequest client has been created, but the open() method hasn\'t been called yet.';
-    }else{
-    	msg = error +": "+errorThrown;
-    }
-   return msg;
+  if (response.status === 0) {
+      msg = 'The XMLHttpRequest client has been created, but the open() method hasn\'t been called yet.';
+  }else{
+  	msg = error +": "+errorThrown;
+  }
+ return msg;
 }
 
 /**
@@ -136,10 +159,8 @@ function ajaxErorMessage(response,error,errorThrown){
  * @param cssColour required color theme for the message to be displayed
  * @param message the required message to be displayed
  */
+
 function displayLabelMessage(messagePopUpId,labelid,cssColour,message){
 	$('#'+messagePopUpId).modal('show'); // display the modal window
 	jQuery('#'+labelid).css({'color':cssColour,'font-weight':'bold'}).html("<h2>"+message+"</h2>");
-	
 }
-
-
