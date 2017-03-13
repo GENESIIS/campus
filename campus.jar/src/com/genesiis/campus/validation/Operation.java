280cad55c27c30a2bc2c33b7693cf77696e498d1
chinthakacw <chinthaka@genesiis.com>
2017-03-13 13:56:22 +0530

    c148-tutor-verify-hashcode-reset-password-cw add TUTOR_HASH_VERIFICATION command in Operation.java

--- campus.jar/src/com/genesiis/campus/validation/Operation.java
+++ campus.jar/src/com/genesiis/campus/validation/Operation.java
@@ -7,6 +7,7 @@ package com.genesiis.campus.validation;
 //20170307 CW c147-tutor-reset-password-cw added CHECK_EMAIL to the class.
 //20170307 CW c147-tutor-reset-password-cw added TUTOR_EMAIL_VERIFICATION to the class.
 //20170308 CW c147-tutor-reset-password-cw removed CHECK_EMAIL command
+//20170313 CW c148-tutor-verify-hashcode-reset-password-cw add TUTOR_HASH_VERIFICATION command
 
 /**
  * Operation class facilitate in binding the CCO
@@ -30,6 +31,7 @@ public enum Operation {
 	CHANGED_PASSWORD("RESETPASS",ResponseType.JSON,""),
 	LIST_INSTITUTE_DATA("LIST_INSTITUTE_DATA", ResponseType.JSP, "index.jsp"),
 	TUTOR_EMAIL_VERIFICATION("TUTOR_EMAIL_VERIFICATION", ResponseType.JSON, ""),	
+	TUTOR_HASH_VERIFICATION("TUTOR_HASH_VERIFICATION", ResponseType.JSON, ""),
 	LIST_CATEGORY_LANDING_PAGE("LIST_CATEGORY_LANDING_PAGE", ResponseType.JSP, "/dist/partials/category-landing-page.jsp");
 			
 	private final String commandString;
