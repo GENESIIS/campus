468af0710e8a6a66f7a9e24702832072743a028b
chinthakacw <chinthaka@genesiis.com>
2017-03-14 15:24:05 +0530

    c148-tutor-verify-hashcode-reset-password-cw comment changed & modified execute method to encrypt & update the password in CmdTutorPasswordChange.java

--- campus.jar/src/com/genesiis/campus/command/CmdTutorPasswordChange.java
+++ campus.jar/src/com/genesiis/campus/command/CmdTutorPasswordChange.java
@@ -1,12 +1,15 @@
 package com.genesiis.campus.command;
 
-//20170314 CW c147-tutor-reset-password-cw CmdTutorPasswordChange class created.
+//20170314 CW c148-tutor-verify-hashcode-reset-password-cw CmdTutorPasswordChange class created.
+//20170314 CW c148-tutor-verify-hashcode-reset-password-cw comment changed & modified execute method to encrypt & update the password
 
 import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
-import com.genesiis.campus.entity.SigningUpStudentDAO;
-import com.genesiis.campus.entity.model.Student;
+import com.genesiis.campus.entity.SigningUpTutorDAO;
+import com.genesiis.campus.entity.model.Tutor;
 import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.util.security.Encryptable;
+import com.genesiis.campus.util.security.TripleDesEncryptor;
 import com.genesiis.campus.validation.SystemMessage;
 
 import org.apache.log4j.Logger;
@@ -16,36 +19,42 @@ import java.util.Collection;
 
 public class CmdTutorPasswordChange implements ICommand {
 	static Logger log = Logger.getLogger(CmdTutorPasswordChange.class.getName());
-	private Student data;
-	private Collection<Collection<String>> dataCollection = null;
 
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
 		String message = "";
 		String pageURL = "/index.jsp";
-		/*try {
-			String gsonData = helper.getParameter("jsonData");
-			data = getStudentdetails(gsonData);
-
-			ICrud passwordRest = new SigningUpStudentDAO();
-			int result = passwordRest.update(data);
+		try {
+			Tutor tutor = new Tutor();
+			
+			if(helper.getParameter("code") != null){
+				tutor.setCode(Integer.parseInt(helper.getParameter("code")));
+			}			
+			
+			//password encryption 
+			Encryptable passwordEncryptor = new TripleDesEncryptor(helper.getParameter("password").trim());
+			String encryptPassword = passwordEncryptor.encryptSensitiveDataToString().trim();
+			tutor.setPassword(encryptPassword);
+			
+			ICrud passwordRest = new SigningUpTutorDAO();
+			int result = passwordRest.update(tutor);
 			if (result > 0) {
 				message = SystemMessage.PASSWORD_SUCCESS.message();
 				pageURL = "/index.jsp?showLogin=true";
 			} else {
 				message = SystemMessage.PASSWORD_UNSUCCESS.message();
 			}
-		} catch (SQLException sexp) {
-			log.error("execute(): SQLException " + sexp.toString());
-			throw sexp;
+		} catch (SQLException sqle) {
+			log.error("execute(): SQLException " + sqle.toString());
+			throw sqle;
 		} catch (IllegalArgumentException ilexp) {
 			log.error("execute(): IllegalArgumentException" + ilexp.toString());
 			throw ilexp;
 		} catch (Exception exp) {
 			log.error("execute():Exception " + exp.toString());
 			throw exp;
-		}*/
+		}
 
 		helper.setAttribute("message", message);
 		helper.setAttribute("pageURL", pageURL);
