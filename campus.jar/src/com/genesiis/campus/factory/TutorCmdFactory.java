e1bf1f8177c7d47b3f21726019a7709eb22ef630
chinthakacw <chinthaka@genesiis.com>
2017-03-14 12:10:39 +0530

    c148-tutor-verify-hashcode-reset-password-cw add TUTOR_RESET_PASSWORD command & modified getCommand method accordingly in TutorCmdFactory.java

--- campus.jar/src/com/genesiis/campus/factory/TutorCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/TutorCmdFactory.java
@@ -6,9 +6,11 @@ package com.genesiis.campus.factory;
 //20170308 CW c147-tutor-reset-password-cw removed CHECK_EMAIL command
 //20170313 CW c148-tutor-verify-hashcode-reset-password-cw add TUTOR_HASH_VERIFICATION command
 //20170313 CW c148-tutor-verify-hashcode-reset-password-cw modified TUTOR_HASH_VERIFICATION details in getCommand method
+//20170314 CW c148-tutor-verify-hashcode-reset-password-cw add TUTOR_RESET_PASSWORD command & modified getCommand method accordingly
 
 import com.genesiis.campus.command.CmdTutorEmailVerification;
 import com.genesiis.campus.command.CmdTutorHashVerification;
+import com.genesiis.campus.command.CmdTutorPasswordChange;
 import com.genesiis.campus.command.ICommand;
 import com.genesiis.campus.validation.Operation;
 
@@ -16,6 +18,7 @@ public class TutorCmdFactory implements ICmdFactory {
 	static {
 		map.put(Operation.TUTOR_EMAIL_VERIFICATION, new CmdTutorEmailVerification());
 		map.put(Operation.TUTOR_HASH_VERIFICATION, new CmdTutorHashVerification());
+		map.put(Operation.TUTOR_RESET_PASSWORD, new CmdTutorPasswordChange());
 	}
 	
 	@Override
@@ -28,6 +31,9 @@ public class TutorCmdFactory implements ICmdFactory {
 			command = map.get(o);	
 			break;
 		case TUTOR_HASH_VERIFICATION:
+			command = map.get(o);	
+			break;
+		case TUTOR_RESET_PASSWORD:
 			command = map.get(o);	
 			break;
 		default:
