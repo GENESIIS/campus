2d9438aa182bc753f78f3c3381b590083fd3494b
chinthakacw <chinthaka@genesiis.com>
2017-03-14 12:12:17 +0530

    c148-tutor-verify-hashcode-reset-password-cw changedPassword, validatePasswordResetData, convertPassWordToString, clearAllFields, passwordAndConfirmPassword methods added in tutor-login.js

--- campus.war/web/dist/js/tutor-login.js
+++ campus.war/web/dist/js/tutor-login.js
@@ -4,6 +4,8 @@
  * //20170308 CW c147-tutor-reset-password-cw modified forgotPassword, ValidateEmail method calling
  * //20170308 CW c147-tutor-reset-password-cw add ajax call from c22
  * //20170313 CW c148-tutor-verify-hashcode-reset-password-cw removed ValidateEmail method & add verifyCode method
+ * //20170314 CW c148-tutor-verify-hashcode-reset-password-cw changedPassword, validatePasswordResetData, 
+ * 						convertPassWordToString, clearAllFields, passwordAndConfirmPassword methods added.
  */
 
 /**
@@ -224,4 +226,158 @@ function hashDecode(data) {
 	var decodedString = atob(data);
 	
 	return decodedString;
+}
+
+//password changed ajax function
+function changedPassword() {
+	var code = $("#userTypeCode").val();
+	var password = $("#passWord").val();
+	var confirmpassword = $("#confrmpsw").val();
+	var paaswordEmpty = isempty(password);
+	var validation = validatePasswordResetData();
+	var passvadidation = passwordAndConfirmPassword(password,confirmpassword);
+	// code filed validation error messages handling
+	if (!(paaswordEmpty)) {
+	//	document.getElementById('emailtbError').innerHTML = "  ** Verify Code can not be Empty.";
+		jQuery('#passWordError').addClass("fp-msg-error").html('  ** Verify Code can not be Empty.');
+		flag = false;
+		return false;
+	}
+
+	if (code != null && validation && passvadidation) {
+/*		var jsonData = {
+			"code" : code,
+			"password" : password
+		};*/
+
+		$
+				.ajax({
+					type : "POST",
+					url : '/TutorController',
+					data : {
+						//jsonData : JSON.stringify(jsonData),
+						code : code,
+						password : password,
+						CCO : "TUTOR_RESET_PASSWORD"
+
+					},
+					dataType : "json",
+					success : function(response) {
+					
+						if (response['message'] === "Password successfully changed.") {
+							jQuery('#message').addClass("fp-msg-success").html(response['message']);
+							setTimeout(function() {
+								window.location.href = response['pageURL'];
+							}, 4000);
+						}
+						else{
+							jQuery('#message').addClass("fp-msg-error").html(response['message']);
+						}
+					},
+					error : function(response, error, errorThrown) {
+						alert("Error " + error);
+						console.log(error);
+						var msg = '';
+						if (response.status === 0) {
+							msg = 'Not connect.\n Verify Network.';
+						} else if (response.status == 404) {
+							msg = 'Requested page not found. [404]';
+						} else if (response.status == 500) {
+							msg = 'Internal Server Error [500].';
+						} else if (error === 'parsererror') {
+							msg = 'Requested JSON parse failed.';
+						} else if (error === 'timeout') {
+							msg = 'Time out error.';
+						} else if (error === 'abort') {
+							msg = 'Ajax request aborted.';
+						} else {
+							msg = 'Uncaught Error.\n' + response.responseText;
+						}
+					}
+
+				});
+	}
+}
+
+/**
+ * validateSignUpWoThirdPartyPageEmbedData() validates all the current critical
+ * fields placed on /dist/partials/signUpWoThirdParty.jsp page. It's the custom
+ * field validator dedicated for the page above.
+ * 
+ * @author anuradha
+ * @returns {Boolean}
+ */
+function validatePasswordResetData() {
+	var validationPass = true;
+
+	if (!(isFieldFilled(
+			isStringHasValiCharsAndLength($('#passWord').val(),
+					/^([a-zA-Z0-9]+)([a-zA-Z0-9_]+){7,}$/g),
+			"Check Field Contains Invalid Characters Or Should Be > 7 Characters and ",
+			"passWordError"))) {
+		return !validationPass;
+	} else if (!(isFieldFilled(isempty($('#passWord').val()), "Password Field",
+			"passWordError"))) {
+		return !validationPass;
+	} else if (!(isFieldFilled(isempty($('#confrmpsw').val()),
+			"Confirm Password Field", "confPassWordError"))) {
+		return !validationPass;
+	} else if (!(isFieldFilled(passwordAndConfirmPassword($('#passWord').val(),
+			$('#confrmpsw').val()), "PassWords Does Not Match ,The Field(s)",
+			"confPassWordError"))) {
+		return !validationPass;
+	}else if(!(isFieldFilled(passwordAndConfirmPassword($('#passWord').val(),$('#confrmpsw').val()),"PassWords Does Not Match ,The Field(s)","confPassWordError"))){
+		return !validationPass;
+	}
+	return validationPass;
+
+}
+
+/**
+ * convertPassWordToString() method displays the pass word to text
+ * 
+ * @author anuradha
+ * @param checkboxId
+ *            check box id which used to toggle the command
+ * @param passWordElementId
+ *            password field element id
+ * @param confirmWordElementId
+ *            confirming field element id
+ * @returns void
+ */
+
+function convertPassWordToString(checkboxId, passWordElementId,
+		confirmWordElementId) {
+	var value = ($('#' + checkboxId).is(':checked')) ? "text" : "password";
+	$('#' + passWordElementId).attr("type", value);
+	$('#' + confirmWordElementId).attr("type", value);
+}
+
+/**
+ * clearAllFields willclear all the data input text fields once it'scalled
+ */
+function clearAllFields() {
+
+	$('#showpasscheckbox').prop('checked', false);
+	var value = $('#showpasscheckbox').is(':checked') ? "text" : "password";
+	$('#passWord').attr("type", value);
+	$('#confrmpsw').attr("type", value);
+}
+
+/**
+ * Here the method confirms if both fields contain logically
+ * equal string values.
+ * @author anuradha
+ * @param password password field element id
+ * @param reconfirmPassWord confirming field element id
+ * @returns {Boolean} if the both fields are logically equal, then,
+ * returns true else false.
+ */
+function passwordAndConfirmPassword(password, reconfirmPassWord){
+	var passwordTrimed = password.trim();
+	var isBothValueAreIdentical= false;
+	if(passwordTrimed!=null|passwordTrimed !=""){
+		isBothValueAreIdentical= (passwordTrimed === reconfirmPassWord.trim()) ;
+	}
+	return isBothValueAreIdentical;
 }
