fe9dd7be982cdd0329a36543195b8bde4586c912
Anuradha Silva <anuradha@genesiis.com>
2017-03-14 16:42:58 +0530

    CAM-23 admin login command class created

---
+++ campus.jar/src/com/genesiis/campus/command/CmdAdminLogin.java
@@ -0,0 +1,18 @@
+package com.genesiis.campus.command;
+//20170314 AS c23-admin-login-logout-function-as CmdAdminLogin class created 
+import java.sql.SQLException;
+
+import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.util.IDataHelper;
+
+
+public class CmdAdminLogin implements ICommand{
+
+	@Override
+	public IView execute(IDataHelper helper, IView view) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
