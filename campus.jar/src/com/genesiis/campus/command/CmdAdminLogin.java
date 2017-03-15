c436ff30770f0d262941929c51acfc4e9d361697
Anuradha Silva <anuradha@genesiis.com>
2017-03-15 18:57:06 +0530

    CAM-23 CmdadminLogin command class modification WIP

--- campus.jar/src/com/genesiis/campus/command/CmdAdminLogin.java
+++ campus.jar/src/com/genesiis/campus/command/CmdAdminLogin.java
@@ -1,18 +1,89 @@
 package com.genesiis.campus.command;
 //20170314 AS c23-admin-login-logout-function-as CmdAdminLogin class created 
 import java.sql.SQLException;
+import java.util.Collection;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
 
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.model.Admin;
+import com.genesiis.campus.entity.model.Student;
 import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.validation.SystemMessage;
+import com.google.gson.Gson;
 
 
 public class CmdAdminLogin implements ICommand{
-
+	static Logger log = Logger.getLogger(CmdAdminLogin.class.getName());
+	
+	private Collection<Collection<String>> dataCollection = null;
+	String pageURL;
+	String message;
+	private Admin adminData;
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
-		// TODO Auto-generated method stub
+		log.info("hit here");
+		HttpSession session;
+		try {
+			message = SystemMessage.LOGINUNSUCCESSFULL.message();
+			session = helper.getRequest().getSession(false);
+			String currentSessionUser = (String) session.getAttribute("currentSessionUser");
+			//check the user already logged or not 
+			if (currentSessionUser == null) {
+
+				String gsonData = helper.getParameter("jsonData");
+				adminData = getAdmindetails(gsonData);
+				
+			}else{
+				
+			}
+			
+			
+		} catch (SQLException e) {
+			log.error("execute(IDataHelper helper, IView view):  SQLException"
+					+ e.toString());
+			throw e;
+		} catch (Exception e) {
+			log.error("execute(IDataHelper helper, IView view):  Exception"
+					+ e.toString());
+			throw e;
+		}
+		
 		return null;
+	}
+	
+	
+	
+	/**
+	 * extract data from json object and assign to Admin
+	 * object
+	 * 
+	 * @author anuradha
+	 * @param gsonData
+	 * @return Admin object
+	 */
+
+	private Admin getAdmindetails(String gsonData) {
+		Admin admin = (Admin) extractFromJason(gsonData);
+
+		return admin;
+	}
+
+	public Object extractFromJason(String gsonData) {
+		Gson gson = new Gson();
+		String message = "";
+		Admin admin = null;
+		try {
+			admin = gson.fromJson(gsonData, Admin.class);
+
+		} catch (Exception exception) {
+			log.error("extractFromJason(): " + exception.toString());
+			throw exception;
+		}
+		return admin;
 	}
 
 }
