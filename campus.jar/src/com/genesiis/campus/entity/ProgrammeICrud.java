c5d07d31deb5df5da9ce9055f2502c0db72fa866
Dumani Jayasinghe <dumani@genesiis.com>
2017-03-16 17:19:36 +0530

    c152-add-enhanced-programme-insertion: create method;addIntakeDetails(),addSemesterDetails(),addModuleDetails

--- campus.jar/src/com/genesiis/campus/entity/ProgrammeICrud.java
+++ campus.jar/src/com/genesiis/campus/entity/ProgrammeICrud.java
@@ -3,8 +3,12 @@ package com.genesiis.campus.entity;
 //20170107 c6-list-available-institutes-on-the-view Initiated ProgrammeICrud.java
 //20170108 c6-list-available-institutes-on-the-view Declaring new methods
 //20170202 DJ c138-add-basic-programme-MP-dj Initiate getAllClassTypes() method.
+//20170316 DJ c152-add-enhanced-programme-insertion: create method;addIntakeDetails(),addSemesterDetails(),addModuleDetails
 
+import com.genesiis.campus.entity.model.IntakeDTO;
+import com.genesiis.campus.entity.model.ModuleDTO;
 import com.genesiis.campus.entity.model.ProgrammeDTO;
+import com.genesiis.campus.entity.model.SemesterDTO;
 
 import java.sql.SQLException;
 import java.util.Collection;
@@ -33,6 +37,12 @@ public interface ProgrammeICrud extends ICrud {
 	
 	public Collection<Collection<String>> findLevelsByLevelCodes(Set<Integer> levelCodeSet)throws SQLException,Exception;
 
-	public int addProgrammeDetails(ProgrammeDTO programmeDTO) throws SQLException, Exception;	
+	public int addProgrammeDetails(ProgrammeDTO programmeDTO) throws SQLException, Exception;
+	
+	public int addIntakeDetails(final IntakeDTO intakeDTO) throws SQLException, Exception;
+	
+	public int addSemesterDetails(final SemesterDTO semesterDTO) throws SQLException, Exception;	
+	
+	public int addModuleDetails(final ModuleDTO moduleDTO) throws SQLException, Exception;	
 
 }
