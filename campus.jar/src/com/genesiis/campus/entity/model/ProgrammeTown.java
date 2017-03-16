605dd24a305bfe1eb1a722a1be39f479d04c5cef
Dumani Jayasinghe <dumani@genesiis.com>
2017-03-16 16:31:30 +0530

    c152-add-enhanced-programme-insertion- Initiate ProgrammeTown.java

---
+++ campus.jar/src/com/genesiis/campus/entity/model/ProgrammeTown.java
@@ -0,0 +1,36 @@
+package com.genesiis.campus.entity.model;
+
+//20170316 DJ c152-add-enhanced-programme-insertion- Initiate ProgrammeTown.java
+
+public class ProgrammeTown {
+	
+	private int programmeTownCOde;
+	private int isActive;
+	private int programmeCode;
+	private int townCOde;
+	public int getProgrammeTownCOde() {
+		return programmeTownCOde;
+	}
+	public void setProgrammeTownCOde(int programmeTownCOde) {
+		this.programmeTownCOde = programmeTownCOde;
+	}
+	public int getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(int isActive) {
+		this.isActive = isActive;
+	}
+	public int getProgrammeCode() {
+		return programmeCode;
+	}
+	public void setProgrammeCode(int programmeCode) {
+		this.programmeCode = programmeCode;
+	}
+	public int getTownCOde() {
+		return townCOde;
+	}
+	public void setTownCOde(int townCOde) {
+		this.townCOde = townCOde;
+	}	
+
+}
