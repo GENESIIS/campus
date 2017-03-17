87c96ff5b01f3c3dd324b64efba52854cc2ff847
Anuradha Silva <anuradha@genesiis.com>
2017-03-17 17:08:03 +0530

    CAM-23 admin, super admin and company DEO attributes added toenum

--- campus.jar/src/com/genesiis/campus/validation/UserType.java
+++ campus.jar/src/com/genesiis/campus/validation/UserType.java
@@ -1,7 +1,7 @@
 package com.genesiis.campus.validation;
 
 //20161206 JH c39-add-course-provider UserType.java created
-
+//20170317 AS c23-admin-login-logout-function-as admin, super admin and company DEO attributes added
 /**
 * UserType enum class used to handle default user types of 
 * the application.
@@ -12,6 +12,8 @@ public enum UserType {
 	ADMIN("ADMIN"),
 	FEATURED_COURSE_PROVIDER("FEATURED_COURSE_PROVIDER"),
 	TUTOR("TUTOR"),
+	SUPER_ADMIN("SUPER_ADMIN"),
+	COMPANY_DEO("COMPANY_DEO"),
 	STUDENT("STUDENT");
 	
 	private final String userType;
