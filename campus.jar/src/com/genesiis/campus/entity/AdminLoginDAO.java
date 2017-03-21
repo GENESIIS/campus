37b859df3988b99041e8661b3ad5a301d0daec73
Anuradha Rushmitha <anuradha@genesiis.com>
2017-03-21 07:58:38 +0530

    CAM-23 DAO class coding WIP

--- campus.jar/src/com/genesiis/campus/entity/AdminLoginDAO.java
+++ campus.jar/src/com/genesiis/campus/entity/AdminLoginDAO.java
@@ -1,12 +1,24 @@
 package com.genesiis.campus.entity;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class AdminLoginDAO implements ICrud{
+import org.apache.log4j.Logger;
 
+import com.genesiis.campus.entity.model.Admin;
+import com.genesiis.campus.util.ConnectionManager;
+import com.genesiis.campus.util.security.Encryptable;
+import com.genesiis.campus.util.security.TripleDesEncryptor;
+import com.genesiis.campus.validation.ApplicationStatus;
+import com.genesiis.campus.validation.SystemMessage;
+
+
+public class AdminLoginDAO implements ICrud{
+	static Logger log = Logger.getLogger(AdminLoginDAO.class.getName());
 	@Override
 	public int add(Object object) throws SQLException, Exception {
 		// TODO Auto-generated method stub
@@ -28,10 +40,44 @@ public class AdminLoginDAO implements ICrud{
 	@Override
 	public Collection<Collection<String>> findById(Object code)
 			throws SQLException, Exception {
-		
+		String encryptPassword = null;
 		Collection<Collection<String>> dataCollection = new ArrayList<Collection<String>>();
 		Connection conn = null;
+		PreparedStatement preparedStatement = null;
+		String message = SystemMessage.NOTREGISTERD.message();
+
+		ResultSet rs = null;
+		String query = "SELECT CODE, USERNAME, PASSWORD, EMAIL, ISACTIVE, USERTYPE FROM CAMPUS.ADMIN  WHERE USERNAME=? AND ISACTIVE = ? OR EMAIL =? AND ISACTIVE = ? "; 
 		
+		try {
+		final Admin admin = (Admin) code;
+			Encryptable passwordEncryptor = new TripleDesEncryptor(admin
+					.getPassword().trim());
+			encryptPassword = passwordEncryptor.encryptSensitiveDataToString()
+					.trim();
+
+			conn = ConnectionManager.getConnection();
+			preparedStatement = conn.prepareStatement(query);
+			preparedStatement.setString(1, admin.getUsername());
+			preparedStatement
+			.setString(2, Integer.toString(ApplicationStatus.ACTIVE
+					.getStatusValue()));
+			preparedStatement.setString(3, admin.getEmail());
+			preparedStatement
+					.setString(4, Integer.toString(ApplicationStatus.ACTIVE
+							.getStatusValue()));
+			rs = preparedStatement.executeQuery();
+			boolean check = rs.next();
+			
+		}catch (SQLException exception) {
+				log.error("findById(Object code):  SQLexception"
+						+ exception.toString());
+				throw exception;
+			} catch (Exception exception) {
+				log.error("findById(Object code):  exception"
+						+ exception.toString());
+				throw exception;
+			}
 		
 		return null;
 	}
