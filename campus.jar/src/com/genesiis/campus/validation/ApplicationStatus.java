e22394242ea72a228ca7714ef16782a48c6db090
dushanthacb <dushantha@genesiis.com>
2017-03-23 17:04:34 +0530

    CAM-83: updated the older version with newly added enum values

--- campus.jar/src/com/genesiis/campus/validation/ApplicationStatus.java
+++ campus.jar/src/com/genesiis/campus/validation/ApplicationStatus.java
@@ -1,7 +1,9 @@
 package com.genesiis.campus.validation;
 
 //20161124 JH c7-higher-education-landing-page-MP ApplicationStatus.java created
+//20170103 JH changed the value of INACTIVE to '0' to support isActive field values.
 //20170309 DN c81-admin-manage-banner-add-and-view-banner-dn add a new enum UNDEFINED to the list
+//20170323 DN c83-admin-manage-banner-update-banner-info-dn changed the literal value of UNDEFINED to -1
 
 /**
  * ApplicationStatus enum created to manage all the status values
@@ -10,13 +12,12 @@ package com.genesiis.campus.validation;
  *
  */
 public enum ApplicationStatus {
-
-	UNDEFINED(0),
-	ACTIVE(1),
-	INACTIVE(2),
-	PENDING(3),
-	EXPIRED(4);	
 	
+	UNDEFINED(-1),
+	INACTIVE(0),
+	ACTIVE(1),
+	PENDING(2),
+	EXPIRED(3);	
 	
 	private final int statusValue;
 
