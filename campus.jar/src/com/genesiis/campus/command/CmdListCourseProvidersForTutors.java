d4578f0980793b7ddd23dc73d89ec43834e676e7
chinthakacw <chinthaka@genesiis.com>
2017-03-23 12:17:05 +0530

    c157-add-tutor-employment-details-cw INIT CmdListCourseProvidersForTutors.java

---
+++ campus.jar/src/com/genesiis/campus/command/CmdListCourseProvidersForTutors.java
@@ -0,0 +1,18 @@
+package com.genesiis.campus.command;
+
+//20170323 CW c157-add-tutor-employment-details-cw INIT CmdListCourseProvidersForTutors.java
+
+import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.util.IDataHelper;
+
+import java.sql.SQLException;
+
+public class CmdListCourseProvidersForTutors implements ICommand  {
+
+	@Override
+	public IView execute(IDataHelper helper, IView view) throws SQLException, Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
