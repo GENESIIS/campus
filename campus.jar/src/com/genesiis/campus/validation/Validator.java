fd6670e14d9e6bf1696fef7718e2fb99917b04a2
chinthakacw <chinthaka@genesiis.com>
2017-03-24 14:37:12 +0530

    c37-tutor-update-tutor-profile-cw modify OLD_NEW_PASSWORD_SAME to CURRENT_NEW_PASSWORD_SAME in Validator.java

--- campus.jar/src/com/genesiis/campus/validation/Validator.java
+++ campus.jar/src/com/genesiis/campus/validation/Validator.java
@@ -27,6 +27,7 @@ package com.genesiis.campus.validation;
 				// Modified validateTutorFields method to work with isValidNetworkCode & isValidContactCode methods
 //20170321 CW c37-tutor-update-tutor-profile-cw modified validateTutorFields() to validate townDetails & towncodeOld correctly
 //20170324 CW c37-tutor-update-tutor-profile-cw modified isValidPassword method & fix password length validation error
+//20170324 CW c37-tutor-update-tutor-profile-cw modify OLD_NEW_PASSWORD_SAME to CURRENT_NEW_PASSWORD_SAME
 
 import com.genesiis.campus.entity.TutorDAO;
 import com.genesiis.campus.util.IDataHelper;
@@ -524,8 +525,8 @@ public class Validator {
 						}
 					}else{						
 						// old password & new password are same
-						helper.setAttribute("oldPasswordError", SystemMessage.OLD_NEW_PASSWORD_SAME.message());
-						helper.setAttribute("newPasswordError", SystemMessage.OLD_NEW_PASSWORD_SAME.message());
+						helper.setAttribute("oldPasswordError", SystemMessage.CURRENT_NEW_PASSWORD_SAME.message());
+						helper.setAttribute("newPasswordError", SystemMessage.CURRENT_NEW_PASSWORD_SAME.message());
 						isValid = false;
 					}
 				}else{
