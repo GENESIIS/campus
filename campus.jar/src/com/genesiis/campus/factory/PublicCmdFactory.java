86d0d04892a34bcaf459d4d5645afbf402b54d3d
jayani1991 <jayani@genesiis.com>
2017-03-27 00:25:50 +0530

    c95-list-tutors-ui-add-ddl-changes INIT bracnh

--- campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
+++ campus.jar/src/com/genesiis/campus/factory/PublicCmdFactory.java
@@ -5,17 +5,14 @@ package com.genesiis.campus.factory;
 //20170302 JH c96-public-list-all-tutors added case PUBLIC_LIST_ALL_TUTORS
 //20170306 JH c96-public-list-all-tutors added map for PUBLIC_LIST_ALL_TUTORS
 
-import com.genesiis.campus.command.CmdPublicListTutors;
 import com.genesiis.campus.command.ICommand;
 import com.genesiis.campus.validation.Operation;
-import com.genesiis.campus.command.CmdListCategories;
 
 public class PublicCmdFactory implements ICmdFactory {
 
 	private ICommand command = null;
 	static {	
-	map.put(Operation.LIST_CATEGORY_DATA, new CmdListCategories());
-	map.put(Operation.PUBLIC_LIST_ALL_TUTORS, new CmdPublicListTutors());
+
 	}
 
 	@Override
@@ -23,12 +20,6 @@ public class PublicCmdFactory implements ICmdFactory {
 		Operation o = Operation.BAD_OPERATION;
 		o = Operation.getOperation(cco);
 		switch (o) {
-		case LIST_CATEGORY_DATA:
-			command = map.get(o);
-			break;
-		case PUBLIC_LIST_ALL_TUTORS:
-			command = map.get(o);
-			break;
 		default:
 			break;
 		}
