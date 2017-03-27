79ea2a0fe91e7dc3b2801709aeda92a827238491
chinthakacw <chinthaka@genesiis.com>
2017-03-27 12:47:02 +0530

    c157-add-tutor-employment-details-cw add method comments & modified getCourseProviderData method data filling item name in experience.js

--- campus.war/web/dist/js/tutor/experience.js
+++ campus.war/web/dist/js/tutor/experience.js
@@ -2,12 +2,17 @@
  * 20170323 CW c157-add-tutor-employment-details-cw created experience.js file to work on tutor related experience details
  * 				//add displayCourseProviders, getCourseProviderData & ready functions
  * 20170326 CW c157-add-tutor-employment-details-cw modified getCourseProviderData method to view course provider list correctly
+ * 20170327 CW c157-add-tutor-employment-details-cw add method comments & modified getCourseProviderData method data filling item name
  */
 
 $(document).ready(function() {
 	displayCourseProviders();
 });
 
+/**
+ * This method used to query featured course provider details from the database
+ * @author CW
+ */
 function displayCourseProviders() {
 	
 	$.ajax({
@@ -25,8 +30,13 @@ function displayCourseProviders() {
 	});
 }
 
+/**
+ * This method used to fill list item from the data
+ * @author CW
+ * @param response
+ */
 function getCourseProviderData(response) {
-	var categories = $("#employeeDetails");
+	var categories = $("#employerDetails");
 	categories.find('option').remove();
 	$('<option>').val("-1").text("--- Select employee ---").appendTo(categories);
 	
@@ -34,8 +44,8 @@ function getCourseProviderData(response) {
 		var res = value.toString();
 		var data = res.split(",");
 		if(data[3].toString() != 'Default'){
-			var x = data[0].toString();
-			var y = data[3].toString();
+			var x = data[0].toString(); // code
+			var y = data[3].toString(); // name
 			$('<option>').val(x).text(y).appendTo(categories);
 		}
 	});
