2818e4648f7983dbffbc832a12bbac473c864f17
chinthakacw <chinthaka@genesiis.com>
2017-03-27 16:21:46 +0530

    c157-add-tutor-employment-details-cw modified execute method to add employment data to the database & created setEmploymentDetails method in CmdAddTutorEmploymentDetails.java

--- campus.jar/src/com/genesiis/campus/command/CmdAddTutorEmploymentDetails.java
+++ campus.jar/src/com/genesiis/campus/command/CmdAddTutorEmploymentDetails.java
@@ -1,11 +1,16 @@
 package com.genesiis.campus.command;
 
 //20170327 CW c157-add-tutor-employment-details-cw INIT CmdAddTutorEmploymentDetails.java
+//20170327 CW c157-add-tutor-employment-details-cw modified execute method to add employment data to the database
+				// created setEmploymentDetails method
 
+import com.genesiis.campus.entity.EmploymentDAO;
 import com.genesiis.campus.entity.FeaturedCourseProviderDAO;
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.model.Employment;
 import com.genesiis.campus.entity.model.Tutor;
 import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.validation.ApplicationStatus;
 
 import org.apache.log4j.Logger;
 
@@ -20,7 +25,7 @@ import java.util.Collection;
  */
 public class CmdAddTutorEmploymentDetails implements ICommand {
 
-	static Logger log = Logger.getLogger(CmdListCourseProvidersForTutors.class.getName());
+	static Logger log = Logger.getLogger(CmdAddTutorEmploymentDetails.class.getName());
 	private String message = "";
 
 	/**
@@ -32,9 +37,15 @@ public class CmdAddTutorEmploymentDetails implements ICommand {
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException, Exception {
 
-		try {
-			log.info(" tutor code = " + helper.getParameter("code"));
-			log.info(" tutor employerDetails = " + helper.getParameter("employerDetails"));
+		try {			
+			Employment employmentDetails = new Employment();
+			
+			if(helper.getParameter("tutorCode") != null && helper.getParameter("employerCode") != null){
+				employmentDetails = setEmploymentDetails(helper.getParameter("tutorCode"), helper.getParameter("employerCode"));
+			}
+			
+			EmploymentDAO addEmployment = new EmploymentDAO();
+			int status = addEmployment.add(employmentDetails);			
 			
 			final FeaturedCourseProviderDAO featuredCourseProviders = new FeaturedCourseProviderDAO();
 			final Tutor tutor = new Tutor();
@@ -52,4 +63,27 @@ public class CmdAddTutorEmploymentDetails implements ICommand {
 		}
 		return view;
 	}
+	
+	
+	/**
+	 * This method is created to populate data into the Employment object
+	 * @param tutorCode
+	 * @param employerCode
+	 * @return Employment object
+	 */
+	private Employment setEmploymentDetails(String tutorCode, String employerCode){
+		
+		Employment employment = new Employment();
+		
+		if(tutorCode != null && employerCode != null){
+			employment.setVarificationstatus(ApplicationStatus.PENDING.getStatusValue());
+			employment.setCrtby("chinthaka");
+			employment.setModby("-");
+			employment.setTutor(Integer.parseInt(tutorCode));
+			employment.setCourseprovider(Integer.parseInt(employerCode));
+		}
+			
+		return employment;
+	}
+	
 }
