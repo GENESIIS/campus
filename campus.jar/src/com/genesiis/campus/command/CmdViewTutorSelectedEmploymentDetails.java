7cca4729fef87bff1e97df41d35cdbc2762870e9
chinthakacw <chinthaka@genesiis.com>
2017-03-28 13:23:56 +0530

    c157-add-tutor-employment-details-cw Init CmdViewTutorSelectedEmploymentDetails Class

---
+++ campus.jar/src/com/genesiis/campus/command/CmdViewTutorSelectedEmploymentDetails.java
@@ -0,0 +1,38 @@
+package com.genesiis.campus.command;
+
+//20170328 CW c157-add-tutor-employment-details-cw Init CmdViewTutorSelectedEmploymentDetails Class
+
+import com.genesiis.campus.entity.FeaturedCourseProviderDAO;
+import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.validation.Validator;
+
+import org.apache.log4j.Logger;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CmdViewTutorSelectedEmploymentDetails implements ICommand {
+	
+	static Logger log = Logger.getLogger(CmdAddTutorEmploymentDetails.class.getName());
+
+	@Override
+	public IView execute(IDataHelper helper, IView view) throws SQLException, Exception {
+
+		try {			
+			String tutorCode = helper.getParameter("tutorCode");
+			
+			if(Validator.isNotEmpty(tutorCode)){
+				Collection<Collection<String>> allFeaturedCourseProviderList = new ArrayList<Collection<String>>();
+				allFeaturedCourseProviderList = FeaturedCourseProviderDAO.getTutorSelectedFCP(tutorCode);
+				
+				view.setCollection(allFeaturedCourseProviderList);			
+			}
+		} catch (Exception exception) {
+			log.error("execute() : Exception" + exception.toString());
+			throw exception;
+		}
+		return view;
+	}
+}
