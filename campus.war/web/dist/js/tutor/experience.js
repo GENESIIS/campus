eca59e6e7805dca94df89e04f26e58e93d7cfffe
chinthakacw <chinthaka@genesiis.com>
2017-03-28 16:56:26 +0530

    c157-add-tutor-employment-details-cw add fillSelectedCourseProviderData & clearField methods in experience.js

--- campus.war/web/dist/js/tutor/experience.js
+++ campus.war/web/dist/js/tutor/experience.js
@@ -5,6 +5,7 @@
  * 20170327 CW c157-add-tutor-employment-details-cw add method comments & modified getCourseProviderData method data filling item name
  * 20170328 CW c157-add-tutor-employment-details-cw add method displaySelectedCourseProviders
  * 20170328 CW c157-add-tutor-employment-details-cw modified method displaySelectedCourseProviders to fix errors wip
+ * 20170328 CW c157-add-tutor-employment-details-cw add fillSelectedCourseProviderData & clearField methods
  */
 
 $(document).ready(function() {
@@ -68,12 +69,56 @@ function displaySelectedCourseProviders() {
 			CCO : 'LIST_SELECTED_COURSE_PROVIDERS_FOR_TUTORS',
 			tutorCode : tutorCode
 		},
-		dataType : "jsp",
+		dataType : "json",
 		success : function(response) {
-			resp = response;
+			fillSelectedCourseProviderData(response);
 		},
 		error : function(response) {
 			alert("Error: " + response);
 		}
 	});
+}
+
+/**
+ * This method used to fill list item from the data
+ * @author CW
+ * @param response
+ */
+function fillSelectedCourseProviderData(response) {
+
+    var trHTML = '';
+        
+    $.each(response.result, function (index, value) {
+/*		var res = value.toString();
+		var data = res.split(",");*/
+        
+        trHTML += '<tr><td>' + index + '</td><td>' + value[1].toString() + '</td><td>' + value[2].toString() + '</td><td>' + value[4].toString() + '</td><td>' + value[5].toString() + '</td><td>' + <input type="checkbox" name="isApproved" value="1" checked disabled readonly>'</td></tr>';
+    });
+    
+    $('#employers').append(trHTML);
+    /*
+	<th>Short Name</th>
+	<th>Name</th>
+	<th>Speciality</th>
+	<th>Address</th>
+    */
+    
+/*	var categories = $("#employerDetails");
+	categories.find('option').remove();
+	$('<option>').val("-1").text("--- Select employee ---").appendTo(categories);
+	
+	$.each(response.result, function(index, value) {
+		var res = value.toString();
+		var data = res.split(",");
+		if(data[3].toString() != 'Default'){
+			var x = data[0].toString(); // code
+			var y = data[3].toString(); // name
+			$('<option>').val(x).text(y).appendTo(categories);
+		}
+	});*/
+}
+
+function clearField(elementId) {
+	$(document).find('#' + elementId).text('');
+	$(document).find('#message').text('');
 }
