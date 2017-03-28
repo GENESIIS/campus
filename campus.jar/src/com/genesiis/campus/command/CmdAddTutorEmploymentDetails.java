ebe226278ce1e022e8d2d12f29ff3a9f43d027c0
chinthakacw <chinthaka@genesiis.com>
2017-03-28 10:48:31 +0530

    c157-add-tutor-employment-details-cw modified execute method to call getTutorSelectedFCP & get the tutor selected course provider list in CmdAddTutorEmploymentDetails.java

--- campus.jar/src/com/genesiis/campus/command/CmdAddTutorEmploymentDetails.java
+++ campus.jar/src/com/genesiis/campus/command/CmdAddTutorEmploymentDetails.java
@@ -4,6 +4,8 @@ package com.genesiis.campus.command;
 //20170327 CW c157-add-tutor-employment-details-cw modified execute method to add employment data to the database
 				// created setEmploymentDetails method
 //20170327 CW c157-add-tutor-employment-details-cw modified execute method & fix data saving error
+//20170328 CW c157-add-tutor-employment-details-cw modified execute method to call getTutorSelectedFCP & get the tutor selected course provider list
+
 
 import com.genesiis.campus.entity.EmploymentDAO;
 import com.genesiis.campus.entity.FeaturedCourseProviderDAO;
@@ -49,12 +51,9 @@ public class CmdAddTutorEmploymentDetails implements ICommand {
 							
 				EmploymentDAO addEmployment = new EmploymentDAO();
 				int status = addEmployment.add(employmentDetails);			
-				
-				final FeaturedCourseProviderDAO featuredCourseProviders = new FeaturedCourseProviderDAO();
-				final Tutor tutor = new Tutor();
+
 				Collection<Collection<String>> allFeaturedCourseProviderList = new ArrayList<Collection<String>>();
-				
-				allFeaturedCourseProviderList = featuredCourseProviders.getAll();
+				allFeaturedCourseProviderList = FeaturedCourseProviderDAO.getTutorSelectedFCP(tutorCode);
 				
 				view.setCollection(allFeaturedCourseProviderList);			
 			}
