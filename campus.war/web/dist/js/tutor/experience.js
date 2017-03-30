84c37578dd0d440c46e30dd2f6552ddfcefa952a
chinthakacw <chinthaka@genesiis.com>
2017-03-30 13:39:19 +0530

    c157-add-tutor-employment-details-cw modified fillSelectedCourseProviderData method & make dynamic field names in experience.js

--- campus.war/web/dist/js/tutor/experience.js
+++ campus.war/web/dist/js/tutor/experience.js
@@ -10,6 +10,7 @@
  * 20170329 CW c157-add-tutor-employment-details-cw modified & done testing on fillSelectedCourseProviderData method to add button to the table
  * 20170329 CW c157-add-tutor-employment-details-cw modified the button of the table
  * 20170329 CW c157-add-tutor-employment-details-cw modified fillSelectedCourseProviderData method & remove the button & add a check box into the table
+ * 20170330 CW c157-add-tutor-employment-details-cw modified fillSelectedCourseProviderData method & make dynamic field names
  */
 
 $(document).ready(function() {
@@ -91,68 +92,24 @@ function displaySelectedCourseProviders() {
 function fillSelectedCourseProviderData(response) {
 
     var trHTML = '';
+    var maxIndex = 0;
         
     $.each(response.result, function (index, value) {
-/*		var res = value.toString();
-		var data = res.split(",");*/
-        
-/*        trHTML += '<tr><td>' + index + '</td><td>' + value[1].toString() + '</td><td>' 
-        		+ value[2].toString() + '</td><td>' + value[4].toString() + '</td><td>' + value[5].toString() + '</td><td>'
-        		+ '<input id=' + Button + ' type=' + button + ' value='+' class=' + plusButton + ' /></td></tr>';*/
     	
+    	maxIndex = index;
     	var sequence = index + 1;
-    	/*
         trHTML += '<tr><td>' + sequence + '</td><td>' + value[1].toString() + '</td><td>' 
 		+ value[2].toString() + '</td><td>' + value[4].toString() + '</td><td>' + value[5].toString() + '</td><td>'
-		+ '<input id= Button type= button value= Remove class= pure-button pure-button-primary /></td></tr>';*/
-        
-/*
-        trHTML += '<tr><td>' + sequence + '</td><td>' + value[1].toString() + '</td><td>' 
-		+ value[2].toString() + '</td><td>' + value[4].toString() + '</td><td>' + value[5].toString() + '</td><td>'
-		+ '<input type=checkbox name=isApproved value=1/></td><td>'
-		+ '<input type=hidden name=tutorCode id=tutorCode value=' + value[6].toString() + '/>'
-		+ '<input type=hidden name=employerCode id=employerCode value=' + value[0].toString() + '/>'
-		+ '<button type=submit name=CCO id=CCO value=REMOVE_EMPLOYMENT class=pure-button pure-button-primary>Remove</button></td></tr>';*/
-    	
-
-        trHTML += '<tr><td>' + sequence + '</td><td>' + value[1].toString() + '</td><td>' 
-		+ value[2].toString() + '</td><td>' + value[4].toString() + '</td><td>' + value[5].toString() + '</td><td>'
-		+ '<input type=hidden name=tutorCode id=tutorCode value=' + value[6].toString() + '/>'
-		+ '<input type=hidden name=employerCode id=employerCode value=' + value[0].toString() + '/>'
-		+ '<input type=checkbox name=isApproved value=1/></td></tr>';
+		+ '<input type=hidden name=sequence id=sequence value=' + sequence + '>'
+		+ '<input type=hidden name=tutorCode'+sequence+' id=tutorCode'+sequence+'  value=' + value[6].toString() + '>'
+		+ '<input type=hidden name=employerCode'+sequence+' id=employerCode'+sequence+' value=' + value[0].toString() + '>'
+		+ '<input type=checkbox id=isSelected'+sequence+' name=isSelected'+sequence+' value=1></td></tr>';
         
     });
     
-    $('#employers').append(trHTML);
+    $('#employers').append(trHTML);	    
+    document.getElementById('maxSequence').value = maxIndex;
     
-    
-/*
- <input type="checkbox" name="isApproved" value="1" checked disabled readonly>
-  
-	<input id="tutorCode" name="tutorCode" value="<c:out value="${companies[6]}"/>" hidden="true" />
-	<input id="employerCode" name="employerCode" value="<c:out value="${companies[0]}"/>" hidden="true" />
-	<button type="submit" name="CCO" id="CCO" value="REMOVE_EMPLOYMENT" class="pure-button pure-button-primary">Remove</button>
-*/    
-    /*
-	<th>Short Name</th>
-	<th>Name</th>
-	<th>Speciality</th>
-	<th>Address</th>
-    */
-    
-/*	var categories = $("#employerDetails");
-	categories.find('option').remove();
-	$('<option>').val("-1").text("--- Select employee ---").appendTo(categories);
-	
-	$.each(response.result, function(index, value) {
-		var res = value.toString();
-		var data = res.split(",");
-		if(data[3].toString() != 'Default'){
-			var x = data[0].toString(); // code
-			var y = data[3].toString(); // name
-			$('<option>').val(x).text(y).appendTo(categories);
-		}
-	});*/
 }
 
 function clearField(elementId) {
