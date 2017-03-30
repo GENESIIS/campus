e27a40f471d1984878140586da831985943d5f14
chinthakacw <chinthaka@genesiis.com>
2017-03-30 13:38:53 +0530

    c157-add-tutor-employment-details-cw modified execute method & add all the selected row tutor Codes & Course provider codes into allSelectedListToRemove collection in CmdRemoveSelectedEmploymentDetails.java

--- campus.jar/src/com/genesiis/campus/command/CmdRemoveSelectedEmploymentDetails.java
+++ campus.jar/src/com/genesiis/campus/command/CmdRemoveSelectedEmploymentDetails.java
@@ -1,6 +1,7 @@
 package com.genesiis.campus.command;
 
 //20170329 CW c157-add-tutor-employment-details-cw INIT CmdRemoveSelectedEmploymentDetails.java
+//20170330 CW c157-add-tutor-employment-details-cw modified execute method & add all the selected row tutor Codes & Course provider codes into allSelectedListToRemove collection
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -12,12 +13,12 @@ import com.genesiis.campus.entity.FeaturedCourseProviderDAO;
 import com.genesiis.campus.entity.IView;
 import com.genesiis.campus.entity.model.Tutor;
 import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.validation.Validator;
 
 /**
  * this class will remove all the tutor selected course provider details which were added by the tutor earlier, 
  * further it implements ICommand interface
  * @author CW
- *
  */
 public class CmdRemoveSelectedEmploymentDetails implements ICommand  {
 
@@ -35,9 +36,24 @@ public class CmdRemoveSelectedEmploymentDetails implements ICommand  {
 
 		try {
 			
-			String tutorCode = helper.getParameter("tutorCode");
-			String employerCode = helper.getParameter("employerCode");
+			final Collection<Collection<String>> allSelectedListToRemove = new ArrayList<Collection<String>>();
 			
+			String sequence = helper.getParameter("maxSequence");
+			
+			if(Validator.isNotEmpty(sequence)){
+				int maxIndex = Integer.parseInt(sequence);				
+				
+				for(int i = 1; i <= maxIndex+1; i++){
+					final Collection<String> singleSelectedListToRemove = new ArrayList<String>();
+
+					if(Validator.isNotEmpty(helper.getParameter("isSelected"+i)) && helper.getParameter("isSelected"+i).equals("1")){
+						singleSelectedListToRemove.add(helper.getParameter("tutorCode"+i));
+						singleSelectedListToRemove.add(helper.getParameter("employerCode"+i));
+						allSelectedListToRemove.add(singleSelectedListToRemove);						
+					}
+				}
+			}
+
 			final FeaturedCourseProviderDAO featuredCourseProviders = new FeaturedCourseProviderDAO();
 			final Tutor tutor = new Tutor();
 			Collection<Collection<String>> allFeaturedCourseProviderList = new ArrayList<Collection<String>>();
