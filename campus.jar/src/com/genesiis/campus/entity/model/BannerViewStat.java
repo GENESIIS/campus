13adfd77de74926204ef7cc5fc4596555ef674c4
Miyuru-Genesiis <miyuru@genesiis.com>
2017-03-31 19:22:01 +0530

    c117-display-banners-record-viewcount-back-end Modified fields and getters/setters to reflect the changes made to the BANNERVIEWSTAT table

--- campus.jar/src/com/genesiis/campus/entity/model/BannerViewStat.java
+++ campus.jar/src/com/genesiis/campus/entity/model/BannerViewStat.java
@@ -1,4 +1,5 @@
 // 20170330 MM c117-display-banners-record-viewcount-back-end INIT class added properties and getters/setters
+// 20170331 MM c117-display-banners-record-viewcount-back-end Modified fields and getters/setters to reflect the changes made to the BANNERVIEWSTAT table
 
 package com.genesiis.campus.entity.model;
 
@@ -6,68 +7,75 @@ import java.sql.Date;
 
 public class BannerViewStat {
 	private int code;
-	private String callerPage;
-	private java.sql.Date viewDate;
-	private java.sql.Time viewTime;
 	private int banner;
+	private int viewCount;
+	private String callerPage;
+	private java.sql.Date lastViewDate;
+	private java.sql.Time lastViewTime;
 	private Date crtOn;
 	private String crtBy;
 	private Date modOn;
 	private String modBy;
-	
 	public int getCode() {
 		return code;
-	}
-	public void setCode(int code) {
-		this.code = code;
-	}
-	public String getCallerPage() {
-		return callerPage;
-	}
-	public void setCallerPage(String callerPage) {
-		this.callerPage = callerPage;
-	}
-	public java.sql.Date getViewDate() {
-		return viewDate;
-	}
-	public void setViewDate(java.sql.Date viewDate) {
-		this.viewDate = viewDate;
-	}
-	public java.sql.Time getViewTime() {
-		return viewTime;
-	}
-	public void setViewTime(java.sql.Time viewTime) {
-		this.viewTime = viewTime;
 	}
 	public int getBanner() {
 		return banner;
 	}
-	public void setBanner(int banner) {
-		this.banner = banner;
+	public int getViewCount() {
+		return viewCount;
+	}
+	public String getCallerPage() {
+		return callerPage;
+	}
+	public java.sql.Date getLastViewDate() {
+		return lastViewDate;
+	}
+	public java.sql.Time getLastViewTime() {
+		return lastViewTime;
 	}
 	public Date getCrtOn() {
 		return crtOn;
 	}
-	public void setCrtOn(Date crtOn) {
-		this.crtOn = crtOn;
-	}
 	public String getCrtBy() {
 		return crtBy;
-	}
-	public void setCrtBy(String crtBy) {
-		this.crtBy = crtBy;
 	}
 	public Date getModOn() {
 		return modOn;
 	}
-	public void setModOn(Date modOn) {
-		this.modOn = modOn;
-	}
 	public String getModBy() {
 		return modBy;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public void setBanner(int banner) {
+		this.banner = banner;
+	}
+	public void setViewCount(int viewCount) {
+		this.viewCount = viewCount;
+	}
+	public void setCallerPage(String callerPage) {
+		this.callerPage = callerPage;
+	}
+	public void setLastViewDate(java.sql.Date lastViewDate) {
+		this.lastViewDate = lastViewDate;
+	}
+	public void setLastViewTime(java.sql.Time lastViewTime) {
+		this.lastViewTime = lastViewTime;
+	}
+	public void setCrtOn(Date crtOn) {
+		this.crtOn = crtOn;
+	}
+	public void setCrtBy(String crtBy) {
+		this.crtBy = crtBy;
+	}
+	public void setModOn(Date modOn) {
+		this.modOn = modOn;
 	}
 	public void setModBy(String modBy) {
 		this.modBy = modBy;
 	}
 
+	
 }
