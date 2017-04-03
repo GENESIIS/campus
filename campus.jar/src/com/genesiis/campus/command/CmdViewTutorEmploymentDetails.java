f33f8cbb55ae2a84c0b6ddd8e6aa4168938ca512
chinthakacw <chinthaka@genesiis.com>
2017-04-03 16:59:09 +0530

    c157-add-tutor-employment-details-cw re-order Import Statements

--- campus.jar/src/com/genesiis/campus/command/CmdViewTutorEmploymentDetails.java
+++ campus.jar/src/com/genesiis/campus/command/CmdViewTutorEmploymentDetails.java
@@ -4,14 +4,11 @@ package com.genesiis.campus.command;
 //20170331 CW c157-add-tutor-employment-details-cw modified execute method to get the list of details from getTutorSelectedFCP
 //20170402 CW c157-add-tutor-employment-details-cw modified execute method & add validations to allFeaturedCourseProviderList
 //20170403 CW c157-add-tutor-employment-details-cw modified singleTutorEmploymentViewCollection filling with tutor code
+//20170403 CW c157-add-tutor-employment-details-cw re-order Import Statements
 
 import com.genesiis.campus.entity.FeaturedCourseProviderDAO;
 import com.genesiis.campus.entity.IView;
-import com.genesiis.campus.entity.TutorDAO;
-import com.genesiis.campus.entity.model.Tutor;
 import com.genesiis.campus.util.IDataHelper;
-import com.genesiis.campus.validation.SystemMessage;
-import com.genesiis.campus.validation.Validator;
 
 import org.apache.log4j.Logger;
 
