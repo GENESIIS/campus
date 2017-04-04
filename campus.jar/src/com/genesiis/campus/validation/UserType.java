4fbf0cf9b77e1b7f92c2f3ca0de820602cab4c14
Anuradha Silva <anuradha@genesiis.com>
2017-04-04 17:07:12 +0530

    CAM-23 userTypes Defualt value added and getDefaultValue() method created

--- campus.jar/src/com/genesiis/campus/validation/UserType.java
+++ campus.jar/src/com/genesiis/campus/validation/UserType.java
@@ -2,6 +2,7 @@ package com.genesiis.campus.validation;
 
 //20161206 JH c39-add-course-provider UserType.java created
 //20170317 AS c23-admin-login-logout-function-as admin, super admin and company DEO attributes added
+//20170404 AS c23-admin-login-logout-function-as added admin,tutor, course provider default values and getDefaultValue()
 /**
 * UserType enum class used to handle default user types of 
 * the application.
@@ -9,17 +10,21 @@ package com.genesiis.campus.validation;
 
 public enum UserType {
 
-	ADMIN("ADMIN"),
-	FEATURED_COURSE_PROVIDER("FEATURED_COURSE_PROVIDER"),
-	TUTOR("TUTOR"),
-	SUPER_ADMIN("SUPER_ADMIN"),
-	COMPANY_DEO("COMPANY_DEO"),
-	STUDENT("STUDENT");
+	ADMIN("ADMIN","-1"),
+	FEATURED_COURSE_PROVIDER("FEATURED_COURSE_PROVIDER","-1"),
+	TUTOR("TUTOR","-1"),
+	SUPER_ADMIN("SUPER_ADMIN","-1"),
+	COMPANY_DEO("COMPANY_DEO",""),
+	STUDENT("STUDENT","");
 	
 	private final String userType;
+	private  String defaultValue;
 	
-	private UserType(final String userType) {
+	
+
+	private UserType(final String userType, String defaultValue) {
 		this.userType = userType;
+		this.defaultValue = defaultValue;
 	}
 
 	/**
@@ -27,6 +32,14 @@ public enum UserType {
 	 */
 	public String getUserType() {
 		return userType;
+	}
+	
+	/**
+	 * @return the userType default value
+	 */
+	
+	public String getDefaultValue() {
+		return defaultValue;
 	}
 	
 }
