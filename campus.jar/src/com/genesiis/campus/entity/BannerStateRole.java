280d3daf7b9661fb28445dd2f10122fa08af6bcf
dushanthacb <dushantha@genesiis.com>
2017-04-18 16:56:57 +0530

    CAM-86:The initial class BannerStateRole.java is created

---
+++ campus.jar/src/com/genesiis/campus/entity/BannerStateRole.java
@@ -0,0 +1,7 @@
+package com.genesiis.campus.entity;
+/*
+ * 20170418 DN c86-admin-manage-banner-search-banner-dn. The initial class BannerStateRole.java is created
+ */
+public class BannerStateRole {
+
+}
