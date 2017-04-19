e4d63647856b32f3182c28b86b883e80400fb661
dushanthacb <dushantha@genesiis.com>
2017-04-19 15:23:26 +0530

    CAM-86:The method execute() has been implemented.

--- campus.jar/src/com/genesiis/campus/command/CmdAmendBannerState.java
+++ campus.jar/src/com/genesiis/campus/command/CmdAmendBannerState.java
@@ -1,6 +1,7 @@
 package com.genesiis.campus.command;
 /*
  * 20170418 DN c86-admin-manage-banner-search-banner-dn Created the initial class CmdAmendBannerState.java
+ * 20170419 DN c86-admin-manage-banner-search-banner-dn. The method execute() has been implemented.
  */
 
 import com.genesiis.campus.entity.BannerStateRole;
@@ -22,14 +23,40 @@ public class CmdAmendBannerState implements ICommand {
 
 	private static final Logger log = Logger.getLogger(CmdAmendBannerState.class.getName());
 	private BannerStateRole stateModifier= null;
+	
 	@Override
-	public IView execute(IDataHelper helper, IView view) throws SQLException,
-			Exception {
+	public IView execute(IDataHelper helper, IView view) throws Exception {
+		if(this.getStateModifier() == null)
+			this.setStateModifier(new BannerStateRole(helper));
 		
+		try{
+			getStateModifier().updateBannerStatus();
+	
+		} catch (Exception exp) {
+			log.error("execute(IDataHelper,IView) :Exception "+ exp.toString());
+			throw exp;
+		}
 		
-		
-		
-		return null;
+		return view;
+	}
+
+	/**
+	 * getStateModifier() method returns the internal
+	 * BannerStateRole instance
+	 * @author dushantha DN
+	 * @return BannerStateRole 
+	 */
+	public BannerStateRole getStateModifier() {
+		return stateModifier;
+	}
+
+	/**
+	 * Method sets the BannerStateRole
+	 * @author dushantha DN
+	 * @param stateModifier
+	 */
+	public void setStateModifier(BannerStateRole stateModifier) {
+		this.stateModifier = stateModifier;
 	}
 
 }
