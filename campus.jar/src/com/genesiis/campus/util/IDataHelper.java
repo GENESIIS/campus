9821397c37f963e89cfc1f73060149a52659650a
chinthakacw <chinthaka@genesiis.com>
2017-04-20 10:59:17 +0530

    c158-send-email-tutor-employment-confirmation-cw add getUrl, getUri methods in IDataHelper.java

--- campus.jar/src/com/genesiis/campus/util/IDataHelper.java
+++ campus.jar/src/com/genesiis/campus/util/IDataHelper.java
@@ -2,6 +2,8 @@ package com.genesiis.campus.util;
 
 //20161024 DN c10-contacting-us-page created the initial version of the IDataHelper.java
 //20161107 DN, JH, DJ, AS, CM, MM Added getAttribute(String) method
+//20170420 CW  c158-send-email-tutor-employment-confirmation-cw add getUrl, getUri methods 
+
 import javax.servlet.http.HttpSession;
 
 import com.genesiis.campus.entity.IView;
@@ -33,5 +35,9 @@ public interface IDataHelper {
 	String getRedirectPage();
 
 	void setRedirectPage(String pageName);
+	
+	public String getUrl();
+	
+	public String getUri();
 
 }
