be6f05633060597a7f8b4774772d1313735e9dce
chinthakacw <chinthaka@genesiis.com>
2017-04-20 16:53:23 +0530

    comment changes in tutorList.js

--- campus.war/web/dist/js/courseprovider/tutorList.js
+++ campus.war/web/dist/js/courseprovider/tutorList.js
@@ -1,3 +1,3 @@
 /**
- * 20170420 CW c159-courseprovider-accept-tutor-request-cw created tutorList.js file to work on tutor related details
+ * 20170420 CW c159-courseprovider-accept-tutor-request-cw created tutorList.js file to work on tutor related details for course providers
  */
