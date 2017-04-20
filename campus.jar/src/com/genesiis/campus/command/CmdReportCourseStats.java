3c5b4640e4be113d30ff5943d18408429c88623c
Dumani Jayasinghe <dumani@genesiis.com>
2017-04-20 13:20:12 +0530

    c54-report-course-stats-MP-dj create:method generateCourseStatsView().

--- campus.jar/src/com/genesiis/campus/command/CmdReportCourseStats.java
+++ campus.jar/src/com/genesiis/campus/command/CmdReportCourseStats.java
@@ -1,11 +1,19 @@
 package com.genesiis.campus.command;
 
-/*20170418 c54-report-course-stats-MP-dj created CmdReportCourseStats*/
+/*20170418 c54-report-course-stats-MP-dj created CmdReportCourseStats
+ *20170420 c54-report-course-stats-MP-dj create:method generateCourseStatsView().
+ * 
+ * */
 
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.dao.CourseProviderDAOImpl;
 import com.genesiis.campus.entity.dao.DistrictDAOImpl;
+import com.genesiis.campus.entity.model.CourseProviderSearchDTO;
 import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.validation.ApplicationStatus;
 import com.genesiis.campus.validation.Operation;
+
+import org.apache.log4j.Logger;
 
 import java.sql.SQLException;
 import java.util.Collection;
@@ -15,7 +23,7 @@ import java.util.Collection;
  * @author dumani DJ   
  */
 public class CmdReportCourseStats implements ICommand{
-
+	static Logger log = Logger.getLogger(CmdReportCourseStats.class.getName());
 	@Override
 	public IView execute(IDataHelper helper, IView iView) throws SQLException,
 			Exception {
@@ -23,7 +31,11 @@ public class CmdReportCourseStats implements ICommand{
 			String cco = helper.getParameter("CCO");
 			
 			switch (Operation.getOperation(cco)) {
-			case REPORT_COURSE_STATS:				
+			case SEARCH_VIEW_COURSE_STATS:
+				generateCourseStatsView(helper);
+				break;			
+			case REPORT_COURSE_STATS:	
+				//generateReportResults(helper);
 				break;			
 			default:
 				break;
@@ -33,6 +45,24 @@ public class CmdReportCourseStats implements ICommand{
 			throw exception;
 		}
 		return iView;
+	}
+	
+	
+	/** Retrieve course providers for drop down list in search view.
+	 * @author dumani DJ
+	 * @param helper -IDataHelper object
+	 * @throws Exception
+	 */
+	private void generateCourseStatsView(IDataHelper helper)throws Exception {
+		try {
+			final CourseProviderSearchDTO providerSearchDTO = new CourseProviderSearchDTO();
+			providerSearchDTO.setCourseProviderStatus(ApplicationStatus.ACTIVE.getStatusValue());
+			final Collection<Collection<String>> courseProviderList = new CourseProviderDAOImpl().getLightAllCourseProviders(providerSearchDTO);
+			helper.setAttribute("courseProviderList", courseProviderList);
+		} catch (Exception exception) {
+			log.error("generateCourseStatsView() : Exception " + exception);
+			throw exception;
+		}
 	}
 
 }
