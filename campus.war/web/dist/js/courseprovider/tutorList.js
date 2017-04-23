794a482aa78f3806f0f79852be5717418ca2e099
chinthakacw <chinthaka@genesiis.com>
2017-04-23 14:51:27 +0530

    c159-courseprovider-accept-tutor-request-cw created ready, displayTutorList, populateTuterData functions in tutorList.js

--- campus.war/web/dist/js/courseprovider/tutorList.js
+++ campus.war/web/dist/js/courseprovider/tutorList.js
@@ -1,3 +1,34 @@
 /**
  * 20170420 CW c159-courseprovider-accept-tutor-request-cw created tutorList.js file to work on tutor related details for course providers
+ * 20170423 CW c159-courseprovider-accept-tutor-request-cw created ready, displayTutorList, populateTuterData functions
  */
+
+$(document).ready(function() {
+    cpCode = request.getParameter("courseProviderCode");
+	displayTutorList(cpCode);  
+});
+
+/**
+ * This method used to query featured course provider details from the database
+ * @author CW
+ */
+function displayTutorList(cpCode) {
+	$.ajax({
+		url : '/TutorController',
+		data : {
+			CCO : 'VIEW_TUTORS_FOR_CP',
+			courseProviderCode : cpCode
+		},
+		dataType : "json",
+		success : function(response) {
+			populateTuterData(response);
+		},
+		error : function(response) {
+			alert("Error: " + response);
+		}
+	});
+}
+
+function populateTuterData(response){
+	
+}
