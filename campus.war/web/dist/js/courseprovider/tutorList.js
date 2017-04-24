13a57ea884985f77cc0a957132f1845896201118
chinthakacw <chinthaka@genesiis.com>
2017-04-24 14:28:51 +0530

    c159-courseprovider-accept-tutor-request-cw complete ready & displayTutorList methods to send url parameters in tutorList.js

--- campus.war/web/dist/js/courseprovider/tutorList.js
+++ campus.war/web/dist/js/courseprovider/tutorList.js
@@ -1,10 +1,12 @@
 /**
  * 20170420 CW c159-courseprovider-accept-tutor-request-cw created tutorList.js file to work on tutor related details for course providers
  * 20170423 CW c159-courseprovider-accept-tutor-request-cw created ready, displayTutorList, populateTuterData functions
+ * 20170424 CW c159-courseprovider-accept-tutor-request-cw complete ready & displayTutorList methods to send url parameters
  */
 
 $(document).ready(function() {
-    cpCode = request.getParameter("courseProviderCode");
+
+	var cpCode = $("#cpCode").val();
 	displayTutorList(cpCode);  
 });
 
@@ -17,7 +19,7 @@ function displayTutorList(cpCode) {
 		url : '/TutorController',
 		data : {
 			CCO : 'VIEW_TUTORS_FOR_CP',
-			courseProviderCode : cpCode
+			cpCode : cpCode
 		},
 		dataType : "json",
 		success : function(response) {
