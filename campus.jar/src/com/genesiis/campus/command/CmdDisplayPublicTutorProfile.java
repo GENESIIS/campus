a216ff9b506979d3123f5d1cda7d9a0c366d182c
jayani1991 <jayani@genesiis.com>
2017-04-24 11:17:41 +0530

    c135-public-display-tutor-profile INIT CmdDisplayPublicTutorProfile.java to load tutor details for the public profile

---
+++ campus.jar/src/com/genesiis/campus/command/CmdDisplayPublicTutorProfile.java
@@ -0,0 +1,19 @@
+package com.genesiis.campus.command;
+
+//20170424 JH c135-public-display-tutor-profile INIT CmdDisplayPublicTutorProfile.java
+
+import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.util.IDataHelper;
+
+import java.sql.SQLException;
+
+public class CmdDisplayPublicTutorProfile implements ICommand{
+
+	@Override
+	public IView execute(IDataHelper helper, IView view) throws SQLException,
+			Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
