a453382d050ca358c73103f04b89e22388e8e0c2
jayani1991 <jayani@genesiis.com>
2017-04-24 15:03:32 +0530

    c135-public-display-tutor-profile INIT CmdDisplayPublicTutorProfile.java command class and execute method coding wip

--- campus.jar/src/com/genesiis/campus/command/CmdDisplayPublicTutorProfile.java
+++ campus.jar/src/com/genesiis/campus/command/CmdDisplayPublicTutorProfile.java
@@ -2,17 +2,53 @@ package com.genesiis.campus.command;
 
 //20170424 JH c135-public-display-tutor-profile INIT CmdDisplayPublicTutorProfile.java
 
+import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
+import com.genesiis.campus.entity.PublicTutorDAO;
 import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.validation.SystemConfig;
+import com.genesiis.campus.validation.SystemMessage;
+import com.genesiis.campus.validation.Validator;
+
+import org.apache.log4j.Logger;
 
 import java.sql.SQLException;
+import java.util.Collection;
 
 public class CmdDisplayPublicTutorProfile implements ICommand{
+	
+	static Logger log = Logger.getLogger(CmdDisplayPublicTutorProfile.class.getName());
 
 	@Override
 	public IView execute(IDataHelper helper, IView view) throws SQLException,
 			Exception {
-		// TODO Auto-generated method stub
+
+		final ICrud publicTutorDAO = new PublicTutorDAO();
+		SystemMessage message = SystemMessage.UNKNOWN;
+		
+		try{
+			if(Validator.validNumber(helper.getParameter("tutorCode"))){
+				Collection<Collection<String>> tutorCollection = publicTutorDAO.findById(helper.getParameter("tutorCode"));
+				
+				
+				if(tutorCollection.size() > 0){
+					log.info(">>>>>>>>>>>>>>not empty");
+				}
+			}
+
+		}catch (SQLException sqlException) {
+			log.error("execute() SQLException : " + sqlException.toString());
+			throw sqlException;
+		} catch (Exception exception){
+			log.error("execute() Exception : "+ exception.toString());
+			throw exception;
+		}finally{
+			final String tutorImagePath = SystemConfig.TUTOR_PROFILE_IMAGE_PATH.getValue1();
+			helper.setAttribute("userMessage", message.message());
+			helper.setAttribute("tutorProfileImagePath", tutorImagePath);
+		}
+		
+		
 		return null;
 	}
 
