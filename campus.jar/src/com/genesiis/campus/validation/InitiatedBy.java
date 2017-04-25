a9fc87d502333128f20312eb503bea425db4adaf
chinthakacw <chinthaka@genesiis.com>
2017-04-25 15:01:49 +0530

    c159-courseprovider-accept-tutor-request-cw - Initialised InitiatedBy enum

--- campus.jar/src/com/genesiis/campus/validation/InitiatedBy.java
+++ campus.jar/src/com/genesiis/campus/validation/InitiatedBy.java
@@ -1,6 +1,6 @@
 package com.genesiis.campus.validation;
 
-//20170425 CW Initialised InitiatedBy enum
+//20170425 CW c159-courseprovider-accept-tutor-request-cw - Initialised InitiatedBy enum 
 
 public enum InitiatedBy {
 	TUTOR,
