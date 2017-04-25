3809032a04c3f118122b5df7718c87d009ff8794
dushanthacb <dushantha@genesiis.com>
2017-04-25 17:26:41 +0530

    CAM-88:The method getAddAdvertiserPageOnLoadData() changed to include AllPurposeDAO class, when calls to Database.

--- campus.jar/src/com/genesiis/campus/entity/AddAdvertiserPrerequisiteData.java
+++ campus.jar/src/com/genesiis/campus/entity/AddAdvertiserPrerequisiteData.java
@@ -3,17 +3,24 @@ package com.genesiis.campus.entity;
  * 20170424 DN c88-admin-manage-advertiser-add-new-advertiser-dn. created the initial 
  * 				class deceleration of AddAdvertiserPrerequisiteData.java.
  * 			execute() modified to retrieve all the registered active course providers.
+ * 20170425 DN c88-admin-manage-advertiser-add-new-advertiser-dn. The method getAddAdvertiserPageOnLoadData() changed to 
+ *             include AllPurposeDAO class, when calls to Database.
  */
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import com.genesiis.campus.util.ConnectionManager;
+import com.genesiis.campus.util.DaoHelper;
 import com.genesiis.campus.util.IDataHelper;
+import com.genesiis.campus.validation.ApplicationStatus;
 import com.genesiis.campus.validation.Operation;
 
 import org.apache.log4j.Logger; 
-
 
 /**
  * The Class AddAdvertiserPrerequiaiteData.
@@ -62,7 +69,50 @@ public class AddAdvertiserPrerequisiteData {
 					ICrud country2Dao = new Country2DAO();
 					preRequisteCollnWrapper = country2Dao.getAll();
 					// retrieve all the registered active advertisers.
-					ICrud registeredActivatedAdverTiser = new AdvertiserDAO();
+					ICrud registeredActivatedAdverTiser =new AllPurposeDAO(){
+						
+						/**
+						 * getAll() method bears the responsibility of extracting all the course providers
+						 * who are featured and status been 1
+						 * 
+						 */
+						@Override
+						public Collection<Collection<String>> getAll() throws SQLException,
+								Exception {
+							// Extract all the course providers who are featured providers and status is one
+									StringBuilder CourseProvidersSQL =new StringBuilder("SELECT [CODE] ,[SHORTNAME]  FROM [CAMPUS].[COURSEPROVIDER]");
+									CourseProvidersSQL.append("WHERE [COURSEPROVIDERSTATUS] = "+ApplicationStatus.ACTIVE.getStatusValue());		
+									String allCourseProviderSQL = CourseProvidersSQL.toString();
+									Connection courseProviderConnection =null;
+									PreparedStatement preparedStatement = null;
+									ResultSet resultSet = null;
+									
+									try{
+										courseProviderConnection =  ConnectionManager.getConnection();
+										preparedStatement =courseProviderConnection.prepareStatement(allCourseProviderSQL);
+										resultSet = preparedStatement.executeQuery();
+										final Collection<Collection<String>> outerWrapper = new ArrayList<Collection<String>>();
+										
+										while(resultSet.next()){
+											final ArrayList<String>courseProviderRecord = new ArrayList<String>();
+											courseProviderRecord.add(resultSet.getString("CODE"));
+											courseProviderRecord.add(resultSet.getString("SHORTNAME"));
+											outerWrapper.add(courseProviderRecord);
+										}
+										
+										return outerWrapper;
+										
+									}catch (SQLException sqle){
+										log.error("getAll() :SQLException"+sqle.toString());
+										throw sqle;
+									} catch (Exception exp) {
+										log.error("getAll() :Exception"+exp.toString());
+										throw exp;
+									} finally{
+										DaoHelper.cleanup(courseProviderConnection, preparedStatement, resultSet);
+									}
+						}
+					};
 					helper.setAttribute("advertisers", registeredActivatedAdverTiser.getAll());
 					
 					this.setSuccessCode(1);
@@ -94,13 +144,97 @@ public class AddAdvertiserPrerequisiteData {
 		}
 		return preRequisteCollnWrapper;
 	}
-
+	
+	/**
+	 * Method returns the course provider informations bound to<br>
+	 * the request courseProvider code which is passed in as type String.
+	 *
+	 * @param courseProviderCode the course provider code :String
+	 * @return Collection &lt;Collection&lt;String&gt;&gt: contains the details of the Course Provider.<br>
+	 * If the returning Collection of collection of string becomes length 1<br>
+	 * i.e it contains only one element which is Collection&lt;String&gt;.<br>
+	 * size of which reflects the number of columns which has been extracted.<br>
+	 * If that collection (the latter one) is zero length, then there are no records for the selected course provider code
+	 * @author dushantha DN
+	 * @throws Exception
+	 */
+	public Collection<Collection<String>> getCourseProviderboundToSelectedCode(String courseProviderCode) throws Exception {
+		
+		Collection<Collection<String>> adviserCredentials = new ArrayList<Collection<String>>();
+		try{
+			
+			adviserCredentials = new AllPurposeDAO(){				
+				public Collection<Collection<String>> findById(Object object) throws SQLException, Exception {
+					
+					Collection<Collection<String>> outerWrapper = new ArrayList<Collection<String>>();					
+					if(!(object instanceof String)){
+						throw new IllegalArgumentException("CourseProvider Code is not Type String..Check for null ");//return an empty collection of collection.
+					}
+					
+			Connection conn = null;
+			PreparedStatement prepare = null;
+			ResultSet result = null;
+			String sqlSelect = "SELECT * FROM [CAMPUS].COURSEPROVIDER WHERE Code = ?;";
+			try{
+				conn = ConnectionManager.getConnection();
+				prepare = conn.prepareStatement(sqlSelect);
+				prepare.setInt(1, Integer.parseInt((String)object));
+				result = prepare.executeQuery();
+				while(result.next()){
+					Collection <String> innerWrapper = new ArrayList<String>();
+					innerWrapper.add(result.getString("CODE")); 
+					innerWrapper.add(result.getString("SHORTNAME"));
+					innerWrapper.add(result.getString("GENERALEMAIL")); 
+					innerWrapper.add(result.getString("LANDPHONECOUNTRYCODE"));
+					innerWrapper.add(result.getString("LANDPHONEAREACODE"));
+					innerWrapper.add(result.getString("LANDPHONENO"));					
+					innerWrapper.add(result.getString("MOBILEPHONECOUNTRYCODE")); 
+					innerWrapper.add(result.getString("MOBILEPHONENETWORKCODE"));
+					innerWrapper.add(result.getString("MOBILEPHONENO")); 
+					innerWrapper.add(result.getString("DESCRIPTION"));
+					innerWrapper.add(result.getString("ADDRESS1"));
+					innerWrapper.add(result.getString("ADDRESS2"));
+					innerWrapper.add(result.getString("ADDRESS3"));					
+					innerWrapper.add(result.getString("COURSEPROVIDERSTATUS"));
+					//town is missing; 
+					outerWrapper.add(innerWrapper);
+				}
+				return outerWrapper;
+			} catch (SQLException sqle){
+				log.error("findById(Object): SQLException "+ sqle.toString());
+				throw sqle;				
+			} catch (NumberFormatException nfexp){
+				log.error("findById(Object) : NumberFormatException "+nfexp.toString());
+				throw nfexp;
+			} catch (Exception exp){
+				log.error("findById(Object) : Exception "+exp.toString());
+				throw exp;
+			} finally {
+				DaoHelper.cleanup(conn, prepare, result);
+				sqlSelect =null;				
+			}
+				} 
+			}.findById(courseProviderCode);
+			this.setSuccessCode(1);
+			
+		}catch (SQLException sqle){
+			log.error("getAdvertisersBoundToCourseProviderCode():SQLException"+sqle.toString());
+			throw sqle;
+		} catch (Exception exp){
+			log.error("getAdvertisersBoundToCourseProviderCode() : Exception"+exp.toString());
+			throw exp;			
+		} finally{
+			this.setResponseCridentials(helper);;
+		}
+		return adviserCredentials;
+	}
+	
 	/**
 	 * Method sets the response credentials, It sets the successfulness or the failure code,
-	 * and the message to be dispatched to the view to the response as attributes
+	 * and the message to be dispatched to the view to the response as attributes.
 	 * @author dushantha DN
 	 * setResponseCridentials sets the request attributes
-	 * @param helper: It is the HttpServletrequest wrapper instance.
+	 * @param helper the new response cridentials
 	 */
 	private void setResponseCridentials(IDataHelper helper){
 		helper.setAttribute("successCode", getSuccessCode());
@@ -140,6 +274,24 @@ public class AddAdvertiserPrerequisiteData {
 	 */
 	public void setMessage(String message) {
 		this.message = message;
+	}
+
+
+	/**
+	 * Gets the helper.	 
+	 * @return the helper
+	 */
+	public IDataHelper getHelper() {
+		return helper;
+	}
+
+
+	/**
+	 * Sets the helper.
+	 * @param helper the new helper
+	 */
+	public void setHelper(IDataHelper helper) {
+		this.helper = helper;
 	}
 	
 	
