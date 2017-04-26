31bdbf5271c3ac14cb2ca2b2121e44f00909405f
chinthakacw <chinthaka@genesiis.com>
2017-04-26 20:05:36 +0530

    c159-courseprovider-accept-tutor-request-cw add getApplicationStatus method in ApplicationStatus.java

--- campus.jar/src/com/genesiis/campus/validation/ApplicationStatus.java
+++ campus.jar/src/com/genesiis/campus/validation/ApplicationStatus.java
@@ -5,6 +5,7 @@ package com.genesiis.campus.validation;
 //20161221 DJ c51-report-courses-by-course-provider-MP-dj Implement generic method getApplicationStatus(int statusValue)
 //20170403 CW c157-add-tutor-employment-details-cw copied the ApplicationStatus from c51-report-courses-by-course-provider-MP-dj issue
 //20170425 CW c159-courseprovider-accept-tutor-request-cw command DELETED added
+//20170426 CW c159-courseprovider-accept-tutor-request-cw add getApplicationStatus method
 
 /**
 * ApplicationStatus enum created to manage all the status values
@@ -31,8 +32,38 @@ public enum ApplicationStatus {
 	 */
 	public int getStatusValue() {
 		return statusValue;
-	}
+	}		
+	
+	/**
+	 * Generic method for map the status.Able to call from whole campus application
+	 * @author DJ
+	 * @param statusValue selected status Value string
+	 * @return integer applicationStatus
+	 * @throws Exception
+	 */
+	public static int getApplicationStatus(String statusValue){
+		int applicationStatus=0; 
+		if (Validator.isNotEmpty(statusValue)) {
+			if (statusValue.equalsIgnoreCase("ACTIVE")) {
+				applicationStatus = ACTIVE.getStatusValue();
+			}
+			if (statusValue.equalsIgnoreCase("INACTIVE")) {
+				applicationStatus = INACTIVE.getStatusValue();
+			}
+			if (statusValue.equalsIgnoreCase("PENDING")) {
+				applicationStatus = PENDING.getStatusValue();
+			}
+			if (statusValue.equalsIgnoreCase("EXPIRED")) {
+				applicationStatus = EXPIRED.getStatusValue();
+			}
+			if (statusValue.equalsIgnoreCase("DELETED")) {
+				applicationStatus = DELETED.getStatusValue();
+			}
+		}
 		
+		return applicationStatus;
+	}
+	
 	/**
 	 * Generic method for map the status int value with String.Purpose of status view in front end.Able to call from whole campus application
 	 * @author DJ
