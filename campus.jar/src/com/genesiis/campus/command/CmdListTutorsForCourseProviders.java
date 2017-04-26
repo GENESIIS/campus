0d60cc7488372a89f2f423c0e0000a6314915088
chinthakacw <chinthaka@genesiis.com>
2017-04-26 20:04:58 +0530

    c159-courseprovider-accept-tutor-request-cw unit testing & applicationStatusMap data filling error fixing in CmdListTutorsForCourseProviders.java

--- campus.jar/src/com/genesiis/campus/command/CmdListTutorsForCourseProviders.java
+++ campus.jar/src/com/genesiis/campus/command/CmdListTutorsForCourseProviders.java
@@ -5,6 +5,8 @@ package com.genesiis.campus.command;
 //20170421 CW c159-courseprovider-accept-tutor-request-cw add tutorsForCP variable into execute method
 //20170424 CW c159-courseprovider-accept-tutor-request-cw modified execute method to set values of courseProviderCode from cpCode
 //20170425 CW c159-courseprovider-accept-tutor-request-cw add applicationStatusMap
+//20170425 CW c159-courseprovider-accept-tutor-request-cw add applicationStatusMap errors fixing
+//20170426 CW c159-courseprovider-accept-tutor-request-cw unit testing & applicationStatusMap data filling error fixing
 
 import com.genesiis.campus.entity.FeaturedCourseProviderDAO;
 import com.genesiis.campus.entity.IView;
@@ -13,12 +15,14 @@ import com.genesiis.campus.util.IDataHelper;
 import com.genesiis.campus.validation.AccountType;
 import com.genesiis.campus.validation.ApplicationStatus;
 import com.genesiis.campus.validation.Validator;
+import com.sun.javafx.collections.MappingChange.Map;
 
 import org.apache.log4j.Logger;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 
 /**
  * This class returns the list of tutors for given Course Provider 
@@ -48,13 +52,14 @@ public class CmdListTutorsForCourseProviders implements ICommand  {
 				Collection<Collection<String>> allEmploymentTutorsList = tutorsForCP.getTutorsListOfCourseprovider(courseProviderCode);
 				
 
-				Map<Integer, String> applicationStatusMap = new HashMap<Integer, String>();
+				HashMap<Integer, String> applicationStatusMap = new HashMap<Integer, String>();
 				
 				for (ApplicationStatus statusValues : ApplicationStatus.values()) {
-					applicationStatusMap.put(ApplicationStatus.getValue(statusValues). , statusValues);
+					System.out.println(statusValues);
+					applicationStatusMap.put(ApplicationStatus.getApplicationStatus(statusValues.toString()), statusValues.toString());
 				}
-				
-				helper.setAttribute("enumList", someMap);
+
+				helper.setAttribute("applicationStatusMap", applicationStatusMap);
 				
 				view.setCollection(allEmploymentTutorsList);		
 			}else{
