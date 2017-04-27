4138e1caf46e511c28117395fa060bf265fcf623
jayani1991 <jayani@genesiis.com>
2017-04-27 16:48:13 +0530

    c135-public-display-tutor-profile created command class method getTutorDetails() method to get tutor basic details from the tutor collection

--- campus.jar/src/com/genesiis/campus/command/CmdDisplayPublicTutorProfile.java
+++ campus.jar/src/com/genesiis/campus/command/CmdDisplayPublicTutorProfile.java
@@ -1,6 +1,7 @@
 package com.genesiis.campus.command;
 
 //20170424 JH c135-public-display-tutor-profile INIT CmdDisplayPublicTutorProfile.java
+//20170427 JH c135-public-display-tutor-profile created getTutorDetails() method to get tutor basic details from the tutor collection 
 
 import com.genesiis.campus.entity.ICrud;
 import com.genesiis.campus.entity.IView;
@@ -13,7 +14,10 @@ import com.genesiis.campus.validation.Validator;
 import org.apache.log4j.Logger;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
 
 public class CmdDisplayPublicTutorProfile implements ICommand{
 	
@@ -25,6 +29,7 @@ public class CmdDisplayPublicTutorProfile implements ICommand{
 
 		final ICrud publicTutorDAO = new PublicTutorDAO();
 		SystemMessage message = SystemMessage.UNKNOWN;
+		ArrayList<String> tutor = null;
 		
 		try{
 			if(Validator.validNumber(helper.getParameter("tutorCode"))){
@@ -32,6 +37,7 @@ public class CmdDisplayPublicTutorProfile implements ICommand{
 				
 				
 				if(tutorCollection.size() > 0){
+					tutor = (ArrayList<String>) getTutorDetails(tutorCollection);
 				}
 			}
 
@@ -45,10 +51,73 @@ public class CmdDisplayPublicTutorProfile implements ICommand{
 			final String tutorImagePath = SystemConfig.TUTOR_PROFILE_IMAGE_PATH.getValue1();
 			helper.setAttribute("userMessage", message.message());
 			helper.setAttribute("tutorProfileImagePath", tutorImagePath);
+			helper.setAttribute("tutor", tutor);
 		}
 		
 		
 		return null;
+	}
+	
+	
+	
+	public static Collection<String> getTutorDetails(Collection<Collection<String>> tutorCollection){
+		
+		Iterator tutorIterator = tutorCollection.iterator();
+		ArrayList<String> tutor = new ArrayList<String>();
+		
+		int count = 0;
+		
+		while(tutorIterator.hasNext()){
+			
+			count++;
+			ArrayList<String> tutorList = (ArrayList<String>) tutorIterator.next();
+			
+			if(count == 1){
+				tutor.add(0, tutorList.get(0));
+				tutor.add(1, tutorList.get(1));
+				tutor.add(2, tutorList.get(2));
+				tutor.add(3, tutorList.get(3));
+				tutor.add(4, tutorList.get(4));
+				tutor.add(5, tutorList.get(5));
+				tutor.add(6, tutorList.get(6));
+				tutor.add(7, tutorList.get(7));
+				tutor.add(8, tutorList.get(8));
+				tutor.add(9, tutorList.get(9));
+				tutor.add(10, tutorList.get(10));
+				tutor.add(11, tutorList.get(11));
+				tutor.add(12, tutorList.get(12));
+				tutor.add(13, tutorList.get(13));
+				tutor.add(14, tutorList.get(14));
+				tutor.add(15, tutorList.get(15));
+				tutor.add(16, tutorList.get(16));
+				tutor.add(17, tutorList.get(17));
+				tutor.add(18, tutorList.get(18));
+				tutor.add(19, tutorList.get(19));
+				tutor.add(20, tutorList.get(20));
+				tutor.add(21, tutorList.get(21));
+				tutor.add(22, tutorList.get(22));
+				tutor.add(23, tutorList.get(23));
+				tutor.add(24, tutorList.get(24));
+				tutor.add(25, tutorList.get(25));
+				tutor.add(26, tutorList.get(26));
+				tutor.add(27, tutorList.get(27));
+				tutor.add(28, tutorList.get(28));
+				tutor.add(29, tutorList.get(29));
+				tutor.add(30, tutorList.get(30));
+				tutor.add(31, tutorList.get(31));
+				tutor.add(32, tutorList.get(32));
+				tutor.add(33, tutorList.get(33));
+				tutor.add(34, tutorList.get(34));
+				tutor.add(35, tutorList.get(35));
+							
+				
+			}
+			
+			
+		}
+		
+		log.info(tutor.toString());
+		return tutor;
 	}
 
 }
