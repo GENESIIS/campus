42a082bdb65c901cb24f611e3c6bd896a6bb94d4
jayani1991 <jayani@genesiis.com>
2017-04-27 16:49:40 +0530

    c135-public-display-tutor-profile javascript file tutor-profile-helper.js created, added loadTutor() method to get tutor profile details wip

---
+++ campus.war/web/dist/js/public/tutor-profile-helper.js
@@ -0,0 +1,48 @@
+/**
+ * 20170427 JH c135-public-display-tutor-profile tutor-profile-helper.js created, added loadTutor() method
+ * 
+ */
+
+window.onload = function() {
+	
+};
+
+function loadTutor(code){
+	
+	$.ajax({
+		url : '/PublicController',
+		method : 'POST',
+		data : {
+			'CCO' : 'DISPLAY_PUBLIC_TUTOR_PROFILE',
+			'tutorCode' : code
+		},
+		dataType : "json",
+		async : false,
+		success : function(response) {
+
+			if (response !== undefined && response !== null) {
+				 var tutorRecord = response.tutor;
+				 
+				 $('#fullName').val(tutorRecord[3] + tutorRecord[4] + tutorRecord[5]);
+			}
+
+//			if (response !== undefined && response !== null) {
+//				window.tutorList = response.result;
+//				window.tutorProfileImagePath = response.tutorProfileImagePath;
+//				window.majorList =  response.majorMap;
+//				window.categoryList = response.categoryMap;
+//				window.qualificationList = response.qualificationMap;
+//
+//				DisplayTutorTable();
+//
+//			}
+			
+			alert("sdfjkldsf");
+		},
+		error : function(x, status, error) {
+			var err = displayErrorMessage(x, status, error);
+			document.getElementById("userMessage").style.display = "block";
+			$("#userMessage").html(err);
+		}
+	});
+}
