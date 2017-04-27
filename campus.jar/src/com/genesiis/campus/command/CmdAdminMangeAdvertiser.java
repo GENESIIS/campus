8480cb5fc52e2a091881d0626dcc5b22feeb5be3
dushanthacb <dushantha@genesiis.com>
2017-04-27 16:00:11 +0530

    CAM-88:getters and setters are implemented.try and catch block is implemented for the execute().

--- campus.jar/src/com/genesiis/campus/command/CmdAdminMangeAdvertiser.java
+++ campus.jar/src/com/genesiis/campus/command/CmdAdminMangeAdvertiser.java
@@ -1,6 +1,8 @@
 package com.genesiis.campus.command;
 /*
  * 20170426 DN c88-admin-manage-advertiser-add-new-advertiser-dn. The Class  CmdAdminMangeAdvertiser.java has been created.
+ * 20170427 DN c88-admin-manage-advertiser-add-new-advertiser-dn. getters and setters are implemented.
+ * 				try and catch block is implemented for the execute().
  */
 import java.sql.SQLException;
 
@@ -11,24 +13,48 @@ import com.genesiis.campus.util.IDataHelper;
 
 import org.apache.log4j.Logger;
 
+
 /**
- * The CmdAdminMangeAdvertiser class
- * 
+ * The CmdAdminMangeAdvertiser class.
  * @author dushantha
- *
  */
 
 public class CmdAdminMangeAdvertiser implements ICommand {
 
+	
 	private static Logger log = Logger.getLogger(CmdAdminMangeAdvertiser.class.getName());
 	private AdvertiserFacilitator advertiser =null;
+	
+	/* 
+	 * @see com.genesiis.campus.command.ICommand#execute(com.genesiis.campus.util.IDataHelper, com.genesiis.campus.entity.IView)
+	 */
 	@Override
-	public IView execute(IDataHelper helper, IView view) throws SQLException,
-			Exception {
-		
-		
-		
-		return null;
+	public IView execute(IDataHelper helper, IView view) throws Exception {
+		try{
+			setAdvertiser(new AdvertiserFacilitator(helper));
+			view.setCollection(advertiser.processAdvertiser());
+			
+		} catch (Exception exp){
+			log.error("execute() : Exception: "+ exp.toString());
+			throw exp;
+		}
+		return view;
+	}
+	
+	/**
+	 * Gets the advertiser.
+	 * @return the advertiser
+	 */
+	public AdvertiserFacilitator getAdvertiser() {
+		return advertiser;
+	}
+	
+	/**
+	 * Sets the advertiser.
+	 * @param advertiser the new advertiser
+	 */
+	public void setAdvertiser(AdvertiserFacilitator advertiser) {
+		this.advertiser = advertiser;
 	}
 
 }
