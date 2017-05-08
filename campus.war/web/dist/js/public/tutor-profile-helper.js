5c344aee5be943e5223d6cf0932f6e91965493b6
jayani1991 <jayani@genesiis.com>
2017-05-08 16:19:53 +0530

    c162-public-display-tutor-full-profile displayQualifications(), displayEmployment(), displayExperiences() created and display tutor data completed, displayModules() created

--- campus.war/web/dist/js/public/tutor-profile-helper.js
+++ campus.war/web/dist/js/public/tutor-profile-helper.js
@@ -6,7 +6,7 @@
  * 20170502 JH c135-public-display-tutor-profile fixed default image error, handled empty values in social media information and check optional 
  * 				address lines before concatenating them into a final string
  * 20170505 JH c135-public-display-tutor-profile check if the tutor details are empty before displaying details
- * 20170508 JH c162-public-display-tutor-full-profile display tutor qualifications wip
+ * 20170508 JH c162-public-display-tutor-full-profile displayQualifications(), displayEmployment(), displayExperiences() created and display tutor data completed, displayModules() created
  */
 
 $( document ).ready(function() {
@@ -127,20 +127,33 @@ function loadTutor(code){
 					 
 					 /* display tutor qualfications */
 					 var tutorQualification = response.qualifications;
-					 alert(tutorQualification);
 					 if (tutorQualification !== undefined && tutorQualification !== null && tutorQualification.length > 0) {
-						 alert(tutorQualification);
 						 
-						 var qualificationDiv = $('#qualificationDiv');
-						 $.each( tutorQualification, function(index, value){
-							 
-							 qualificationDiv += '<h5>'+ value[1]+'</h5>';
-							 qualificationDiv += '<span class="fa fa-bookmark" aria-hidden="true"></span>';
-							 qualificationDiv +='<span class="edu-level">' +  + value[3]+ +'</span>';
-							 qualificationDiv +='	<p>' + value[2]+'</p>';
-						 });
-						 $('#qualificationDiv').html(qualificationDiv);
+						 displayQualifications(tutorQualification);
 					 }
+					 
+					 
+					 /* display tutor experiences */
+					 var tutorExperiences = response.experiences;
+					 if(tutorExperiences !== undefined && tutorExperiences !== null && tutorExperiences.length >0){
+						 displayExperiences(tutorExperiences);
+
+					 }
+					 
+					 /* display tutor employment */
+					 var tutorEmployment = response.employments;
+					 if(tutorEmployment !== undefined && tutorEmployment !== null && tutorEmployment.length >0){
+						 displayEmployment(tutorEmployment);
+
+					 }
+					 
+					 /* display tutor related modules */
+					 var tutorModules = response.modules;
+					 if(tutorModules !== undefined && tutorModules !== null && tutorModules.length >0){
+						 displayModules(tutorModules);
+
+					 }
+					 
 					 
 				 }else{ // if no tutor details are found, redirect the list tutor page
 					  var url = '/dist/partials/public/display-tutors.jsp';
@@ -176,4 +189,64 @@ function hashDecode(data) {
 	var decodedString = atob(data);
 
 	return decodedString;
+}
+
+
+function displayQualifications(tutorQualification){
+	 
+	 var qualificationDiv = '';
+	 $.each( tutorQualification, function(index, value){
+		 
+		 qualificationDiv += '<div class="edu-content repeat-content col-xs-12">';
+		 qualificationDiv += '<h5>'+ value[1]+'</h5>';
+		 qualificationDiv += '<span class="fa fa-bookmark" aria-hidden="true"></span>';
+		 qualificationDiv += '<span class="edu-level">' +  value[3] +'</span>';
+		 qualificationDiv += '<p>' + value[2]+'</p>';
+		 qualificationDiv += '</div>';
+	 });
+	 $('#qualificationDiv').html(qualificationDiv);
+}
+
+
+function displayExperiences(tutorExperiences){
+	 var experiences = '';
+	 $.each(tutorExperiences, function(index, value){
+			
+		 experiences += '<div class="experience-content repeat-content col-xs-12">';
+		 experiences += '<div class="job-title col-sm-4">';
+		 experiences += '<h5>' + value[4] +' <span>' + value[3] +'</span></h5>	</div>';
+		 experiences += '<div class="job-des col-sm-4">';
+		 experiences += '<h5>'  + value[1] + '</h5></div>';
+			 
+		 experiences += '<div class="job-duration col-sm-4">';
+		 experiences += '<time>'  + value[5] + '</time> - <time>'  + value[6] + '</time>';							
+		 experiences += '</div></div> ';
+	 });
+	 
+	 $('#experienceDiv').html(experiences);
+}
+
+
+function  displayEmployment(tutorEmployment){
+	 var employments = '';
+	 $.each(tutorEmployment, function(index, value){
+			
+		 employments += '<div class="experience-content repeat-content col-xs-12">';
+		 employments +=	'<h5>' +value[1]+ '</h5></div>';
+
+	 });
+	 
+	 $('#employmentDiv').html(employments);
+}
+
+function  displayModules(tutorModules){
+	 var modules = '';
+	 $.each(tutorModules, function(index, value){
+			
+		 modules += '<div class="experience-content repeat-content col-xs-12">';
+		 employments +=	'<h5>' +value[1]+ '</h5></div>';
+
+	 });
+	 
+	 $('#employmentDiv').html(employments);
 }
